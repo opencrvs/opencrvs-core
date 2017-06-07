5820e8371bcf66f7443beadd7bc9fc89d0692850
Euan Millar <euanmillar77@gmail.com>
2017-06-07 09:43:23 +0100

    production webpack config created

---
+++ front/opencrvs-web-client/webpack.config.prod.js
@@ -0,0 +1,89 @@
+const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: '../src/index.js',
+    output: {
+      filename: 'static/js/bundle.[name].[chunkhash].js',
+      path: resolve(__dirname, 'build/'),
+      publicPath: 'http://46.101.36.211:3000/',
+    },
+    context: resolve(__dirname, 'src'),
+    resolve: {
+      modules: ['./src', './node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/, 
+          exclude: /node_modules/, 
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-0'],
+            cacheDirectory: true, 
+          },
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: 'eslint-loader',
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  importLoaders: 1,
+                  localIdentName: '[path][name]__[local]__[hash:base64:5]',
+                },
+              },
+              'postcss-loader',
+            ],
+          }),
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 1000, 
+            name: '[path][name].[hash].[ext]',
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ExtractTextPlugin( {
+        filename: 'static/css/style.[chunkhash].css',
+        publicPath: 'http://46.101.36.211:3000/',
+        allChunks: true,
+      }),
+      new HtmlWebpackPlugin( {
+        template: './index.html',
+      }),
+      new CopyWebpackPlugin([{from: 'static/img', to: 'static/img'}]),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true,
+        },
+        compress: {
+          screw_ie8: true,
+        },
+        comments: false,
+      }),
+    ],
+    
+};
