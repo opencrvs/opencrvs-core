37fe1b284b5120c3ea546a5c8d121b673afdae6b
Euan Millar <euanmillar77@gmail.com>
2017-06-22 02:05:51 +0300

    build for production

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -1,4 +1,4 @@
-//export const BASE_URL: string = 'http://46.101.36.211:3001/api/';
-//export const UNPROTECTED_URL: string = 'http://46.101.36.211:3001/';
-export const BASE_URL: string = 'http://localhost:3001/api/';
-export const UNPROTECTED_URL: string = 'http://localhost:3001/';
+export const BASE_URL: string = 'http://46.101.36.211:3001/api/';
+export const UNPROTECTED_URL: string = 'http://46.101.36.211:3001/';
+//export const BASE_URL: string = 'http://localhost:3001/api/';
+//export const UNPROTECTED_URL: string = 'http://localhost:3001/';
