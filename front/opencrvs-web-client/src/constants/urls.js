b78b0435bff0799621c8d5710f52185876c18fc6
Euan Millar <euanmillar77@gmail.com>
2017-07-21 02:02:50 +0100

    testing continuous deployemnt

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -2,7 +2,7 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:18:05 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-07-21 01:53:25
+ * @Last Modified time: 2017-07-21 02:02:20
  */
 //export const BASE_URL: string = 'http://46.101.36.211:3000/api/';
 //export const UNPROTECTED_URL: string = 'http://46.101.36.211:3000/';
