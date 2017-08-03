4872ef84e73a050998c52f52eb56346edfbbd3cd
Euan Millar <euanmillar77@gmail.com>
2017-08-03 17:22:56 +0100

    api urls for ssl

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -2,13 +2,13 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:18:05 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-08-03 17:14:14
+ * @Last Modified time: 2017-08-03 17:22:06
  */
 
 // @flow
-export const BASE_URL: string = 'https://46.101.36.211:3000/api/';
-export const UPLOADS_URL: string = 'https://46.101.36.211:3000/api/documents/uploads/';
-export const UNPROTECTED_URL: string = 'https://46.101.36.211:3000/';
+export const BASE_URL: string = 'https://opencrvs.jembi.org/api/';
+export const UPLOADS_URL: string = 'https://opencrvs.jembi.org/api/documents/uploads/';
+export const UNPROTECTED_URL: string = 'https://opencrvs.jembi.org/';
 
 // @flow
 // export const BASE_URL: string = 'http://localhost:3000/api/';
