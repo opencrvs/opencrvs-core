0b4e0cc6ec171203e7a92a92ff889b87b2f630ff
Euan Millar <euanmillar77@gmail.com>
2017-08-16 13:19:07 +0100

    SMS testing

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -2,7 +2,7 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:18:05 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-08-16 11:47:42
+ * @Last Modified time: 2017-08-16 13:18:40
  */
 
 export const BASE_URL = 'https://opencrvs.jembi.org/api/';
@@ -12,4 +12,4 @@ export const UNPROTECTED_URL = 'https://opencrvs.jembi.org/';
 export const UPLOADS_URL = 'http://localhost:3000/api/documents/uploads/';
 export const UNPROTECTED_URL = 'http://localhost:3000/'; */
 export const OPEN_HIM_URL = 'http://opencrvs.jembi.org:5000/fhir/';
-export const SMS_API_URL = 'http://api.clickatell.com/http/sendmsg?user=jembihealthus&password=32lR31Sp17Ps77i&api_id=3415097&';
+export const SMS_API_URL = 'https://api.clickatell.com/http/sendmsg?user=jembihealthus&password=32lR31Sp17Ps77i&api_id=3415097&';
