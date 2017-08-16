93da1e0a67f031f1c0d3bda795ef2d550e682beb
Euan Millar <euanmillar77@gmail.com>
2017-08-16 10:15:40 +0100

    testing SMS on the server

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -2,15 +2,15 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:18:05 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-08-15 21:48:47
+ * @Last Modified time: 2017-08-16 10:15:09
  */
 
-// export const BASE_URL = 'https://opencrvs.jembi.org/api/';
-// export const UPLOADS_URL = 'https://opencrvs.jembi.org/api/documents/uploads/';
-// export const UNPROTECTED_URL = 'https://opencrvs.jembi.org/';
+export const BASE_URL = 'https://opencrvs.jembi.org/api/';
+export const UPLOADS_URL = 'https://opencrvs.jembi.org/api/documents/uploads/';
+export const UNPROTECTED_URL = 'https://opencrvs.jembi.org/';
 
-export const BASE_URL = 'http://localhost:3000/api/';
+/* export const BASE_URL = 'http://localhost:3000/api/';
 export const UPLOADS_URL = 'http://localhost:3000/api/documents/uploads/';
-export const UNPROTECTED_URL = 'http://localhost:3000/';
+export const UNPROTECTED_URL = 'http://localhost:3000/'; */
 export const OPEN_HIM_URL = 'http://opencrvs.jembi.org:5000/fhir/';
 export const SMS_API_URL = 'http://api.clickatell.com/http/sendmsg?user=jembihealthus&password=32lR31Sp17Ps77i&api_id=3415097&';
