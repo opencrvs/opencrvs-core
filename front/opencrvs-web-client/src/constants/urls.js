09f1ce75ce31a1bc92013f9cb60ade54acae02e1
Euan Millar <euanmillar77@gmail.com>
2017-08-16 14:26:24 +0100

    different account

--- front/opencrvs-web-client/src/constants/urls.js
+++ front/opencrvs-web-client/src/constants/urls.js
@@ -2,7 +2,7 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:18:05 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-08-16 14:01:26
+ * @Last Modified time: 2017-08-16 14:24:37
  */
 
 export const BASE_URL = 'https://opencrvs.jembi.org/api/';
@@ -12,5 +12,5 @@ export const UNPROTECTED_URL = 'https://opencrvs.jembi.org/';
 export const UPLOADS_URL = 'http://localhost:3000/api/documents/uploads/';
 export const UNPROTECTED_URL = 'http://localhost:3000/'; */
 export const OPEN_HIM_URL = 'https://opencrvs.jembi.org:5000/fhir/';
-export const SMS_API_URL = 'https://api.clickatell.com/http/sendmsg?user=jembihealthus&password=32lR31Sp17Ps77i&api_id=3415097&';
+export const SMS_API_URL = 'https://api.clickatell.com/http/sendmsg?user=jembihealth&password=BdFGAZMDTZSgKX&api_id=3414601&';
 export const CORS_API_URL = 'https://cors-anywhere.herokuapp.com/';
