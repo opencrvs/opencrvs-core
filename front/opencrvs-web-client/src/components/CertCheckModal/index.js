d7cea66edf3309653d9424548afb09c17a8a5b00
Euan Millar <euanmillar77@gmail.com>
2017-09-19 12:46:51 +0100

    added reject and print buttons in order to complete text amends. Handing back the ticket to Ryan

--- front/opencrvs-web-client/src/components/CertCheckModal/index.js
+++ front/opencrvs-web-client/src/components/CertCheckModal/index.js
@@ -2,11 +2,12 @@
  * @Author: Euan Millar 
  * @Date: 2017-07-05 01:19:12 
  * @Last Modified by: Euan Millar
- * @Last Modified time: 2017-09-07 15:28:31
+ * @Last Modified time: 2017-09-19 12:27:41
  */
 import React from 'react';
 import styles from './styles.css';
 import Dialog from 'react-toolbox/lib/dialog';
+import { Button } from 'react-toolbox/lib/button';
 
 class CertCheckModal extends React.Component {
   constructor(props) {
@@ -14,6 +15,15 @@ class CertCheckModal extends React.Component {
   }
   
   closeCertCheckModal = (event) => {
+    this.props.onModalCloseClick('certCheck');
+  }
+  
+  rejectCert = (event) => {
+    this.props.onModalCloseClick('certCheck');
+  }
+
+  printCert = (event) => {
+    this.props.onPrintProceed();
     this.props.onModalCloseClick('certCheck');
   }
 
@@ -32,7 +42,12 @@ class CertCheckModal extends React.Component {
         title="Check Certification"
       >
 
-       
+      <section className={styles.detailsSection}>
+        <h1 className={ styles.submitConfirmHeader }>Enter details form as per ticket OCRVS-109.</h1>
+        <p>Grey out print and reject buttons until form successfully completed.</p>
+        <Button icon="print" label="Print" flat onClick={this.printCert} />
+        <Button icon="cancel" label="Reject" flat onClick={this.rejectCert} />
+      </section>
 
       </Dialog>
     );
