a8d102a121da4aafe9c3a2efb8a1279c848696a3
Ryan Crichton <ryan@jembi.org>
2017-09-20 15:14:50 +0200

    Fix autoformatting issues

--- front/opencrvs-web-client/src/containers/LocationContainer/index.js
+++ front/opencrvs-web-client/src/containers/LocationContainer/index.js
@@ -1,6 +1,6 @@
 /*
- * @Author: Euan Millar 
- * @Date: 2017-07-05 01:18:48 
+ * @Author: Euan Millar
+ * @Date: 2017-07-05 01:18:48
  * @Last Modified by: Euan Millar
  * @Last Modified time: 2017-09-05 15:47:41
  */
@@ -32,7 +32,7 @@ class LocationContainer extends React.Component {
     const { reportOption,
       caseGraphData,
       caseNotes } = this.props;
-   
+
     return (
       <div className={styles.locationContainer + ' pure-u-1'}>
         <Tabs index={reportOption} onChange={this.handleOptionChange} className={styles.tabs}>
@@ -50,12 +50,12 @@ class LocationContainer extends React.Component {
   }
 }
 
-   
 
-const mapStateToProps = ({  
+
+const mapStateToProps = ({
   managerReducer,
   globalReducer }) => {
-  const { 
+  const {
     mapLocations,
     fetchingMapView,
     subLocations,
@@ -66,11 +66,11 @@ const mapStateToProps = ({
     caseNotes,
     caseGraphData,
   } = managerReducer;
-  const { 
+  const {
     country,
     region } = globalReducer;
   return {
-    
+
     mapLocations,
     fetchingMapView,
     subLocations,
@@ -87,7 +87,7 @@ const mapStateToProps = ({
 
 const mapDispatchToProps = dispatch => {
   return {
-    
+
     onRegionClick: name => {
       dispatch(selectRegion(name));
     },
