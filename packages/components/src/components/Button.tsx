d413a519f7e1d91159f57f0e6720ba60abbf39ce
Riku Rouvila <riku.rouvila@gmail.com>
2018-05-18 10:03:13 +0100

    add type definition for Button component

--- packages/components/src/components/Button.tsx
+++ packages/components/src/components/Button.tsx
@@ -1,13 +1,18 @@
-import * as React from 'react';
-import { Text } from 'react-native';
-import styled from 'styled-components/native';
+import * as React from 'react'
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
 
 const StyledButton = styled.TouchableHighlight`
   background-color: green;
-`;
+`
 
-export const Button = (props: any) => (
-  <StyledButton onPress={props.onPress}>
+export interface IButtonProps {
+  onPress: () => void
+  title: string
+}
+
+export const Button = (props: IButtonProps) => (
+  <StyledButton accessibilityComponentType="button" onPress={props.onPress}>
     <Text>{props.title}</Text>
   </StyledButton>
-);
+)
