cc358a95220694bf19b076404aa9789ccbf2a94d
Euan Millar <euanmillar77@gmail.com>
2018-05-18 12:06:07 +0100

    first commit of typed mobile app

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+      <View style = {
+        styles.container
+      } >
+        <Text>Hi</Text>
+        <Text> Open up src / App.tsx to start working on your app OK!</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
