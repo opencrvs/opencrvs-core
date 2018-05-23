c52e299e2a96ca2cf917903eda8128cca96bbb0b
Ryan Crichton <ryan@jembi.org>
2018-05-23 12:23:23 +0200

    Add basic FHIR conversion for registration
    THis change adds an initial service that can convert a registration into
    a FHIR document bundle whihc is indexed by a composition resource. This
    isn't yet comprehensive, it is just enough to get us started.
    
    OCRVS-248

---
+++ packages/gateway/src/features/registration/service.test.ts
@@ -0,0 +1,17 @@
+import { toFHIR } from './service'
+
+test('should build a correct FHIR registration document without error', () => {
+  const fhir = toFHIR({
+    mother: { gender: 'female', givenName: 'Jane', familyName: 'Doe' },
+    father: { gender: 'male' },
+    child: { gender: 'male' },
+    createdAt: new Date()
+  })
+  expect(fhir).toBeDefined()
+  expect(fhir.entry[0].resource.section.length).toBe(3)
+  expect(fhir.entry[1].resource.gender).toBe('female')
+  expect(fhir.entry[2].resource.gender).toBe('male')
+  expect(fhir.entry[3].resource.gender).toBe('male')
+  expect(fhir.entry[1].resource.name[0].given[0]).toBe('Jane')
+  expect(fhir.entry[1].resource.name[0].family[0]).toBe('Doe')
+})
---
+++ packages/gateway/src/features/registration/service.ts
@@ -0,0 +1,147 @@
+import { v4 as uuid } from 'uuid'
+
+function createMotherSection(refUuid: string) {
+  return {
+    title: "Mother's details",
+    code: {
+      coding: {
+        system: 'http://opencrvs.org/doc-sections',
+        code: 'mother-details'
+      },
+      text: "Mother's details"
+    },
+    text: '',
+    entry: [
+      {
+        reference: `urn:uuid:${refUuid}`
+      }
+    ]
+  }
+}
+
+function createFatherSection(refUuid: string) {
+  return {
+    title: "Father's details",
+    code: {
+      coding: {
+        system: 'http://opencrvs.org/doc-sections',
+        code: 'father-details'
+      },
+      text: "Father's details"
+    },
+    text: '',
+    entry: [
+      {
+        reference: `urn:uuid:${refUuid}`
+      }
+    ]
+  }
+}
+
+function createChildSection(refUuid: string) {
+  return {
+    title: 'Child details',
+    code: {
+      coding: {
+        system: 'http://opencrvs.org/doc-sections',
+        code: 'child-details'
+      },
+      text: 'Child details'
+    },
+    text: '',
+    entry: [
+      {
+        reference: `urn:uuid:${refUuid}`
+      }
+    ]
+  }
+}
+
+function createComposition(
+  reg: any,
+  { motherRefUuid, fatherRefUuid, childRefUuid }: any
+) {
+  return {
+    resource: {
+      identifier: {
+        system: 'urn:ietf:rfc:3986',
+        value: uuid()
+      },
+      resourceType: 'Composition',
+      status: 'final',
+      type: {
+        coding: {
+          system: 'http://opencrvs.org/doc-types',
+          code: 'birth-declaration'
+        },
+        text: 'Birth Declaration'
+      },
+      class: {
+        coding: {
+          system: 'http://opencrvs.org/doc-classes',
+          code: 'crvs-document'
+        },
+        text: 'CRVS Document'
+      },
+      subject: {
+        reference: 'Patient/xyz'
+      },
+      date: reg.createdAt,
+      author: [
+        {
+          reference: 'Practitioner/xyz'
+        }
+      ],
+      title: 'Birth Declaration',
+      section: [
+        createMotherSection(motherRefUuid),
+        createFatherSection(fatherRefUuid),
+        createChildSection(childRefUuid)
+      ]
+    }
+  }
+}
+
+function createPersonEntry(person: any, refUuid: string) {
+  return {
+    fullUrl: `urn:uuid:${refUuid}`,
+    resource: {
+      resourceType: 'Patient',
+      active: true,
+      name: [
+        {
+          use: 'english',
+          family: [person.familyName],
+          given: [person.givenName]
+        }
+      ],
+      gender: person.gender
+    }
+  }
+}
+
+export function toFHIR(reg: any) {
+  const refUuids = {
+    motherRefUuid: uuid(),
+    fatherRefUuid: uuid(),
+    childRefUuid: uuid()
+  }
+
+  return {
+    resourceType: 'Bundle',
+    type: 'document',
+    meta: {
+      lastUpdated: reg.createdAt
+    },
+    entry: [
+      createComposition(reg, refUuids),
+      createPersonEntry(reg.mother, refUuids.motherRefUuid),
+      createPersonEntry(reg.father, refUuids.fatherRefUuid),
+      createPersonEntry(reg.child, refUuids.childRefUuid)
+    ]
+  }
+}
+
+export function fromFHIR(fhir: any) {
+  return {}
+}
