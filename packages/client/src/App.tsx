e4c083666a9e3a442fac00876be1c14104f167d2
Riku Rouvila <riku.rouvila@gmail.com>
2018-05-24 09:21:52 +0300

    create i18n directory for all internationalisation related stuff, fix tests and start writing a README

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,38 +1,43 @@
-import * as React from 'react';
-import { injectIntl, InjectedIntlProps } from 'react-intl';
-import { Button } from '@opencrvs/components/lib/Button';
-import './App.css';
+import * as React from 'react'
+import { IntlProvider, injectIntl, defineMessages } from 'react-intl'
 
-import logo from './logo.svg';
+import { Button } from '@opencrvs/components/lib/Button'
+import './App.css'
+
+import logo from './logo.svg'
 
 // tslint:disable
-const foo = () => console.log('sdf');
+const foo = () => console.log('sdf')
 
 interface IAppProps {}
 
-const welcomeMessage = {
-  id: 'app.welcome',
-  defaultMessage: 'Welcome',
-  description: 'Test text'
-}
+const messages = defineMessages({
+  welcome: {
+    id: 'app.welcome',
+    defaultMessage: 'Welcome',
+    description: 'Test text'
+  }
+})
 
-class Component extends React.Component<IAppProps & InjectedIntlProps, {}> {
+const Title = injectIntl(({ intl }) => (
+  <h1 className="App-title">{intl.formatMessage(messages.welcome)}</h1>
+))
+
+export class App extends React.Component<IAppProps, {}> {
   public render() {
-    const { intl } = this.props
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{intl.formatMessage(welcomeMessage)}</h1>
-          <Button onClick={foo}>Hello</Button>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+      <IntlProvider locale="en">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Title />
+            <Button onClick={foo}>Hello</Button>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+      </IntlProvider>
+    )
   }
 }
-
-export const App = injectIntl<IAppProps>(Component);
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { IntlProvider } from 'react-intl'
 
 import { App } from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-  <IntlProvider locale="en">
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
