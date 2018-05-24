bbc03cedf2bfcdd86dbd88d3ba93cf5cc4ea1271
Euan Millar <euanmillar77@gmail.com>
2018-05-24 14:45:53 +0100

    skip components without example added

--- packages/components/styleguide.config.js
+++ packages/components/styleguide.config.js
@@ -10,6 +10,7 @@ module.exports = {
   components: 'src/components/**/*.{ts,tsx}',
   propsParser: require('react-docgen-typescript').parse,
   styleguideDir: './lib',
+  skipComponentsWithoutExample: true,
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/components/ThemeWrapper')
