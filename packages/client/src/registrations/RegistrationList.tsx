ae7725f8949d8e026ff4a8cca950b28da868ee6f
Euan Millar <euanmillar77@gmail.com>
2018-06-08 13:38:33 +0100

    editor formatOnSave not overridden

--- packages/client/src/registrations/RegistrationList.tsx
+++ packages/client/src/registrations/RegistrationList.tsx
@@ -4,64 +4,64 @@ import styled, { StyledFunction } from 'styled-components'
 import { Query } from 'react-apollo'
 
 interface IRegistration {
-	children?: React.ReactChild
-	className?: string
+  children?: React.ReactChild
+  className?: string
 }
 
 class RegistrationsListQuery extends React.Component<IRegistration, {}> {
-	render() {
-		return (
-			<Query
-				query={gql`
-					{
-						listRegistrations(status: "declared") {
-							id
-							mother {
-								gender
-								name {
-									givenName
-									familyName
-								}
-							}
-							father {
-								gender
-								name {
-									givenName
-									familyName
-								}
-							}
-							child {
-								gender
-								name {
-									givenName
-									familyName
-								}
-							}
-							createdAt
-						}
-					}
-				`}
-			>
-				{({ loading, error, data }) => {
-					if (loading) {
-						return <p>Loading...</p>
-					}
-					if (error) {
-						return <p className={this.props.className}>Error :(</p>
-					}
-					return (
-						<p className={this.props.className}>
-							Mother's name: {data.listRegistrations[0].mother.name[0].givenName}
-						</p>
-					)
-				}}
-			</Query>
-		)
-	}
+  render() {
+    return (
+      <Query
+        query={gql`
+          {
+            listRegistrations(status: "declared") {
+              id
+              mother {
+                gender
+                name {
+                  givenName
+                  familyName
+                }
+              }
+              father {
+                gender
+                name {
+                  givenName
+                  familyName
+                }
+              }
+              child {
+                gender
+                name {
+                  givenName
+                  familyName
+                }
+              }
+              createdAt
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <p>Loading...</p>
+          }
+          if (error) {
+            return <p className={this.props.className}>Error :(</p>
+          }
+          return (
+            <p className={this.props.className}>
+              Mother's name: {data.listRegistrations[0].mother.name[0].givenName}
+            </p>
+          )
+        }}
+      </Query>
+    )
+  }
 }
 
 const styledRegistrations: StyledFunction<IRegistration & React.HTMLProps<HTMLInputElement>> = styled(
-	RegistrationsListQuery
+  RegistrationsListQuery
 )
 
 export const RegistrationList = styledRegistrations`
