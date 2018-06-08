753520c5cf445454eb161d75fcae4127ad83955a
Euan Millar <euanmillar77@gmail.com>
2018-06-08 12:20:27 +0100

    changed theme import in App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,82 +1,86 @@
 import * as React from 'react'
-import { Provider } from 'react-redux'
-import { IntlProvider, injectIntl, defineMessages } from 'react-intl'
-import { ConnectedRouter } from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {IntlProvider, injectIntl, defineMessages} from 'react-intl'
+import {ConnectedRouter} from 'react-router-redux'
 import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import { resolve } from 'url'
-import { Button } from '@opencrvs/components/lib/Button'
-import { injectGlobal } from 'styled-components'
-import { globalStyles } from '@opencrvs/components/lib/common/global'
-import { theme } from '@opencrvs/components/lib/themes/theme'
+import {ApolloProvider} from 'react-apollo'
+import {resolve} from 'url'
+import {Button} from '@opencrvs/components/lib/Button'
+import {injectGlobal} from 'styled-components'
+import {globalStyles} from '@opencrvs/components/lib/common/global'
+import {LocaleThemes} from '@opencrvs/components/lib/common/global/LocaleThemes'
 import './App.css'
 
 import logo from './logo.svg'
-import { store, history } from './store'
-import { Route } from 'react-router'
-import { ThemeProvider } from 'styled-components'
-import { RegistrationList } from './registrations/RegistrationList'
+import {store, history} from './store'
+import {Route} from 'react-router'
+import {ThemeProvider} from 'styled-components'
+import {RegistrationList} from './registrations/RegistrationList'
 
 const GRAPHQL_URL = `${process.env.REACT_APP_API_GATEWAY_URL}`
-const LANGUAGE = `${process.env.REACT_APP_LANGUAGE}`
-const LOCALE = `${process.env.REACT_APP_LOCALE}`
+  const LANGUAGE = `${process.env.REACT_APP_LANGUAGE}`
+    const LOCALE = `${process.env.REACT_APP_LOCALE}`
 
-// Injecting global styles for @font-face and the body tag - used only once per application
-// tslint:disable-next-line
-injectGlobal`${globalStyles}`
+      // Injecting global styles for @font-face and the body tag - used only once
+      // tslint:disable-next-line
+      injectGlobal `${globalStyles}`
 
-const foo = () => alert('sdf')
+      const foo = () => alert('sdf')
 
-const messages = defineMessages({
-	welcome: {
-		id: 'app.welcome',
-		defaultMessage: 'Welcome',
-		description: 'Test text'
-	}
-})
+        const messages = defineMessages({
+            welcome: {
+              id: 'app.welcome',
+              defaultMessage: 'Welcome',
+              description: 'Test text'
+            }
+          })
 
-const Title = injectIntl(({ intl }) => <h1 className="App-title">{intl.formatMessage(messages.welcome)}</h1>)
+          const Title = injectIntl(({intl}) => <h1 className="App-title">{intl.formatMessage(messages.welcome)}</h1>)
 
-const Home = () => (
-	<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<Title />
-			<Button onClick={foo}>Hello</Button>
-		</header>
-		<p className="App-intro">
-			To get started, edit <code>src/App.tsx</code> and save to reload.
-		</p>
-		<RegistrationList />
-	</div>
-)
-const Other = () => (
-	<div className="App">
-		<h1>page 2</h1>
-	</div>
-)
+            const Home = () => (
+                <div className="App">
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Title/>
+                    <Button onClick={foo}>Hello</Button>
+                  </header>
+                  <p className="App-intro">
+                    To get started, edit
+                    <code>src/App.tsx</code>
+                    and save to reload.
+                  </p>
+                  <RegistrationList/>
+                </div>
+              )
+              const Other = () => (
+                  <div className="App">
+                    <h1>page 2</h1>
+                  </div>
+                )
 
-const client = new ApolloClient({
-	uri: resolve(GRAPHQL_URL, 'graphql')
-})
+                const client = new ApolloClient({
+                    uri: resolve(GRAPHQL_URL, 'graphql')
+                  })
 
-export class App extends React.Component<{ client?: ApolloClient<{}> }, {}> {
-	public render() {
-		return (
-			<ApolloProvider client={this.props.client || client}>
-				<Provider store={store}>
-					<IntlProvider locale={LANGUAGE}>
-						<ThemeProvider theme={theme[LOCALE]}>
-							<ConnectedRouter history={history}>
-								<div>
-									<Route exact path="/" component={Home} />
-									<Route exact path="/other" component={Other} />
-								</div>
-							</ConnectedRouter>
-						</ThemeProvider>
-					</IntlProvider>
-				</Provider>
-			</ApolloProvider>
-		)
-	}
-}
+                  export class App extends React.Component < {
+                    client?: ApolloClient < {} >
+                  }, {} > {
+                    public render() {
+                      return (
+                        <ApolloProvider client={this.props.client || client}>
+                          <Provider store={store}>
+                            <IntlProvider locale={LANGUAGE}>
+                              < ThemeProvider theme={LocaleThemes[LOCALE]}>
+                                <ConnectedRouter history={history}>
+                                  <div>
+                                    <Route exact path="/" component={Home}/>
+                                    <Route exact path="/other" component={Other}/>
+                                  </div>
+                                </ConnectedRouter>
+                              </ThemeProvider>
+                            </IntlProvider>
+                          </Provider>
+                        </ApolloProvider>
+                      )
+                    }
+                  }
