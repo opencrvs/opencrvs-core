2bf81d77411688c3da933c272478016fbadaadaa
Riku Rouvila <riku.rouvila@gmail.com>
2018-06-19 10:40:29 +0100

    move config.ts file contents to constants.ts

--- packages/auth/src/config/index.ts
+++
@@ -1,2 +0,0 @@
-export const USER_MANAGEMENT_URL =
-  process.env.USER_MANAGEMENT_URL || 'http://localhost:3030/'
--- packages/auth/src/constants.ts
+++ packages/auth/src/constants.ts
@@ -1,2 +1,4 @@
 export const AUTH_HOST = process.env.AUTH_HOST || 'localhost'
 export const AUTH_PORT = process.env.AUTH_PORT || 4040
+export const USER_MANAGEMENT_URL =
+  process.env.USER_MANAGEMENT_URL || 'http://localhost:3030/'
