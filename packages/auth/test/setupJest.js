0696f96dda7eb86dda56c3b495d07c66b1607084
Riku Rouvila <riku.rouvila@gmail.com>
2018-06-20 11:58:06 +0100

    do not start listening for http connections when tests are run

--- packages/auth/test/setupJest.js
+++ packages/auth/test/setupJest.js
@@ -3,7 +3,16 @@ const fetch = require('jest-fetch-mock')
 const redis = require('redis-mock')
 
 jest.setMock('node-fetch', { default: fetch })
-jest.setMock('redis', redis)
+
+const database = {}
+jest.setMock('src/database', {
+  set: (key, value) => {
+    database[key] = value
+  },
+  get: key => database[key],
+  start: () => {},
+  stop: () => {}
+})
 
 process.env.CERT_PRIVATE_KEY_PATH = join(__dirname, './cert.key')
 process.env.CERT_PUBLIC_KEY_PATH = join(__dirname, './cert.key.pub')
