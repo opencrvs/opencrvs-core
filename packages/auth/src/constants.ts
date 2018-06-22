9a760658bcd56ab3487b01f423a92376b6611f8a
Ryan Crichton <ryan@jembi.org>
2018-06-22 09:40:47 +0200

    Add dev secrets generation and injection
    OCRVS-319

--- packages/auth/src/constants.ts
+++ packages/auth/src/constants.ts
@@ -8,5 +8,9 @@ export const CLICKATELL_USER = process.env.CLICKATELL_USER
 export const CLICKATELL_PASSWORD = process.env.CLICKATELL_PASSWORD
 export const CLICKATELL_API_ID = process.env.CLICKATELL_API_ID
 
-export const CERT_PRIVATE_KEY_PATH = process.env.CERT_PRIVATE_KEY_PATH as string
-export const CERT_PUBLIC_KEY_PATH = process.env.CERT_PUBLIC_KEY_PATH as string
+export const CERT_PRIVATE_KEY_PATH =
+  (process.env.CERT_PRIVATE_KEY_PATH as string) ||
+  '../../.secrets/private-key.pem'
+export const CERT_PUBLIC_KEY_PATH =
+  (process.env.CERT_PUBLIC_KEY_PATH as string) ||
+  '../../.secrets/public-key.pem'
--- packages/gateway/src/constants.ts
+++ packages/gateway/src/constants.ts
@@ -1,1 +1,5 @@
 export const fhirUrl = process.env.FHIR_URL || 'http://localhost:5001/fhir'
+
+export const CERT_PUBLIC_KEY_PATH =
+  (process.env.CERT_PUBLIC_KEY_PATH as string) ||
+  '../../.secrets/public-key.pem'
