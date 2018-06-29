a98b55bb443dee52048a4e7b9df534357fa1d48a
Riku Rouvila <riku.rouvila@gmail.com>
2018-06-29 15:12:09 +0100

    add test to verify the handler returns the right response also when sms service fails

--- packages/notification/src/features/sms/handler.test.ts
+++ packages/notification/src/features/sms/handler.test.ts
@@ -8,15 +8,31 @@ describe('smsHandler', () => {
     server = await createServer()
   })
 
-  it('should return OK if the sms gets sent', async () => {
+  it('returns OK if the sms gets sent', async () => {
     const spy = jest.spyOn(service, 'sendSMS').mockResolvedValueOnce(null)
 
-    await server.server.inject({
+    const res = await server.server.inject({
       method: 'POST',
       url: '/sms',
       payload: { msisdn: '447789778823', message: 'foo' }
     })
 
     expect(spy).toHaveBeenCalled()
+
+    expect(res.statusCode).toBe(200)
+  })
+  it("returns 500 if the sms isn't sent", async () => {
+    const spy = jest
+      .spyOn(service, 'sendSMS')
+      .mockImplementation(() => Promise.reject(new Error()))
+
+    const res = await server.server.inject({
+      method: 'POST',
+      url: '/sms',
+      payload: { msisdn: '447789778823', message: 'foo' }
+    })
+
+    expect(spy).toHaveBeenCalled()
+    expect(res.statusCode).toBe(500)
   })
 })
