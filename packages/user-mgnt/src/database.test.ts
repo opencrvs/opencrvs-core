f3d3e0a3596b602a5d6ef196377f970c8de9159f
Riku Rouvila <riku.rouvila@gmail.com>
2018-07-03 09:54:14 +0100

    add test for database connector logging

--- packages/user-mgnt/src/database.test.ts
+++ packages/user-mgnt/src/database.test.ts
@@ -1,13 +1,15 @@
 jest.mock('mongoose', () => ({
   connection: {
     // tslint:disable-next-line no-empty
-    on: () => {}
+    on: jest.fn()
   },
-  connect: () => Promise.reject()
+  connect: () => Promise.reject(),
+  // tslint:disable-next-line no-empty
+  disconnect: () => {}
 }))
 
 import * as mongoose from 'mongoose'
-import { start } from './database'
+import { start, stop } from './database'
 import { logger } from 'src/logger'
 
 const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time))
@@ -20,5 +22,18 @@ describe('Database connector', () => {
     expect(spy).toHaveBeenCalled()
     jest.spyOn(mongoose, 'connect').mockResolvedValueOnce('hello!')
     await promise
+  })
+  it('attaches loggers to database events', async () => {
+    const spy = jest.spyOn(logger, 'info')
+    const [[, disconnectFn], [, connectedFn]] = (mongoose.connection
+      .on as any).mock.calls
+    disconnectFn()
+    connectedFn()
+    expect(spy).toHaveBeenCalledTimes(2)
+  })
+  it('calls mongoose disconnect when stop method is called', async () => {
+    const spy = jest.spyOn(mongoose, 'disconnect')
+    await stop()
+    expect(spy).toHaveBeenCalled()
   })
 })
