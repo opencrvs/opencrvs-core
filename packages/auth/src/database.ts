44976918ea04ef1f2ea47d175fb747e3666e1a13
Riku Rouvila <riku.rouvila@gmail.com>
2018-07-05 15:47:13 +0100

    force database mock to implement the same interface with the actual implementation

--- packages/auth/src/database.ts
+++ packages/auth/src/database.ts
@@ -4,11 +4,18 @@ import { promisify } from 'util'
 
 let redisClient: redis.RedisClient
 
-export async function stop() {
+export interface IDatabaseConnector {
+  stop: () => void
+  start: () => void
+  set: (key: string, value: string) => Promise<void>
+  get: (key: string) => Promise<string | null>
+}
+
+async function stop() {
   redisClient.quit()
 }
 
-export async function start() {
+async function start() {
   redisClient = redis.createClient({
     host: REDIS_HOST,
     retry_strategy: options => {
@@ -17,8 +24,11 @@ export async function start() {
   })
 }
 
-export const get = (...args: string[]) =>
-  promisify(redisClient.get).bind(redisClient)(...args)
+const get = (key: string) => promisify(redisClient.get).bind(redisClient)(key)
 
-export const set = (...args: string[]) =>
-  promisify(redisClient.set).bind(redisClient)(...args)
+const set = (key: string, value: string) =>
+  promisify(redisClient.set).bind(redisClient)(key, value)
+
+const connector: IDatabaseConnector = { set, get, stop, start }
+
+export default connector
