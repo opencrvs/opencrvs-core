24cda0e3afba67d4e929a2b975bc307449d04c81
Euan Millar <euanmillar77@gmail.com>
2018-07-25 00:40:56 +0100

    authUtils.tests

---
+++ packages/register/src/utils/authUtils.test.ts
@@ -0,0 +1,27 @@
+import * as authUtils from './authUtils'
+
+describe('authUtils tests', () => {
+  let mockValidToken: any
+  let mockInvalidToken: any
+
+  beforeEach(async () => {
+    mockValidToken = {
+      subject: '123',
+      exp: new Date(new Date().getTime() / 1000 + 10 * 60000),
+      role: 'test'
+    }
+    mockInvalidToken = {
+      subject: '123',
+      exp: new Date(new Date().getTime() / 1000 - 10 * 60000),
+      role: 'chw'
+    }
+  })
+  it('isTokenStillValid confirms a token that has not expired', () => {
+    jest.spyOn(authUtils, 'getProfile').mockReturnValue(mockValidToken)
+    expect(authUtils.isTokenStillValid('12345')).toEqual(true)
+  })
+  it('isTokenStillValid confirms a token that has expired', () => {
+    jest.spyOn(authUtils, 'getProfile').mockReturnValue(mockInvalidToken)
+    expect(authUtils.isTokenStillValid('12345')).toEqual(false)
+  })
+})
