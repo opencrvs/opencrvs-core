5c9fbd56bad21e8a936a6a29f9e7d155e3e720b6
Riku Rouvila <riku.rouvila@gmail.com>
2018-08-15 13:10:48 +0100

    move components from common/ to components/

--- packages/register/src/components/Page.tsx
+++ packages/register/src/components/Page.tsx
@@ -2,9 +2,15 @@ import * as React from 'react'
 import styled from 'styled-components'
 import { RouteComponentProps } from 'react-router'
 import * as QueryString from 'query-string'
-import { IURLParams } from '../utils/authUtils'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
-export interface IProps {
+import { IURLParams } from '@opencrvs/register/src/utils/authUtils'
+import { getLanguage } from '@opencrvs/register/src/i18n/intlSelectors'
+import { IStoreState } from '@opencrvs/register/src/store'
+import * as actions from '@opencrvs/register/src/profile/profileActions'
+
+interface IPageProps {
   language?: string
 }
 
@@ -12,7 +18,7 @@ export interface IDispatchProps {
   checkAuth: (urlValues: IURLParams) => void
 }
 
-type IPage = IProps & IDispatchProps
+type IPage = IPageProps & IDispatchProps
 
 const StyledPage = styled.div.attrs<IPage>({})`
   background: #f4f4f4;
@@ -64,7 +70,7 @@ const StyledPage = styled.div.attrs<IPage>({})`
   }
 `
 
-export class Page extends React.Component<RouteComponentProps<{}> & IPage> {
+class Component extends React.Component<RouteComponentProps<{}> & IPage> {
   componentWillMount() {
     const values = QueryString.parse(this.props.location.search)
     this.props.checkAuth(values)
@@ -78,3 +84,18 @@ export class Page extends React.Component<RouteComponentProps<{}> & IPage> {
     )
   }
 }
+
+const mapStateToProps = (store: IStoreState): IPageProps => {
+  return {
+    language: getLanguage(store)
+  }
+}
+
+const mapDispatchToProps = {
+  checkAuth: actions.checkAuth
+}
+
+export const Page = connect<IPageProps, IDispatchProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(Component))
--- packages/register/src/components/ProtectedRoute.tsx
+++ packages/register/src/components/ProtectedRoute.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Route, RouteProps } from 'react-router'
 import { connect } from 'react-redux'
-import { IStoreState } from '../store'
-import { getAuthenticated } from '../profile/profileSelectors'
-import { redirectToAuthentication } from '../profile/profileActions'
+import { IStoreState } from '@opencrvs/register/src/store'
+import { getAuthenticated } from '@opencrvs/register/src/profile/profileSelectors'
+import { redirectToAuthentication } from '@opencrvs/register/src/profile/profileActions'
 
 export interface IProps {
   isAuthenticated: boolean
