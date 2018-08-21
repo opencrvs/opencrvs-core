d4275f59944f39ea5f866b10801b4af28ff12b53
Riku Rouvila <riku.rouvila@gmail.com>
2018-08-21 15:36:50 +0300

    add test for verifying tab navigation works inside the form

--- packages/components/src/components/interface/Tabs.tsx
+++ packages/components/src/components/interface/Tabs.tsx
@@ -8,22 +8,15 @@ export const Tabs = styled.div`
   white-space: nowrap;
 `
 
-export interface IProps extends Omit<IButtonProps, 'onClick'> {
+export interface IProps extends IButtonProps {
   active?: boolean
   id: string
-  onClick: (id: string) => void
 }
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-export const TabWrapper = styled(Button).attrs<IProps>({})`
+export const Tab = styled(Button).attrs<IProps>({})`
   color: #fff;
   font-family: ${({ theme, active }) =>
     active ? theme.fonts.regularFont : theme.fonts.lightFont};
   font-size: 18px;
   ${({ active }) => (active ? 'border-bottom: 3px solid #5E93ED' : '')};
 `
-
-export const Tab = (props: IProps) => (
-  <TabWrapper {...props} onClick={() => props.onClick(props.id)} />
-)
