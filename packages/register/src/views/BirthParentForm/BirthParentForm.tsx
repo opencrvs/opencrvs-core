188862f437283df795b85aa1398e3cad69ffd86c
Riku Rouvila <riku.rouvila@gmail.com>
2018-08-21 17:50:33 +0300

    move common form components to their own modules so they can be used to build new forms
    I wanted to still keep the main component birth - parent specific, since we won't have any other forms for the MVP

--- packages/register/src/views/BirthParentForm/BirthParentForm.tsx
+++ packages/register/src/views/BirthParentForm/BirthParentForm.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { connect } from 'react-redux'
 import { defineMessages } from 'react-intl'
-import { withFormik } from 'formik'
 
-import { Tab, Tabs, Box } from '@opencrvs/components/lib/interface'
-import { InputField } from '@opencrvs/components/lib/forms'
-
-import { ViewHeader } from '../../components/ViewHeader'
-import { goToTab as goToTabAction } from '../../navigation/navigationActions'
 import styled from '../../styled-components'
+
+import { goToTab as goToTabAction } from '../../navigation/navigationActions'
 import { birthParentForm } from '../../forms/birth-parent'
-import { IFormField, IForm } from '@opencrvs/register/src/forms'
+import { IForm } from '../../forms'
+import { Form, FormTabs, ViewHeaderWithTabs } from '../../components/form'
 
 export const messages = defineMessages({})
 
@@ -19,84 +16,8 @@ const FormContainer = styled.div`
   padding: 35px 25px;
 `
 
-const ViewHeaderWithTabs = styled(ViewHeader)`
-  padding-bottom: 0;
-  ${Tabs} {
-    overflow-x: auto;
-    width: 100%;
-  }
-`
-
-const FormItem = styled.div`
-  margin-bottom: 2em;
-`
-
-const FormSectionTitle = styled.h2`
-  font-family: ${({ theme }) => theme.fonts.lightFont};
-`
-
-const FormSection = ({
-  handleSubmit,
-  fields,
-  title
-}: {
-  handleSubmit: () => void
-  fields: IFormField[]
-  title: string
-}) => {
-  return (
-    <Box>
-      <section>
-        <FormSectionTitle id="form_section_title">{title}</FormSectionTitle>
-        <form onSubmit={handleSubmit}>
-          {fields.map(field => {
-            return (
-              <FormItem key={`${field.name}`}>
-                <InputField
-                  id={field.name}
-                  type={field.type}
-                  label={field.label}
-                />
-              </FormItem>
-            )
-          })}
-        </form>
-      </section>
-    </Box>
-  )
-}
-
-const Form = withFormik<{ fields: IFormField[]; title: string }, any>({
-  handleSubmit: values => {
-    console.log(values)
-  }
-})(FormSection)
-
 function getActiveTabId(form: IForm, viewParams: { tab?: string }) {
   return viewParams.tab || form.sections[0].id
-}
-
-interface IFormTabProps {
-  form: IForm
-  activeTabId: string
-  onTabClick: (tabId: string) => void
-}
-
-function FormTabs({ form, activeTabId, onTabClick }: IFormTabProps) {
-  return (
-    <Tabs>
-      {form.sections.map(({ name, id }) => (
-        <Tab
-          id={`tab_${id}`}
-          onClick={() => onTabClick(id)}
-          key={id}
-          active={activeTabId === id}
-        >
-          {name}
-        </Tab>
-      ))}
-    </Tabs>
-  )
 }
 
 class BirthParentFormView extends React.Component<
@@ -123,7 +44,7 @@ class BirthParentFormView extends React.Component<
           title="New Birth Registration"
         >
           <FormTabs
-            form={birthParentForm}
+            sections={birthParentForm.sections}
             activeTabId={activeTabId}
             onTabClick={goToTab}
           />
