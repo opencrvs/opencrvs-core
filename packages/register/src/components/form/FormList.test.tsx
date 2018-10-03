6f01b791757b1df6acc9e2d01667b2122ed7ec45
armanbhuiyan-dsi <armanbhuiyan.dsi@gmail.com>
2018-10-03 19:30:59 +0600

    Use hardcoded message

--- packages/register/src/components/form/FormList.test.tsx
+++ packages/register/src/components/form/FormList.test.tsx
@@ -2,48 +2,36 @@ import * as React from 'react'
 import { createTestComponent } from 'src/tests/util'
 import { FormList } from './FormList'
 import { ReactWrapper } from 'enzyme'
-import { defineMessages } from 'react-intl'
 import * as actions from 'src/i18n/actions'
 import { Store } from 'redux'
 
 describe('when user is in the document upload page', () => {
   let store: Store
   let formListComponent: ReactWrapper<{}, {}>
-  const messages = defineMessages({
-    informantAttestation: {
+
+  const listItems = [
+    {
       id: 'register.form.section.documents.list.informantAttestation',
-      defaultMessage: 'Attestation of the informant, or',
-      description: 'Attested document of the informant'
+      defaultMessage: 'Attestation of the informant, or'
     },
-    attestedVaccination: {
+    {
       id: 'register.form.section.documents.list.attestedVaccination',
-      defaultMessage: 'Attested copy of the vaccination (EPI) card, or',
-      description: 'Attested copy of the vaccination card'
+      defaultMessage: 'Attested copy of the vaccination (EPI) card, or'
     },
-    attestedBirthRecord: {
+    {
       id: 'register.form.section.documents.list.attestedBirthRecord',
-      defaultMessage: 'Attested copy of hospital document or birth record, or',
-      description: 'Attested copy of hospital document'
+      defaultMessage: 'Attested copy of hospital document or birth record, or'
     },
-    certification: {
+    {
       id: 'register.form.section.documents.list.certification',
       defaultMessage:
-        'Certification regarding NGO worker authorized by registrar in favour of date of birth, or',
-      description: 'Certification regarding NGO worker'
+        'Certification regarding NGO worker authorized by registrar in favour of date of birth, or'
     },
-    otherDocuments: {
+    {
       id: 'register.form.section.documents.list.otherDocuments',
       defaultMessage:
-        'Attested copy(s) of the document as prescribed by the Registrar',
-      description: 'Attested copy(s) of the document'
+        'Attested copy(s) of the document as prescribed by the Registrar'
     }
-  })
-  const listItems = [
-    messages.informantAttestation,
-    messages.attestedVaccination,
-    messages.attestedBirthRecord,
-    messages.certification,
-    messages.otherDocuments
   ]
 
   beforeEach(async () => {
