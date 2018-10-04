a4f4ce662d4cd7536c5d1ff696b5a9a35b1620d5
Euan Millar <euanmillar77@gmail.com>
2018-10-04 13:35:05 +0100

    Submit declaration

--- packages/e2e/cypress/integration/register.spec.ts
+++ packages/e2e/cypress/integration/register.spec.ts
@@ -101,5 +101,51 @@ context('Register', () => {
       'Upper secondary'
     )
     cy.get('#addressSameAsMother_false').click()
+    cy.selectOption('#country', 'Bangladesh', 'Bangladesh')
+    cy.selectOption('#state', 'Dhaka Division', 'Dhaka Division')
+    cy.selectOption('#district', 'Gazipur District', 'Gazipur District')
+    // Depends on fix for bug & better approach for administrative structure:  https://jembiprojects.jira.com/browse/OCRVS-588
+
+    /* cy.selectOption('#addressLine4', 'Kaliganj Upazila', 'Kaliganj Upazila')
+    cy.selectOption('#addressLine3Options1', 'Jamalpur', 'Jamalpur')
+    cy.get('#addressLine2').type('My street')
+    cy.get('#addressLine1').type('40')
+    cy.get('#postcode').type('10024')*/
+    cy.get('#permanentAddressSameAsMother_false').click()
+    cy.selectOption('#countryPermanent', 'Bangladesh', 'Bangladesh')
+    cy.selectOption('#statePermanent', 'Dhaka Division', 'Dhaka Division')
+    cy.selectOption(
+      '#districtPermanent',
+      'Gazipur District',
+      'Gazipur District'
+    )
+    /*cy.selectOption(
+      '#addressLine4Permanent',
+      'Kaliganj Upazila',
+      'Kaliganj Upazila'
+    )
+    cy.selectOption('#addressLine3Options1Permanent', 'Jamalpur', 'Jamalpur')
+    cy.get('#addressLine2Permanent').type('My street')
+    cy.get('#addressLine1Permanent').type('40')
+    cy.get('#postcodePermanent').type('10024')*/
+    cy.get('#next_section').click()
+    // REGISTRATION
+    cy.selectOption('#whoIsPresent', 'Both Parents', 'Both Parents')
+    cy.selectOption('#whoseContactDetails', 'Both Parents', 'Both Parents')
+    cy.get('#registrationEmail').type('test@test.com')
+    cy.get('#registrationPhone').type('07111111111')
+    cy.get('#registrationCertificateLanguageBangla').click()
+    cy.get('#registrationCertificateLanguageEnglish').click()
+    cy.get('#paperFormNumber').type('1')
+    cy.get('#commentsOrNotes').type('note')
+    cy.get('#next_section').click()
+    // DOCUMENTS
+    cy.get('#next_section').click()
+    // PREVIEW
+    cy.get('#submit_form').click()
+    // MODAL
+    cy.get('#submit_confirm').click()
+    // SUCCESS
+    cy.get('#saved_registration_view').contains('Declaration submitted')
   })
 })
