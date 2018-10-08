9e82c86957cba299dcbf0665162268efc40d168a
Tofael Ahmed <tofael.ahmed@dsinnovators.com>
2018-10-08 15:58:14 +0600

    Add mock reload function

--- packages/register/src/setupTests.ts
+++ packages/register/src/setupTests.ts
@@ -9,5 +9,6 @@ const navigatorMock = {
 ;(window as any).localStorage = localStorageMock
 ;(window as any).location.assign = jest.fn()
 ;(window as any).navigator = navigatorMock
+;(window as any).location.reload = jest.fn()
 // tslint:disable-next-line no-empty
 ;(window as any).scrollTo = () => {}
