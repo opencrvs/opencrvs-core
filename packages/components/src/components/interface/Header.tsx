cb37d4c43f773f87729b0a5e177cbbb1f2fb0766
armanbhuiyan-dsi <armanbhuiyan.dsi@gmail.com>
2018-10-15 17:34:35 +0600

    Update header style

--- packages/components/src/components/interface/Header.tsx
+++ packages/components/src/components/interface/Header.tsx
@@ -7,12 +7,12 @@ export const Header = styled.section`
 
   color: ${({ theme }) => theme.colors.white};
 
-  overflow: auto;
+  overflow: visible;
   background: linear-gradient(
     270deg,
     ${({ theme }) => theme.colors.headerGradientLight} 0%,
     ${({ theme }) => theme.colors.headerGradientDark} 100%
   );
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-  position: static;
+  position: relative;
 `
