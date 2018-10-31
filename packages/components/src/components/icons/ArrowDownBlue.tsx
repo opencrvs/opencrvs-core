ac78a0ffb46fe80a87802399b27b62086b13e87a
tahmid <tahmidrahman.dsi@gmail.com>
2018-10-31 16:40:54 +0600

    updated arrow down icon

--- packages/components/src/components/icons/ArrowDownBlue.tsx
+++ packages/components/src/components/icons/ArrowDownBlue.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 
 export const ArrowDownBlue = (props: React.HTMLAttributes<SVGElement>) => (
-  <svg width={10} height={7} {...props}>
-    <title>{`65D8643E-7596-4024-9985-9B26995710E3-565-000007DCDE59313F`}</title>
+  <svg width={13} height={8} {...props}>
+    <title>{`7D6915CC-0656-4F98-8168-2D4286A217F4`}</title>
     <path
-      d="M4.343 6.679L.272 1.872a1.241 1.241 0 0 1 0-1.55A.863.863 0 0 1 .929 0H9.07C9.584 0 10 .49 10 1.096c0 .291-.098.57-.272.776L5.657 6.679c-.363.428-.951.428-1.314 0z"
+      d="M5.586 6.95L.763 2.127A1.1 1.1 0 0 1 1.541.25h9.646a1.1 1.1 0 0 1 .778 1.877L7.142 6.95a1.1 1.1 0 0 1-1.556 0z"
       fill="#5E93ED"
       fillRule="evenodd"
     />
