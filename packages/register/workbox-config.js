f12ae316a06662fe8c52d9cd7e809d9882e38855
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2018-11-01 18:05:56 +0600

    Removing Comment From workbox-config

--- packages/register/workbox-config.js
+++ packages/register/workbox-config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  // cacheId: 'ocrvs-register',
   globDirectory: 'build/',
   globPatterns: ['**/*.{json,ico,ttf,html,js}'],
-  // navigateFallback: '/index.html',
-  // fallback for anything that doesn't start with /__ which is used by cypress
-  // navigateFallbackBlacklist: [/^\/__.*$/],
   swDest: 'build/service-worker.js',
   swSrc: 'src/src-sw.js'
 }
