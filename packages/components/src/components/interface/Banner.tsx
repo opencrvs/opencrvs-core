67d2af6577201665a6bb5e070417cc5aa181120d
Atiq Zaman <atiq.zaman@dsinnovators.com>
2018-11-05 18:41:41 +0600

    Banner component updated

--- packages/components/src/components/interface/Banner.tsx
+++ packages/components/src/components/interface/Banner.tsx
@@ -1,63 +1,44 @@
 import * as React from 'react'
 import styled from 'styled-components'
-
+import { StatusOrange } from '../icons'
 export interface IBannerProps {
   text: string
   count: number
 }
 
 const StyledBanner = styled.div`
-  padding: 25px;
-  height: 109px;
-  width: 940px;
+  padding: 0 30px;
+  width: 100%;
   border-radius: 1px;
+  display: flex;
+  align-items: center;
   font-family: ${({ theme }) => theme.fonts.regularFont};
   background-color: ${({ theme }) => theme.colors.accentLight};
+  min-height: 109px;
+  margin: 20px 0;
+  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.32);
 `
-
-const StatusOrange = () => (
-  <svg width={16} height={16}>
-    <title>{`0D0FCF6B-B898-4A22-808D-F0BAA2FF4146-565-000007DCE1416776`}</title>
-    <g fillRule="nonzero" fill="none">
-      <circle fill="#F4F4F4" cx={8} cy={8} r={8} />
-      <circle fill="#F4C78A" cx={8} cy={8} r={8} />
-      <circle fill="#F4A34E" cx={8} cy={8} r={4} />
-    </g>
-  </svg>
-)
-
 const StyledStatus = styled.div`
-  width: 64px;
   display: flex;
   flex-flow: row nowrap;
   border-radius: 16px;
   padding: 5px 10px 5px 7px;
-  margin: 13px 5px 2px 0;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.blackAlpha20};
   height: 32px;
+  margin-left: 10px;
 `
 const StyledText = styled.div`
   color: ${({ theme }) => theme.colors.white};
   font-family: ${({ theme }) => theme.fonts.regularFont};
-  float: left;
-  height: 38px;
-  width: 475px;
-  margin-top: 13px;
-  margin-left: 10px;
-  display: inline-block;
-  font-size: 32px;
+  font-size: 28px;
   font-weight: 300;
-  line-height: 25px;
+  min-height: 42px;
 `
 
 const StyledNumber = styled.span`
-  height: 25px;
-  width: 46px;
   font-size: 20px;
-  line-height: 27px;
-  text-align: center;
-  margin-left: 10px;
+  margin-left: 5px;
   color: ${({ theme }) => theme.colors.white};
   font-family: ${({ theme }) => theme.fonts.regularFont};
 `
