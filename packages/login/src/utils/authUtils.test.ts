91bcb7a13c471be3044d00a8b07c1e08542c212b
Euan Millar <euanmillar77@gmail.com>
2018-11-07 07:54:14 +0000

    tokens without noise

--- packages/login/src/utils/authUtils.test.ts
+++ packages/login/src/utils/authUtils.test.ts
@@ -2,21 +2,20 @@ import { getTokenPayload, getRedirectURL } from './authUtils'
 import { config } from '../config'
 
 const performanceJWT =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1NDExNjU0NjEsImV4cCI6MTU0MTUyNDY5NywiYXVkIjoiIiwic3ViIjoiMSIsInNjb3BlIjoiWydwZXJmb3JtYW5jZSddIiwianRpIjoiMGQ3NzVhMTQtNDdjNS00OWQzLWIxNTQtNDhlYzM4YjUxNDg2In0.9k5QtB4D-6b-xZLjrOwaPDC0aNhmpwonaQrkMVdjDW4'
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1NDE1NzY5NjUsImV4cCI6MTU3MzExMjk2NSwiYXVkIjoiIiwic3ViIjoiMSIsInNjb3BlIjoiWydwZXJmb3JtYW5jZSddIn0.huK3iFFi01xkwHvQZQAOnScrz0rJ50EsxpZA3a1Ynao'
 
 const declareJWT =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1NDExNjU0NjEsImV4cCI6MTU0MTUyNDc2NCwiYXVkIjoiIiwic3ViIjoiMSIsInNjb3BlIjoiWydkZWNsYXJlJ10iLCJqdGkiOiI0ZjVmZTliYS04NTQ3LTRjOTgtOTY4Ni0zOGQyN2U4MTgwMjUifQ.Uc_2ynPEcJAB9aNEEhydj4l2wYSF_ClAggtLd2lmvF4'
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1NDE1NzY5NjUsImV4cCI6MTU3MzExMjk2NSwiYXVkIjoiIiwic3ViIjoiMSIsInNjb3BlIjoiWydkZWNsYXJlJ10ifQ.yMSf_4TJaFqgczHg1YgC0ev_bPneLaJwRsrA9eZkmUk'
 
 describe('authUtils', () => {
   describe('getTokenPayload. Returns the correct payload from a token', () => {
     it('should return the right payload', () => {
       const response = {
         iss: '',
-        iat: 1541165461,
-        exp: 1541524697,
+        iat: 1541576965,
+        exp: 1573112965,
         aud: '',
         sub: '1',
-        jti: '0d775a14-47c5-49d3-b154-48ec38b51486', // NEED TO REMOVE THIS
         scope: "['performance']"
       }
       expect(getTokenPayload(performanceJWT)).toEqual(response)
