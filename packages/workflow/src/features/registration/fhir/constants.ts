7d71b784eb36a1ad9f2735c560a0ecd8b7568c02
anik <mushraful.hoque@dsinnovators.com>
2018-11-17 20:51:08 +0600

    adding test cases for reg workflow

--- packages/workflow/src/features/registration/fhir/constants.ts
+++ packages/workflow/src/features/registration/fhir/constants.ts
@@ -3,6 +3,6 @@ export const MOTHER_SECTION_CODE = 'mother-details'
 export const FATHER_SECTION_CODE = 'father-details'
 export const CHILD_SECTION_CODE = 'child-details'
 export enum EVENT_TYPE {
-  BIRTH,
-  DEATH
+  BIRTH = 'BIRTH',
+  DEATH = 'DEATH'
 }
--- packages/workflow/src/features/registration/fhir/fhir-bundle-modifier.test.ts
+++ packages/workflow/src/features/registration/fhir/fhir-bundle-modifier.test.ts
@@ -1,9 +1,15 @@
-import { pushTrackingId } from './fhir-bundle-modifier'
-import { OPENCRVS_SPECIFICATION_URL } from './constants'
+import {
+  pushTrackingId,
+  setupRegistrationType,
+  setupRegistrationWorkflow,
+  setupLastRegUser
+} from './fhir-bundle-modifier'
+import { OPENCRVS_SPECIFICATION_URL, EVENT_TYPE } from './constants'
 import { testFhirBundle } from 'src/test/utils'
+import { cloneDeep } from 'lodash'
 
-describe('Verify pushTrackingId function', () => {
-  it('Successfully modified the provided fhirBundle with trackingid', () => {
+describe('Verify fhir bundle modifier functions', () => {
+  it('PushTrackingId successfully modified the provided fhirBundle with trackingid', () => {
     const fhirBundle = pushTrackingId(testFhirBundle)
     const composition = fhirBundle.entry[0].resource as fhir.Composition
     const task = fhirBundle.entry[1].resource as fhir.Task
@@ -16,14 +22,14 @@ describe('Verify pushTrackingId function', () => {
     })
   })
 
-  it('Throws error if invalid fhir bundle is provided', () => {
+  it('PushTrackingId throws error if invalid fhir bundle is provided', () => {
     const invalidData = { ...testFhirBundle, entry: [] }
     expect(() => pushTrackingId(invalidData)).toThrowError(
       'Invalid FHIR bundle found for declaration'
     )
   })
 
-  it('Will push the composite resource identifier if it is missing on fhirDoc', () => {
+  it('PushTrackingId will push the composite resource identifier if it is missing on fhirDoc', () => {
     const fhirBundle = pushTrackingId({
       ...testFhirBundle,
       entry: [{ resource: {} }]
@@ -32,5 +38,131 @@ describe('Verify pushTrackingId function', () => {
 
     expect(composition.identifier.value).toMatch(/^B/)
     expect(composition.identifier.value.length).toBe(7)
+  })
+
+  it('SetupRegistrationType will push the proper event type on fhirDoc', () => {
+    const fhirBundle = setupRegistrationType(testFhirBundle, EVENT_TYPE.BIRTH)
+
+    expect(fhirBundle.entry[1].resource.code.coding[0].code).toBeDefined()
+    expect(fhirBundle.entry[1].resource.code.coding[0].code).toEqual(
+      EVENT_TYPE.BIRTH.toString()
+    )
+  })
+
+  it('SetupRegistrationType will push code section with proper event type on fhirDoc if it is missing', () => {
+    let fhirBundle = cloneDeep(testFhirBundle)
+    fhirBundle.entry[1].resource.code = undefined
+    fhirBundle = setupRegistrationType(fhirBundle, EVENT_TYPE.BIRTH)
+
+    expect(fhirBundle.entry[1].resource.code).toBeDefined()
+    expect(fhirBundle.entry[1].resource.code).toEqual({
+      coding: [
+        {
+          system: `${OPENCRVS_SPECIFICATION_URL}types`,
+          code: EVENT_TYPE.BIRTH.toString()
+        }
+      ]
+    })
+  })
+
+  it('SetupRegistrationWorkflow will push the registration status on fhirDoc', () => {
+    const tokenPayload = {
+      iss: '',
+      iat: 1541576965,
+      exp: 1573112965,
+      aud: '',
+      sub: '1',
+      scope: ['declare']
+    }
+    const fhirBundle = setupRegistrationWorkflow(testFhirBundle, tokenPayload)
+
+    expect(
+      fhirBundle.entry[1].resource.businessStatus.coding[0].code
+    ).toBeDefined()
+    expect(fhirBundle.entry[1].resource.businessStatus.coding[0].code).toEqual(
+      'DECLARED'
+    )
+  })
+  it('SetupRegistrationWorkflow will update existing registration status on fhirDoc', () => {
+    const tokenPayload = {
+      iss: '',
+      iat: 1541576965,
+      exp: 1573112965,
+      aud: '',
+      sub: '1',
+      scope: ['register']
+    }
+    let fhirBundle = cloneDeep(testFhirBundle)
+    fhirBundle.entry[1].resource.businessStatus = {
+      coding: [
+        {
+          system: `${OPENCRVS_SPECIFICATION_URL}reg-status`,
+          code: 'DECLARED'
+        }
+      ]
+    }
+    fhirBundle = setupRegistrationWorkflow(fhirBundle, tokenPayload)
+
+    expect(fhirBundle.entry[1].resource.businessStatus.coding.length).toBe(1)
+    expect(fhirBundle.entry[1].resource.businessStatus.coding[0].code).toEqual(
+      'REGISTERED'
+    )
+  })
+
+  it('SetupLastRegUser will push the last modified by userinfo on fhirDoc', () => {
+    const tokenPayload = {
+      iss: '',
+      iat: 1541576965,
+      exp: 1573112965,
+      aud: '',
+      subject: 'anik',
+      scope: ['declare']
+    }
+    const fhirBundle = setupLastRegUser(testFhirBundle, tokenPayload)
+
+    expect(fhirBundle.entry[1].resource.extension[1].valueString).toBeDefined()
+    expect(fhirBundle.entry[1].resource.extension[1].valueString).toEqual(
+      'anik'
+    )
+  })
+
+  it('SetupLastRegUser will push the last modified by userinfo even if no extension is defined yet on task resource', () => {
+    const tokenPayload = {
+      iss: '',
+      iat: 1541576965,
+      exp: 1573112965,
+      aud: '',
+      subject: 'anik',
+      scope: ['declare']
+    }
+    let fhirBundle = cloneDeep(testFhirBundle)
+    fhirBundle.entry[1].resource.extension = undefined
+    fhirBundle = setupLastRegUser(fhirBundle, tokenPayload)
+
+    expect(fhirBundle.entry[1].resource.extension[0].valueString).toBeDefined()
+    expect(fhirBundle.entry[1].resource.extension[0].valueString).toEqual(
+      'anik'
+    )
+  })
+
+  it('SetupLastRegUser will update the last modified by userinfo instead of always adding a new extension', () => {
+    const tokenPayload = {
+      iss: '',
+      iat: 1541576965,
+      exp: 1573112965,
+      aud: '',
+      subject: 'DUMMY',
+      scope: ['declare']
+    }
+    const lengthOfTaskExtensions =
+      testFhirBundle.entry[1].resource.extension.length
+    const fhirBundle = setupLastRegUser(testFhirBundle, tokenPayload)
+
+    expect(fhirBundle.entry[1].resource.extension.length).toBe(
+      lengthOfTaskExtensions
+    )
+    expect(fhirBundle.entry[1].resource.extension[1].valueString).toEqual(
+      'DUMMY'
+    )
   })
 })
