f90153749ca3a4032e2c583671fa518e4b57d41a
tahmid <tahmidrahman.dsi@gmail.com>
2018-11-19 13:57:07 +0600

    added theme in component and percentage value in markdown

--- packages/components/src/components/charts/VerticalBar.tsx
+++ packages/components/src/components/charts/VerticalBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 import {
   BarChart,
   Bar,
@@ -12,10 +12,11 @@ import {
   // Legend,
   Cell
 } from 'recharts'
+import { ITheme } from '../theme'
 
 const Container = styled.div`
   box-sizing: border-box;
-  height: 350px;
+  height: 250px;
   width: 100%;
   align-items: center;
 `
@@ -43,48 +44,64 @@ function CustomizedAxisTick(props: ICustomizedAxisTick) {
       <text x={0} y={0} dy={16} textAnchor="end" fill="#666">
         {payload && payload.value}
       </text>
-      <text x={0} y={20} dy={16} textAnchor="end" fill="#666">
-        {payload && payload.value}
-      </text>
-      <text x={0} y={40} dy={16} textAnchor="end" fill="#666">
+      <text x={0} y={20} dy={16} textAnchor="end" fill="#4C68C1">
         {payload && payload.value}
       </text>
     </g>
   )
 }
 
-export function VerticalBar(props: IVerticalBarProps) {
-  return (
-    <Container>
-      <ResponsiveContainer>
-        <BarChart width={600} height={300} data={props.data} barCategoryGap={0}>
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis
-            height={80}
-            tick={<CustomizedAxisTick />}
-            tickLine={false}
-            axisLine={false}
-            dataKey="name"
-          />
+export const VerticalBar = withTheme(
+  (props: IVerticalBarProps & { theme: ITheme }) => {
+    const { data, theme } = props
+    const colours = [
+      theme.colors.chartPrimary,
+      theme.colors.chartSecondary,
+      theme.colors.chartTertiary
+    ]
 
-          <CartesianGrid strokeOpacity={0} fill="rgba(94, 147, 237, 0.05)" />
+    return (
+      <Container>
+        <ResponsiveContainer>
+          <BarChart
+            width={600}
+            height={300}
+            data={props.data}
+            barCategoryGap={0}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={colours[1]} stopOpacity={0.9} />
+                <stop offset="95%" stopColor={colours[2]} stopOpacity={0.5} />
+              </linearGradient>
+            </defs>
+            <XAxis
+              height={80}
+              tick={<CustomizedAxisTick />}
+              tickLine={false}
+              axisLine={false}
+              dataKey={({ name, percentage }) => name}
+            />
 
-          <Bar dataKey="value">
-            {props.data.map((entry, index) => (
-              <Cell
-                cursor="pointer"
-                fill={index % 2 === 0 ? '#8884d8' : 'url(#colorUv)'}
-                key={`cell-${index}`}
-              />
-            ))}
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
-    </Container>
-  )
-}
+            <CartesianGrid
+              vertical={false}
+              horizontal={false}
+              fillOpacity="0.05"
+              fill={theme.colors.accentLight}
+            />
+
+            <Bar dataKey="value">
+              {data.map((entry, index) => (
+                <Cell
+                  cursor="pointer"
+                  fill={index % 2 === 0 ? colours[0] : 'url(#colorUv)'}
+                  key={`cell-${index}`}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </Container>
+    )
+  }
+)
