68bc0fca3a4f95e32947c72d7456542745f77cfa
Atiq Zaman <atiq.zaman@dsinnovators.com>
2018-11-20 13:13:02 +0600

    Fixed path issue

--- packages/register/src/forms/review/selectors.test.ts
+++ packages/register/src/forms/review/selectors.test.ts
@@ -1,5 +1,5 @@
 import { getReviewForm } from './selectors'
-import { createStore } from '../../../../register/src/store'
+import { createStore } from '@opencrvs/register/src/store'
 
 describe('when user in review form review tab should be there', () => {
   const { store } = createStore()
