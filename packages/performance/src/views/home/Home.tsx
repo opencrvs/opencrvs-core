c630eefcfccf4ed7f3aa539edd01634387f314a2
tahmid <tahmidrahman.dsi@gmail.com>
2018-11-20 12:54:03 +0600

    added chart in app

--- packages/performance/src/views/home/Home.tsx
+++ packages/performance/src/views/home/Home.tsx
@@ -5,7 +5,7 @@ import { Header, Box } from '@opencrvs/components/lib/interface'
 import styled from 'src/styled-components'
 import Logo from 'src/components/Logo'
 import { Page } from 'src/components/Page'
-import { Bar, Legend } from '@opencrvs/components/lib/charts'
+import { Bar, Legend, VerticalBar } from '@opencrvs/components/lib/charts'
 
 const data = [
   {
@@ -32,6 +32,21 @@ const data = [
   }
 ]
 
+const birthRegistrationData = [
+  { name: '45d', value: 2100 },
+  { name: '46d - 1yr', value: 2400 },
+  { name: '1', value: 1398 },
+  { name: '2', value: 6800 },
+  { name: '3', value: 3908 },
+  { name: '4', value: 4800 },
+  { name: '5', value: 3800 },
+  { name: '6', value: 4300 },
+  { name: '7', value: 2500 },
+  { name: '8', value: 5680 },
+  { name: '9', value: 4980 },
+  { name: '10', value: 2570 }
+]
+
 const StyledHeader = styled(Header)`
   padding: 0 26px;
   box-shadow: none;
@@ -40,6 +55,10 @@ const StyledHeader = styled(Header)`
 const ChartContainer = styled(Box)`
   max-width: ${({ theme }) => theme.grid.breakpoints.lg}px;
   margin: auto;
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
 `
 
 const Container = styled.div`
@@ -56,6 +75,13 @@ class HomeView extends React.Component<InjectedIntlProps> {
           <ChartContainer>
             <Bar data={data} />
             <Legend data={data} />
+          </ChartContainer>
+          <ChartContainer>
+            <VerticalBar
+              data={birthRegistrationData}
+              xAxisLabel="Age(years)"
+              yAxisLabel="Total Births Registered (%)"
+            />
           </ChartContainer>
         </Container>
       </Page>
