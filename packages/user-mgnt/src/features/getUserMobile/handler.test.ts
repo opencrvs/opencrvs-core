0f9067f938a9462cacd2ef8bd570e34ea3342c56
Euan Millar <euanmillar77@gmail.com>
2018-11-27 17:31:28 +0000

    fixed test description

--- packages/user-mgnt/src/features/getUserMobile/handler.test.ts
+++ packages/user-mgnt/src/features/getUserMobile/handler.test.ts
@@ -35,8 +35,8 @@ test("getUserMobile should throw with 401 when user doesn't exist", async () => 
   expect(spy).toBeCalled()
 })
 
-test('getUserMobile should return 200 and the user scope when the user exists and the password hash matches', async () => {
-  const spy = jest.spyOn(User, 'findOne').mockResolvedValueOnce({
+test('getUserMobile should return 200 and the mobile number', async () => {
+  const spy = jest.spyOn(User, 'findById').mockResolvedValueOnce({
     mobile: '27555555555',
     passwordHash:
       'b8be6cae5215c93784b1b9e2c06384910f754b1d66c077f1f8fdc98fbd92e6c17a0fdc790b30225986cadb9553e87a47b1d2eb7bd986f96f0da7873e1b2ddf9c',
