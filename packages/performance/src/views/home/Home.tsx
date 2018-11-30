87571adeeb427e1c3e87a3600d58f1f1f8814750
tahmid <tahmidrahman.dsi@gmail.com>
2018-11-30 12:44:48 +0600

    updated mock data

--- packages/performance/src/views/home/Home.tsx
+++ packages/performance/src/views/home/Home.tsx
@@ -165,18 +165,18 @@ const birthRegistrationData = [
 ]
 
 const birthRegistrationDataPerMonth = [
-  { name: 'Jan', value: 2100, total: 10000 },
-  { name: 'Feb', value: 2400, total: 10000 },
-  { name: 'Mar', value: 1398, total: 10000 },
-  { name: 'Apr', value: 6800, total: 10000 },
-  { name: 'May', value: 3908, total: 10000 },
-  { name: 'Jun', value: 4800, total: 10000 },
-  { name: 'Jul', value: 3800, total: 10000 },
-  { name: 'Aug', value: 4300, total: 10000 },
-  { name: 'Sep', value: 2500, total: 10000 },
-  { name: 'Oct', value: 5680, total: 10000 },
-  { name: 'Nov', value: 4980, total: 10000 },
-  { name: 'Dec', value: 8570, total: 10000 }
+  { label: 'Jan', value: 2100, totalEstimate: 10000 },
+  { label: 'Feb', value: 2400, totalEstimate: 10000 },
+  { label: 'Mar', value: 1398, totalEstimate: 10000 },
+  { label: 'Apr', value: 6800, totalEstimate: 10000 },
+  { label: 'May', value: 3908, totalEstimate: 10000 },
+  { label: 'Jun', value: 4800, totalEstimate: 10000 },
+  { label: 'Jul', value: 3800, totalEstimate: 10000 },
+  { label: 'Aug', value: 4300, totalEstimate: 10000 },
+  { label: 'Sep', value: 2500, totalEstimate: 10000 },
+  { label: 'Oct', value: 5680, totalEstimate: 10000 },
+  { label: 'Nov', value: 4980, totalEstimate: 10000 },
+  { label: 'Dec', value: 8570, totalEstimate: 10000 }
 ]
 
 const StyledHeader = styled(Header)`
