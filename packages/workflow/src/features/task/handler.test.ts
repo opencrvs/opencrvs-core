49b45dc36edc4b4d2ec736a7c4f2587954bc860f
Euan Millar <euanmillar77@gmail.com>
2018-11-30 16:31:44 +0000

    handler error tests

--- packages/workflow/src/features/task/handler.test.ts
+++ packages/workflow/src/features/task/handler.test.ts
@@ -43,4 +43,59 @@ describe('Verify handler', () => {
     })
     expect(res.statusCode).toBe(200)
   })
+  it('updateTaskHandler throws error if invalid fhir data is provided', async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        resourceType: 'Bundle',
+        entry: [
+          {
+            response: { resourceType: 'Task' }
+          }
+        ]
+      })
+    )
+
+    const token = jwt.sign(
+      { scope: ['declare'] },
+      readFileSync('../auth/test/cert.key'),
+      {
+        algorithm: 'RS256',
+        issuer: 'opencrvs:auth-service',
+        audience: 'opencrvs:workflow-user'
+      }
+    )
+
+    const res = await server.server.inject({
+      method: 'POST',
+      url: '/updateTask',
+      payload: { data: 'INVALID' },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    expect(res.statusCode).toBe(500)
+  })
+  it('updateTaskHandler throws error if fhir returns an error', async () => {
+    fetch.mockImplementationOnce(() => new Error('boom'))
+
+    const token = jwt.sign(
+      { scope: ['declare'] },
+      readFileSync('../auth/test/cert.key'),
+      {
+        algorithm: 'RS256',
+        issuer: 'opencrvs:auth-service',
+        audience: 'opencrvs:workflow-user'
+      }
+    )
+
+    const res = await server.server.inject({
+      method: 'POST',
+      url: '/updateTask',
+      payload: testFhirTaskBundle,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    expect(res.statusCode).toBe(500)
+  })
 })
