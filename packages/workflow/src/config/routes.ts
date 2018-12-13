f897fc79f1edf64c775c2c4952d6e4c19b9b4a55
Ryan Crichton <ryan@jembi.org>
2018-12-13 09:03:09 +0200

    Add new single root handler
    The workflow service should accept FHIR-style requests only. We need to
    remove the custom endpoint we are using and rather detect the event that
    is happening from the data received in the FHIR request rather than by
    the endpont used. This is so the service remains standards based for 3rd
    party users.
    
    We are temporarily keeping the old routes for reference.
    
    OCRVS-918

--- packages/workflow/src/config/routes.ts
+++ packages/workflow/src/config/routes.ts
@@ -1,8 +1,4 @@
-import updateTaskHandler from '../features/task/handler'
-import {
-  createBirthRegistrationHandler,
-  markBirthAsRegisteredHandler
-} from '../features/registration/handler'
+import { fhirWorkflowEventHandler } from 'src/features/events/handler'
 
 const enum RouteScope {
   DECLARE = 'declare',
@@ -24,67 +20,93 @@ export const getRoutes = () => {
       }
     },
     {
-      method: 'POST',
-      path: '/createBirthRegistration',
-      handler: createBirthRegistrationHandler,
+      method: '*',
+      path: '/fhir/{path*}',
+      handler: fhirWorkflowEventHandler,
       config: {
         tags: ['api'],
         description:
-          'Push trackingid, note author and appropriate status before submitting to Hearth and send notification to the shared contact',
+          'Mimics the fhir API, detects OpenCRVS event and calls the correct workflow handler. Else, just forwards the request to Hearth.',
         auth: {
-          scope: [RouteScope.DECLARE, RouteScope.REGISTER]
+          scope: [RouteScope.DECLARE, RouteScope.REGISTER] // TODO
         },
         plugins: {
           'hapi-swagger': {
             responses: {
-              200: { description: 'Birth declaration is successfully synced' },
-              400: { description: 'Bad request, check your request body' }
-            }
-          }
-        }
-      }
-    },
-    {
-      method: 'POST',
-      path: '/updateTask',
-      handler: updateTaskHandler,
-      config: {
-        tags: ['api'],
-        description: 'Update Task with the user information and send to fhir',
-        auth: {
-          scope: [RouteScope.DECLARE, RouteScope.REGISTER, RouteScope.CERTIFY]
-        },
-        plugins: {
-          'hapi-swagger': {
-            responses: {
-              200: { description: 'Task is successfully synced' },
-              400: { description: 'Bad request, check your request body' }
-            }
-          }
-        }
-      }
-    },
-    {
-      method: 'POST',
-      path: '/markBirthAsRegistered',
-      handler: markBirthAsRegisteredHandler,
-      config: {
-        tags: ['api'],
-        description:
-          'Push BRN number and registered status before submitting to Hearth',
-        auth: {
-          scope: [RouteScope.REGISTER]
-        },
-        plugins: {
-          'hapi-swagger': {
-            responses: {
-              200: { description: 'Birth registration is successfull' },
-              400: { description: 'Bad request, check your request body' }
+              200: {
+                description: 'Successful, see standard fhir response types'
+              },
+              400: {
+                description:
+                  'Bad request, see standard fhir error response types'
+              }
             }
           }
         }
       }
     }
+    // {
+    //   method: 'POST',
+    //   path: '/createBirthRegistration',
+    //   handler: createBirthRegistrationHandler,
+    //   config: {
+    //     tags: ['api'],
+    //     description:
+    //       'Push trackingid, note author and appropriate status before submitting to Hearth and send notification to the shared contact',
+    //     auth: {
+    //       scope: [RouteScope.DECLARE, RouteScope.REGISTER]
+    //     },
+    //     plugins: {
+    //       'hapi-swagger': {
+    //         responses: {
+    //           200: { description: 'Birth declaration is successfully synced' },
+    //           400: { description: 'Bad request, check your request body' }
+    //         }
+    //       }
+    //     }
+    //   }
+    // },
+    // {
+    //   method: 'POST',
+    //   path: '/updateTask',
+    //   handler: updateTaskHandler,
+    //   config: {
+    //     tags: ['api'],
+    //     description: 'Update Task with the user information and send to fhir',
+    //     auth: {
+    //       scope: [RouteScope.DECLARE, RouteScope.REGISTER, RouteScope.CERTIFY]
+    //     },
+    //     plugins: {
+    //       'hapi-swagger': {
+    //         responses: {
+    //           200: { description: 'Task is successfully synced' },
+    //           400: { description: 'Bad request, check your request body' }
+    //         }
+    //       }
+    //     }
+    //   }
+    // },
+    // {
+    //   method: 'POST',
+    //   path: '/markBirthAsRegistered',
+    //   handler: markBirthAsRegisteredHandler,
+    //   config: {
+    //     tags: ['api'],
+    //     description:
+    //       'Push BRN number and registered status before submitting to Hearth',
+    //     auth: {
+    //       scope: [RouteScope.REGISTER]
+    //     },
+    //     plugins: {
+    //       'hapi-swagger': {
+    //         responses: {
+    //           200: { description: 'Birth registration is successfull' },
+    //           400: { description: 'Bad request, check your request body' }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   ]
   return routes
 }
