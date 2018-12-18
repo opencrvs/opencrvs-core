81649a322fefa831e5ecbf1ab311ad18c8fb2823
armanbhuiyan-dsi <armanbhuiyan.dsi@gmail.com>
2018-12-18 17:18:09 +0600

    Create informative radio group component

---
+++ packages/register/src/views/PrintCertificate/InformativeRadioGroup.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import { RadioGroup } from '@opencrvs/components/lib/forms'
+import { InputField } from 'src/components/form/InputField'
+import {
+  Ii18nFormField,
+  IFormFieldValue,
+  Ii18nRadioGroupFormField
+} from 'src/forms'
+import { injectIntl } from 'react-intl'
+
+type IInputProps = {
+  id: string
+  onChange: (e: React.ChangeEvent<any>) => void
+  onBlur: (e: React.FocusEvent<any>) => void
+  value: IFormFieldValue
+  disabled: boolean
+  error: boolean
+  touched: boolean
+}
+
+type IInputFieldProps = {
+  id: string
+  label: string
+  description: string
+  required: boolean
+  disabled: boolean
+  prefix: string
+  postfix: string
+  error: string
+  touched: boolean
+}
+
+type IProps = {
+  value: IFormFieldValue
+  inputProps: IInputProps
+  inputFieldProps: IInputFieldProps
+  fieldDefinition: Ii18nFormField
+  onSetFieldValue: (name: string, value: IFormFieldValue) => void
+}
+
+function InformativeRadioGroupComponent({
+  value,
+  inputProps,
+  inputFieldProps,
+  onSetFieldValue,
+  fieldDefinition
+}: IProps) {
+  return (
+    <InputField {...inputFieldProps}>
+      <RadioGroup
+        {...inputProps}
+        onChange={(val: string) => onSetFieldValue(fieldDefinition.name, val)}
+        options={(fieldDefinition as Ii18nRadioGroupFormField).options}
+        name={fieldDefinition.name}
+        value={value as string}
+      />
+    </InputField>
+  )
+}
+
+export const InformativeRadioGroup = injectIntl(InformativeRadioGroupComponent)
