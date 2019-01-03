0dcff681dad0a9b0150df5213667d466c48e379f
Tahmid <tahmidrahman.dsi@gmail.com>
2019-01-03 19:52:05 +0600

    added print icon in library

--- packages/register/src/views/WorkQueue/WorkQueue.tsx
+++ packages/register/src/views/WorkQueue/WorkQueue.tsx
@@ -942,9 +942,9 @@ export class WorkQueueView extends React.Component<
         <Container>
           <Query
             query={FETCH_REGISTRATION_QUERY}
-            // variables={{
-            //   locationIds: [this.getLocalLocationId()]
-            // }}
+            variables={{
+              locationIds: [this.getLocalLocationId()]
+            }}
           >
             {({ loading, error, data }) => {
               if (loading) {
