74d26fccd9a2cc33ca92b0b259dc337039865946
MaheshKJembi <mahesh.kandagatla@jembi.org>
2019-01-07 11:54:28 +0200

    changed local host to staging
    OCRVS-662

--- packages/register/src/config.ts
+++ packages/register/src/config.ts
@@ -3,6 +3,8 @@ export const config = {
     process.env.REACT_APP_API_GATEWAY_URL || 'http://localhost:7070/',
   LANGUAGE: process.env.REACT_APP_LANGUAGE || 'en',
   COUNTRY: process.env.REACT_APP_COUNTRY || 'bgd',
-  LOGIN_URL: process.env.REACT_APP_LOGIN_URL || 'http://localhost:3020',
+  LOGIN_URL:
+    process.env.REACT_APP_LOGIN_URL ||
+    'https://login.opencrvs-staging.jembi.org',
   BACKGROUND_SYNC_BROADCAST_CHANNEL: 'backgroundSynBroadCastChannel'
 }
