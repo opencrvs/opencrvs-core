a06c11cc7c025c9fe8c98b997d89986ebb9889c0
Sadman Ilham <sadman.ilham@gmail.com>
2019-01-09 14:29:16 +0600

    Fix Position

--- packages/register/src/components/ConnectivityStatus.tsx
+++ packages/register/src/components/ConnectivityStatus.tsx
@@ -5,13 +5,13 @@ import styled from 'src/styled-components'
 const StyledOnline = styled.div`
   position: absolute;
   top: 32px;
-  right: 234px;
+  right: 210px;
 `
 
 const StyledOffline = styled.div`
   position: absolute;
   top: 32px;
-  right: 234px;
+  right: 210px;
 `
 
 const ConnectivityStatus = () => {
