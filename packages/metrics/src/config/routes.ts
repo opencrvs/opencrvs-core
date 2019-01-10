def1eb2ae7c219c10e1efaddb795209551234b89
Ryan Crichton <ryan@jembi.org>
2019-01-10 12:15:52 +0200

    Boostrap metrics routes for upcoming tasks
    OCRVS-1002

--- packages/metrics/src/config/routes.ts
+++ packages/metrics/src/config/routes.ts
@@ -10,6 +10,47 @@ export const getRoutes = () => {
       config: {
         tags: ['api']
       }
+    },
+
+    // Event receiver routes
+    {
+      method: 'GET',
+      path: '/events/new-registration',
+      handler: (request: any, h: any) => {
+        // TODO: generate metrics from this event
+        return {}
+      },
+      config: {
+        tags: ['api']
+      }
+    },
+    {
+      method: 'GET',
+      path: '/events/registration',
+      handler: (request: any, h: any) => {
+        // TODO: generate metrics from this event
+        return {}
+      },
+      config: {
+        tags: ['api']
+      }
+    },
+
+    // Metrics query API
+    {
+      method: 'GET',
+      path: '/metrics/birth',
+      handler: (request: any, h: any) => {
+        // TODO: generate all birth metrics, use a timePeriod search param to control whether the points in the series are month or years
+        return {
+          keyFigures: {},
+          regByAge: {},
+          regWithin$5d: {}
+        }
+      },
+      config: {
+        tags: ['api']
+      }
     }
   ]
   return routes
