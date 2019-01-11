d2dd44f4039db60ae9643ee749f1d5ebf8fea3f1
MaheshKJembi <mahesh.kandagatla@jembi.org>
2019-01-11 14:22:00 +0200

    Removed login code to skip login
    OCRVS-662

--- packages/e2e/cypress/integration/menu.spec.ts
+++ packages/e2e/cypress/integration/menu.spec.ts
@@ -5,26 +5,8 @@ context('Login', () => {
     cy.visit(Cypress.env('LOGIN_URL'))
   })
 
-  it('takes user to the registration app once correct credentials are given', () => {
-    cy.get('#mobile').type('01711111111')
-    cy.get('#password').type('test')
-    cy.get('#login-mobile-submit').click()
-    cy.get('#code1').type('0')
-    cy.get('#code2').type('0')
-    cy.get('#code3').type('0')
-    cy.get('#code4').type('0')
-    cy.get('#code5').type('0')
-    cy.get('#code6').type('0')
-
-    cy.get('#login-mobile-submit').click()
-
-    cy.get('#new_event_declaration').click()
-
-    cy.wait(5000);
-
-    //it('fills in all data into the register form', () => {
-    //  cy.visit(`${Cypress.env('REGISTER_URL')}events`)
-      // CHILD DETAILS
+  it('fills in all data into the register form', () => {
+    cy.visit(`${Cypress.env('REGISTER_URL')}events`)
 
     cy.get('#sub-menu').click()
     //Click on change Lanuage
