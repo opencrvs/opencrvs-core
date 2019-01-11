03533b6f6203625c5b0b3ec55b3081f20945d23c
MaheshKJembi <mahesh.kandagatla@jembi.org>
2019-01-11 11:42:38 +0200

    Updated class with IDs
    OCRVS-662

--- packages/e2e/cypress/integration/menu.spec.ts
+++ packages/e2e/cypress/integration/menu.spec.ts
@@ -24,12 +24,11 @@ context('Login', () => {
     //  cy.visit(`${Cypress.env('REGISTER_URL')}events`)
       // CHILD DETAILS
 
-    cy.get('[class*=rc-menu-submenu-title]').click()
-
+    cy.get('#sub-menu').click()
     //Click on change Lanuage
-    cy.get('[class*="rc-menu-submenu rc-menu-submenu-inline sc-cMljjf eKDCjU"]').click()
+    cy.get('#ChangeLanguage-nested-menu').click();
     // select Bengali
-    cy.get('[class*="rc-menu-item sc-jAaTju filVPB"]').contains('Bengali').click()
+    cy.get('#Bengali-nested-menu-item').contains('Bengali').click()
 
     cy.get('#select_vital_event_view').should('be.visible')
 
