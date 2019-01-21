5cf5f3d09e878980fdf328c1662eaa774d1d1278
Sadman Ilham <sadman.ilham@gmail.com>
2019-01-21 14:15:43 +0600

    Fix Message and Test

--- packages/register/src/utils/validate.ts
+++ packages/register/src/utils/validate.ts
@@ -39,7 +39,7 @@ export const messages = defineMessages({
     defaultMessage:
       'Must be 11 digit valid mobile phone number that stars with 01',
     description:
-      'The error message that appears on phone numbers where the first character must be a 0'
+      'The error message that appears on phone numbers where the first two characters must be a 01 and length must be 11'
   },
   dateFormat: {
     id: 'validations.dateFormat',
