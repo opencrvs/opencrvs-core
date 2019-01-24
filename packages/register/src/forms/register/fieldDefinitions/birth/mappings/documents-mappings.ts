56a7d426c509aed4ee2f17ba3bcaa6781615344f
anik <mushraful.hoque@dsinnovators.com>
2019-01-24 19:58:30 +0600

    removing unnecessary code

--- packages/register/src/forms/register/fieldDefinitions/birth/mappings/documents-mappings.ts
+++ packages/register/src/forms/register/fieldDefinitions/birth/mappings/documents-mappings.ts
@@ -28,26 +28,3 @@ export function birthAttachmentTransformer(
     documentTypeFhirMapping
   )
 }
-
-export function registrationSectionTransformer(
-  transformedData: any,
-  draftData: IFormData,
-  sectionId: string
-) {
-  if (draftData[sectionId].trackingId) {
-    transformedData[sectionId].trackingId = draftData[sectionId].trackingId
-  }
-
-  if (draftData[sectionId].registrationNumber) {
-    transformedData[sectionId].registrationNumber =
-      draftData[sectionId].registrationNumber
-  }
-
-  if (!transformedData[sectionId].status) {
-    transformedData[sectionId].status = [
-      {
-        timestamp: new Date()
-      }
-    ]
-  }
-}
