1eece61f09d6d06015d7886958c42105b1fd4b2d
Tofael Ahmed <tofael.ahmed@dsinnovators.com>
2019-01-24 13:35:04 +0600

    Enable data table to have default sort and fitler

--- packages/register/src/views/WorkQueue/WorkQueue.tsx
+++ packages/register/src/views/WorkQueue/WorkQueue.tsx
@@ -901,7 +901,7 @@ export class WorkQueueView extends React.Component<
                 label: intl.formatMessage(messages.filtersNewestToOldest)
               }
             ],
-            value: '',
+            value: 'desc',
             type: SelectFieldType.Date
           }
         ]
