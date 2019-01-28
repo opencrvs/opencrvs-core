9f42a8b7de6de32f82c68207e2b4efafb35ea459
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-01-28 17:45:06 +0600

    OCRVS-1095: Increase in test cov

--- packages/register/src/views/PrintCertificate/PrintCertificateAction.test.tsx
+++ packages/register/src/views/PrintCertificate/PrintCertificateAction.test.tsx
@@ -784,15 +784,20 @@ describe('when user wants to print certificate', async () => {
     it('timeElapsedInWords function returns required time duration in words', () => {
       Date.now = jest.fn(() => new Date('2019-01-01'))
 
-      let days = calculateDays('2018-08-18')
+      let days = calculateDays('1985-08-18')
 
       let time = timeElapsed(days)
-      expect(time.value).toBe(4)
-      expect(time.unit).toBe('Month')
+      expect(time.value).toBe(33)
+      expect(time.unit).toBe('Year')
       days = calculateDays('2018-12-16')
       time = timeElapsed(days)
       expect(time.value).toBe(16)
       expect(time.unit).toBe('Day')
+
+      days = calculateDays('2018-10-16')
+      time = timeElapsed(days)
+      expect(time.value).toBe(2)
+      expect(time.unit).toBe('Month')
 
       let error
       try {
