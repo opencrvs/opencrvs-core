c879cf61efbbc8b589bb52d25a9bc6dc2ee02be8
Euan Millar <euanmillar77@gmail.com>
2019-01-29 22:23:08 +0000

    change menu spec

--- packages/e2e/cypress/integration/menu.spec.ts
+++ packages/e2e/cypress/integration/menu.spec.ts
@@ -2,11 +2,23 @@
 
 context('Menu', () => {
   beforeEach(() => {
-    cy.login('fieldWorker')
+    cy.visit(Cypress.env('LOGIN_URL'))
   })
 
   it('Opens menu and selects Bengali', () => {
-    cy.wait(60000)
+    cy.get('#mobile').type('01711111111')
+    cy.get('#password').type('test')
+    cy.get('#login-mobile-submit').click()
+    cy.get('#code1').type('0')
+    cy.get('#code2').type('0')
+    cy.get('#code3').type('0')
+    cy.get('#code4').type('0')
+    cy.get('#code5').type('0')
+    cy.get('#code6').type('0')
+
+    cy.get('#login-mobile-submit').click()
+    cy.location('search').should('match', /\?token=.*/)
+    cy.wait(1000)
     cy.get('#sub-menu').click()
     //Click on change Language
     cy.get('#ChangeLanguage-nested-menu').click()
