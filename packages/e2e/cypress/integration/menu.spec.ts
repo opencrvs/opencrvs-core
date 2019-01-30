3bb82293e1d1c7396575a70de08b9755ad8c4ad0
Euan Millar <euanmillar77@gmail.com>
2019-01-30 12:24:08 +0000

    test data import for Travis

--- packages/e2e/cypress/integration/menu.spec.ts
+++ packages/e2e/cypress/integration/menu.spec.ts
@@ -2,30 +2,16 @@
 
 context('Menu', () => {
   beforeEach(() => {
-    cy.visit(Cypress.env('LOGIN_URL'))
+    cy.login('fieldWorker')
   })
 
   it('Opens menu and selects Bengali', () => {
-    cy.get('#mobile').type('01711111111')
-    cy.get('#password').type('test')
-    cy.get('#login-mobile-submit').click()
-    cy.get('#code1').type('0')
-    cy.get('#code2').type('0')
-    cy.get('#code3').type('0')
-    cy.get('#code4').type('0')
-    cy.get('#code5').type('0')
-    cy.get('#code6').type('0')
+    cy.get('#sub-menu').click()
+    cy.get('#ChangeLanguage-nested-menu').click()
+    cy.get('#Bengali-nested-menu-item')
+      .contains('Bengali')
+      .click()
 
-    cy.get('#login-mobile-submit').click()
-    cy.location('search').should('match', /\?token=.*/)
-    cy.wait(1000)
-    cy.document()
-    // cy.get('#sub-menu').click()
-    //Click on change Language
-    // cy.get('#ChangeLanguage-nested-menu').click()
-    // select Bengali
-    // cy.get('#Bengali-nested-menu-item').contains('Bengali').click()
-
-    // cy.get('#new_event_declaration').should('be.visible')
+    cy.get('#new_event_declaration').should('be.visible')
   })
 })
