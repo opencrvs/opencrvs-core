6a2646b45f433a48f806779266dc7a28585a3e06
Tofael Ahmed <tofael.ahmed@dsinnovators.com>
2019-02-01 12:33:56 +0600

    Redirect to workqueue when there is no duplicates

--- packages/register/src/views/Duplicates/ReviewDuplicates.tsx
+++ packages/register/src/views/Duplicates/ReviewDuplicates.tsx
@@ -386,7 +386,11 @@ class ReviewDuplicatesClass extends React.Component<Props, IState> {
 
   successfulDuplicateRemoval = (response: string) => {
     this.toggleNotDuplicateModal()
-    window.location.reload()
+    if (response === this.state.selectedCompositionID) {
+      window.location.assign('/work-queue')
+    } else {
+      window.location.reload()
+    }
   }
 
   render() {
@@ -424,6 +428,12 @@ class ReviewDuplicatesClass extends React.Component<Props, IState> {
                   {this.props.intl.formatMessage(messages.queryError)}
                 </ErrorText>
               )
+            }
+
+            if (
+              data.fetchBirthRegistration.registration.duplicates.length <= 0
+            ) {
+              window.location.assign('/work-queue')
             }
 
             let duplicateIds = [applicationId]
