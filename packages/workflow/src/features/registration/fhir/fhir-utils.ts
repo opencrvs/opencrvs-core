c1786fcce22a14302aa6e6ef36a34bdcf0c6ba21
Sadman Ilham <sadman.ilham@gmail.com>
2019-02-07 19:27:17 +0600

    Handle 'BOTH' Contact Person
    - Return section code of 'MOTHER' for contact type 'BOTH'

--- packages/workflow/src/features/registration/fhir/fhir-utils.ts
+++ packages/workflow/src/features/registration/fhir/fhir-utils.ts
@@ -19,7 +19,8 @@ import { getEventType } from '../utils'
 
 enum CONTACT {
   MOTHER,
-  FATHER
+  FATHER,
+  BOTH
 }
 
 export async function getSharedContactMsisdn(fhirBundle: fhir.Bundle) {
@@ -195,6 +196,7 @@ export function getPaperFormID(taskResource: fhir.Task) {
 function getContactSection(contact: CONTACT) {
   switch (contact) {
     case CONTACT.MOTHER:
+    case CONTACT.BOTH:
       return MOTHER_SECTION_CODE
     case CONTACT.FATHER:
       return FATHER_SECTION_CODE
