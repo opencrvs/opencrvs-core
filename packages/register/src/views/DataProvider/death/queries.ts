c0a8e6b487b26b99c3d6298f0da6c7d758f60f76
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-02-22 15:54:28 +0600

    ocrvs-1252: seperated review & certification query

--- packages/register/src/views/DataProvider/death/queries.ts
+++ packages/register/src/views/DataProvider/death/queries.ts
@@ -74,6 +74,102 @@ export const GET_DEATH_REGISTRATION_FOR_REVIEW = gql`
           contentType
           subject
         }
+        type
+        trackingId
+        registrationNumber
+      }
+      eventLocation {
+        id
+        type
+        address {
+          type
+          line
+          district
+          state
+          postalCode
+          country
+        }
+      }
+      mannerOfDeath
+      causeOfDeathMethod
+      causeOfDeath
+    }
+  }
+`
+
+export const GET_DEATH_REGISTRATION_FOR_CERTIFICATION = gql`
+  query data($id: ID!) {
+    fetchDeathRegistration(id: $id) {
+      _fhirIDMap
+      id
+      deceased {
+        id
+        name {
+          use
+          firstNames
+          familyName
+        }
+        birthDate
+        gender
+        maritalStatus
+        nationality
+        identifier {
+          id
+          type
+          otherType
+        }
+        gender
+        deceased {
+          deathDate
+        }
+        address {
+          type
+          line
+          district
+          state
+          postalCode
+          country
+        }
+      }
+      informant {
+        id
+        relationship
+        individual {
+          id
+          identifier {
+            id
+            type
+            otherType
+          }
+          name {
+            use
+            firstNames
+            familyName
+          }
+          nationality
+          birthDate
+          telecom {
+            system
+            value
+          }
+          address {
+            type
+            line
+            district
+            state
+            postalCode
+            country
+          }
+        }
+      }
+      registration {
+        id
+        attachments {
+          data
+          type
+          contentType
+          subject
+        }
         status {
           comments {
             comment
@@ -124,7 +220,7 @@ export function getDeathQueryMappings(action: Action) {
       }
     case Action.LOAD_CERTIFICATE_APPLICATION:
       return {
-        query: GET_DEATH_REGISTRATION_FOR_REVIEW,
+        query: GET_DEATH_REGISTRATION_FOR_CERTIFICATION,
         dataKey: 'fetchDeathRegistration'
       }
     default:
