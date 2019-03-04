1b9bec946ce2f5d13d8a6abce8b53db7cb1b5273
Tahmid <tahmidrahman.dsi@gmail.com>
2019-03-04 19:31:48 +0600

    removed title elements from svg icon components

--- packages/components/src/components/icons/Next.tsx
+++ packages/components/src/components/icons/Next.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 
 export const Next = (props: React.HTMLAttributes<SVGElement>) => (
   <svg width={48} height={40} {...props}>
-    <title>{`65E1FE3A-633C-420A-9DE2-BC31B1F59AB3-565-00008E0CCBF0CB65`}</title>
     <g fill="none" fillRule="evenodd">
       <rect fill="#FFF" width={48} height={40} rx={20} />
       <path
