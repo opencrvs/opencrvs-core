a31c60b197b568b7d62c2e6647631088248ab726
Sadman Ilham <sadman.ilham@gmail.com>
2019-03-12 13:55:01 +0600

    Initiate and Add Utilities for InfluxDB

---
+++ packages/metrics/src/influxdb/constants.ts
@@ -0,0 +1,3 @@
+export const INFLUX_HOST = process.env.INFLUX_HOST || 'localhost'
+export const INFLUX_PORT = process.env.INFLUX_PORT || 8086
+export const INFLUX_DB = process.env.INFLUX_DB || 'ocrvs'
