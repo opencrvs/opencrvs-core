ea88a527aa9fa1b06915dc7a16a0741956259d1b
Sadman Ilham <sadman.ilham@gmail.com>
2019-03-12 19:46:49 +0600

    Add try catch block

--- packages/metrics/src/features/registration/handler.ts
+++ packages/metrics/src/features/registration/handler.ts
@@ -1,14 +1,22 @@
 import * as Hapi from 'hapi'
 import { writePoints } from 'src/influxdb/client'
 import { generateBirthRegPoint } from './pointGenerator'
+import { internal } from 'boom'
 
 export async function newBirthRegistrationHandler(
   request: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
   const points = []
-  points.push(generateBirthRegPoint(request.payload as fhir.Bundle, 'new-reg'))
-  writePoints(points)
+  try {
+    points.push(
+      generateBirthRegPoint(request.payload as fhir.Bundle, 'new-reg')
+    )
+    writePoints(points)
+  } catch (err) {
+    return internal(err)
+  }
+
   return h.response().code(200)
 }
 
@@ -17,9 +25,14 @@ export async function birthRegistrationHandler(
   h: Hapi.ResponseToolkit
 ) {
   const points = []
-  points.push(
-    generateBirthRegPoint(request.payload as fhir.Bundle, 'update-reg')
-  )
-  writePoints(points)
+  try {
+    points.push(
+      generateBirthRegPoint(request.payload as fhir.Bundle, 'update-reg')
+    )
+    writePoints(points)
+  } catch (err) {
+    return internal(err)
+  }
+
   return h.response().code(200)
 }
