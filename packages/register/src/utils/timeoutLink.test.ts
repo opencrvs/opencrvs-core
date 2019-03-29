3949f65628a66b0e2f9300f6d1cc6fab796899d4
Tahmid <tahmidrahman.dsi@gmail.com>
2019-03-29 15:45:03 +0600

    Add timeoutLink to apollo client
    - A link that waits for response for a specific time duration and aborts request otherwise
    - Add tests for timeoutLink

---
+++ packages/register/src/utils/timeoutLink.test.ts
@@ -0,0 +1,85 @@
+import TimeoutLink from './timeoutLink'
+import gql from 'graphql-tag'
+import {
+  ApolloLink,
+  Observable,
+  Operation,
+  execute,
+  GraphQLRequest
+} from 'apollo-link'
+
+const testOperation: GraphQLRequest = {
+  query: gql`
+    query {
+      hello
+    }
+  `,
+  operationName: 'testQuery',
+  variables: {},
+  context: {},
+  extensions: {}
+}
+
+const fastResponseLink = new ApolloLink((operation: Operation) => {
+  return new Observable(observer => {
+    new Promise(resolve => {
+      setTimeout(() => resolve({ response: true }), 100)
+    }).then(res => observer.next(res))
+  })
+})
+
+const slowResponseLink = new ApolloLink((operation: Operation) => {
+  return new Observable(observer => {
+    new Promise(resolve => {
+      setTimeout(() => resolve({ response: true }), 300)
+    }).then(res => observer.next(res))
+  })
+})
+
+const timeoutLink = new TimeoutLink(200)
+const fakeTimeoutLink = new TimeoutLink(-1)
+
+const composedPassingLink = timeoutLink.concat(fastResponseLink)
+const composedFailingLink = timeoutLink.concat(slowResponseLink)
+const composedTimelessLink = fakeTimeoutLink.concat(slowResponseLink)
+
+describe('tests for timeout link', () => {
+  it('request served within timeout', done => {
+    execute(composedPassingLink, testOperation).subscribe({
+      next: data => {
+        expect(data).toEqual({ response: true })
+        done()
+      },
+      error: e => {
+        expect(e).toBe(null)
+        done()
+      }
+    })
+  })
+
+  it('resquest aborted with error when exceeds timeout', done => {
+    execute(composedFailingLink, testOperation).subscribe({
+      next: data => {
+        expect(data).toBe(null)
+        done()
+      },
+      error: e => {
+        expect(e.message).toBe('Timeout exceeded for query "testQuery"')
+        done()
+      }
+    })
+  })
+
+  it('skips timeout link when timeout is explicitly set <0 even if slow response', done => {
+    execute(composedTimelessLink, testOperation).subscribe({
+      next: data => {
+        expect(data).toEqual({ response: true })
+        done()
+      },
+      error: e => {
+        expect(e).toBe(null)
+        done()
+      }
+    })
+  })
+})
---
+++ packages/register/src/utils/timeoutLink.ts
@@ -0,0 +1,105 @@
+import { ApolloLink, Observable, Operation, NextLink } from 'apollo-link'
+import { DefinitionNode, OperationDefinitionNode } from 'graphql'
+
+const DEFAULT_TIMEOUT: number = 30000
+const REQUEST_TIMEOUT_ERROR_CODE = 408
+
+/**
+ * Aborts the request if the timeout expires before the response is received.
+ */
+export type TimeoutError = Error & {
+  statusCode: number
+}
+
+export const throwTimeoutError = (message: string) => {
+  const error = new Error(message) as TimeoutError
+
+  error.name = 'RequestTimeoutError'
+  error.statusCode = REQUEST_TIMEOUT_ERROR_CODE
+
+  return error
+}
+
+export default class TimeoutLink extends ApolloLink {
+  private timeout: number
+
+  constructor(timeout: number = DEFAULT_TIMEOUT) {
+    super()
+    this.timeout = timeout
+  }
+
+  public request(operation: Operation, forward: NextLink) {
+    let controller: AbortController
+    let ctxTimeout: number | null
+
+    // override timeout from operation context
+    ctxTimeout = operation.getContext().timeout || null
+    if ((ctxTimeout as number) <= 0) {
+      ctxTimeout = null
+    }
+
+    // add abort controller and signal object to fetchOptions if they don't already exist
+    if (typeof AbortController !== 'undefined') {
+      const context = operation.getContext()
+      let fetchOptions = context.fetchOptions || {}
+
+      controller = fetchOptions.controller || new AbortController()
+
+      fetchOptions = { ...fetchOptions, controller, signal: controller.signal }
+      operation.setContext({ fetchOptions })
+    }
+
+    const chainObservable = forward(operation) // observable for remaining link chain
+
+    const operationType: string = ((operation.query
+      .definitions as ReadonlyArray<DefinitionNode>).find(
+      (def: DefinitionNode) => def.kind === 'OperationDefinition'
+    ) as OperationDefinitionNode).operation
+
+    if (this.timeout <= 0 || operationType === 'subscription') {
+      return chainObservable // skip this link if timeout is zero or it's a subscription request
+    }
+
+    // create local observable with timeout functionality (unsubscibe from chain observable and
+    // return an error if the timeout expires before chain observable resolves)
+    const localObservable = new Observable(observer => {
+      let timer: NodeJS.Timer
+
+      // listen to chainObservable for result and pass to localObservable if received before timeout
+      const subscription = chainObservable.subscribe(
+        result => {
+          clearTimeout(timer)
+          observer.next(result)
+          observer.complete()
+        },
+        error => {
+          clearTimeout(timer)
+          observer.error(error) // pass error to the errorLink
+          observer.complete()
+        }
+      )
+
+      // if timeout expires before observable completes, abort call, unsubscribe, and return error
+      timer = setTimeout(() => {
+        if (controller) {
+          controller.abort() // abort fetch operation
+        }
+
+        observer.error(
+          throwTimeoutError(
+            `Timeout exceeded for ${operationType} "${operation.operationName}"`
+          )
+        )
+        subscription.unsubscribe()
+      }, ctxTimeout || this.timeout)
+
+      // this function is called when a client unsubscribes from localObservable
+      return () => {
+        clearTimeout(timer)
+        subscription.unsubscribe()
+      }
+    })
+
+    return localObservable
+  }
+}
