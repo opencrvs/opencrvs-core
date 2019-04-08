c322b841381bddadde95aa917464381ecea9ab40
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-04-08 12:17:03 +0600

    OCRVS-1442: Added Logout icon

---
+++ packages/components/src/components/icons/Logout.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+export const Logout = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg width={24} height={24} fill="none" {...props}>
+    <path
+      d="M14 22h5a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-5"
+      stroke="#fff"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M11 16l4-4-4-4M15 12H3"
+      stroke="#fff"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
