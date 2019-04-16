496041f21fe97f791e563b007de45fef73f1c7f0
anik <mushraful.hoque@dsinnovators.com>
2019-04-16 16:46:53 +0600

    fixing device detection issue

--- packages/register/src/utils/commonUtils.ts
+++ packages/register/src/utils/commonUtils.ts
@@ -1,5 +1,22 @@
-import { isMobile } from 'react-device-detect'
-
 export function isMobileDevice() {
-  return isMobile
+  const IS_MOBILE = true
+  const IS_DESKTOP = false
+
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return IS_MOBILE
+  }
+
+  if (window.outerWidth < 1033) {
+    return IS_MOBILE
+  }
+
+  return IS_DESKTOP
 }
