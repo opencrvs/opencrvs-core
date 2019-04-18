50afef3c820bb144f4ed425d6e19ab7885a039e3
Ryan Crichton <ryan@jembi.org>
2019-04-18 10:46:32 +0200

    Change infrastructure to add replicaset when deploying
    OCRVS-306

--- infrastructure/restore-metadata.sh
+++ infrastructure/restore-metadata.sh
@@ -1,11 +1,18 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 echo "Working dir: $DIR"
 
-docker run --rm -v $DIR/backups:/backups --network=opencrvs_overlay_net mongo:3.6 bash \
- -c 'mongorestore --host mongo --drop --gzip --archive=/backups/hearth-dev.gz'
+if [ "$DEV" = "true" ]; then
+  HOST=mongo1
+  echo "Working in DEV mode"
+else
+  HOST=rs0/mongo1,mongo2,mongo3
+fi
 
 docker run --rm -v $DIR/backups:/backups --network=opencrvs_overlay_net mongo:3.6 bash \
- -c 'mongorestore --host mongo --drop --gzip --archive=/backups/openhim-dev.gz'
+ -c "mongorestore --host $HOST --drop --gzip --archive=/backups/hearth-dev.gz"
 
 docker run --rm -v $DIR/backups:/backups --network=opencrvs_overlay_net mongo:3.6 bash \
- -c 'mongorestore --host mongo --drop --gzip --archive=/backups/user-mgnt.gz'
+ -c "mongorestore --host $HOST --drop --gzip --archive=/backups/openhim-dev.gz"
+
+docker run --rm -v $DIR/backups:/backups --network=opencrvs_overlay_net mongo:3.6 bash \
+ -c "mongorestore --host $HOST --drop --gzip --archive=/backups/user-mgnt.gz"
