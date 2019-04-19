26004ee2b44b7944bc125a96c0ff2d6b6fac72f3
Golam Rabbani <knight3rrantt@gmail.com>
2019-04-19 12:28:29 +0600

    Add ZoomIn,ZoomOut,RotateLeft Icons

---
+++ packages/components/src/components/icons/RotateLeft.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+export const RotateLeft = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg width={24} height={24} fill="none">
+    <path
+      d="M14 15l-5 5-5-5"
+      stroke="#4C68C1"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M20 4h-7a4 4 0 0 0-4 4v12"
+      stroke="#4C68C1"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default RotateLeft
---
+++ packages/components/src/components/icons/ZoomIn.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export const ZoomIn = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg width={24} height={24} fill="none">
+    <path
+      d="M4 12h16M12 4v16"
+      stroke="#4C68C1"
+      strokeWidth={2}
+      strokeLinecap="round"
+    />
+  </svg>
+)
+
+export default ZoomIn
