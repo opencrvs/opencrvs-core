11a239764ccaa8d0b710110444ede684c073d253
Golam Rabbani <knight3rrantt@gmail.com>
2019-04-29 14:33:44 +0600

    Fix svg compatible with react

--- packages/components/src/components/icons/EyeOn.tsx
+++ packages/components/src/components/icons/EyeOn.tsx
@@ -1,31 +1,22 @@
 import * as React from 'react'
 
 export const EyeOn = (props: React.HTMLAttributes<SVGElement>) => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
+  <svg width={24} height={24} fill="none" {...props}>
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M4 12C4 12 7 6 12.25 6C17.5 6 20.5 12 20.5 12C20.5 12 17.5 18 12.25 18C7 18 4 12 4 12Z"
+      clipRule="evenodd"
+      d="M4 12s3-6 8.25-6 8.25 6 8.25 6-3 6-8.25 6S4 12 4 12z"
       stroke="#4C68C1"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M12.25 14.25C13.4926 14.25 14.5 13.2426 14.5 12C14.5 10.7574 13.4926 9.75 12.25 9.75C11.0074 9.75 10 10.7574 10 12C10 13.2426 11.0074 14.25 12.25 14.25Z"
+      clipRule="evenodd"
+      d="M12.25 14.25a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5z"
       stroke="#4C68C1"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 )
