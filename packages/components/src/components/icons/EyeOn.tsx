04dfae56caeaf75c12063d86ea59e4ccb6a89058
Golam Rabbani <knight3rrantt@gmail.com>
2019-04-29 14:31:07 +0600

    Update Reveal/hide password eye icon size to 24x

--- packages/components/src/components/icons/EyeOn.tsx
+++ packages/components/src/components/icons/EyeOn.tsx
@@ -2,30 +2,30 @@ import * as React from 'react'
 
 export const EyeOn = (props: React.HTMLAttributes<SVGElement>) => (
   <svg
-    width="20"
-    height="18"
-    viewBox="0 0 20 18"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M1.75 9C1.75 9 4.75 3 10 3C15.25 3 18.25 9 18.25 9C18.25 9 15.25 15 10 15C4.75 15 1.75 9 1.75 9Z"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M4 12C4 12 7 6 12.25 6C17.5 6 20.5 12 20.5 12C20.5 12 17.5 18 12.25 18C7 18 4 12 4 12Z"
       stroke="#4C68C1"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M10 11.25C11.2426 11.25 12.25 10.2426 12.25 9C12.25 7.75736 11.2426 6.75 10 6.75C8.75736 6.75 7.75 7.75736 7.75 9C7.75 10.2426 8.75736 11.25 10 11.25Z"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12.25 14.25C13.4926 14.25 14.5 13.2426 14.5 12C14.5 10.7574 13.4926 9.75 12.25 9.75C11.0074 9.75 10 10.7574 10 12C10 13.2426 11.0074 14.25 12.25 14.25Z"
       stroke="#4C68C1"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 )
