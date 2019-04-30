f7c7eaed462266213aa73e24f5a9da385a900d25
Ryan Crichton <ryan@jembi.org>
2019-04-30 11:48:42 +0200

    Add invalidate token and verify token functionality
    OCRVS-1403

--- packages/auth/test/setupJest.ts
+++ packages/auth/test/setupJest.ts
@@ -1,6 +1,5 @@
 import { join } from 'path'
 import * as fetch from 'jest-fetch-mock'
-import * as redis from 'redis-mock'
 
 import { IDatabaseConnector } from '../src/database'
 
@@ -9,19 +8,24 @@ jest.setMock('node-fetch', { default: fetch })
 const database: { [key: string]: string } = {}
 
 const mock: IDatabaseConnector = {
-  set: async (key, value) => {
+  set: jest.fn().mockImplementation(async (key, value) => {
     database[key] = value
-  },
-  get: async key => database[key] || null,
-  del: async key => {
+  }),
+  setex: jest.fn().mockImplementation(async (key, ttl, value) => {
+    database[key] = value
+  }),
+  get: jest.fn().mockImplementation(async key => {
+    return database[key] || null
+  }),
+  del: jest.fn().mockImplementation(async key => {
     const keyExists = !!database[key]
     delete database[key]
     return keyExists ? 1 : 0
-  } ,
+  }),
   // tslint:disable-next-line no-empty
-  start: () => {},
+  start: jest.fn(),
   // tslint:disable-next-line no-empty
-  stop: () => {}
+  stop: jest.fn()
 }
 
 jest.setMock('src/database', mock)
