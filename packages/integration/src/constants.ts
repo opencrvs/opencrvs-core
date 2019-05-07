de0cf7745370b48ea3e6ca3fd05682e33b3572d1
Ryan Crichton <ryan@jembi.org>
2019-05-07 16:33:22 +0200

    Allow script to be configurable via env vars

--- packages/integration/src/constants.ts
+++ packages/integration/src/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL = 'http://172.17.0.1:5001/fhir'
-export const AUTH_URL = 'http://172.17.0.1:4040'
+// @ts-ignore
+export const API_URL = __ENV.API_URL || 'http://172.17.0.1:5001/fhir'
+// @ts-ignore
+export const AUTH_URL = __ENV.AUTH_URL || 'http://172.17.0.1:4040'
