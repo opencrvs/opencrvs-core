c9dc052056de5bfec99f3e5db248c3d10224b7c9
Euan Millar <euanmillar@users.noreply.github.com>
2019-05-15 10:21:28 +0100

    Revert "Revert "Invalidate token check""

--- packages/user-mgnt/src/constants.ts
+++ packages/user-mgnt/src/constants.ts
@@ -1,8 +1,13 @@
-export const AUTH_HOST = process.env.AUTH_HOST || 'localhost'
-export const AUTH_PORT = process.env.AUTH_PORT || 3030
+export const HOST = process.env.HOST || 'localhost'
+export const PORT = process.env.PORT || 3030
 export const MONGO_URL =
   process.env.MONGO_URL || 'mongodb://localhost/user-mgnt'
 
 export const CERT_PUBLIC_KEY_PATH =
   (process.env.CERT_PUBLIC_KEY_PATH as string) ||
   '../../.secrets/public-key.pem'
+
+export const AUTH_URL = process.env.AUTH_URL || 'http://localhost:4040'
+// Check if the token has been invalided in the auth service before it has expired
+// This needs to be a string to make it easy to pass as an ENV var.
+export const CHECK_INVALID_TOKEN = process.env.CHECK_INVALID_TOKEN || 'false'
