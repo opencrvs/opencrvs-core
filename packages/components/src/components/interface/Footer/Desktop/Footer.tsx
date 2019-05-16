4b2a60fcc9a2bb20d928ac935f83c1ad749c2be4
Golam Rabbani <knight3rrantt@gmail.com>
2019-05-16 15:11:43 +0600

    Add Footer Component

---
+++ packages/components/src/components/interface/Footer/Desktop/Footer.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface IProps {
+  left?: React.ReactNode
+  right?: React.ReactNode
+}
+
+const FooterContainer = styled.section`
+  @media (max-width: ${({ theme }) => theme.grid.breakpoints.lg}px) {
+    display: none;
+  }
+  padding: 8px 16px;
+  display: flex;
+  flex-grow: 1;
+  max-height: 60px;
+  & > p {
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.white};
+    font-family: ${({ theme }) => theme.fonts.regularFont};
+  }
+  flex-direction: row;
+  & a {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  background: linear-gradient(
+    180deg,
+    ${({ theme }) => theme.colors.headerGradientDark} 0%,
+    ${({ theme }) => theme.colors.headerGradientLight} 100%
+  );
+`
+
+export class Footer extends React.Component<IProps> {
+  render() {
+    return (
+      <FooterContainer>
+        {!this.props.left && <p>© OpenCRVS {new Date().getFullYear()}</p>}
+        {this.props.left}
+        {this.props.right}
+      </FooterContainer>
+    )
+  }
+}
