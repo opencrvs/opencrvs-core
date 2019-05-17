b28d1a660d8985475d38f337a18d4688beb2ef27
Atiq Zaman <atiq.zaman@dsinnovators.com>
2019-05-17 12:42:11 +0600

    New component for data view

---
+++ packages/components/src/components/interface/ViewData/DataSection.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { IDataProps, DataRow } from './DataRow'
+
+const Container = styled.div`
+  margin-top: 52px;
+`
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 36px;
+  margin: 0 0 16px 8px;
+`
+interface IProps {
+  title: string
+  items: IDataProps[]
+}
+
+export class DataSection extends React.Component<IProps> {
+  render() {
+    const { title, items } = this.props
+
+    return (
+      <Container>
+        <Title>{title}</Title>
+        {items.map((item: IDataProps) => (
+          <DataRow {...item} />
+        ))}
+      </Container>
+    )
+  }
+}
