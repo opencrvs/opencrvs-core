fd3110258d59df2502d7949d1df464ff6175d2da
Atiq Zaman <atiq.zaman@dsinnovators.com>
2019-05-17 10:55:01 +0600

    Added button that looks like a link

--- packages/components/src/components/buttons/LinkButton.tsx
+++ packages/components/src/components/buttons/LinkButton.tsx
@@ -1,23 +1,27 @@
 import styled from 'styled-components'
+import { Button } from './Button'
 
-export const LinkButton = styled.a`
-  width: auto;
-  margin: 0 8px;
-  text-decoration: underline;
+export const LinkButton = styled(Button)`
   font-family: ${({ theme }) => theme.fonts.regularFont};
-  align-items: center;
-  display: inline-flex;
-  border: 0;
-  cursor: pointer;
-  color: ${({ theme }) => theme.colors.primary};
-  &:disabled {
-    background: ${({ theme }) => theme.colors.disabledButton};
+  padding: 0;
+  height: auto;
+  color: ${({ theme }) => theme.colors.accent};
+  text-decoration-line: underline;
+  & div {
+    padding: 0;
   }
+
+  &:active {
+    outline: 0;
+    background-color: ${({ theme }) => theme.colors.creamCan};
+  }
+
   &:hover {
     opacity: 0.8;
   }
 
-  &:active {
-    background: ${({ theme }) => theme.colors.creamCan};
+  &:disabled {
+    color: ${({ theme }) => theme.colors.disabled};
+    background-color: transparent;
   }
 `
