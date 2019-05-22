0705349f34b9df5536a5cfefdc2024a000586b0d
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-05-22 11:30:08 +0600

    ocrvs-1530: Adding Submission controller to Main app

--- packages/register/src/App.tsx
+++ packages/register/src/App.tsx
@@ -31,6 +31,7 @@ import { StyledErrorBoundary } from './components/StyledErrorBoundary'
 import { RegistrarHome } from './views/RegistrarHome/RegistrarHome'
 import { FieldAgentHome } from './views/FieldAgentHome/FieldAgentHome'
 import { Footer } from '@opencrvs/components/lib/interface/'
+import { SubmissionController } from './SubmissionController'
 interface IAppProps {
   client?: ApolloClient<{}>
   store: AppStore
@@ -60,6 +61,7 @@ export class App extends React.Component<IAppProps> {
                       <SessionExpireConfirmation />
                       <NotificationComponent>
                         <Page>
+                          <SubmissionController />
                           <MainSection>
                             <ProtectedPage>
                               <Switch>
