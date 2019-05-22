2c594e66d0e0884b4501ee5541960bfbd21bb35d
Atiq Zaman <atiq.zaman@dsinnovators.com>
2019-05-22 15:52:20 +0600

    Extra error due to extra have an extra comma

--- packages/register/src/i18n/locales/en.ts
+++ packages/register/src/i18n/locales/en.ts
@@ -989,7 +989,7 @@ export const ENGLISH_STATE: ILanguage = {
     'register.registrarHome.inProgress': 'In progress',
     'register.registrarHome.readyForReview': 'Ready for review',
     'register.registrarHome.sentForUpdates': 'Sent for updates',
-    'register.registrarHome.action': 'Action',,
+    'register.registrarHome.action': 'Action',
     'settings.title': 'Settings',
     'profile.title': 'Profile',
     'security.title': 'Security',
@@ -1004,6 +1004,6 @@ export const ENGLISH_STATE: ILanguage = {
     'label.language': 'Language',
     'action.change': 'Change',
     'createPIN.pinSameDigitsError': 'PIN cannot have same 4 digits',
-    'createPIN.pinSeqDigitsError': 'PIN cannot contain sequential digits',
+    'createPIN.pinSeqDigitsError': 'PIN cannot contain sequential digits'
   }
 }
