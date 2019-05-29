bd50a3c9cbc80100bd8394822ca2017c75f53e55
anik <mushraful.hoque@dsinnovators.com>
2019-05-29 22:46:02 +0600

    Updated user populate script with practitioner,primaryofficeid and catchmentareaids

--- packages/user-mgnt/src/model/user.ts
+++ packages/user-mgnt/src/model/user.ts
@@ -1,13 +1,20 @@
 import { model, Schema, Document } from 'mongoose'
 
+interface IUserName {
+  use: string
+  family: string[]
+  given: string[]
+}
 interface IUser {
-  name: string
-  email?: string
+  name: IUserName[]
+  email: string
   mobile: string
   passwordHash: string
   salt: string
   role: string
-  practitionerId?: string
+  practitionerId: string
+  primaryOfficeId: string
+  catchmentAreaIds: string[]
   scope: string[]
   active: boolean
   creationDate: number
@@ -15,13 +22,15 @@ interface IUser {
 export interface IUserModel extends IUser, Document {}
 
 const userSchema = new Schema({
-  name: String,
+  name: [Schema.Types.Mixed],
   email: String,
   mobile: String,
   passwordHash: { type: String, required: true },
   salt: { type: String, required: true },
   role: String,
   practitionerId: String,
+  primaryOfficeId: String,
+  catchmentAreaIds: [String],
   scope: { type: [String], required: true },
   active: { type: Boolean, default: true },
   creationDate: { type: Number, default: Date.now }
