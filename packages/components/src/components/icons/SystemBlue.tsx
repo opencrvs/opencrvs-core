0fd209cd868397eacc4ad679218d1dc1b9aacaf3
Sahriar Toufiq <sahriartoufiq@gmail.com>
2019-05-31 15:49:31 +0600

    SystemBlack & SystemBlue icons created

---
+++ packages/components/src/components/icons/SystemBlue.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export const SystemBlue = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg width={24} height={24} fill="none" {...props}>
+    <g
+      style={{
+        mixBlendMode: 'darken'
+      }}
+      stroke="#5E93ED"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M4 21v-7M4 10V3M12 21v-9M12 8V3M20 21v-5M20 12V3M1 14h6M9 8h6M17 16h6" />
+    </g>
+  </svg>
+)
