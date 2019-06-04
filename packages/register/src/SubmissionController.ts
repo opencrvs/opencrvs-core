c26ebfcf59b5df9af6e245e9215951c36bd03bdb
Ryan Crichton <ryan@jembi.org>
2019-06-04 14:07:43 +0200

    Update sync to allow all pending applications to be sync'd per sync call
    Syncing one application per sync call seems inefficient. Also this
    change simplifies some of the code preparing metadata that is used for
    syncing an application.
    
    OCRVS-1530

--- packages/register/src/SubmissionController.ts
+++ packages/register/src/SubmissionController.ts
@@ -1,5 +1,4 @@
 import ApolloClient from 'apollo-client'
-import { indexOf } from 'lodash'
 import {
   IApplication,
   modifyApplication,
@@ -21,50 +20,55 @@ const ALLOWED_STATUS_FOR_RETRY = [
 export class SubmissionController {
   private store: AppStore
   private client: ApolloClient<{}>
-  private applications: IApplication[] = []
-  private registerForms: Array<{ [key: string]: IForm }> = []
+  private registerForms: { [key: string]: IForm }
   private syncRunning: boolean = false
 
   constructor(store: AppStore) {
     this.store = store
     this.client = createClient(store)
-    this.subscribeToStore()
+    this.registerForms = getRegisterForm(this.store.getState())
   }
 
   public start = () => {
-    let application
-
     setInterval(() => {
-      application = this.getSubmitableApplication()
-      this.callMutation(application)
+      this.sync()
     }, INTERVAL_TIME)
   }
 
-  private subscribeToStore = () => {
-    let application
-    this.store.subscribe(() => {
-      this.applications =
-        this.store.getState().applicationsState.applications || []
-      this.applications.map(app => {
-        this.registerForms[app.event] = getRegisterForm(this.store.getState())[
-          app.event
-        ]
-      })
-      application = this.getSubmitableApplication()
-      this.callMutation(application)
-    })
-  }
+  private getApplications = () =>
+    this.store.getState().applicationsState.applications || []
 
-  private getSubmitableApplication = () => {
-    return this.applications.find(
-      app => indexOf(ALLOWED_STATUS_FOR_RETRY, app.submissionStatus) > -1
+  private getSubmitableApplications = () => {
+    return this.getApplications().filter(
+      app =>
+        app.submissionStatus &&
+        ALLOWED_STATUS_FOR_RETRY.includes(app.submissionStatus)
     )
   }
 
-  private callMutation = (application: IApplication | undefined) => {
-    if (!navigator.onLine || this.syncRunning || !application) {
+  private sync = async () => {
+    console.debug('Starting sync...')
+    if (!navigator.onLine || this.syncRunning) {
+      console.debug('Sync exiting early (offline or already syncing)')
       return
     }
+
+    this.syncRunning = true
+
+    const applications = this.getSubmitableApplications()
+    for (const application of applications) {
+      await this.callMutation(application)
+    }
+
+    this.syncRunning = false
+    console.debug('Finish sync.')
+  }
+
+  private callMutation = async (application: IApplication | undefined) => {
+    if (!application) {
+      return
+    }
+
     const result = getMutationMapping(
       application.event,
       Action.SUBMIT_FOR_REVIEW,
@@ -77,31 +81,28 @@ export class SubmissionController {
       variables: null
     }
 
-    this.syncRunning = true
-    application.submissionStatus =
-      SUBMISSION_STATUS[SUBMISSION_STATUS.SUBMITTING]
+    application.submissionStatus = SUBMISSION_STATUS.SUBMITTING
     modifyApplication(application)
-    this.client
-      .mutate({ mutation, variables })
-      .then(() => this.onSuccess(application))
-      .catch(exception =>
-        this.onError(application, exception.networkError.statusCode)
-      )
+
+    try {
+      await this.client.mutate({ mutation, variables })
+      this.onSuccess(application)
+    } catch (exception) {
+      this.onError(application, exception.networkError.statusCode)
+    }
   }
 
   private onSuccess = (application: IApplication) => {
-    this.syncRunning = false
     application.submissionStatus =
       SUBMISSION_STATUS[SUBMISSION_STATUS.SUBMITTED]
     this.store.dispatch(modifyApplication(application))
   }
 
+  // TODO: fix error detection when network is out
   private onError = (application: IApplication, httpCode: number) => {
-    const status =
-      ALLOWED_STATUS_CODE.indexOf(httpCode) > -1
-        ? SUBMISSION_STATUS.FAILED_NETWORK
-        : SUBMISSION_STATUS.FAILED
-    this.syncRunning = false
+    const status = ALLOWED_STATUS_CODE.includes(httpCode)
+      ? SUBMISSION_STATUS.FAILED_NETWORK
+      : SUBMISSION_STATUS.FAILED
     application.submissionStatus = status
     this.store.dispatch(modifyApplication(application))
   }
