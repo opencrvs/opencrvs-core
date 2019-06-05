2b5e89753e7c52316cf2b4fd4bb889e50dc8ffde
Euan Millar <euanmillar77@gmail.com>
2019-06-05 08:35:26 +0100

    Resolve missing plugins to enable eslint to work Prior to this change, Import plugin was not successfully added and I need to test relative import warning This change Correctly adds the plugin and yarn lint:ts now functions

--- packages/login/.eslintrc.js
+++ packages/login/.eslintrc.js
@@ -4,9 +4,11 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
-    'prettier/react'
+    'prettier/react',
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
   env: {
     es6: true,
     browser: true,
@@ -20,14 +22,15 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    'jsx-a11y/label-has-for': 'off',
-    'jsx-a11y/label-has-associated-control': 'off',
+    'import/no-relative-parent-imports': 2,
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
     'react/no-unescaped-entities': 'off',
     'react/destructuring-assignment': 'off',
     'react/jsx-filename-extension': [
@@ -37,8 +40,6 @@ module.exports = {
       }
     ],
     'react/boolean-prop-naming': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     'react/sort-comp': 'off',
     'react/sort-prop-types': 'off'
   },
@@ -51,6 +52,6 @@ module.exports = {
   globals: {},
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: '<rootDir>/tsconfig.json'
+    project: 'tsconfig.json'
   }
 }
--- packages/performance/.eslintrc.js
+++ packages/performance/.eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
     'prettier/@typescript-eslint',
     'prettier/react',
     'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript'
+    'plugin:import/warnings'
   ],
   plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
   env: {
@@ -23,13 +22,15 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
+    'import/no-relative-parent-imports': 2,
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'import/no-relative-parent-imports': 1,
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
     'react/no-unescaped-entities': 'off',
     'react/destructuring-assignment': 'off',
     'react/jsx-filename-extension': [
@@ -39,8 +40,6 @@ module.exports = {
       }
     ],
     'react/boolean-prop-naming': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     'react/sort-comp': 'off',
     'react/sort-prop-types': 'off'
   },
@@ -53,6 +52,6 @@ module.exports = {
   globals: {},
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: '<rootDir>/tsconfig.json'
+    project: 'tsconfig.json'
   }
 }
