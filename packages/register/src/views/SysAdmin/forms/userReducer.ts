e57bbdd6bfa43aae74676e14e3447dfbf3b6053a
Tahmid <tahmidrahman.dsi@gmail.com>
2019-06-18 15:59:09 +0600

    Add modify action creator to userReducer

--- packages/register/src/views/SysAdmin/forms/userReducer.ts
+++ packages/register/src/views/SysAdmin/forms/userReducer.ts
@@ -1,14 +1,36 @@
 import { LoopReducer, Loop } from 'redux-loop'
 import { userSection } from './fieldDefinitions/user-section'
-import { Action } from 'redux'
-import { IFormSection } from 'src/forms'
+import { IFormSection, IFormSectionData } from 'src/forms'
+
+const MODIFY_USER_FORM_DATA = 'MODIFY_USER_FORM_DATA'
 
 const initialState: IUserFormState = {
-  userForm: userSection
+  userForm: userSection,
+  userFormData: {}
 }
 
+interface IUserFormDataModifyAction {
+  type: typeof MODIFY_USER_FORM_DATA
+  payload: {
+    data: IFormSectionData
+  }
+}
+
+export function modifyUserFormData(
+  data: IFormSectionData
+): IUserFormDataModifyAction {
+  return {
+    type: MODIFY_USER_FORM_DATA,
+    payload: {
+      data
+    }
+  }
+}
+
+type Action = IUserFormDataModifyAction
 export interface IUserFormState {
   userForm: IFormSection
+  userFormData: IFormSectionData
 }
 
 export const userFormReducer: LoopReducer<IUserFormState, Action> = (
@@ -16,6 +38,8 @@ export const userFormReducer: LoopReducer<IUserFormState, Action> = (
   action: Action
 ): IUserFormState | Loop<IUserFormState, Action> => {
   switch (action.type) {
+    case MODIFY_USER_FORM_DATA:
+      return { ...state, userFormData: action.payload.data }
     default:
       return state
   }
