c267ae9b699f63f65b0b83d476fccd947cda61b7
Sahriar Toufiq <sahriartoufiq@gmail.com>
2019-06-24 14:25:45 +0600

    Asteric functionality moved down to InputLabel component

--- packages/components/src/components/forms/InputField/InputLabel.tsx
+++ packages/components/src/components/forms/InputField/InputLabel.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import styled, { StyledFunction } from 'styled-components'
+import styled from 'styled-components'
 
 export type IInputLabel = {
   disabled?: boolean
   ignoreMediaQuery?: boolean
   color?: string
+  required?: boolean
+  hideAsterisk?: boolean
 } & React.LabelHTMLAttributes<HTMLLabelElement>
 
 const styledInputLabel = styled.label.attrs<IInputLabel>({})
@@ -26,8 +28,25 @@ const StyledInputLabel = styledInputLabel`
   }}
 `
 
+const Required = styled.span.attrs<
+  { disabled?: boolean } & React.LabelHTMLAttributes<HTMLLabelElement>
+>({})`
+  ${({ theme }) => theme.fonts.bigBodyStyle};
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.colors.disabled : theme.colors.error};
+  flex-grow: 0;
+`
+
 export class InputLabel extends React.Component<IInputLabel> {
   render() {
-    return <StyledInputLabel {...this.props} />
+    const { required, hideAsterisk, children } = this.props
+    return (
+      <StyledInputLabel {...this.props}>
+        {children}
+        {required && !hideAsterisk && (
+          <Required disabled={this.props.disabled}>&nbsp;*</Required>
+        )}
+      </StyledInputLabel>
+    )
   }
 }
