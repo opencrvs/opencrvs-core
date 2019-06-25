c0c7207ab1ad25cd67d182411379e90051007ed3
Sadman Ilham <sadman.ilham@gmail.com>
2019-06-25 18:26:13 +0600

    Update Service Test
    - Update username generation test
    - Add user credentials notification test

--- packages/user-mgnt/src/features/createUser/service.test.ts
+++ packages/user-mgnt/src/features/createUser/service.test.ts
@@ -1,30 +1,94 @@
-import { generateUsername } from './service'
+import { generateUsername, sendCredentialsNotification } from './service'
+import UsernameRecord from '@user-mgnt/model/usernameRecord'
+import mockingoose from 'mockingoose'
+import * as fetchMock from 'jest-fetch-mock'
+import * as jwt from 'jsonwebtoken'
+import { readFileSync } from 'fs'
+import { logger } from '@user-mgnt/logger'
 
-describe('.generateUsername()', () => {
-  it('generates valid username with given and family names', () => {
+const fetch = fetchMock as fetchMock.FetchMock
+const mockUsernameRecord = {
+  count: 1,
+  username: 'je.doe'
+}
+
+const token = jwt.sign(
+  { scope: ['system'] },
+  readFileSync('../auth/test/cert.key'),
+  {
+    algorithm: 'RS256',
+    issuer: 'opencrvs:auth-service',
+    audience: 'opencrvs:user-mgnt-user'
+  }
+)
+
+describe('Username generation', () => {
+  it('generates valid username with given and family names', async () => {
+    mockingoose(UsernameRecord).toReturn(null, 'findOne')
+    mockingoose(UsernameRecord).toReturn(null, 'save')
     const names = [{ given: ['John', 'Evan'], family: 'Doe', use: 'en' }]
-    const username = generateUsername(names)
+    const username = await generateUsername(names)
     expect(username).toBe('je.doe')
+    mockingoose.resetAll()
   })
 
-  it('generates valid username with just family name', () => {
+  it('generates valid username with given and family names with appended number', async () => {
+    mockingoose(UsernameRecord).toReturn(mockUsernameRecord, 'findOne')
+    mockingoose(UsernameRecord).toReturn(null, 'update')
+    const names = [{ given: ['John', 'Evan'], family: 'Doe', use: 'en' }]
+    const username = await generateUsername(names)
+    expect(username).toBe('je.doe1')
+    mockingoose.resetAll()
+  })
+
+  it('generates valid username with just family name', async () => {
+    mockingoose(UsernameRecord).toReturn(null, 'findOne')
     const names = [{ given: [], family: 'Doe', use: 'en' }]
-    const username = generateUsername(names)
+    const username = await generateUsername(names)
     expect(username).toBe('doe')
   })
 
-  it('generates valid username with no spaces', () => {
+  it('generates valid username with no spaces', async () => {
     const names = [
       { given: ['John Mark', 'Evan'], family: 'van der Linde', use: 'en' }
     ]
-    const username = generateUsername(names)
+    const username = await generateUsername(names)
     expect(username).toBe('je.van-der-linde')
   })
 
-  it('throws an error if username is < 3 chars long', () => {
+  it('append 0s to make the length of username 3, if username has less than 3 characters', async () => {
     const names = [{ given: [], family: 'Wu', use: 'en' }]
-    expect(() => generateUsername(names)).toThrowError(
-      'username cannot be less than 3 characters, please provide more name details'
+    const username = await generateUsername(names)
+    expect(username).toBe('wu0')
+  })
+})
+
+describe('User credentials notification', () => {
+  it('returns ok when successfully called', async () => {
+    const spy = jest.spyOn(logger, 'error')
+    fetch.mockResponse(
+      JSON.stringify({
+        status: 200
+      })
     )
+
+    await sendCredentialsNotification('01730037449', 'Name1', 'Name2', {
+      Authorization: `Bearer ${token}`
+    })
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('thows error when operation is not successful', async () => {
+    const spy = jest.spyOn(logger, 'error')
+    fetch.mockImplementation(() => {
+      throw new Error('error')
+    })
+
+    await sendCredentialsNotification('01730037449', 'Name1', 'Name2', {
+      Authorization: `Bearer ${token}`
+    })
+
+    expect(spy).toHaveBeenCalled()
   })
 })
