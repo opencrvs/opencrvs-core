08e206c9ebd26c53de106dfcd380ed1e9b6f073a
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-07-01 17:33:30 +0600

    ocrvs-1696-1697: Correcting the color of Cross icon

--- packages/components/src/components/icons/Cross.tsx
+++ packages/components/src/components/icons/Cross.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react'
 
 export const Cross = (props: React.HTMLAttributes<SVGElement>) => (
-  <svg width={16} height={16} {...props}>
-    <title>D6E6AA6C-249D-4C8E-85BB-1BD3B5A7365B</title>
-    <g
-      transform="rotate(45 11.164 6.482)"
-      fill={props.color || '#5E93ED'}
-      fillRule="nonzero"
-    >
-      <rect y={9} width={20} height={2.5} rx={1.25} />
-      <rect
-        transform="rotate(90 10 10.25)"
-        y={9}
-        width={20}
-        height={2.5}
-        rx={1.25}
-      />
-    </g>
+  <svg width={24} height={24} fill="none" {...props}>
+    <path
+      d="M17.793 5.207a1 1 0 1 1 1.414 1.414L6.48 19.35a1 1 0 1 1-1.414-1.414L17.793 5.207z"
+      fill="#4C68C1"
+    />
+    <path
+      d="M4.793 5.207a1 1 0 0 1 1.414 0l12.728 12.728a1 1 0 1 1-1.414 1.414L4.793 6.621a1 1 0 0 1 0-1.414z"
+      fill="#4C68C1"
+    />
   </svg>
 )
