bce9e853cf52b0ec44d3a1418c15695a80a204eb
Ahad Chowdhury <maacpiash@outlook.com>
2019-07-05 18:18:57 +0600

    Add phone number to UserSettings and fix tests

--- packages/register/src/views/UserSetup/SetupReviewPage.tsx
+++ packages/register/src/views/UserSetup/SetupReviewPage.tsx
@@ -158,15 +158,10 @@ class UserSetupReviewComponent extends React.Component<IFullProps, IState> {
 
     const typeRole =
       (userDetails &&
-        (userDetails.type
-          ? `${intl.formatMessage(
-              roleMessages[userDetails.role as string]
-            )} / ${intl.formatMessage(
-              typeMessages[userDetails.type as string]
-            )}`
-          : `${intl.formatMessage(
-              roleMessages[userDetails.role as string]
-            )}`)) ||
+        ((userDetails.type &&
+          `${intl.formatMessage(typeMessages[userDetails.type])}`) ||
+          (userDetails.role &&
+            `${intl.formatMessage(roleMessages[userDetails.role])}`))) ||
       ''
 
     const primaryOffice =
