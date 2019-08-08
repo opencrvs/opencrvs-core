57550addd7cbd04eda5bd0647c1b9747f2d032be
Sahriar Toufiq <sahriartoufiq@gmail.com>
2019-08-08 19:44:15 +0600

    Merge conflicts resolved

--- packages/register/src/i18n/locales/bn.ts
+++ packages/register/src/i18n/locales/bn.ts
@@ -1097,10 +1097,6 @@ export const BENGALI_STATE: ILanguage = {
     'form.field.label.userSignatureAttachmentTitle':
       'নিবন্ধকের স্বাক্ষর সংযুক্ত করুন',
     'form.field.label.userSignatureAttachment': 'নিবন্ধকের স্বাক্ষর',
-    'validate.application.action.modal.title': 'অনুমোদনের জন্য প্রেরিত?',
-    'validate.application.action.modal.description':
-      'এই আবেদনটি  নিবন্ধকের কাছে  নিবন্দনের জন্য  প্রেরণ করা হবে ',
-    'buttons.approve': 'অনুমোদন',
     'form.field.label.userSignatureAttachmentDesc':
       'নিবন্ধককে  কাগজের টুকরোতে স্বাক্ষর করে  স্ক্যান করতে নিতে বা ছবি তুলে নিতে  বলুন।',
     'form.field.label.userAttachmentSection': 'সংযুক্তি',
