5be1177b20451ec78673750dab9b694cdf430443
Maruf Hossain <maruf.hossaint@dsinnovators.com>
2019-08-08 19:39:28 +0600

    Remove footer

--- packages/components/src/components/interface/Footer/Desktop/Footer.tsx
+++ packages/components/src/components/interface/Footer/Desktop/Footer.tsx
@@ -2,28 +2,16 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 const FooterContainer = styled.footer`
+  height: 80px;
+  width: 100%;
+
   @media (max-width: ${({ theme }) => theme.grid.breakpoints.lg}px) {
     display: none;
   }
-  padding: 8px 16px;
-  display: flex;
-  flex-grow: 1;
-  max-height: 40px;
-  & > p {
-    color: ${({ theme }) => theme.colors.white};
-    ${({ theme }) => theme.fonts.captionStyle};
-  }
-  flex-direction: row;
-  & a {
-    color: ${({ theme }) => theme.colors.white};
-  }
-  align-items: center;
-  width: 100%;
-  ${({ theme }) => theme.gradients.gradientNightshade};
 `
 
 export class Footer extends React.Component {
   render() {
-    return <FooterContainer>{this.props.children}</FooterContainer>
+    return <FooterContainer />
   }
 }
