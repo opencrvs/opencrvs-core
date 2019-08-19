5e11a191f54a0e16e7cef17cb8a5dcb1aae48406
Maruf Hossain <maruf.hossaint@dsinnovators.com>
2019-08-19 18:19:19 +0600

    Pass tests

--- packages/register/src/components/form/FormFieldGenerator.test.tsx
+++ packages/register/src/components/form/FormFieldGenerator.test.tsx
@@ -17,7 +17,7 @@ import {
 import { countries } from '@register/forms/countries'
 import { OFFLINE_LOCATIONS_KEY } from '@register/offline/reducer'
 
-import { formMessages as addressMessages } from '@register/i18n/messages'
+import { formMessages } from '@register/i18n/messages'
 
 export interface IMotherSectionFormData {
   firstName: string
@@ -49,7 +49,7 @@ describe('form component', () => {
         {
           name: 'countryPermanent',
           type: SELECT_WITH_OPTIONS,
-          label: addressMessages.country,
+          label: formMessages.country,
           required: true,
           initialValue: window.config.COUNTRY.toUpperCase(),
           validate: [],
@@ -58,7 +58,7 @@ describe('form component', () => {
         {
           name: 'statePermanent',
           type: SELECT_WITH_DYNAMIC_OPTIONS,
-          label: addressMessages.state,
+          label: formMessages.state,
           required: true,
           initialValue: '',
           validate: [],
@@ -70,9 +70,10 @@ describe('form component', () => {
         {
           name: 'districtPermanent',
           type: SELECT_WITH_DYNAMIC_OPTIONS,
-          label: addressMessages.district,
+          label: formMessages.district,
           required: true,
           initialValue: '',
+          placeholder: formMessages.select,
           validate: [],
           dynamicOptions: {
             resource: OFFLINE_LOCATIONS_KEY,
@@ -82,7 +83,7 @@ describe('form component', () => {
         {
           name: 'phone',
           type: TEL,
-          label: addressMessages.district,
+          label: formMessages.district,
           required: true,
           initialValue: '',
           validate: []
