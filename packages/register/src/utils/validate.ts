9a1838d581653e45e0047fa90278e51a1c8b83da
Maruf Hossain <maruf.hossaint@dsinnovators.com>
2019-08-20 18:33:59 +0600

    WIP

--- packages/register/src/utils/validate.ts
+++ packages/register/src/utils/validate.ts
@@ -1,6 +1,6 @@
 import { FormattedMessage, MessageValue } from 'react-intl'
 import { validationMessages as messages } from '@register/i18n/messages'
-import { IFormFieldValue } from '@opencrvs/register/src/forms'
+import { IFormFieldValue, IFormData } from '@opencrvs/register/src/forms'
 import {
   REGEXP_BLOCK_ALPHA_NUMERIC_DOT,
   REGEXP_ALPHA_NUMERIC,
@@ -32,8 +32,15 @@ export type MaxLengthValidation = (
 ) => (value: IFormFieldValue) => IValidationResult | undefined
 
 export type Validation = (
-  value: IFormFieldValue
+  value: IFormFieldValue,
+  drafts?: IFormData,
+  maruf?: number
 ) => IValidationResult | undefined
+
+// export type DeathDateValidation = (
+//   value: IFormFieldValue,
+//   drafts: IFormData
+// ) => IValidationResult | undefined
 
 export type ValidationInitializer = (...value: any[]) => Validation
 
@@ -192,6 +199,12 @@ export const dateFormat: Validation = (value: IFormFieldValue) => {
 
 export const isDateNotInFuture = (date: string) => {
   return new Date(date) <= new Date(new Date())
+}
+
+export const isDateNotBeforeBirth = (date: string, drafts: IFormData) => {
+  console.log(date)
+  console.log(JSON.stringify(drafts))
+  return false
 }
 
 export const isValidBirthDate: Validation = (value: IFormFieldValue) => {
@@ -488,10 +501,14 @@ export const validIDNumber: ValidationInitializer = (
 }
 
 export const isValidDeathOccurrenceDate: Validation = (
-  value: IFormFieldValue
+  value: IFormFieldValue,
+  drafts
 ) => {
   const cast = value as string
-  return value && isDateNotInFuture(cast) && isAValidDateFormat(cast)
+  return value &&
+    isDateNotInFuture(cast) &&
+    isAValidDateFormat(cast) &&
+    isDateNotBeforeBirth(cast, drafts as IFormData)
     ? undefined
     : {
         message: messages.isValidDateOfDeath
