738ffa27f1b2dbc76e27913dc39c56a6ef0f4138
Ryan Crichton <ryan@jembi.org>
2019-08-21 16:11:33 +0200

    Add the config vars to the infrastructure config

--- infrastructure/register-config.js
+++ infrastructure/register-config.js
@@ -2,9 +2,14 @@ window.config = {
   API_GATEWAY_URL: 'https://gateway.{{hostname}}/',
   BACKGROUND_SYNC_BROADCAST_CHANNEL: 'backgroundSynBroadCastChannel',
   COUNTRY: 'bgd',
+  DESKTOP_TIME_OUT_MILLISECONDS: 900000, // 15 mins
   HEALTH_FACILITY_FILTER: 'UPAZILA',
   LANGUAGES: 'en,bn',
   LOGIN_URL: 'https://login.{{hostname}}',
   PERFORMANCE_URL: 'https://performance.{{hostname}}',
-  RESOURCES_URL: 'https://resources.{{hostname}}/'
+  RESOURCES_URL: 'https://resources.{{hostname}}/',
+  CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: 45, // days
+  CERTIFICATE_PRINT_CHARGE_UP_LIMIT: 1825, // 5 years =  (5 * 365) days
+  CERTIFICATE_PRINT_LOWEST_CHARGE: 25, // taka
+  CERTIFICATE_PRINT_HIGHEST_CHARGE: 50 // taka
 }
