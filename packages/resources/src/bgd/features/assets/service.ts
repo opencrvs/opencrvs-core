26ab41cb876f80f32f9f1e43aa07df811385be3e
anik <mushraful.hoque@dsinnovators.com>
2019-09-03 14:01:56 +0600

    Read asset files asynchronously

--- packages/resources/src/bgd/features/assets/service.ts
+++ packages/resources/src/bgd/features/assets/service.ts
@@ -1,9 +1,13 @@
-import { readFileSync } from 'fs'
+import { readFile } from 'fs'
 import { join } from 'path'
 import { lookup } from 'mime-types'
 
-export function getAssest(file: string) {
-  return readFileSync(join(__dirname, `./files/${file}`))
+export async function getAssest(file: string): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    readFile(join(__dirname, `./files/${file}`), (err, data) => {
+      err ? reject(err) : resolve(data)
+    })
+  })
 }
 
 export function getAssestMimeType(file: string) {
--- packages/resources/src/zmb/features/assets/handler.ts
+++ packages/resources/src/zmb/features/assets/handler.ts
@@ -8,7 +8,7 @@ export async function assetHandler(
   request: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
-  const response = h.response(getAssest(request.params.file))
+  const response = h.response(await getAssest(request.params.file))
   response.type(getAssestMimeType(request.params.file))
   return response
 }
