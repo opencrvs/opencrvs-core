e03c1598b8eb3da5925cf010ea01decd79716466
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2019-09-04 17:34:56 +0600

    ocrvs-1968: adding more test case to register package

--- packages/register/src/forms/mappings/query/field-mapping.test.ts
+++ packages/register/src/forms/mappings/query/field-mapping.test.ts
@@ -20,4 +20,32 @@ describe('Query FieldMapping', () => {
 
     expect(result).toEqual(expectedResult)
   })
+
+  it('Should return a new ', () => {
+    const factory = identityToFieldTransformer('id', 'nationalId')
+    const expectedResult = { person: { nationalId: 151515 } }
+    const queryData = {
+      person: {
+        identifier: [
+          {
+            type: 'nationalId',
+            id: 151515
+          }
+        ]
+      }
+    }
+    const sectionId = 'person'
+    const field = { name: 'nationalId' } as IFormField
+    const transformedData = {
+      person: {}
+    }
+    const result = factory(
+      transformedData,
+      queryData,
+      sectionId,
+      field as IFormField
+    )
+
+    expect(result).toEqual(expectedResult)
+  })
 })
