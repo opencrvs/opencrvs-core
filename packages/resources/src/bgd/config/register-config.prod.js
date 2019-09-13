f7c59d237805fe378d43a25632f7bf70e3fecab6
Euan Millar <euanmillar77@gmail.com>
2019-09-13 11:33:53 +0100

    config file fix

--- packages/resources/src/bgd/config/register-config.prod.js
+++ packages/resources/src/bgd/config/register-config.prod.js
@@ -1,14 +1,14 @@
 window.config = {
-  API_GATEWAY_URL: 'http://localhost:7070/',
   BACKGROUND_SYNC_BROADCAST_CHANNEL: 'backgroundSynBroadCastChannel',
   COUNTRY: 'bgd',
   COUNTRY_LOGO_FILE: 'logo.png',
   DESKTOP_TIME_OUT_MILLISECONDS: 900000, // 15 mins
   HEALTH_FACILITY_FILTER: 'UPAZILA',
   LANGUAGES: 'en,bn',
-  LOGIN_URL: 'http://localhost:3020',
-  PERFORMANCE_URL: 'http://localhost:3001',
-  RESOURCES_URL: 'http://localhost:3040/bgd',
+  LOGIN_URL: 'https://login.{{hostname}}',
+  API_GATEWAY_URL: 'https://gateway.{{hostname}}/',
+  PERFORMANCE_URL: 'https://performance.{{hostname}}',
+  RESOURCES_URL: 'https://resources.{{hostname}}/zmb',
   CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: 45, // days
   CERTIFICATE_PRINT_CHARGE_UP_LIMIT: 1825, // 5 years =  (5 * 365) days
   CERTIFICATE_PRINT_LOWEST_CHARGE: 25, // taka
