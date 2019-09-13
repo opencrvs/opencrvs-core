9b09327e8c5952c356b4975aa1da3639603a866e
Euan Millar <euanmillar77@gmail.com>
2019-09-13 12:53:18 +0100

    fix Zambia config and finsihed removing tribe

--- packages/resources/src/zmb/config/login-config.js
+++ packages/resources/src/zmb/config/login-config.js
@@ -1,7 +1,7 @@
 window.config = {
-  AUTH_API_URL: 'https://auth.{{hostname}}/',
+  AUTH_API_URL: 'http://localhost:4040/',
   COUNTRY: 'zmb',
   LANGUAGES: 'en',
-  PERFORMANCE_APP_URL: 'https://performance.{{hostname}}/',
-  REGISTER_APP_URL: 'https://register.{{hostname}}/'
+  PERFORMANCE_APP_URL: 'http://localhost:3001/',
+  REGISTER_APP_URL: 'http://localhost:3000/'
 }
