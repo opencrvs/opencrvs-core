3bfee61642339fa315081accbea860c9976685ae
Atiq Zaman <atiq.zaman@dsinnovators.com>
2019-09-24 13:49:37 +0600

    Deleted auto generated config files
    The files were added unintentionally.

--- packages/login/public/config.js
+++
@@ -1,7 +0,0 @@
-window.config = {
-  AUTH_API_URL: 'http://localhost:4040/',
-  COUNTRY: 'zmb',
-  LANGUAGES: 'en',
-  PERFORMANCE_APP_URL: 'http://localhost:3001/',
-  REGISTER_APP_URL: 'http://localhost:3000/'
-}
