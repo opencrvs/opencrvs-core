1fa41a025fe68bfbbdcfd020cf58856e2914a33c
Sadman Ilham <sadman.ilham@gmail.com>
2019-09-27 14:36:06 +0600

    Add Routes

--- packages/login/src/navigation/routes.ts
+++ packages/login/src/navigation/routes.ts
@@ -1,5 +1,8 @@
 export const STEP_ONE = '/'
 export const STEP_TWO = '/step-two'
 
+export const FORGOTTEN_ITEM = '/forgotten-item'
+export const PHONE_NUMBER_VERIFICATION = '/phone-number-verification'
+
 export const REGISTER_APP = 'register'
 export const PERFORMANCE_APP = 'performance'
