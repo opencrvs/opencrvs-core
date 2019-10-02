50e8b44e2b6a1fc021b81de8f00957bbc992d4bf
Riku Rouvila <riku.rouvila@gmail.com>
2019-10-02 13:46:05 +0300

    make sure we don't actually submit anything to notification service in dev or with demo users

--- packages/auth/src/features/retrievalSteps/sendUserName/handler.ts
+++ packages/auth/src/features/retrievalSteps/sendUserName/handler.ts
@@ -9,6 +9,8 @@ import {
   RetrievalSteps,
   deleteRetrievalStepInformation
 } from '@auth/features/retrievalSteps/verifyUser/service'
+import { logger } from '@auth/logger'
+import { PRODUCTION } from '@auth/constants'
 
 interface IPayload {
   nonce: string
@@ -28,10 +30,19 @@ export default async function sendUserNameHandler(
   if (retrivalStepInformation.status !== RetrievalSteps.SECURITY_Q_VERIFIED) {
     return h.response().code(401)
   }
-  await sendUserName(
-    retrivalStepInformation.mobile,
-    retrivalStepInformation.username
-  )
+
+  const isDemoUser = retrivalStepInformation.scope.indexOf('demo') > -1
+  if (!PRODUCTION || isDemoUser) {
+    logger.info('Sending a verification SMS', {
+      mobile: retrivalStepInformation.mobile,
+      username: retrivalStepInformation.username
+    })
+  } else {
+    await sendUserName(
+      retrivalStepInformation.mobile,
+      retrivalStepInformation.username
+    )
+  }
   await deleteRetrievalStepInformation(payload.nonce)
   return h.response().code(200)
 }
