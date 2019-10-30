95f07444a525780c41b2b56e4b7a73fe8244a0b6
Maruf Hossain <maruf.hossaint@dsinnovators.com>
2019-10-30 20:09:05 +0600

    Fix issue for applicant relationship for zmb

--- packages/register/public/config.js
+++ packages/register/public/config.js
@@ -1,19 +1,19 @@
 window.config = {
+  API_GATEWAY_URL: 'http://localhost:7070/',
   BACKGROUND_SYNC_BROADCAST_CHANNEL: 'backgroundSynBroadCastChannel',
-  COUNTRY: 'bgd',
+  COUNTRY: 'zmb',
   COUNTRY_LOGO_FILE: 'logo.png',
   DESKTOP_TIME_OUT_MILLISECONDS: 900000, // 15 mins
-  HEALTH_FACILITY_FILTER: 'UPAZILA',
-  LANGUAGES: 'en,bn',
+  HEALTH_FACILITY_FILTER: 'DISTRICT',
+  LANGUAGES: 'en',
   LOGIN_URL: 'http://localhost:3020',
-  API_GATEWAY_URL: 'http://localhost:7070/',
   PERFORMANCE_URL: 'http://localhost:3001',
-  RESOURCES_URL: 'http://localhost:3040/bgd',
-  CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: 45, // days
-  CERTIFICATE_PRINT_CHARGE_UP_LIMIT: 1825, // 5 years =  (5 * 365) days
-  CERTIFICATE_PRINT_LOWEST_CHARGE: 25, // taka
-  CERTIFICATE_PRINT_HIGHEST_CHARGE: 50, // taka
+  RESOURCES_URL: 'http://localhost:3040/zmb',
+  CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: 36500, // 100 years =  (100 * 365) days
+  CERTIFICATE_PRINT_CHARGE_UP_LIMIT: 36500, // 100 years =  (100 * 365) days
+  CERTIFICATE_PRINT_LOWEST_CHARGE: 0,
+  CERTIFICATE_PRINT_HIGHEST_CHARGE: 0,
   UI_POLLING_INTERVAL: 5000,
-  SENTRY: 'https://2ed906a0ba1c4de2ae3f3f898ec9df0b@sentry.io/1774551',
-  LOGROCKET: 'opencrvs-foundation/opencrvs-bangladesh'
+  SENTRY: 'https://f892d643aab642108f44e2d1795706bc@sentry.io/1774604',
+  LOGROCKET: 'opencrvs-foundation/opencrvs-zambia'
 }
