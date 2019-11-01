8bc650c66580e0d5d9f83e0eb1a3822b6ff1cc01
Euan Millar <euanmillar77@gmail.com>
2019-11-01 12:23:06 +0000

    Add all national health and org office facilities to the backup files Prior to this change, Only a hardcoded list of health facilities and org offices were loaded This change Pulls from the DGHIS Facility API and ORG csv, maps ORG facilities to A2I locations using the name of the facility because ORG did not supply a csv with statistical BBS geocodes.

--- packages/resources/createReferenceDataBgd.sh
+++ packages/resources/createReferenceDataBgd.sh
@@ -12,15 +12,31 @@
 if [ -z "$1" ] 
   then
     echo 'Error: Argument for a2i secret is required in position 1.'
-    echo 'Usage: db:populate:bgd {secret}'
+    echo 'Usage: db:populate:bgd {secret} {HRIS client id} {HRIS token}'
     echo "Script must receive a parameter of a2i secret"
+    exit 1
+fi
+
+if [ -z "$2" ] 
+  then
+    echo 'Error: Argument for HRIS client id is required in position 2.'
+    echo 'Usage: db:populate:bgd {secret} {HRIS client id} {HRIS token}'
+    echo "Script must receive a parameter of HRIS client id"
+    exit 1
+fi
+
+if [ -z "$3" ] 
+  then
+    echo 'Error: Argument for HRIS token is required in position 3.'
+    echo 'Usage: db:populate:bgd {secret} {HRIS client id} {HRIS token}'
+    echo "Script must receive a parameter of HRIS token"
     exit 1
 fi
 
 ts-node -r tsconfig-paths/register src/bgd/features/administrative/scripts/assign-admin-structure-to-locations.ts -- $1
 ts-node -r tsconfig-paths/register src/bgd/features/administrative/scripts/assign-geodata-to-locations.ts
 ts-node -r tsconfig-paths/register src/bgd/features/administrative/scripts/update-location-data.ts
-ts-node -r tsconfig-paths/register src/bgd/features/facilities/scripts/prepare-source-facilities.ts
+ts-node -r tsconfig-paths/register src/bgd/features/facilities/scripts/prepare-source-facilities.ts -- $2 $3
 ts-node -r tsconfig-paths/register src/bgd/features/facilities/scripts/assign-facilities-to-locations.ts
 ts-node -r tsconfig-paths/register src/bgd/features/employees/scripts/prepare-source-employees.ts
 ts-node -r tsconfig-paths/register src/bgd/features/employees/scripts/assign-employees-to-practitioners.ts
