01f5546132fe65b27262e9141f98176db62a92ef
Sadman Ilham <sadman.ilham@gmail.com>
2019-11-07 13:00:08 +0600

    Add Performance Routes URL

--- packages/register/src/navigation/routes.ts
+++ packages/register/src/navigation/routes.ts
@@ -63,3 +63,6 @@ export const SYS_ADMIN_HOME_TAB = '/sys-admin-home/:tabId'
 
 export const CREATE_USER = '/createUser'
 export const CREATE_USER_SECTION = '/createUser/:sectionId/:groupId'
+
+export const PERFORMANCE_REPORT_LIST = '/performance/reports/:reportType?'
+export const PERFORMANCE_REPORT = '/performance/report'
