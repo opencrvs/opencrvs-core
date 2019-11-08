44b5fcdb2099a64384f0c8a3e97100e2d3ea63e4
Sadman Ilham <sadman.ilham@gmail.com>
2019-11-08 18:35:58 +0600

    Integrate List Table with Dummy Data

--- packages/register/src/views/Performance/WeeklyReports.tsx
+++ packages/register/src/views/Performance/WeeklyReports.tsx
@@ -9,12 +9,19 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import { LinkButton } from '@opencrvs/components/lib/buttons'
+import { ArrowDownBlue } from '@opencrvs/components/lib/icons'
+import { ListTable } from '@opencrvs/components/lib/interface'
 import { messages } from '@register/i18n/messages/views/performance'
 import { goToPerformanceReport } from '@register/navigation'
+import { PERFORMANCE_REPORT_TYPE_WEEKY } from '@register/utils/constants'
 import { Header } from '@register/views/Performance/utils'
+import moment from 'moment'
 import * as React from 'react'
 import { injectIntl, WrappedComponentProps } from 'react-intl'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+import { constantsMessages } from '@register/i18n/messages'
 
 interface ReportProps {
   goToPerformanceReport: typeof goToPerformanceReport
@@ -25,11 +32,71 @@ type Props = ReportProps & WrappedComponentProps
 type State = {}
 
 class WeeklyReportsComponent extends React.Component<Props, State> {
+  getContent() {
+    moment.locale(this.props.intl.locale)
+    let content = []
+
+    const startDayOfYear = moment([2019, 0]).startOf('month')
+    const endDayOfYear = moment([2019, 11]).endOf('month')
+
+    while (startDayOfYear < endDayOfYear) {
+      const title = `${startDayOfYear.format(
+        'DD MMMM'
+      )} ${this.props.intl.formatMessage(
+        constantsMessages.to
+      )} ${startDayOfYear.add(7, 'days').format('DD MMMM YYYY')}`
+      content.push({
+        week: (
+          <LinkButton
+            onClick={() =>
+              this.props.goToPerformanceReport(
+                PERFORMANCE_REPORT_TYPE_WEEKY,
+                title
+              )
+            }
+          >
+            {title}
+          </LinkButton>
+        ),
+        export: (
+          <>
+            <LinkButton>CSV</LinkButton> <LinkButton>PDF</LinkButton>
+          </>
+        )
+      })
+    }
+    return content
+  }
+
   render() {
     const { intl } = this.props
 
     return (
-      <Header>{intl.formatMessage(messages.weeklyReportsBodyHeader)}</Header>
+      <>
+        <Header>{intl.formatMessage(messages.weeklyReportsBodyHeader)}</Header>
+
+        <ListTable
+          tableTitle={intl.formatMessage(constantsMessages.birth)}
+          isLoading={false}
+          content={this.getContent()}
+          columns={[
+            {
+              label: intl.formatMessage(constantsMessages.week),
+              width: 70,
+              key: 'week',
+              isSortable: true,
+              icon: <ArrowDownBlue />,
+              sortFunction: () => console.log('sorted')
+            },
+            {
+              label: intl.formatMessage(constantsMessages.export),
+              width: 30,
+              key: 'export'
+            }
+          ]}
+          noResultText={intl.formatMessage(constantsMessages.noResults)}
+        />
+      </>
     )
   }
 }
