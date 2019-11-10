7b1b1d907859b1321b82527defb48f0e464b2217
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2019-11-11 03:34:39 +0600

    Add polyfill for focus-visible property

--- packages/register/src/index.tsx
+++ packages/register/src/index.tsx
@@ -9,6 +9,7 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import 'focus-visible/dist/focus-visible.js'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from '@register/App'
