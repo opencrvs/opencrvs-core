075e2f3764b99185d3ee42f8256186ae225a8a01
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2019-11-18 12:27:22 +0600

    Fixes the expansion buttons

--- packages/components/src/components/icons/KeyboardArrowUp.tsx
+++ packages/components/src/components/icons/KeyboardArrowUp.tsx
@@ -16,7 +16,7 @@ export const KeyboardArrowUp = (props: React.HTMLAttributes<SVGElement>) => (
   <svg width={24} height={24} fill="none" {...props}>
     <path
       d="M18 15l-6-5.96L6 15"
-      stroke={props.color || colors.primary}
+      stroke="currentColor"
       strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
