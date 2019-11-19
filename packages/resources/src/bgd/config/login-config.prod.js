4a43178a47f821c4ede65cb5bafd213af883bea5
Euan Millar <euanmillar77@gmail.com>
2019-11-19 12:33:28 +0000

    Added pen config

--- packages/resources/src/bgd/config/login-config.prod.js
+++ packages/resources/src/bgd/config/login-config.prod.js
@@ -12,7 +12,7 @@
 window.config = {
   AUTH_API_URL: 'https://auth.{{hostname}}/',
   COUNTRY: 'bgd',
-  LANGUAGES: 'bn,en',
+  LANGUAGES: 'en,bn',
   PERFORMANCE_APP_URL: 'https://performance.{{hostname}}/',
   REGISTER_APP_URL: 'https://register.{{hostname}}/',
   SENTRY: 'https://2ed906a0ba1c4de2ae3f3f898ec9df0b@sentry.io/1774551',
