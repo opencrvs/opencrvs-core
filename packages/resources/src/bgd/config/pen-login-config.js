7d50af949e8dff40ed774d211dc9977bdf73ef1d
Euan Millar <euanmillar77@gmail.com>
2019-11-25 13:42:02 +0000

    Added new user phone numbers in backup zip

--- packages/resources/src/bgd/config/pen-login-config.js
+++ packages/resources/src/bgd/config/pen-login-config.js
@@ -14,7 +14,7 @@ window.config = {
   COUNTRY: 'gbr',
   LANGUAGES: 'en,bn',
   PERFORMANCE_APP_URL: 'http://localhost:3001/',
-  REGISTER_APP_URL: 'http://localhost:3000/',
+  CLIENT_APP_URL: 'http://localhost:3000/',
   SENTRY: 'https://2ed906a0ba1c4de2ae3f3f898ec9df0b@sentry.io/1774551',
   LOGROCKET: 'opencrvs-foundation/opencrvs-bangladesh'
 }
