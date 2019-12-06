508cc3be141820669dec57327106b6f6e9eaa7ff
Sadman Ilham <sadman.ilham@gmail.com>
2019-12-06 20:41:01 +0600

    Refactor Controller

--- packages/client/src/ListSyncController.ts
+++ packages/client/src/ListSyncController.ts
@@ -9,30 +9,23 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import { client } from '@client/utils/apolloClient'
 import { REGISTRATION_HOME_QUERY } from '@client/views/RegistrationHome/queries'
-import { IQueryData } from '@client/views/RegistrationHome/RegistrationHome'
-import ApolloClient, { ApolloError } from 'apollo-client'
-import { Dispatch } from 'redux'
-import { updateRegistrarWorkQueue } from './applications'
 
-const onSuccess = (dispatch: Dispatch, result: IQueryData) => {
-  dispatch(updateRegistrarWorkQueue(false, false, result))
-}
-
-const onError = (dispatch: Dispatch, error: ApolloError) => {
-  dispatch(updateRegistrarWorkQueue(false, true))
-}
-const queryData = async (
-  dispatch: Dispatch,
+export async function syncRegistrarWorkqueue(
   locationId: string,
   reviewStatuses: string[],
+  inProgressCount: number,
+  reviewCount: number,
+  rejectCount: number,
+  approvalCount: number,
+  printCount: number,
   inProgressSkip: number,
   reviewSkip: number,
   rejectSkip: number,
   approvalSkip: number,
-  printSkip: number,
-  client: ApolloClient<{}>
-) => {
+  printSkip: number
+) {
   try {
     const queryResult = await client.query({
       query: REGISTRATION_HOME_QUERY,
@@ -48,35 +41,8 @@ const queryData = async (
       },
       fetchPolicy: 'no-cache'
     })
-    onSuccess(dispatch, queryResult.data)
+    return queryResult.data
   } catch (exception) {
-    onError(dispatch, exception)
-  }
-}
-export const syncRegistrarWorkqueue = (
-  dispatch: Dispatch,
-  requireLoading: boolean = false,
-  locationId: string,
-  reviewStatuses: string[],
-  client: ApolloClient<{}>,
-  inProgressSkip: number = 0,
-  reviewSkip: number = 0,
-  rejectSkip: number = 0,
-  approvalSkip: number = 0,
-  printSkip: number = 0
-) => {
-  if (requireLoading) {
-    dispatch(updateRegistrarWorkQueue(true, false))
-    queryData(
-      dispatch,
-      locationId,
-      reviewStatuses,
-      inProgressSkip,
-      reviewSkip,
-      rejectSkip,
-      approvalSkip,
-      printSkip,
-      client
-    )
+    return undefined
   }
 }
