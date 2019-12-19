d9c2353c08c51ba01fe5c0d7ee6b9d37db0c3168
Euan Millar <euanmillar77@gmail.com>
2019-12-19 17:04:50 +0000

    removed console log

--- packages/client/src/utils/locationUtils.ts
+++ packages/client/src/utils/locationUtils.ts
@@ -20,8 +20,6 @@ export function filterLocations(
   userDetails: IUserDetails
 ): { [key: string]: ILocation } {
   const locationsCopy = Object.assign({}, locations)
-  console.log('locations', JSON.stringify(locations))
-  console.log('filterValue', JSON.stringify(filterValue))
   Object.values(locationsCopy).forEach((location: ILocation) => {
     if (
       location.partOf !== `Location/${filterValue}` &&
