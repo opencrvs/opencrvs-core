94742df3fe94c75abcc6487f3c35e093c761a0df
Sadman Ilham <sadman.ilham@gmail.com>
2020-01-08 12:36:50 +0600

    Add Test

---
+++ packages/client/src/views/Unlock/utils.test.ts
@@ -0,0 +1,47 @@
+import { storage } from '@client/storage'
+import { pinLoader } from './utils'
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+
+describe('Unlock page loads Properly', () => {
+  const expectedPin =
+    '$2a$10$xQBLcbPgGQNu9p6zVchWuu6pmCrQIjcb6k2W1PIVUxVTE/PumWM82'
+  beforeEach(async () => {
+    // mock indexeddb
+    const indexedDB = {
+      USER_DETAILS: JSON.stringify({ userMgntUserID: 'shakib75' }),
+      USER_DATA: JSON.stringify([
+        {
+          userID: 'shakib75',
+          userPIN: expectedPin,
+          drafts: []
+        }
+      ])
+    }
+
+    storage.getItem = jest.fn(async (key: string) =>
+      // @ts-ignore
+      Promise.resolve(indexedDB[key])
+    )
+
+    storage.setItem = jest.fn(
+      // @ts-ignore
+      async (key: string, value: string) => (indexedDB[key] = value)
+    )
+  })
+
+  it('Should retrieve user pin from index db', async () => {
+    const receivedPin = await pinLoader.loadUserPin()
+    expect(receivedPin).toEqual(expectedPin)
+  })
+})
