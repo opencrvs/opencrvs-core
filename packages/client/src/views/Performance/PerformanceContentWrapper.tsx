09f759e0041acf84235a0e9cb26a14ed8d30079b
Sadman Ilham <sadman.ilham@gmail.com>
2020-03-31 20:17:27 +0600

    Show Topbar based on Prop

--- packages/client/src/views/Performance/PerformanceContentWrapper.tsx
+++ packages/client/src/views/Performance/PerformanceContentWrapper.tsx
@@ -9,18 +9,18 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-import { ICON_ALIGNMENT } from '@opencrvs/components/lib/buttons'
-import { TopBar } from '@opencrvs/components/lib/interface'
-import { Container } from '@opencrvs/components/lib/layout'
 import { Header } from '@client/components/interface/Header/Header'
 import { messages } from '@client/i18n/messages/views/performance'
 import { PERFORMANCE_REPORT_TYPE_MONTHLY } from '@client/utils/constants'
 import { IconTab } from '@client/views/RegistrationHome/RegistrationHome'
+import { ICON_ALIGNMENT } from '@opencrvs/components/lib/buttons'
+import { TopBar } from '@opencrvs/components/lib/interface'
+import { Container } from '@opencrvs/components/lib/layout'
 import * as React from 'react'
 import { injectIntl, WrappedComponentProps } from 'react-intl'
 import styled from 'styled-components'
 
-type Props = WrappedComponentProps & { tabId: string }
+type Props = WrappedComponentProps & { tabId?: string; hideTopBar?: boolean }
 
 type State = {}
 
@@ -53,7 +53,7 @@ class PerformanceContentWrapperComponent extends React.Component<Props, State> {
     return (
       <Container>
         <Header />
-        {this.renderTopBar()}
+        {!this.props.hideTopBar && this.renderTopBar()}
 
         <Content>{this.props.children}</Content>
       </Container>
