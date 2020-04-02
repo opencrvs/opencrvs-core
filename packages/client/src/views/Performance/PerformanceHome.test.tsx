1f93405b82e3de0af832192d8d8bbc94ead091bb
Sadman Ilham <sadman.ilham@gmail.com>
2020-04-02 22:31:53 +0600

    Test Performance Home

---
+++ packages/client/src/views/Performance/PerformanceHome.test.tsx
@@ -0,0 +1,112 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+import { PERFORMANCE_HOME } from '@client/navigation/routes'
+import { createStore } from '@client/store'
+import {
+  createTestApp,
+  createTestComponent,
+  flushPromises
+} from '@client/tests/util'
+import { ReactWrapper } from 'enzyme'
+import { History } from 'history'
+import * as React from 'react'
+import { PerformanceHome } from './PerformanceHome'
+
+describe('Performance home test', () => {
+  describe('Performance home without location in props', () => {
+    const { store, history } = createStore()
+    let app: ReactWrapper
+
+    beforeAll(async () => {
+      app = (await createTestComponent(
+        <PerformanceHome history={history} />,
+        store
+      )).component
+      app.update()
+    })
+
+    it('loads nothing in the search input box', () => {
+      expect(
+        app
+          .find('#locationSearchInput')
+          .hostNodes()
+          .props().value
+      ).toEqual('')
+    })
+
+    it('loads the selected location in search input box', () => {
+      app
+        .find('#locationSearchInput')
+        .hostNodes()
+        .simulate('change', {
+          target: { id: 'locationSearchInput', value: 'Dhaka' }
+        })
+      app.update()
+
+      app
+        .find('#locationOption6e1f3bce-7bcb-4bf6-8e35-0d9facdf158b')
+        .hostNodes()
+        .simulate('click')
+      app.update()
+
+      expect(
+        app
+          .find('#locationSearchInput')
+          .hostNodes()
+          .props().value
+      ).toEqual('Dhaka Division')
+    })
+
+    it('redirect to operatoins on search button click', () => {
+      app
+        .find('#location-search-btn')
+        .hostNodes()
+        .simulate('click')
+      app.update()
+      flushPromises()
+
+      expect(history.location.state).toEqual({
+        locationId: '6e1f3bce-7bcb-4bf6-8e35-0d9facdf158b'
+      })
+      expect(history.location.pathname).toContain('operations')
+    })
+  })
+
+  describe('Performance home with location in props', () => {
+    let app: ReactWrapper
+    let history: History
+
+    beforeEach(async () => {
+      const testApp = await createTestApp()
+      app = testApp.app
+      history = testApp.history
+
+      history.replace(PERFORMANCE_HOME, {
+        selectedLocation: {
+          id: '',
+          searchableText: '',
+          displayLabel: 'Khulna'
+        }
+      })
+      app.update()
+    })
+
+    it('loads the location in the search input box', () => {
+      expect(
+        app
+          .find('#locationSearchInput')
+          .hostNodes()
+          .props().value
+      ).toEqual('Khulna')
+    })
+  })
+})
