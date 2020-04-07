281d8a55c8c6f57f6799f9addbac265149463be9
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-04-07 19:24:15 +0600

    Amend in KeyboardArrowDown component
    - Make sure custom prop does not go into ReactDOM

--- packages/components/src/components/icons/KeyboardArrowDown.tsx
+++ packages/components/src/components/icons/KeyboardArrowDown.tsx
@@ -12,13 +12,14 @@
 import * as React from 'react'
 import { colors } from '../colors'
 
-export const KeyboardArrowDown = (
-  props: React.HTMLAttributes<SVGElement> & { pathStroke?: keyof typeof colors }
-) => (
-  <svg width={24} height={24} fill="none" {...props}>
+export const KeyboardArrowDown = ({
+  pathStroke,
+  ...otherProps
+}: React.HTMLAttributes<SVGElement> & { pathStroke?: keyof typeof colors }) => (
+  <svg width={24} height={24} fill="none" {...otherProps}>
     <path
       d="M6 9l6 5.96L18 9"
-      stroke={props.pathStroke ? props.pathStroke : 'currentColor'}
+      stroke={pathStroke ? pathStroke : 'currentColor'}
       strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
