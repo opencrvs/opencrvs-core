79a339b48f2568e37517567241841f3f9cd79522
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-04-16 16:46:16 +0600

    Add client query locationsByParent
    - Minor fix in root resolver

--- packages/gateway/src/features/location/root-resolvers.ts
+++ packages/gateway/src/features/location/root-resolvers.ts
@@ -16,11 +16,7 @@ import { fetchFHIR } from '@gateway/features/fhir/utils'
 export const resolvers: GQLResolver = {
   Query: {
     async locationsByParent(_, { parentId }, authHeader) {
-      const bundle = await fetchFHIR(
-        `${FHIR_URL}/Location?partof=${parentId}`,
-        authHeader
-      )
-
+      const bundle = await fetchFHIR(`/Location?partof=${parentId}`, authHeader)
       return bundle.entry.map((entry: { resource: {} }) => entry.resource)
     },
     async locationById(_, { locationId }, authHeader) {
