33984dac8079df7a95e55b6e1a6aecc19d6d186c
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-06-25 15:18:12 +0600

    Add i18n

--- packages/client/src/i18n/messages/views/sysAdmin.ts
+++ packages/client/src/i18n/messages/views/sysAdmin.ts
@@ -15,7 +15,15 @@ interface ISysAdminMessages {
   overviewTab: MessageDescriptor
   officesTab: MessageDescriptor
   usersTab: MessageDescriptor
+  comments: MessageDescriptor
+  deactivateReason: MessageDescriptor
+  deactivateReasonNotEmployee: MessageDescriptor
+  deactivateReasonInvestigated: MessageDescriptor
+  deactivateReasonOther: MessageDescriptor
+  deactivateUserTitle: MessageDescriptor
+  deactivateUserSubtitle: MessageDescriptor
   devicesTab: MessageDescriptor
+  formError: MessageDescriptor
   networkTab: MessageDescriptor
   configTab: MessageDescriptor
   systemTitle: MessageDescriptor
@@ -40,10 +48,52 @@ const messagesToDefine: ISysAdminMessages = {
     defaultMessage: 'Users',
     description: 'The title of users tab'
   },
+  comments: {
+    id: 'register.sysAdminHome.user.audit.comments',
+    defaultMessage: 'Comments: ',
+    description: 'The label for form field deactivate comments'
+  },
+  deactivateReason: {
+    id: 'register.sysAdminHome.user.audit.reason',
+    defaultMessage: 'Please provide a reason: ',
+    description: 'The label for form field deactivate reason'
+  },
+  deactivateReasonNotEmployee: {
+    id: 'register.sysAdminHome.user.audit.deactivation.reasonNotEmployee',
+    defaultMessage: 'No longer an employee',
+    description: 'The label for radio option not employee'
+  },
+  deactivateReasonInvestigated: {
+    id: 'register.sysAdminHome.user.audit.deactivation.reasonInvestigated',
+    defaultMessage:
+      'Being investigated due to suspicious activity on their account',
+    description: 'The label for radio option being investigated'
+  },
+  deactivateReasonOther: {
+    id: 'register.sysAdminHome.user.audit.deactivation.reasonOther',
+    defaultMessage: 'Other (please provide a reason in the comments)',
+    description: 'The label for radio option other'
+  },
+  deactivateUserTitle: {
+    id: 'register.sysAdminHome.user.audit.deactivation.title',
+    defaultMessage: 'Deactivate {name}?',
+    description: 'The title of user deactivation confirmation modal'
+  },
+  deactivateUserSubtitle: {
+    id: 'register.sysAdminHome.user.audit.deactivation.subtitle',
+    defaultMessage:
+      'This will revoke {name}’s ability to login and access the system. The account can be reactivated at a later date.',
+    description: 'The subtitle of user deactivation confirmation modal'
+  },
   devicesTab: {
     id: 'register.sysAdminHome.devices',
     defaultMessage: 'Devices',
     description: 'The title of devices tab'
+  },
+  formError: {
+    id: 'register.sysAdminHome.user.audit.form.error',
+    defaultMessage: 'Please complete all mandatory fields',
+    description: 'The label for form error'
   },
   networkTab: {
     id: 'register.sysAdminHome.network',
