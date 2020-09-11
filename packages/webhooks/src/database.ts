d629bb4ac8f4e2860f31b46a662d805dfb682d85
Euan Millar <euanmillar77@gmail.com>
2020-09-11 13:52:02 +0100

    Handle connection errors

--- packages/webhooks/src/database.ts
+++ packages/webhooks/src/database.ts
@@ -10,7 +10,7 @@
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
 import * as mongoose from 'mongoose'
-import { MONGO_URL, REDIS_HOST } from '@webhooks/constants'
+import { MONGO_URL, REDIS_HOST, REDIS_PORT } from '@webhooks/constants'
 import { logger } from '@webhooks/logger'
 import * as IORedis from 'ioredis'
 
@@ -35,9 +35,18 @@ export function getRedis(): IORedis.Redis {
 
 const connect = async (): Promise<void> => {
   try {
-    redisConnection = new IORedis({
-      port: 6379, // Redis port
-      host: REDIS_HOST
+    redisConnection = new IORedis(REDIS_PORT, REDIS_HOST)
+
+    redisConnection.on('error', error => {
+      logger.error('Redis connection error', error)
+      process.exit(1)
+    })
+
+    redisConnection.on('exit', () => {
+      logger.error(
+        'Exiting...listener count',
+        redisConnection.listenerCount('error')
+      )
     })
   } catch (err) {
     logger.error(`Cant create Redis instance: ${err}`)
