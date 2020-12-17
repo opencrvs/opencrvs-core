fc1d9aed662bfc3649414fc5d41d7041bdaed68d
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-12-17 13:42:04 +0600

    Add handler
    - With unit tests

--- packages/user-mgnt/src/features/resendSMSInvite/handler.ts
+++ packages/user-mgnt/src/features/resendSMSInvite/handler.ts
@@ -11,12 +11,50 @@
  */
 import * as Hapi from 'hapi'
 import * as Joi from 'joi'
+import User from '@user-mgnt/model/user'
+import { unauthorized } from 'boom'
+import {
+  generateRandomPassword,
+  generateSaltedHash
+} from '@user-mgnt/utils/hash'
+import { hasDemoScope, statuses } from '@user-mgnt/utils/userUtils'
+import { sendCredentialsNotification } from '@user-mgnt/features/createUser/service'
+
+interface IResendSMSPayload {
+  mobile: string
+}
 
 export default async function resendSMSInvite(
   request: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
-  return null
+  let randomPassword = null
+  const { mobile } = request.payload as IResendSMSPayload
+
+  const user = await User.findOne({ mobile })
+
+  if (!user) {
+    throw unauthorized()
+  }
+
+  randomPassword = generateRandomPassword(hasDemoScope(request))
+  const { hash, salt } = generateSaltedHash(randomPassword)
+
+  user.passwordHash = hash
+  user.salt = salt
+  user.status = statuses.PENDING
+
+  try {
+    await User.update({ _id: user._id }, user)
+  } catch (err) {
+    return h.response().code(400)
+  }
+
+  sendCredentialsNotification(user.mobile, user.username, randomPassword, {
+    Authorization: request.headers.authorization
+  })
+
+  return h.response().code(200)
 }
 
 export const requestSchema = Joi.object({
