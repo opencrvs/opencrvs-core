8917e6662aca731e3d58d217641e823cb0cfe58e
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2021-06-21 22:48:09 +0600

    Adding intentional added fail test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -156,7 +156,7 @@ describe('it handles react unauthorized errors', () => {
       .simulate('click')
   })
 
-  xit('Should create a failed test', () => {
+  it('Should create a failed test', () => {
     expect(true).toBe(false)
   })
 })
