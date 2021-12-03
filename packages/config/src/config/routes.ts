dd3a0b5bc38bcdb0b67ac55e24d3fe7b34e54f19
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2021-12-03 13:31:43 +0600

    bootstraped new config microservice

---
+++ packages/config/src/config/routes.ts
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+import applicationConfigHandler from '@config/handlers/applicationConfigHandler'
+
+export default function getRoutes() {
+  return [
+    // add ping route by default for health check
+    {
+      method: 'GET',
+      path: '/ping',
+      handler: (request: any, h: any) => {
+        // Perform any health checks and return true or false for success prop
+        return {
+          success: true
+        }
+      },
+      config: {
+        auth: false,
+        tags: ['api'],
+        description: 'Health check endpoint'
+      }
+    },
+    {
+      method: 'GET',
+      path: '/getConfig',
+      handler: applicationConfigHandler,
+      config: {
+        auth: false,
+        tags: ['api'],
+        description: 'Retrieve Application configuration'
+      }
+    }
+  ]
+}
