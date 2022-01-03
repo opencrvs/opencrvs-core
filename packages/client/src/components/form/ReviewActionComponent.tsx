479a91e952caee0cffb01521d6d0efd777dce64a
emon-swe-sust <mahfuzur03@student.sust.edu>
2022-01-03 13:52:35 +0600

    autoHeight introduced in when calling ResponsiveModal from ReviewActionComponent

--- packages/client/src/components/form/ReviewActionComponent.tsx
+++ packages/client/src/components/form/ReviewActionComponent.tsx
@@ -412,7 +412,7 @@ class ReviewActionComponent extends React.Component<
               event: application.event
             })}
             responsive={false}
-            contentHeight={115}
+            autoHeight={true}
             actions={[
               <TertiaryButton
                 id="cancel-btn"
