626ed613ac6afa485e55e3e834506d5185e2b268
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2022-01-04 18:27:34 +0600

    chaning build & test script attempt 28

--- infrastructure/build-and-test-e2e-sh.sh
+++ infrastructure/build-and-test-e2e-sh.sh
@@ -16,24 +16,25 @@ yarn dev:secrets:gen
 echo $ROOT_PASSWORD | sudo -S mkdir -p data/elasticsearch
 sudo chown -R 1000:1000 data/elasticsearch
 
-screen -d -m yarn dev en
-echo "wait-on tcp:4040" && wait-on -l tcp:4040 &&
-    echo "wait-on tcp:3030" && wait-on -l tcp:3030 &&
-    echo "wait-on tcp:2020" && wait-on -l tcp:2020 &&
-    echo "wait-on tcp:7070" && wait-on -l tcp:7070 &&
-    echo "wait-on tcp:5050" && wait-on -l tcp:5050 &&
-    echo "wait-on tcp:9090" && wait-on -l tcp:9090 &&
-    echo "wait-on tcp:1050" && wait-on -l tcp:1050 &&
-    echo "wait-on tcp:3447" && wait-on -l tcp:3447 &&
-    echo "wait-on tcp:5001" && wait-on -l tcp:5001 &&
-    echo "wait-on tcp:27017" && wait-on -l tcp:27017 &&
-    echo "wait-on tcp:6379" && wait-on -l tcp:6379 &&
-    echo "wait-on tcp:9200" && wait-on -l tcp:9200 &&
-    echo "wait-on http://localhost:9200" && wait-on -l http://localhost:9200 &&
-    echo "wait-on tcp:8086" && wait-on -l tcp:8086 &&
-    echo "wait-on http://localhost:8086/ping" && wait-on -l http://localhost:8086/ping &&
-    echo "wait-on http://localhost:3000" && wait-on -l http://localhost:3000 &&
-    echo "wait-on http://localhost:3020" && wait-on -l http://localhost:3020
+screen -d -m yarn compose:deps
+echo "wait-on tcp:3447" && wait-on -l tcp:3447
+echo "wait-on http://localhost:9200" && wait-on -l http://localhost:9200
+echo "wait-on tcp:5001" && wait-on -l tcp:5001
+echo "wait-on tcp:9200" && wait-on -l tcp:9200
+echo "wait-on tcp:27017" && wait-on -l tcp:27017
+echo "wait-on tcp:6379" && wait-on -l tcp:6379
+echo "wait-on tcp:8086" && wait-on -l tcp:8086
+
+screen -dm bash -c 'LANGUAGES=en yarn start'
+echo "wait-on tcp:4040" && wait-on -l tcp:4040
+echo "wait-on http://localhost:3000" && wait-on -l http://localhost:3000
+echo "wait-on http://localhost:3020" && wait-on -l http://localhost:3020
+echo "wait-on tcp:3030" && wait-on -l tcp:3030
+echo "wait-on tcp:2020" && wait-on -l tcp:2020
+echo "wait-on tcp:7070" && wait-on -l tcp:7070
+echo "wait-on tcp:5050" && wait-on -l tcp:5050
+echo "wait-on tcp:9090" && wait-on -l tcp:9090
+echo "wait-on tcp:1050" && wait-on -l tcp:1050
 
 # Setup metadata
 git clone -b ocrvs-2339 --depth=1 https://github.com/opencrvs/opencrvs-zambia.git
