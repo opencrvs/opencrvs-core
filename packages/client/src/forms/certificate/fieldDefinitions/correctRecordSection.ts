7bd101df469f73dee2518fce63d05f085693c57a
soumita-dsi <soumita.ghosh.dsi@gmail.com>
2022-01-12 00:21:21 +0600

    Fixed an import issue

--- packages/client/src/forms/certificate/fieldDefinitions/correctRecordSection.ts
+++ packages/client/src/forms/certificate/fieldDefinitions/correctRecordSection.ts
@@ -9,7 +9,7 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-import { RadioSize } from '@client/../../components/lib/forms'
+import { RadioSize } from '@opencrvs/components/lib/forms'
 import {
   CertificateSection,
   IFormSection,
