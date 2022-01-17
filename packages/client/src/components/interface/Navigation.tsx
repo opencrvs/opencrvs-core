2b6d9bce6bcb4c88a2812a4c27a8cf6640b38e58
emon-swe-sust <mahfuzur03@student.sust.edu>
2022-01-17 12:19:57 +0600

    scope access modified and id added on button

--- packages/client/src/components/interface/Navigation.tsx
+++ packages/client/src/components/interface/Navigation.tsx
@@ -38,12 +38,67 @@ import { getJurisdictionLocationIdFromUserDetails } from '@client/views/SysAdmin
 import { injectIntl, WrappedComponentProps as IntlShapeProps } from 'react-intl'
 import { constantsMessages } from '@client/i18n/messages'
 
-const USER_SCOPE = {
-  FIELD_AGENT: 'FIELD_AGENT',
-  REGISTRATION_AGENT: 'REGISTRATION_AGENT',
-  LOCAL_REGISTRAR: 'LOCAL_REGISTRAR',
-  LOCAL_SYSTEM_ADMIN: 'LOCAL_SYSTEM_ADMIN',
-  NATIONAL_SYSTEM_ADMIN: 'NATIONAL_SYSTEM_ADMIN'
+const TAB_ID = {
+  inProgress: 'progress',
+  sentForReview: 'review',
+  requireUpdates: 'updates',
+  readyForReview: 'review',
+  sentForUpdates: 'updates',
+  sentForApproval: 'approvals',
+  readyToPrint: 'print',
+  externalValidation: 'waitingValidation',
+  application: 'application',
+  performance: 'performance',
+  team: 'team',
+  config: 'config',
+  certificates: 'certificates',
+  settings: 'settings'
+}
+
+const GROUP_ID = {
+  applicationGroup: 'applicationGroup',
+  menuGroup: 'menuGroup'
+}
+
+interface IUSER_SCOPE {
+  [key: string]: string[]
+}
+
+const USER_SCOPE: IUSER_SCOPE = {
+  FIELD_AGENT: [
+    TAB_ID.inProgress,
+    TAB_ID.sentForReview,
+    TAB_ID.requireUpdates,
+    GROUP_ID.applicationGroup
+  ],
+  REGISTRATION_AGENT: [
+    TAB_ID.inProgress,
+    TAB_ID.readyForReview,
+    TAB_ID.sentForUpdates,
+    TAB_ID.sentForApproval,
+    TAB_ID.readyToPrint,
+    TAB_ID.performance,
+    TAB_ID.team,
+    GROUP_ID.applicationGroup,
+    GROUP_ID.menuGroup
+  ],
+  LOCAL_REGISTRAR: [
+    TAB_ID.inProgress,
+    TAB_ID.readyForReview,
+    TAB_ID.sentForUpdates,
+    TAB_ID.readyToPrint,
+    TAB_ID.performance,
+    TAB_ID.team,
+    GROUP_ID.applicationGroup,
+    GROUP_ID.menuGroup
+  ],
+  LOCAL_SYSTEM_ADMIN: [TAB_ID.performance, TAB_ID.team, GROUP_ID.menuGroup],
+  NATIONAL_SYSTEM_ADMIN: [
+    TAB_ID.performance,
+    TAB_ID.team,
+    TAB_ID.config,
+    GROUP_ID.menuGroup
+  ]
 }
 
 interface ICount {
@@ -83,23 +138,6 @@ interface IStateProps {
 }
 
 type IFullProps = IProps & IStateProps & IDispatchProps & IntlShapeProps
-
-const TAB_ID = {
-  inProgress: 'progress',
-  sentForReview: 'review',
-  requireUpdates: 'updates',
-  readyForReview: 'review',
-  sentForUpdates: 'updates',
-  sentForApproval: 'approvals',
-  readyToPrint: 'print',
-  externalValidation: 'waitingValidation',
-  application: 'application',
-  performance: 'performance',
-  team: 'team',
-  config: 'config',
-  certificates: 'certificates',
-  settings: 'settings'
-}
 
 const TAB_LABEL = {
   inProgress: 'In Progress',
@@ -173,62 +211,89 @@ export const NavigationView = (props: IFullProps) => {
     <LeftNavigation
       applicationName={intl.formatMessage(constantsMessages.applicationName)}
     >
-      {(userDetails?.role === USER_SCOPE.FIELD_AGENT ||
-        userDetails?.role === USER_SCOPE.REGISTRATION_AGENT ||
-        userDetails?.role === USER_SCOPE.LOCAL_REGISTRAR) && (
-        <NavigationGroup>
-          <NavigationItem
-            icon={() => <LeftNavigationApplicationIcons />}
-            label={TAB_LABEL.inProgress}
-            count={count && count.inProgress}
-            isSelected={tabId === TAB_ID.inProgress}
-            onClick={() =>
-              userDetails?.role === USER_SCOPE.FIELD_AGENT
-                ? props.goToFieldAgentHomeTab(TAB_ID.inProgress)
-                : props.goToRegistrarHomeTab(TAB_ID.inProgress)
-            }
-          />
-          {userDetails?.role === USER_SCOPE.FIELD_AGENT && (
-            <NavigationItem
-              icon={() => <LeftNavigationApplicationIcons color={'orange'} />}
-              label={TAB_LABEL.sentForReview}
-              count={count && count.sentForReview}
-              isSelected={tabId === TAB_ID.sentForReview}
-              onClick={() => props.goToFieldAgentHomeTab(TAB_ID.sentForReview)}
-            />
-          )}
-          {userDetails?.role !== USER_SCOPE.FIELD_AGENT && (
-            <NavigationItem
-              icon={() => <LeftNavigationApplicationIcons color={'orange'} />}
-              label={TAB_LABEL.readyForReview}
-              count={count && count.readyForReview}
-              isSelected={tabId === TAB_ID.readyForReview}
-              onClick={() => props.goToRegistrarHomeTab(TAB_ID.readyForReview)}
-            />
-          )}
-          {userDetails?.role === USER_SCOPE.FIELD_AGENT && (
-            <NavigationItem
-              icon={() => <LeftNavigationApplicationIcons color={'red'} />}
-              label={TAB_LABEL.requiresUpdate}
-              count={count && count.requiresUpdate}
-              isSelected={tabId === TAB_ID.requireUpdates}
-              onClick={() => props.goToFieldAgentHomeTab(TAB_ID.requireUpdates)}
-            />
-          )}
-          {userDetails?.role !== USER_SCOPE.FIELD_AGENT && (
-            <>
-              <NavigationItem
-                icon={() => <LeftNavigationApplicationIcons color={'red'} />}
-                label={TAB_LABEL.sentForUpdates}
-                count={count && count.sentForUpdates}
-                isSelected={tabId === TAB_ID.sentForUpdates}
-                onClick={() =>
-                  props.goToRegistrarHomeTab(TAB_ID.sentForUpdates)
-                }
-              />
-              {userDetails?.role === USER_SCOPE.REGISTRATION_AGENT && (
+      {userDetails?.role &&
+        USER_SCOPE[userDetails.role].includes(GROUP_ID.applicationGroup) && (
+          <NavigationGroup>
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.inProgress) && (
+                <NavigationItem
+                  icon={() => <LeftNavigationApplicationIcons />}
+                  id={`navigation_${TAB_ID.inProgress}`}
+                  label={TAB_LABEL.inProgress}
+                  count={count && count.inProgress}
+                  isSelected={tabId === TAB_ID.inProgress}
+                  onClick={() =>
+                    userDetails?.role === 'FIELD_AGENT'
+                      ? props.goToFieldAgentHomeTab(TAB_ID.inProgress)
+                      : props.goToRegistrarHomeTab(TAB_ID.inProgress)
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              userDetails?.role === 'FIELD_AGENT' &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.sentForReview) && (
+                <NavigationItem
+                  icon={() => (
+                    <LeftNavigationApplicationIcons color={'orange'} />
+                  )}
+                  id={`navigation_${TAB_ID.sentForReview}`}
+                  label={TAB_LABEL.sentForReview}
+                  count={count && count.sentForReview}
+                  isSelected={tabId === TAB_ID.sentForReview}
+                  onClick={() =>
+                    props.goToFieldAgentHomeTab(TAB_ID.sentForReview)
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              userDetails?.role !== 'FIELD_AGENT' &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.readyForReview) && (
+                <NavigationItem
+                  icon={() => (
+                    <LeftNavigationApplicationIcons color={'orange'} />
+                  )}
+                  id={`navigation_${TAB_ID.readyForReview}`}
+                  label={TAB_LABEL.readyForReview}
+                  count={count && count.readyForReview}
+                  isSelected={tabId === TAB_ID.readyForReview}
+                  onClick={() =>
+                    props.goToRegistrarHomeTab(TAB_ID.readyForReview)
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              userDetails?.role === 'FIELD_AGENT' &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.requireUpdates) && (
+                <NavigationItem
+                  icon={() => <LeftNavigationApplicationIcons color={'red'} />}
+                  id={`navigation_${TAB_ID.requireUpdates}`}
+                  label={TAB_LABEL.requiresUpdate}
+                  count={count && count.requiresUpdate}
+                  isSelected={tabId === TAB_ID.requireUpdates}
+                  onClick={() =>
+                    props.goToFieldAgentHomeTab(TAB_ID.requireUpdates)
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              userDetails?.role !== 'FIELD_AGENT' &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.sentForUpdates) && (
+                <NavigationItem
+                  icon={() => <LeftNavigationApplicationIcons color={'red'} />}
+                  id={`navigation_${TAB_ID.sentForUpdates}`}
+                  label={TAB_LABEL.sentForUpdates}
+                  count={count && count.sentForUpdates}
+                  isSelected={tabId === TAB_ID.sentForUpdates}
+                  onClick={() =>
+                    props.goToRegistrarHomeTab(TAB_ID.sentForUpdates)
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.sentForApproval) && (
                 <NavigationItem
                   icon={() => <LeftNavigationApplicationIcons color={'grey'} />}
+                  id={`navigation_${TAB_ID.sentForApproval}`}
                   label={TAB_LABEL.sentForApproval}
                   count={count && count.sentForApproval}
                   isSelected={tabId === TAB_ID.sentForApproval}
@@ -237,84 +302,111 @@ export const NavigationView = (props: IFullProps) => {
                   }
                 />
               )}
-              {window.config.EXTERNAL_VALIDATION_WORKQUEUE && (
+            {window.config.EXTERNAL_VALIDATION_WORKQUEUE && (
+              <NavigationItem
+                icon={() => <LeftNavigationApplicationIcons color={'teal'} />}
+                id={`navigation_${TAB_ID.externalValidation}`}
+                label={TAB_LABEL.externalValidation}
+                count={count && count.externalValidation}
+                isSelected={tabId === TAB_ID.externalValidation}
+                onClick={() =>
+                  props.goToRegistrarHomeTab(TAB_ID.externalValidation)
+                }
+              />
+            )}
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.readyToPrint) && (
                 <NavigationItem
-                  icon={() => <LeftNavigationApplicationIcons color={'teal'} />}
-                  label={TAB_LABEL.externalValidation}
-                  count={count && count.externalValidation}
-                  isSelected={tabId === TAB_ID.externalValidation}
+                  icon={() => (
+                    <LeftNavigationApplicationIcons color={'green'} />
+                  )}
+                  id={`navigation_${TAB_ID.readyToPrint}`}
+                  label={TAB_LABEL.readyToPrint}
+                  count={count && count.readyToPrint}
+                  isSelected={tabId === TAB_ID.readyToPrint}
                   onClick={() =>
-                    props.goToRegistrarHomeTab(TAB_ID.externalValidation)
+                    props.goToRegistrarHomeTab(TAB_ID.readyToPrint)
                   }
                 />
               )}
-              <NavigationItem
-                icon={() => <LeftNavigationApplicationIcons color={'green'} />}
-                label={TAB_LABEL.readyToPrint}
-                count={count && count.readyToPrint}
-                isSelected={tabId === TAB_ID.readyToPrint}
-                onClick={() => props.goToRegistrarHomeTab(TAB_ID.readyToPrint)}
-              />
-            </>
-          )}
-        </NavigationGroup>
-      )}
-      {userDetails?.role !== USER_SCOPE.FIELD_AGENT && (
-        <NavigationGroup>
-          <NavigationItem
-            icon={() => <Activity stroke={'#595C5F'} height={15} width={15} />}
-            label={TAB_LABEL.performance}
-            onClick={() => goToPerformanceView(props)}
-            isSelected={
-              enableMenuSelection && activeMenuItem === TAB_ID.performance
-            }
-          />
-          <NavigationItem
-            icon={() => <Users stroke={'#595C5F'} height={15} width={15} />}
-            label={TAB_LABEL.team}
-            onClick={() => goToTeamView(props)}
-            isSelected={enableMenuSelection && activeMenuItem === TAB_ID.team}
-          />
-          {userDetails?.role === USER_SCOPE.NATIONAL_SYSTEM_ADMIN && (
-            <>
-              <NavigationItem
-                icon={() => <Configuration />}
-                label={TAB_LABEL.configuration}
-                onClick={() => setIsConfigExpanded(!isConfigExpanded)}
-                isSelected={
-                  enableMenuSelection && activeMenuItem === TAB_ID.config
-                }
-                expandableIcon={() =>
-                  isConfigExpanded || activeMenuItem === TAB_ID.config ? (
-                    <Expandable selected={true} />
-                  ) : (
-                    <Expandable />
-                  )
-                }
-              />
-              {(isConfigExpanded || activeMenuItem === TAB_ID.config) && (
+          </NavigationGroup>
+        )}
+      {userDetails?.role &&
+        USER_SCOPE[userDetails.role].includes(GROUP_ID.menuGroup) && (
+          <NavigationGroup>
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.performance) && (
+                <NavigationItem
+                  icon={() => (
+                    <Activity stroke={'#595C5F'} height={15} width={15} />
+                  )}
+                  id={`navigation_${TAB_ID.performance}`}
+                  label={TAB_LABEL.performance}
+                  onClick={() => goToPerformanceView(props)}
+                  isSelected={
+                    enableMenuSelection && activeMenuItem === TAB_ID.performance
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.team) && (
+                <NavigationItem
+                  icon={() => (
+                    <Users stroke={'#595C5F'} height={15} width={15} />
+                  )}
+                  id={`navigation_${TAB_ID.team}`}
+                  label={TAB_LABEL.team}
+                  onClick={() => goToTeamView(props)}
+                  isSelected={
+                    enableMenuSelection && activeMenuItem === TAB_ID.team
+                  }
+                />
+              )}
+            {userDetails?.role &&
+              USER_SCOPE[userDetails.role].includes(TAB_ID.config) && (
                 <>
-                  <NavigationSubItem
-                    label={TAB_LABEL.certificatesConfiguration}
-                    onClick={goToConfigAction}
+                  <NavigationItem
+                    icon={() => <Configuration />}
+                    id={`navigation_${TAB_ID.config}_main`}
+                    label={TAB_LABEL.configuration}
+                    onClick={() => setIsConfigExpanded(!isConfigExpanded)}
                     isSelected={
                       enableMenuSelection && activeMenuItem === TAB_ID.config
                     }
-                  />
-                  <NavigationSubItem
-                    label={TAB_LABEL.applicationSettings}
-                    onClick={() => {}}
-                    isSelected={
-                      enableMenuSelection &&
-                      activeMenuItem === TAB_ID.application
+                    expandableIcon={() =>
+                      isConfigExpanded || activeMenuItem === TAB_ID.config ? (
+                        <Expandable selected={true} />
+                      ) : (
+                        <Expandable />
+                      )
                     }
                   />
+                  {(isConfigExpanded || activeMenuItem === TAB_ID.config) && (
+                    <>
+                      <NavigationSubItem
+                        label={TAB_LABEL.certificatesConfiguration}
+                        id={`navigation_${TAB_ID.certificates}`}
+                        onClick={goToConfigAction}
+                        isSelected={
+                          enableMenuSelection &&
+                          activeMenuItem === TAB_ID.config
+                        }
+                      />
+                      <NavigationSubItem
+                        label={TAB_LABEL.applicationSettings}
+                        id={`navigation_${TAB_ID.application}`}
+                        onClick={() => {}}
+                        isSelected={
+                          enableMenuSelection &&
+                          activeMenuItem === TAB_ID.application
+                        }
+                      />
+                    </>
+                  )}
                 </>
               )}
-            </>
-          )}
-        </NavigationGroup>
-      )}
+          </NavigationGroup>
+        )}
     </LeftNavigation>
   )
 }
