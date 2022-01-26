bf40b64a001597a0a932a2d1bfec9a9d33351036
Sadman Anik <sadman.anik@dsinnovators.com>
2022-01-26 15:02:37 +0600

    Modified story for Content Component

--- packages/components/src/components/interface/Content.stories.tsx
+++ packages/components/src/components/interface/Content.stories.tsx
@@ -20,7 +20,7 @@ interface IProps {
   icon?: () => React.ReactNode
   title?: string
   topActionButtons?: ReactElement[]
-  supportingCopy?: string
+  subtitle?: string
   children?: React.ReactNode
   bottomActionButtons?: ReactElement[]
   maxWidth?: '778px' | '1140px'
@@ -47,7 +47,7 @@ ContentView.args = {
     </SecondaryButton>,
     <LinkButton key="2">Click</LinkButton>
   ],
-  supportingCopy:
+  subtitle:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   bottomActionButtons: [
     <PrimaryButton
