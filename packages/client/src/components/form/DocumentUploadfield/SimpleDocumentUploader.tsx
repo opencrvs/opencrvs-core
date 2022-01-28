3ca10546860fdd353333af31dce46949c4bbdabb
Sadman Anik <sadman.anik@dsinnovators.com>
2022-01-28 19:49:24 +0600

    Preview certificate template

--- packages/client/src/components/form/DocumentUploadfield/SimpleDocumentUploader.tsx
+++ packages/client/src/components/form/DocumentUploadfield/SimpleDocumentUploader.tsx
@@ -112,6 +112,7 @@ class SimpleDocumentUploaderComponent extends React.Component<
   }
 
   selectForPreview = (previewImage: IFormFieldValue) => {
+    console.log('previewImage', previewImage)
     this.setState({ previewImage: previewImage as IAttachmentValue })
   }
 
