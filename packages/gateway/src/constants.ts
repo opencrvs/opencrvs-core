e4b7ed986ea668181112787a75314e2b37a0f64b
Sadman Anik <sadman.anik@dsinnovators.com>
2022-01-31 16:40:46 +0600

    Added private.key in docker-compose.dev

--- packages/gateway/src/constants.ts
+++ packages/gateway/src/constants.ts
@@ -13,13 +13,12 @@ export const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
 export const HOST = process.env.HOST || '0.0.0.0'
 export const PORT = process.env.PORT || 7070
 export const FHIR_URL = process.env.FHIR_URL || 'http://localhost:5001/fhir'
-export const CERT_PUBLIC_KEY_PATH =
-  (process.env.CERT_PUBLIC_KEY_PATH as string) ||
-  '../../.secrets/public-key.pem'
 export const CERT_PRIVATE_KEY_PATH =
   (process.env.CERT_PRIVATE_KEY_PATH as string) ||
   '../../.secrets/private-key.pem'
-
+export const CERT_PUBLIC_KEY_PATH =
+  (process.env.CERT_PUBLIC_KEY_PATH as string) ||
+  '../../.secrets/public-key.pem'
 // Services
 export const SEARCH_URL = process.env.SEARCH_URL || 'http://localhost:9090/'
 export const METRICS_URL = process.env.METRICS_URL || 'http://localhost:1050'
@@ -49,7 +48,7 @@ export function getLanguages() {
 
 export const NATIVE_LANGUAGE = (() => {
   const languages = getLanguages()
-  return languages.find(language => language !== 'en')
+  return languages.find((language) => language !== 'en')
 })()
 
 export const CONFIG_TOKEN_EXPIRY_SECONDS = process.env
