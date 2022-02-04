f1792d506ce4fa0b8ff1a5a30a266a30bdc71bb9
Riku Rouvila <riku.rouvila@gmail.com>
2022-02-04 12:03:42 +0200

    fix linting error

--- packages/workflow/src/index.ts
+++ packages/workflow/src/index.ts
@@ -11,6 +11,7 @@
  */
 // tslint:disable-next-line no-var-requires
 require('app-module-path').addPath(require('path').join(__dirname, '../'))
+// tslint:disable-next-line no-var-requires
 require('dotenv').config({
   path: `${process.cwd()}/.env`
 })
@@ -80,5 +81,5 @@ export async function createServer() {
 }
 
 if (require.main === module) {
-  createServer().then(server => server.start())
+  createServer().then((server) => server.start())
 }
