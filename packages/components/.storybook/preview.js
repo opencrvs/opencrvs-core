8dac26f1a904e0d141e8c0311fea1b5213eacbb2
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-03-07 14:02:40 +0600

    Added fonts in storybook
    notosans-regular and notosans-semibold were added.
    
    Co-authored-by: emon-swe-sust <mahfuzur03@student.sust.edu>

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -28,7 +28,7 @@ body,
   color: ${({ theme }) => theme.colors.neutralD};
   overflow-x: hidden;
   *:not(i) {
-    font-family: Ubuntu, sans-serif !important;
+    font-family: Ubuntu, sans-serif;
   }
   .page-content {
     position: relative;
@@ -36,6 +36,18 @@ body,
     .table-container {
       height: 100%;
     }
+  }
+
+  @font-face {
+    /* stylelint-disable-next-line opencrvs/no-font-styles */
+    font-family: ${({ theme }) => theme.fonts.semiBoldFont};
+    src: url('/fonts/notosans-semibold-webfont-en.ttf') format('truetype');
+  }
+
+  @font-face {
+    /* stylelint-disable-next-line opencrvs/no-font-styles */
+    font-family: ${({ theme }) => theme.fonts.regularFont};
+    src: url('/fonts/notosans-regular-webfont-en.ttf') format('truetype');
   }
 }
 body,
