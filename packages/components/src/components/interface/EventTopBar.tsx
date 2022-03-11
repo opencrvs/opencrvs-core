ee194d6e5c297538ffc77c52d0fee35535cf1646
jpye-finch <jonathan@pye-finch.co.uk>
2022-03-11 15:09:15 +0000

    further fixes and remove of shadows from headers

--- packages/components/src/components/interface/EventTopBar.tsx
+++ packages/components/src/components/interface/EventTopBar.tsx
@@ -17,9 +17,9 @@ import { ToggleMenu } from '.'
 
 const TopBar = styled.div`
   padding: 0 ${({ theme }) => theme.grid.margin}px;
-  height: 64px;
+  height: 56px;
   background: ${({ theme }) => theme.colors.white};
-  ${({ theme }) => theme.shadows.light};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey300};
   display: flex;
   justify-content: space-between;
   align-items: center;
