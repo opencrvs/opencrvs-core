fa614bc6ea952d2b5137cb56529ddcf8c0432ce9
Riku Rouvila <riku.rouvila@gmail.com>
2022-04-18 11:04:56 +0300

    fix setup users helpers formatting

--- infrastructure/elasticsearch/setup-helpers.sh
+++ infrastructure/elasticsearch/setup-helpers.sh
@@ -12,191 +12,191 @@
 
 # Log a message.
 function log {
-	echo "[+] $1"
+  echo "[+] $1"
 }
 
 # Log a message at a sub-level.
 function sublog {
-	echo "   ⠿ $1"
+  echo "   ⠿ $1"
 }
 
 # Log an error.
 function err {
-	echo "[x] $1" >&2
+  echo "[x] $1" >&2
 }
 
 # Poll the 'elasticsearch' service until it responds with HTTP code 200.
 function wait_for_elasticsearch {
-	local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
+  local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
 
-	local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}' "http://${elasticsearch_host}:9200/" )
+  local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}' "http://${elasticsearch_host}:9200/" )
 
-	if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
-		args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
-	fi
+  if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
+    args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
+  fi
 
-	local -i result=1
-	local output
+  local -i result=1
+  local output
 
-	# retry for max 300s (60*5s)
-	for _ in $(seq 1 60); do
-		output="$(curl "${args[@]}" || true)"
-		if [[ "${output: -3}" -eq 200 ]]; then
-			result=0
-			break
-		fi
+  # retry for max 300s (60*5s)
+  for _ in $(seq 1 60); do
+    output="$(curl "${args[@]}" || true)"
+    if [[ "${output: -3}" -eq 200 ]]; then
+      result=0
+      break
+    fi
 
-		sleep 5
-	done
+    sleep 5
+  done
 
-	if ((result)); then
-		echo -e "\n${output::-3}"
-	fi
+  if ((result)); then
+    echo -e "\n${output::-3}"
+  fi
 
-	return $result
+  return $result
 }
 
 # Verify that the given Elasticsearch user exists.
 function check_user_exists {
-	local username=$1
+  local username=$1
 
-	local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
+  local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
 
-	local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
-		"http://${elasticsearch_host}:9200/_security/user/${username}"
-		)
+  local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
+    "http://${elasticsearch_host}:9200/_security/user/${username}"
+    )
 
-	if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
-		args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
-	fi
+  if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
+    args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
+  fi
 
-	local -i result=1
-	local -i exists=0
-	local output
+  local -i result=1
+  local -i exists=0
+  local output
 
-	output="$(curl "${args[@]}")"
-	if [[ "${output: -3}" -eq 200 || "${output: -3}" -eq 404 ]]; then
-		result=0
-	fi
-	if [[ "${output: -3}" -eq 200 ]]; then
-		exists=1
-	fi
+  output="$(curl "${args[@]}")"
+  if [[ "${output: -3}" -eq 200 || "${output: -3}" -eq 404 ]]; then
+    result=0
+  fi
+  if [[ "${output: -3}" -eq 200 ]]; then
+    exists=1
+  fi
 
-	if ((result)); then
-		echo -e "\n${output::-3}"
-	else
-		echo "$exists"
-	fi
+  if ((result)); then
+    echo -e "\n${output::-3}"
+  else
+    echo "$exists"
+  fi
 
-	return $result
+  return $result
 }
 
 # Set password of a given Elasticsearch user.
 function update_user {
-	local username=$1
-	local password=$2
-	local role=${3:-}
+  local username=$1
+  local password=$2
+  local role=${3:-}
 
-	local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
+  local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
 
-	if [[ -n "${role:-}" ]]; then
-		local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
-			"http://${elasticsearch_host}:9200/_security/user/${username}"
-			'-X' 'PUT'
-			'-H' 'Content-Type: application/json'
-			'-d' "{\"password\" : \"${password}\",\"roles\":[\"${role}\"]}}"
-		)
-	else
-		local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
-			"http://${elasticsearch_host}:9200/_security/user/${username}/_password"
-			'-X' 'POST'
-			'-H' 'Content-Type: application/json'
-			'-d' "{\"password\" : \"${password}\"}"
-		)
-	fi
+  if [[ -n "${role:-}" ]]; then
+    local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
+      "http://${elasticsearch_host}:9200/_security/user/${username}"
+      '-X' 'PUT'
+      '-H' 'Content-Type: application/json'
+      '-d' "{\"password\" : \"${password}\",\"roles\":[\"${role}\"]}}"
+    )
+  else
+    local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
+      "http://${elasticsearch_host}:9200/_security/user/${username}/_password"
+      '-X' 'POST'
+      '-H' 'Content-Type: application/json'
+      '-d' "{\"password\" : \"${password}\"}"
+    )
+  fi
 
-	if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
-		args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
-	fi
+  if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
+    args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
+  fi
 
-	local -i result=1
-	local output
+  local -i result=1
+  local output
 
-	output="$(curl "${args[@]}")"
-	if [[ "${output: -3}" -eq 200 ]]; then
-		result=0
-	fi
+  output="$(curl "${args[@]}")"
+  if [[ "${output: -3}" -eq 200 ]]; then
+    result=0
+  fi
 
-	if ((result)); then
-		echo -e "\n${output::-3}\n"
-	fi
+  if ((result)); then
+    echo -e "\n${output::-3}\n"
+  fi
 
-	return $result
+  return $result
 }
 
 # Create the given Elasticsearch user.
 function create_user {
-	local username=$1
-	local password=$2
-	local role=$3
+  local username=$1
+  local password=$2
+  local role=$3
 
-	local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
+  local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
 
-	local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
-		"http://${elasticsearch_host}:9200/_security/user/${username}"
-		'-X' 'POST'
-		'-H' 'Content-Type: application/json'
-		'-d' "{\"password\":\"${password}\",\"roles\":[\"${role}\"]}"
-		)
+  local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
+    "http://${elasticsearch_host}:9200/_security/user/${username}"
+    '-X' 'POST'
+    '-H' 'Content-Type: application/json'
+    '-d' "{\"password\":\"${password}\",\"roles\":[\"${role}\"]}"
+    )
 
-	if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
-		args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
-	fi
+  if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
+    args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
+  fi
 
-	local -i result=1
-	local output
+  local -i result=1
+  local output
 
-	output="$(curl "${args[@]}")"
-	if [[ "${output: -3}" -eq 200 ]]; then
-		result=0
-	fi
+  output="$(curl "${args[@]}")"
+  if [[ "${output: -3}" -eq 200 ]]; then
+    result=0
+  fi
 
-	if ((result)); then
-		echo -e "\n${output::-3}\n"
-	fi
+  if ((result)); then
+    echo -e "\n${output::-3}\n"
+  fi
 
-	return $result
+  return $result
 }
 
 # Ensure that the given Elasticsearch role is up-to-date, create it if required.
 function ensure_role {
-	local name=$1
-	local body=$2
+  local name=$1
+  local body=$2
 
-	local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
+  local elasticsearch_host="${ELASTICSEARCH_HOST:-elasticsearch}"
 
-	local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
-		"http://${elasticsearch_host}:9200/_security/role/${name}"
-		'-X' 'PUT'
-		'-H' 'Content-Type: application/json'
-		'-d' "$body"
-		)
+  local -a args=( '-s' '-D-' '-m15' '-w' '%{http_code}'
+    "http://${elasticsearch_host}:9200/_security/role/${name}"
+    '-X' 'PUT'
+    '-H' 'Content-Type: application/json'
+    '-d' "$body"
+    )
 
-	if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
-		args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
-	fi
+  if [[ -n "${ELASTIC_PASSWORD:-}" ]]; then
+    args+=( '-u' "elastic:${ELASTIC_PASSWORD}" )
+  fi
 
-	local -i result=1
-	local output
+  local -i result=1
+  local output
 
-	output="$(curl "${args[@]}")"
-	if [[ "${output: -3}" -eq 200 ]]; then
-		result=0
-	fi
+  output="$(curl "${args[@]}")"
+  if [[ "${output: -3}" -eq 200 ]]; then
+    result=0
+  fi
 
-	if ((result)); then
-		echo -e "\n${output::-3}\n"
-	fi
+  if ((result)); then
+    echo -e "\n${output::-3}\n"
+  fi
 
-	return $result
+  return $result
 }
--- infrastructure/elasticsearch/setup-users.sh
+++ infrastructure/elasticsearch/setup-users.sh
@@ -25,10 +25,10 @@ source "$(dirname "${BASH_SOURCE[0]}")/setup-helpers.sh"
 
 declare -A users_passwords
 users_passwords=(
-	[$SEARCH_ELASTIC_USERNAME]="${SEARCH_ELASTIC_PASSWORD:-}"
-	[beats_system]="${METRICBEAT_ELASTIC_PASSWORD:-}"
-	[apm_system]="${APM_ELASTIC_PASSWORD:-}"
-	[$KIBANA_USERNAME]="${KIBANA_PASSWORD:-}"
+  [$SEARCH_ELASTIC_USERNAME]="${SEARCH_ELASTIC_PASSWORD:-}"
+  [beats_system]="${METRICBEAT_ELASTIC_PASSWORD:-}"
+  [apm_system]="${APM_ELASTIC_PASSWORD:-}"
+  [$KIBANA_USERNAME]="${KIBANA_PASSWORD:-}"
 )
 
 # -------------------------------------
@@ -36,8 +36,8 @@ users_passwords=(
 # If you are adding a new user, remember to assign a role. You can use search_user as a template.
 declare -A users_roles
 users_roles=(
-	[$SEARCH_ELASTIC_USERNAME]='search_user'
-	[$KIBANA_USERNAME]='kibana_admin_user'
+  [$SEARCH_ELASTIC_USERNAME]='search_user'
+  [$KIBANA_USERNAME]='kibana_admin_user'
 )
 
 # --------------------------------------------------------
@@ -45,8 +45,8 @@ users_roles=(
 
 declare -A roles_files
 roles_files=(
-	[search_user]='search_user.json'
-	[kibana_admin_user]='kibana_user.json'
+  [search_user]='search_user.json'
+  [kibana_admin_user]='kibana_user.json'
 )
 
 # --------------------------------------------------------
@@ -59,44 +59,44 @@ wait_for_elasticsearch
 sublog 'Elasticsearch is running'
 
 for role in "${!roles_files[@]}"; do
-	log "Role '$role'"
+  log "Role '$role'"
 
-	declare body_file
-	body_file="$(dirname "${BASH_SOURCE[0]}")/roles/${roles_files[$role]:-}"
-	if [[ ! -f "${body_file:-}" ]]; then
-		sublog "No role body found at '${body_file}', skipping"
-		continue
-	fi
+  declare body_file
+  body_file="$(dirname "${BASH_SOURCE[0]}")/roles/${roles_files[$role]:-}"
+  if [[ ! -f "${body_file:-}" ]]; then
+    sublog "No role body found at '${body_file}', skipping"
+    continue
+  fi
 
-	sublog 'Creating/updating'
-	ensure_role "$role" "$(<"${body_file}")"
+  sublog 'Creating/updating'
+  ensure_role "$role" "$(<"${body_file}")"
 done
 
 for user in "${!users_passwords[@]}"; do
-	log "User '$user'"
-	if [[ -z "${users_passwords[$user]:-}" ]]; then
-		sublog 'No password defined, skipping'
-		continue
-	fi
+  log "User '$user'"
+  if [[ -z "${users_passwords[$user]:-}" ]]; then
+    sublog 'No password defined, skipping'
+    continue
+  fi
 
-	declare -i user_exists=0
-	user_exists="$(check_user_exists "$user")"
+  declare -i user_exists=0
+  user_exists="$(check_user_exists "$user")"
 
-	if ((user_exists)); then
-		sublog 'User exists, updating user'
+  if ((user_exists)); then
+    sublog 'User exists, updating user'
 
-		if [[ -z "${users_roles[$user]:-}" ]]; then
-			update_user "$user" "${users_passwords[$user]}"
-		else
-			update_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
-		fi
-	else
-		if [[ -z "${users_roles[$user]:-}" ]]; then
-			err '  No role defined, skipping creation'
-			continue
-		fi
+    if [[ -z "${users_roles[$user]:-}" ]]; then
+      update_user "$user" "${users_passwords[$user]}"
+    else
+      update_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
+    fi
+  else
+    if [[ -z "${users_roles[$user]:-}" ]]; then
+      err '  No role defined, skipping creation'
+      continue
+    fi
 
-		sublog 'User does not exist, creating'
-		create_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
-	fi
+    sublog 'User does not exist, creating'
+    create_user "$user" "${users_passwords[$user]}" "${users_roles[$user]}"
+  fi
 done
