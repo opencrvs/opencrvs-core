3dff323651e6ee6019168ccb98ba8924f6b54894
Riku Rouvila <riku.rouvila@gmail.com>
2022-05-25 15:14:37 +0300

    update and remove tests
    jest wasnt waiting for a test in registration/root-resolvers and it was silently failing

--- packages/gateway/src/features/registration/root-resolvers.test.ts
+++ packages/gateway/src/features/registration/root-resolvers.test.ts
@@ -1293,7 +1293,7 @@ describe('Registration root resolvers', () => {
       )
     })
 
-    it('throws error if no task entry found by given id', async () => {
+    it('throws error if no task entry found by given id', () => {
       const compositionID = 'cd168e0b-0817-4880-a67f-35de777460a5'
       fetch.mockResponseOnce(
         JSON.stringify({
@@ -1313,7 +1313,7 @@ describe('Registration root resolvers', () => {
           entry: []
         })
       )
-      expect(
+      return expect(
         resolvers.Mutation.markBirthAsValidated(
           {},
           { id: compositionID },
@@ -1658,7 +1658,7 @@ describe('Registration root resolvers', () => {
           entry: []
         })
       )
-      expect(
+      return expect(
         resolvers.Mutation.markDeathAsValidated(
           {},
           { id: compositionID },
@@ -1797,34 +1797,6 @@ describe('Registration root resolvers', () => {
         expect.any(String),
         expect.objectContaining({ method: 'POST' })
       )
-    })
-    it('throws error if no task entry found given id', async () => {
-      const compositionID = 'cd168e0b-0817-4880-a67f-35de777460a5'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          resourceType: 'Bundle',
-          id: 'd2ca298f-662f-4086-a8c5-697517a2b5a3',
-          meta: {
-            lastUpdated: '2018-12-13T04:02:42.003+00:00'
-          },
-          type: 'searchset',
-          total: 0,
-          link: [
-            {
-              relation: 'self',
-              url: 'http://localhost:3447/fhir/Task?focus=Composition/cd168e0b-0817-4880-a67f-35de777460a5s'
-            }
-          ],
-          entry: []
-        })
-      )
-      expect(
-        resolvers.Mutation.markBirthAsRegistered(
-          {},
-          { id: compositionID },
-          authHeaderRegCert
-        )
-      ).rejects.toThrowError('Task does not exist')
     })
 
     it("throws an error when the user doesn't have register scope", async () => {
