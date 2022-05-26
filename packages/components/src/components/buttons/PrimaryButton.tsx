cae61a7b2cd237d34d5e2ff748a21ffb85a2b58e
jpye-finch <jonathan@pye-finch.co.uk>
2022-05-26 11:42:47 +0100

    removed no cursor allowed

--- packages/components/src/components/buttons/PrimaryButton.tsx
+++ packages/components/src/components/buttons/PrimaryButton.tsx
@@ -40,7 +40,6 @@ export const PrimaryButton = styled(Button)`
   }
 
   &:not([data-focus-visible-added]):disabled {
-    cursor: not-allowed;
     background: ${({ theme }) => theme.colors.grey300};
     color: ${({ theme }) => theme.colors.white};
     path {
