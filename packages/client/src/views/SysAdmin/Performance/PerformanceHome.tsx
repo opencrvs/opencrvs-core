0ba0f185d279e24b3dc221a03fb231fed2c0a7b7
Sadman Anik <sadman.anik@dsinnovators.com>
2022-06-02 17:54:40 +0600

    Fixed performance offline to online state issue

--- packages/client/src/views/SysAdmin/Performance/PerformanceHome.tsx
+++ packages/client/src/views/SysAdmin/Performance/PerformanceHome.tsx
@@ -535,6 +535,8 @@ class PerformanceHomeComponent extends React.Component<Props, State> {
                           />
                         )
                       }
+
+                      return <></>
                     }}
                   </Query>
                 </>
