88ac325b3ade961eca79e674f5471fe9ffb0e24d
Mahfuzur Rahman Emon <mahfuzur.rahman@dsinnovators.com>
2022-06-03 16:24:56 +0600

    loading in search made same as workqueue

--- packages/client/src/views/SearchResult/SearchResult.tsx
+++ packages/client/src/views/SearchResult/SearchResult.tsx
@@ -54,7 +54,6 @@ import {
   ColumnContentAlignment,
   GridTable,
   IAction,
-  Loader,
   COLUMNS
 } from '@opencrvs/components/lib/interface'
 
@@ -75,6 +74,7 @@ import {
 } from '@client/views/OfficeHome/components'
 import { WQContentWrapper } from '@client/views/OfficeHome/WQContentWrapper'
 import { Downloaded } from '@opencrvs/components/lib/icons/Downloaded'
+import { LoadingIndicator } from '@client/views/OfficeHome/LoadingIndicator'
 
 const ErrorText = styled.div`
   color: ${({ theme }) => theme.colors.negative};
@@ -443,31 +443,10 @@ export class SearchResultView extends React.Component<
                 fetchPolicy="no-cache"
               >
                 {({ loading, error, data }) => {
-                  if (loading) {
-                    return (
-                      <Loader
-                        id="search_loader"
-                        marginPercent={35}
-                        loadingText={intl.formatMessage(messages.searchingFor, {
-                          param: searchText
-                        })}
-                      />
-                    )
-                  }
+                  const total = loading
+                    ? -1
+                    : data?.searchEvents?.results?.length || 0
 
-                  if (error || !data?.searchEvents) {
-                    return (
-                      <ErrorText id="search-result-error-text">
-                        {intl.formatMessage(errorMessages.queryError)}
-                      </ErrorText>
-                    )
-                  }
-
-                  const total =
-                    (data.searchEvents &&
-                      data.searchEvents.results &&
-                      data.searchEvents.results.length) ||
-                    0
                   return (
                     <WQContentWrapper
                       title={intl.formatMessage(messages.searchResultFor, {
@@ -478,26 +457,37 @@ export class SearchResultView extends React.Component<
                         this.state.width < this.props.theme.grid.breakpoints.lg
                       }
                     >
-                      {total > 0 && (
-                        <>
-                          <ReactTooltip id="validateTooltip">
-                            <ToolTipContainer>
-                              {this.props.intl.formatMessage(
-                                registrarHomeMessages.validatedDeclarationTooltipForRegistrar
+                      {loading ? (
+                        <div id="search_loader">
+                          <LoadingIndicator loading={true} />
+                        </div>
+                      ) : error ? (
+                        <ErrorText id="search-result-error-text">
+                          {intl.formatMessage(errorMessages.queryError)}
+                        </ErrorText>
+                      ) : (
+                        data?.searchEvents &&
+                        total > 0 && (
+                          <>
+                            <ReactTooltip id="validateTooltip">
+                              <ToolTipContainer>
+                                {this.props.intl.formatMessage(
+                                  registrarHomeMessages.validatedDeclarationTooltipForRegistrar
+                                )}
+                              </ToolTipContainer>
+                            </ReactTooltip>
+                            <GridTable
+                              content={this.transformSearchContent(
+                                data.searchEvents
                               )}
-                            </ToolTipContainer>
-                          </ReactTooltip>
-                          <GridTable
-                            content={this.transformSearchContent(
-                              data.searchEvents
-                            )}
-                            columns={this.getColumns()}
-                            noResultText={intl.formatMessage(
-                              constantsMessages.noResults
-                            )}
-                            hideLastBorder={true}
-                          />
-                        </>
+                              columns={this.getColumns()}
+                              noResultText={intl.formatMessage(
+                                constantsMessages.noResults
+                              )}
+                              hideLastBorder={true}
+                            />
+                          </>
+                        )
                       )}
                     </WQContentWrapper>
                   )
