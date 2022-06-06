1b2d51f65a31a4bfea1f76b504128114453de703
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2022-06-06 13:02:26 +0600

    Remove template mapping for registrar
    - This would prevent rendering of digital registrar signature regardless svg having handle

--- packages/client/src/forms/configuration/default.ts
+++ packages/client/src/forms/configuration/default.ts
@@ -574,10 +574,6 @@ export const registerForms: IDefaultRegisterForms = {
               operation: 'roleUserTransformer'
             },
             {
-              fieldName: 'registrarSignature',
-              operation: 'registrarSignatureUserTransformer'
-            },
-            {
               fieldName: 'registrationLocation',
               operation: 'registrationLocationUserTransformer'
             }
@@ -2947,10 +2943,6 @@ export const registerForms: IDefaultRegisterForms = {
             {
               fieldName: 'role',
               operation: 'roleUserTransformer'
-            },
-            {
-              fieldName: 'registrarSignature',
-              operation: 'registrarSignatureUserTransformer'
             },
             {
               fieldName: 'registrationLocation',
