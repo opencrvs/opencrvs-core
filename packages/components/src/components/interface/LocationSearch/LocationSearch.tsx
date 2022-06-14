838b29e31091382a0345e0e187312254bec29a48
Sadman Anik <sadman.anik@dsinnovators.com>
2022-06-14 17:33:43 +0600

    Fixed search button message

--- packages/components/src/components/interface/LocationSearch/LocationSearch.tsx
+++ packages/components/src/components/interface/LocationSearch/LocationSearch.tsx
@@ -111,6 +111,7 @@ interface IProps {
   error?: boolean
   touched?: boolean
   className?: string
+  buttonLabel: string
 }
 export class LocationSearch extends React.Component<IProps, IState> {
   searchTimeout: NodeJS.Timeout | undefined
@@ -288,7 +289,7 @@ export class LocationSearch extends React.Component<IProps, IState> {
               onClick={this.props.searchButtonHandler}
               disabled={!(this.state.selectedItem && this.state.selectedText)}
             >
-              Search
+              {this.props.buttonLabel}
             </SearchButton>
           )}
         </LocationSearchContainer>
