dde0dbf6844ba322797edeede74c44297a8c8add
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-06-21 15:53:35 +0600

    Changed Settings icon to use primary color

--- packages/components/src/components/icons/SettingsBlue.tsx
+++ packages/components/src/components/icons/SettingsBlue.tsx
@@ -10,14 +10,18 @@
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
 import * as React from 'react'
+import { colors } from '../colors'
 
-export const SettingsBlue = (props: React.HTMLAttributes<SVGElement>) => (
+export const SettingsBlue = ({
+  color = 'primary',
+  ...props
+}: { color?: keyof typeof colors } & React.SVGAttributes<SVGElement>) => (
   <svg width={24} height={24} fill="none" {...props}>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-      stroke="#5E93ED"
+      stroke={colors[color]}
       strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -26,7 +30,7 @@ export const SettingsBlue = (props: React.HTMLAttributes<SVGElement>) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-1.415 3.417 2 2 0 0 1-1.415-.587l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-3.417-1.415 2 2 0 0 1 .587-1.415l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9c.26.604.852.997 1.51 1H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"
-      stroke="#5E93ED"
+      stroke={colors[color]}
       strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
