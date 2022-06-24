ef95180c4261106cd409dc674abdb4b9ee32327b
euanmillar <euanmillar77@gmail.com>
2022-06-24 14:39:14 +0100

    Add cert link and rename SMS env vars

--- packages/notification/src/constants.ts
+++ packages/notification/src/constants.ts
@@ -41,8 +41,8 @@ export const INFOBIP_API_KEY = process.env.INFOBIP_API_KEY_PATH
   ? readFileSync(process.env.INFOBIP_API_KEY_PATH).toString()
   : ''
 
-export const INFOBIP_SENDER_ID = process.env.INFOBIP_SENDER_ID
-  ? readFileSync(process.env.INFOBIP_SENDER_ID).toString()
+export const INFOBIP_SENDER_ID = process.env.INFOBIP_SENDER_ID_PATH
+  ? readFileSync(process.env.INFOBIP_SENDER_ID_PATH).toString()
   : ''
 
 export const CERT_PUBLIC_KEY_PATH =
