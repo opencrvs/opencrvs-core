8d977895bf1bc36d3acaef38643b772d59aee675
Sadman Anik <sadman.anik@dsinnovators.com>
2022-07-12 13:21:49 +0600

    Replaced storybook public dir CLI option with main.js configuration

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -28,5 +28,6 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   core: { builder: '@storybook/builder-vite' },
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  staticDirs: ['../public'],
   framework: '@storybook/react'
 }
