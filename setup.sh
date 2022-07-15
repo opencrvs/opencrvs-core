43745fad23d92c996fabc0b5aab741db48723081
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-07-15 10:55:05 +0600

    Updated minimum node version in setup.sh

--- setup.sh
+++ setup.sh
@@ -219,12 +219,12 @@ do
         fi
         if [ $i == "node" ] ; then
             echo "You need to install Node, or if you did, we can't find it and perhaps it is not in your PATH. Please fix your node installation."
-            echo "We recommend you install Node v.14.15.0, v14.15.4, 14.17.0 or v14.18.1 as this release has been tested on those versions."
+            echo "We recommend you install Node 14.18.0 or v14.18.1 as this release has been tested on those versions."
             echo "There are various ways you can install Node.  The easiest way to get Node running with the version of your choice is using Node Version Manager."
             echo "Documentation is here: https://nodejs.org/en/download/package-manager/#nvm.  For example run:\033[0m"
             echo "curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash"
             echo "Then use nvm to install the Node version of choice.  For example run:\033[0m"
-            echo "nvm install 14.17.0"
+            echo "nvm install 14.18.0"
         fi
         if [ $i == "yarn" ] ; then
            echo "You need to install the Yarn Package Manager for Node."
@@ -293,7 +293,7 @@ if [ "$versionTest" == "LOWER" ] ; then
   echo "We recommend you install Node v14.18.0 or 14.18.1 as this release has been tested on those versions."
   echo "Documentation is here: https://nodejs.org/en/download/package-manager/#nvm"
   echo "Then use nvm to install the Node version of choice.  For example run:\033[0m"
-  echo "nvm install 14.17.0"
+  echo "nvm install 14.18.0"
   exit 1
   else
     echo -e "Your Node version: $myNodeVersion is \033[32msupported!\033[0m :)"
