f7b0498fc60aa3a638a8db1d9dbffd3f6172d0f8
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-07-21 13:08:00 +0600

    Used a custom plugin for html env

--- packages/login/vite.config.js
+++ packages/login/vite.config.js
@@ -9,24 +9,35 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginHtmlEnv from 'vite-plugin-html-env'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 process.env.VITE_APP_COUNTRY_CONFIG_URL =
   process.env.COUNTRY_CONFIG_URL || 'http://localhost:3040'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: 'build',
-    commonjsOptions: {
-      transformMixedEsModules: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, 'env')
+
+  const htmlPlugin = () => {
+    return {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/%(.*?)%/g, function (_, p1) {
+          return env[p1]
+        })
+      }
     }
-  },
-  plugins: [react(), tsconfigPaths(), VitePluginHtmlEnv()]
+  }
+  return {
+    // This changes the out put dir from dist to build
+    build: {
+      outDir: 'build',
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    plugins: [htmlPlugin(), react(), tsconfigPaths()]
+  }
 })
