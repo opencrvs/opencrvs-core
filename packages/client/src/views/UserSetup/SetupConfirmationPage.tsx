ba05384ee035deb893e12e464d4056f027606905
Sadman Anik <sadman.anik@dsinnovators.com>
2022-07-22 17:18:06 +0600

    Convert SetupConfirmationPage to functional component

--- packages/client/src/views/UserSetup/SetupConfirmationPage.tsx
+++ packages/client/src/views/UserSetup/SetupConfirmationPage.tsx
@@ -11,8 +11,8 @@
  */
 import * as React from 'react'
 import styled from 'styled-components'
-import { injectIntl, WrappedComponentProps as IntlShapeProps } from 'react-intl'
-import { connect } from 'react-redux'
+import { useIntl } from 'react-intl'
+import { useDispatch, useSelector } from 'react-redux'
 import { CountryLogo } from '@opencrvs/components/lib/icons'
 import { PrimaryButton } from '@opencrvs/components/lib/buttons'
 import { redirectToAuthentication } from '@client/profile/profileActions'
@@ -27,7 +27,6 @@ import { messages } from '@client/i18n/messages/views/userSetup'
 import { buttonMessages } from '@client/i18n/messages'
 import { IStoreState } from '@client/store'
 import { getOfflineData } from '@client/offline/selectors'
-import { IOfflineData } from '@client/offline/reducer'
 import {
   Content,
   ContentSize
@@ -45,56 +44,39 @@ const LoginButton = styled(PrimaryButton)`
   box-shadow: 0 0 13px 0 rgba(0, 0, 0, 0.27);
 `
 
-interface IStateData {
-  offlineCountryConfig: IOfflineData
-}
-
-export class SetupConfirmationView extends React.Component<
-  {
-    redirectToAuthentication: typeof redirectToAuthentication
-  } & IStateData &
-    IntlShapeProps
-> {
-  async componentDidMount() {
-    await storage.removeItem(USER_DETAILS)
-  }
-  render() {
-    const { intl, offlineCountryConfig } = this.props
-    return (
-      <Page>
-        <Container id="user-setup-complete-page">
-          <Content size={ContentSize.LARGE}>
-            <LogoContainer>
-              <CountryLogo
-                src={offlineCountryConfig.config.COUNTRY_LOGO.file}
-              />
-            </LogoContainer>
-            <TitleHolder>
-              {intl.formatMessage(messages.setupCompleteTitle)}
-            </TitleHolder>
-            <InstructionHolder>
-              {intl.formatMessage(messages.userSetupInstruction)}
-            </InstructionHolder>
-            <LoginButton
-              id="setup-login-button"
-              onClick={() => this.props.redirectToAuthentication()}
-            >
-              {intl.formatMessage(buttonMessages.login)}
-            </LoginButton>
-          </Content>
-        </Container>
-      </Page>
-    )
-  }
-}
-
-export const SetupConfirmationPage = connect(
-  (state: IStoreState) => {
-    return {
-      offlineCountryConfig: getOfflineData(state)
+export function SetupConfirmationPage() {
+  const intl = useIntl()
+  const dispatch = useDispatch()
+  const offlineCountryConfig = useSelector((store: IStoreState) =>
+    getOfflineData(store)
+  )
+  React.useEffect(() => {
+    const removeItemFromStorage = async () => {
+      await storage.removeItem(USER_DETAILS)
     }
-  },
-  {
-    redirectToAuthentication
-  }
-)(injectIntl(SetupConfirmationView))
+    removeItemFromStorage()
+  }, [])
+  return (
+    <Page>
+      <Container id="user-setup-complete-page">
+        <Content size={ContentSize.LARGE}>
+          <LogoContainer>
+            <CountryLogo src={offlineCountryConfig.config.COUNTRY_LOGO.file} />
+          </LogoContainer>
+          <TitleHolder>
+            {intl.formatMessage(messages.setupCompleteTitle)}
+          </TitleHolder>
+          <InstructionHolder>
+            {intl.formatMessage(messages.userSetupInstruction)}
+          </InstructionHolder>
+          <LoginButton
+            id="setup-login-button"
+            onClick={() => dispatch(redirectToAuthentication())}
+          >
+            {intl.formatMessage(buttonMessages.login)}
+          </LoginButton>
+        </Content>
+      </Container>
+    </Page>
+  )
+}
