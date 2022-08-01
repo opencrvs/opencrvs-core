5e1c3ad1e50aaf99a9f31e4520816625e553c30c
Sadman Anik <sadman.anik@dsinnovators.com>
2022-08-01 15:16:07 +0600

    Removed /generate & /generateEventDurationMetrics routes from metrics microservice

--- packages/metrics/src/features/legacy/handler.ts
+++ packages/metrics/src/features/legacy/handler.ts
@@ -9,24 +9,15 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-import * as Hapi from '@hapi/hapi'
 import {
-  fetchAllFromSearch,
   fetchParentLocationByLocationID,
-  fetchPractitionerRole,
-  fetchTaskIdByCompositionID
+  fetchPractitionerRole
 } from '@metrics/api'
 import {
   IAuthHeader,
   IDeclarationsStartedFields,
-  IDeclarationsStartedPoints,
-  IPointLocation,
-  IDurationPoints,
-  IDurationFields,
-  IDurationTags
+  IPointLocation
 } from '@metrics/features/registration'
-import { writePoints, query } from '@metrics/influxdb/client'
-import { differenceInSeconds } from 'date-fns'
 interface ISearchResult {
   _index: string
   _type: string
@@ -78,41 +69,6 @@ interface ISearchResult {
     updatedBy: string
   }
   sort: number[]
-}
-
-export async function generateLegacyMetricsHandler(
-  request: Hapi.Request,
-  h: Hapi.ResponseToolkit
-) {
-  let result: any
-  const authHeader = {
-    Authorization: request.headers.authorization,
-    'x-correlation-id': request.headers['x-correlation-id']
-  }
-  try {
-    await query('DROP MEASUREMENT declarations_started')
-  } catch (err) {
-    throw new Error('Could not clear influx')
-  }
-  try {
-    result = await fetchAllFromSearch(authHeader)
-  } catch (err) {
-    throw new Error('Could not resolve values from search')
-  }
-  const totalPoints: IDeclarationsStartedPoints[] = []
-  if (result.body.hits.total.value !== result.body.hits.hits.length) {
-    throw new Error(
-      'Not all results returned in search results.  Need to implement Elastic pagination for more than 10,000 records'
-    )
-  }
-  const searchResults: ISearchResult[] = result.body.hits.hits
-  for (const searchResult of searchResults) {
-    const points: IDeclarationsStartedPoints =
-      await generateDeclarationStartedPoint(searchResult, authHeader)
-    totalPoints.push(points)
-  }
-  await writePoints(totalPoints)
-  return h.response(totalPoints).code(200)
 }
 
 const generatePointLocationsFromID = async (
@@ -176,82 +132,4 @@ export async function generateDeclarationStartedPoint(
     fields,
     timestamp: `${parseInt(searchResult._source.createdAt, 10) * 1000000}`
   }
-}
-
-export async function generateLegacyEventDurationHandler(
-  request: Hapi.Request,
-  h: Hapi.ResponseToolkit
-) {
-  let result: any
-  const authHeader = {
-    Authorization: request.headers.authorization,
-    'x-correlation-id': request.headers['x-correlation-id']
-  }
-  try {
-    await query('DROP MEASUREMENT declaration_event_duration')
-  } catch (err) {
-    throw new Error('Could not clear influx')
-  }
-  try {
-    result = await fetchAllFromSearch(authHeader)
-  } catch (err) {
-    throw new Error('Could not resolve values from search')
-  }
-  let totalPoints: IDurationPoints[] = []
-  if (result.body.hits.total.value !== result.body.hits.hits.length) {
-    throw new Error(
-      'Not all results returned in search results.  Need to implement Elastic pagination for more than 10,000 records'
-    )
-  }
-  const searchResults: ISearchResult[] = result.body.hits.hits
-  for (const searchResult of searchResults) {
-    const points: IDurationPoints[] = await generateEventDurationPoint(
-      searchResult,
-      authHeader
-    )
-    totalPoints = totalPoints.concat(points)
-  }
-  await writePoints(totalPoints)
-  return h.response({ totalRecords: searchResults.length }).code(200)
-}
-
-async function generateEventDurationPoint(
-  searchResult: ISearchResult,
-  authHeader: IAuthHeader
-): Promise<any> {
-  const taskId = await fetchTaskIdByCompositionID(searchResult._id, authHeader)
-  return (
-    (searchResult._source &&
-      searchResult._source.operationHistories &&
-      searchResult._source.operationHistories
-        .filter((history, index) => index !== 0)
-        .map((history, index) => {
-          const previousHistory = searchResult._source.operationHistories[index]
-          const fields: IDurationFields = {
-            durationInSeconds: differenceInSeconds(
-              new Date(history.operatedOn),
-              new Date(previousHistory.operatedOn)
-            ),
-            compositionId: searchResult._id,
-            currentTaskId: taskId,
-            previousTaskId: taskId
-          }
-
-          const tags: IDurationTags = {
-            eventType: searchResult._source.event.toUpperCase(),
-            currentStatus: history.operationType,
-            previousStatus: previousHistory.operationType
-          }
-
-          return {
-            measurement: 'declaration_event_duration',
-            tags,
-            fields,
-            timestamp: `${
-              parseInt(searchResult._source.createdAt, 10) * 1000000
-            }`
-          }
-        })) ||
-    []
-  )
 }
