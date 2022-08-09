33516f59c8fe52563c37ff3da0b78d67a811e178
Pyry <pyry.rouvila@gmail.com>
2022-08-09 12:22:29 +0300

    Create initial configuration to theme storybook

---
+++ packages/components/.storybook/theme.js
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'OpenCRVS Design System',
+  brandUrl: 'https://opencrvs.org',
+  // brandImage: 'https://place-hold.it/350x150',
+  brandTarget: '_blank'
+})
