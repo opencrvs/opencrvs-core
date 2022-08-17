a18954d91ac41acfc8db6f62deda7dd72bcd3b80
Pyry <pyry.rouvila@gmail.com>
2022-08-17 18:24:33 +0300

    Support having only 1 child

--- packages/components/src/components/Frame/components/Layout.tsx
+++ packages/components/src/components/Frame/components/Layout.tsx
@@ -9,14 +9,17 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import styled, { css } from 'styled-components'
 import { Content } from '../../interface/Content'
+import React from 'react'
 
-export const Layout = styled.section<{ sideColumn?: boolean }>`
+export const Layout = styled.section`
   display: grid;
   width: 100%;
   gap: 24px;
-  grid-template-columns: 1fr auto;
-  max-width: min(1140px, 100% - 24px - 24px);
+  grid-template-columns: ${({ children }) =>
+    React.Children.count(children) === 1 ? '1fr' : '1fr auto'};
+
   margin: 24px auto;
 
   @media (max-width: ${({ theme }) => theme.grid.breakpoints.xl}px) {
@@ -31,8 +34,17 @@ export const Layout = styled.section<{ sideColumn?: boolean }>`
   }
 
   ${Content} {
-    margin: 0;
+    margin: 0 auto;
     width: 100%;
-    max-width: 100%;
   }
+
+  /* On multiple columns, remove margin in <Content> and apply margin to <Layout> */
+  ${({ children }) =>
+    React.Children.count(children) >= 2 &&
+    css`
+      max-width: min(1140px, 100% - 24px - 24px);
+      ${Content} {
+        max-width: 100%;
+      }
+    `}
 `
