895b557de2f09eb26e5664eb48dd56eb6a2a4bd3
jpye-finch <jonathan@pye-finch.co.uk>
2022-08-23 14:04:37 +0100

    removes storysource added by mistake

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -16,7 +16,6 @@ const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
-    '@storybook/addon-storysource',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app'
