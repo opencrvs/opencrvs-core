4708fc83142b5c57cd582ee44f4fe971958e1cbe
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2022-09-06 18:09:25 +0600

    Date comparison change

--- packages/metrics/src/features/searchMetrics/service.ts
+++ packages/metrics/src/features/searchMetrics/service.ts
@@ -1,5 +1,6 @@
 import { query } from '@metrics/influxdb/client'
 import { getTokenPayload } from '@metrics/utils/authUtils'
+import { format } from 'date-fns'
 
 export function getClientIdFromToken(token: string) {
   const payload = getTokenPayload(token)
@@ -7,7 +8,8 @@ export function getClientIdFromToken(token: string) {
 }
 
 export async function fetchTotalSearchRequestByClientId(clientId: string) {
+  const currentDate = format(new Date(), 'yyyy-MM-dd')
   return await query(
-    `SELECT COUNT(clientId) FROM search_requests WHERE clientId = '${clientId}' AND time > now() - 1d`
+    `SELECT COUNT(clientId) FROM search_requests WHERE clientId = '${clientId}' AND time >= '${currentDate}'`
   )
 }
