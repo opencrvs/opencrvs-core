4f4628f7ddb33774b6a5fdf4fcb4dfc0001dd699
naftis <pyry.rouvila@gmail.com>
2022-09-08 12:48:53 +0300

    Remove unused Header component

--- packages/components/src/interface/index.ts
+++ packages/components/src/interface/index.ts
@@ -9,7 +9,6 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-export * from './Header'
 export * from './ActionPageLight'
 export * from './Header/AppHeader'
 export * from './EventTopBar'
