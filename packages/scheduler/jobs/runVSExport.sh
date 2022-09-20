f1eb486c79fbccdc645b7b3ffe8e904dbaf1ceca
Sadman Anik <sadman.anik@dsinnovators.com>
2022-09-20 12:36:32 +0600

    Fixed vsExport route path on script

--- packages/scheduler/jobs/runVSExport.sh
+++ packages/scheduler/jobs/runVSExport.sh
@@ -7,4 +7,4 @@ YEAR=$(date +%Y)
 ONE_YEAR_FROM_NOW=$(( YEAR - 1 ))
 START_DATE="${ONE_YEAR_FROM_NOW}$(date +-%m-%d)"
 
-curl "$METRICS_URL/annualVSExport?startDate=$START_DATE&endDate=$END_DATE"
+curl "$METRICS_URL/vsExport?startDate=$START_DATE&endDate=$END_DATE"
