2b0ed31bcaf39d9c1f0aaba5ca9eb33cd4884d7e
naftis <pyry.rouvila@gmail.com>
2022-09-27 15:05:58 +0300

    Fix navigation Performance Icon color

--- packages/components/src/SideNavigation/NavigationItem.tsx
+++ packages/components/src/SideNavigation/NavigationItem.tsx
@@ -40,6 +40,8 @@ const ItemContentContainer = styled.div<{ isSelected?: boolean }>`
   flex-flow: row;
   align-items: center;
   padding: 8px 16px 8px 14px;
+  color: ${({ isSelected, theme }) =>
+    isSelected ? theme.colors.copy : theme.colors.grey500};
 `
 const LabelContainer = styled.span<{
   isSelected?: boolean
@@ -77,7 +79,7 @@ export const NavigationItem = ({
 }: INavigationItemProps) => {
   return (
     <ItemContainer isSelected={isSelected} {...otherProps}>
-      <ItemContentContainer>
+      <ItemContentContainer isSelected={isSelected}>
         {icon && <IconContainer>{icon()}</IconContainer>}
         <LabelContainer isSelected={isSelected} isSubItem={isSubItem}>
           {label}
