bffc5786a8a1fb13f528381aec88501ead91903f
jpye-finch <jonathan@pye-finch.co.uk>
2022-10-12 14:27:27 +0100

    Toast ui, waiting on Link

--- packages/components/src/Toast/Toast.tsx
+++ packages/components/src/Toast/Toast.tsx
@@ -11,10 +11,14 @@
  */
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
-import { Alert } from '../Alert'
+import { Cross } from '../icons'
+import { Spinner } from '../Spinner'
+import { Button } from '../Button'
+import { Text } from '../Text'
+import { colors } from '../colors'
 import { useToastVisibility } from './useToastVisibility'
 
-const TOAST_DEFAULT_DURATION_MS = 10000
+const TOAST_DEFAULT_DURATION_MS = 80000
 
 type ToastType = 'success' | 'warning' | 'loading' | 'error'
 
@@ -36,11 +40,26 @@ const shallowToast = keyframes`
   to { bottom: 24px; }
 `
 
-const ToastAlert = styled(Alert)`
+const Container = styled.div<{
+  $type?: ToastType
+}>`
+  --color: ${({ $type, theme }) => `
+    ${$type === 'success' ? theme.colors.positiveDark : ''}
+    ${$type === 'loading' ? theme.colors.primaryDark : ''}
+    ${$type === 'error' ? theme.colors.negativeDark : ''}
+    ${$type === 'warning' ? theme.colors.orangeDark : ''}
+    ${$type === undefined ? theme.colors.positiveDark : ''}
+  `};
+  background: var(--color);
+  border-radius: 8px;
+  min-height: 40px;
   position: fixed;
-  filter: drop-shadow(0px 2px 4px rgba(34, 34, 34, 0.24));
-  width: 50%;
-  max-width: 564px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  padding: 0 16px;
+  ${({ theme }) => theme.shadows.heavy};
   transform: translateX(-50%);
   left: 50%;
   z-index: 1;
@@ -54,24 +73,90 @@ const ToastAlert = styled(Alert)`
   }
 `
 
+const Close = styled(Button)`
+  color: ${({ theme }) => theme.colors.white};
+  svg {
+    height: 24px;
+    width: 24px;
+  }
+  &:hover {
+    background: none;
+  }
+`
+
+/**
+ *  Change to Link component
+ */
+
+const ActionButton = styled(Button)`
+  color: ${({ theme }) => theme.colors.white};
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+    background: none;
+  }
+`
+
+const NotificationMessage = styled.div`
+  ${({ theme }) => theme.fonts.bold16};
+  color: ${({ theme }) => theme.colors.white};
+  position: relative;
+  padding: 8px 0;
+  width: 100%;
+  flex: 1;
+`
+
+export interface IAlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  type: ToastType
+  onClose?: (event?: React.MouseEvent<HTMLButtonElement>) => void
+  onActionClick?: (event?: React.MouseEvent<HTMLButtonElement>) => void
+  actionText?: string
+}
+
 export const Toast = ({
   type,
-  duration = TOAST_DEFAULT_DURATION_MS,
   onClose,
+  duration = TOAST_DEFAULT_DURATION_MS,
+  onActionClick,
+  actionText,
+  children,
   ...props
-}: ToastProps) => {
-  useToastVisibility({
-    duration: type === 'loading' ? null : duration,
-    onClose
-  })
+}: ToastProps) => (
+  <Container $type={type} {...props}>
+    {type === 'loading' && (
+      <Spinner
+        id="in-progress-floating-notification"
+        baseColor={colors.white}
+        size={20}
+      />
+    )}
 
-  return (
-    <ToastAlert
-      type={type ?? 'error'}
-      onClose={onClose}
-      role="alert"
-      aria-live="polite"
-      {...props}
-    />
-  )
-}
+    <NotificationMessage>{children}</NotificationMessage>
+
+    {useToastVisibility({
+      duration: type === 'loading' ? null : duration,
+      onClose
+    })}
+
+    {onActionClick && (
+      <ActionButton
+        type="tertiary"
+        size="small"
+        onClick={onActionClick}
+        data-testid={props['data-testid'] && `${props['data-testid']}-action`}
+      >
+        {actionText}
+      </ActionButton>
+    )}
+    {onClose && type !== 'loading' && (
+      <Close
+        type="icon"
+        size="small"
+        id={props.id + 'Cancel'}
+        data-testid={props['data-testid'] && `${props['data-testid']}-close`}
+        onClick={onClose}
+      >
+        <Cross color="currentColor" />
+      </Close>
+    )}
+  </Container>
+)
