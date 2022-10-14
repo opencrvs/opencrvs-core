0323fb865dae8973f88507ed4c385fb399d4641a
Favour Ifeoma <forajiaku@seamhealthgroup.com>
2022-10-14 06:29:12 +0100

    updated function for popstate event listener

--- packages/client/src/views/OfficeHome/inProgress/InProgress.tsx
+++ packages/client/src/views/OfficeHome/inProgress/InProgress.tsx
@@ -138,14 +138,17 @@ export class InProgressComponent extends React.Component<
   }
 
   componentDidMount() {
-    window.addEventListener('popstate', function (event) {
-      window.history.pushState(null, document.title, window.location.href)
-    })
+    window.addEventListener('popstate', this.popStateListener)
     window.addEventListener('resize', this.recordWindowWidth)
   }
 
   componentWillUnmount() {
+    window.addEventListener('popstate', this.popStateListener)
     window.removeEventListener('resize', this.recordWindowWidth)
+  }
+
+  popStateListener() {
+    window.history.pushState(null, document.title, window.location.href)
   }
 
   recordWindowWidth = () => {
