e9a2881f5dd488daedda3540cc5fa1f873c74c2e
naftis <pyry.rouvila@gmail.com>
2022-10-20 13:50:34 +0300

    Modify icon component to support all icons and update stories

---
+++ packages/components/src/Icon/Icon.tsx
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+
+import * as React from 'react'
+import { colors } from '../colors'
+import * as icons from './all-icons'
+
+enum IconSize {
+  small = 12,
+  medium = 18,
+  large = 24
+}
+
+type IconProps = {
+  name: keyof typeof icons
+  size?: 'small' | 'medium' | 'large'
+  color?: keyof typeof colors | 'currentColor'
+}
+
+export function Icon({
+  name,
+  size = 'medium',
+  color = 'currentColor',
+  ...rest
+}: IconProps) {
+  const IconComponent = icons[name]
+  return <IconComponent size={IconSize[size]} color={colors[color]} {...rest} />
+}
