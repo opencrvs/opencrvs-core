ac3bb9e108e3711e1b3060642d76a3e36f2f8b1d
naftis <pyry.rouvila@gmail.com>
2022-10-21 12:27:30 +0300

    Upload versions of icons that have viewbox 1:1

--- packages/components/src/Icon/custom-icons/Offline.tsx
+++ packages/components/src/Icon/custom-icons/Offline.tsx
@@ -16,7 +16,7 @@ export const Offline: CustomIcon = ({ size, ...rest }) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 24 25"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...rest}
@@ -24,17 +24,17 @@ export const Offline: CustomIcon = ({ size, ...rest }) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M12 3.125C7.02944 3.125 3 7.15444 3 12.125C3 17.0956 7.02944 21.125 12 21.125C16.9706 21.125 21 17.0956 21 12.125C21 7.15444 16.9706 3.125 12 3.125ZM1 12.125C1 6.04987 5.92487 1.125 12 1.125C18.0751 1.125 23 6.04987 23 12.125C23 18.2001 18.0751 23.125 12 23.125C5.92487 23.125 1 18.2001 1 12.125Z"
+      d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12Z"
       fill="#CCCCCC"
     />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M18.2812 5.84375L5.71875 18.4063Z"
+      d="M18.2812 5.71875L5.71875 18.2812L18.2812 5.71875Z"
       fill="#CCCCCC"
     />
     <path
-      d="M18.2812 5.84375L5.71875 18.4063"
+      d="M18.2812 5.71875L5.71875 18.2813"
       stroke="#CCCCCC"
       strokeWidth="2"
       strokeLinecap="round"
@@ -43,70 +43,70 @@ export const Offline: CustomIcon = ({ size, ...rest }) => (
     <ellipse
       rx="0.571023"
       ry="0.571023"
-      transform="matrix(-1 0 0 1 12.0003 16.6932)"
+      transform="matrix(-1 0 0 1 12.0003 16.5682)"
       fill="#CCCCCC"
       stroke="#CCCCCC"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M9.30469 11.5883C8.83696 11.8165 8.3996 12.1023 8.00276 12.4391Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.30469 11.4633C8.83696 11.6915 8.3996 11.9773 8.00276 12.3141L9.30469 11.4633Z"
       fill="#CCCCCC"
     />
     <path
-      d="M9.30469 11.5883C8.83696 11.8165 8.3996 12.1023 8.00276 12.4391"
-      stroke="#CCCCCC"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M15.9971 12.4391C15.1508 11.7315 14.1322 11.2606 13.0449 11.0743Z"
-      fill="#CCCCCC"
-    />
-    <path
-      d="M15.9971 12.4391C15.1508 11.7315 14.1322 11.2606 13.0449 11.0743"
+      d="M9.30469 11.4633C8.83696 11.6915 8.3996 11.9773 8.00276 12.3141"
       stroke="#CCCCCC"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M12.7368 8.15643C10.265 7.95726 7.81839 8.77142 5.95878 10.412Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M15.9971 12.3141C15.1508 11.6065 14.1322 11.1356 13.0449 10.9493L15.9971 12.3141Z"
       fill="#CCCCCC"
     />
     <path
-      d="M12.7368 8.15643C10.265 7.95726 7.81839 8.77142 5.95878 10.412"
+      d="M15.9971 12.3141C15.1508 11.6065 14.1322 11.1356 13.0449 10.9493"
       stroke="#CCCCCC"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M18.0415 10.4119C17.2494 9.71178 16.3412 9.15525 15.3577 8.7674Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12.7368 8.03143C10.265 7.83226 7.81839 8.64642 5.95878 10.287L12.7368 8.03143Z"
       fill="#CCCCCC"
     />
     <path
-      d="M18.0415 10.4119C17.2494 9.71178 16.3412 9.15525 15.3577 8.7674"
+      d="M12.7368 8.03143C10.265 7.83226 7.81839 8.64642 5.95878 10.287"
       stroke="#CCCCCC"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M13.9814 14.4719C12.7932 13.6278 11.201 13.6278 10.0128 14.4719Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M18.0415 10.2869C17.2494 9.58678 16.3412 9.03025 15.3577 8.6424L18.0415 10.2869Z"
       fill="#CCCCCC"
     />
     <path
-      d="M13.9814 14.4719C12.7932 13.6278 11.201 13.6278 10.0128 14.4719"
+      d="M18.0415 10.2869C17.2494 9.58678 16.3412 9.03025 15.3577 8.6424"
+      stroke="#CCCCCC"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.9814 14.3469C12.7932 13.5028 11.201 13.5028 10.0128 14.3469H13.9814Z"
+      fill="#CCCCCC"
+    />
+    <path
+      d="M13.9814 14.3469C12.7932 13.5028 11.201 13.5028 10.0128 14.3469"
       stroke="#CCCCCC"
       strokeWidth="2"
       strokeLinecap="round"
