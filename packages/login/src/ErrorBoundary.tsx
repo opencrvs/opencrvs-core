1c01f123c534ba8f08ca43c8066ea0f312a09562
Euan Millar <euanmillar@users.noreply.github.com>
2022-10-26 16:45:14 +0100

    Revert "Revert "Upgrade Sentry""

--- packages/login/src/ErrorBoundary.tsx
+++ packages/login/src/ErrorBoundary.tsx
@@ -10,44 +10,25 @@
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
 import * as React from 'react'
-import * as Sentry from '@sentry/browser'
 import styled from 'styled-components'
+import * as Sentry from '@sentry/react'
 
 const ErrorMessage = styled.h1`
   text-align: center;
 `
-
-interface IErrorInfo extends React.ErrorInfo {
-  [key: string]: string
-}
+const development = ['127.0.0.1', 'localhost'].includes(
+  window.location.hostname
+)
 
 export class ErrorBoundary extends React.Component {
-  state = { error: null }
-
-  componentDidCatch(error: Error, errorInfo: IErrorInfo) {
-    this.setState({ error })
-    Sentry.withScope((scope) => {
-      Object.keys(errorInfo).forEach((key) => {
-        scope.setExtra(key, errorInfo[key])
-      })
-      Sentry.captureException(error)
-    })
-  }
-
   render() {
-    if (this.state.error) {
-      if (
-        window.location.hostname !== 'localhost' &&
-        window.location.hostname !== '127.0.0.1'
-      ) {
-        Sentry.showReportDialog()
-      }
-
-      // We could render fallback UI here
-      return <ErrorMessage>Something went wrong...</ErrorMessage>
-    } else {
-      // when there's not an error, render children untouched
-      return this.props.children
-    }
+    return (
+      <Sentry.ErrorBoundary
+        showDialog={!development}
+        fallback={<ErrorMessage>Something went wrong...</ErrorMessage>}
+      >
+        {this.props.children}
+      </Sentry.ErrorBoundary>
+    )
   }
 }
