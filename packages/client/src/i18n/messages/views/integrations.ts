7f21ce4aa9d06680f2a2d6dc9b028db97b70a7fb
Pyry Rouvila <pyry.rouvila@gmail.com>
2022-11-01 17:13:03 +0200

    OCRVS-3592 Fetch integrations from config to list them on IntegrationsList (#4188)
    * Get integrations from API in frontend
    
    * Show active / inactive based on the integration status

--- packages/client/src/i18n/messages/views/integrations.ts
+++ packages/client/src/i18n/messages/views/integrations.ts
@@ -1,3 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
 import { defineMessages } from 'react-intl'
 
 const messagesToDefine = {
@@ -23,10 +34,25 @@ const messagesToDefine = {
     defaultMessage: 'Disable',
     description: 'Label for disable option'
   },
+  enable: {
+    id: 'integrations.enable',
+    defaultMessage: 'Enable',
+    description: 'Label for enable option'
+  },
   delete: {
     id: 'integrations.delete',
     defaultMessage: 'Delete',
     description: 'Label for delete option'
+  },
+  active: {
+    id: 'integrations.active',
+    defaultMessage: 'Active',
+    description: 'Label for active integration'
+  },
+  inactive: {
+    id: 'integrations.inactive',
+    defaultMessage: 'Inactive',
+    description: 'Label for inactive integration'
   }
 }
 
