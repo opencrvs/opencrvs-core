611a42d9837cbe9e5cada8454e8136947d22d04a
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2022-11-16 19:25:47 +0600

    Amend verify details to show template data

--- packages/client/src/views/PrintCertificate/VerifyDetails.tsx
+++ packages/client/src/views/PrintCertificate/VerifyDetails.tsx
@@ -14,18 +14,16 @@ import {
   Content,
   ContentSize
 } from '@opencrvs/components/lib/interface/Content'
-import { DeclarationIcon, Cross } from '@opencrvs/components/lib/icons'
-import { IconButton } from '@opencrvs/components/lib/buttons'
-import { GetDeclarationInfo } from '@client/views/RecordAudit/DeclarationInfo'
+import { DeclarationIcon } from '@opencrvs/components/lib/icons'
+import { GetDeclarationInfo } from '@client/views/PrintCertificate/DeclarationInfo'
 import { useIntl } from 'react-intl'
 import gql from 'graphql-tag'
 import { Query } from '@client/components/Query'
 import { useParams } from 'react-router'
 import { gqlToDraftTransformer } from '@client/transformer'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { getOfflineData } from '@client/offline/selectors'
 import { getRegisterForm } from '@client/forms/register/declaration-selectors'
-import { camelCase } from 'lodash'
 import { getDraftDeclarationData } from '@client/views/RecordAudit/utils'
 import { IDeclaration } from '@client/declarations'
 import {
@@ -34,12 +32,49 @@ import {
 } from '@opencrvs/components/lib/interface'
 import { buttonMessages, errorMessages } from '@client/i18n/messages'
 import { STATUSTOCOLOR } from '@client/views/RecordAudit/RecordAudit'
-import { goBack } from '@client/navigation'
+import styled from '@client/styledComponents'
+import { Event } from '@client/utils/gateway'
+import { IFormFieldValue, IFormSectionData } from '@client/forms'
 
+const FullPageCenter = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  & > :only-child {
+    margin-top: -36px;
+  }
+`
+function toString(delimiter = '', ...values: IFormFieldValue[]): string {
+  return values.map((v) => v.toString()).join(delimiter)
+}
+
+function getEvent(event: string): Event {
+  switch (event) {
+    case 'DEATH':
+      return Event.Death
+    case 'BIRTH':
+    default:
+      return Event.Birth
+  }
+}
 const VERIFY_QUERY = gql`
   query fetchRegistration($id: ID!) {
     fetchRegistration(id: $id) {
       id
+      history {
+        action
+        date
+        user {
+          name {
+            use
+            firstNames
+            familyName
+          }
+        }
+      }
       registration {
         id
         type
@@ -68,6 +103,7 @@ const VERIFY_QUERY = gql`
             familyName
           }
           birthDate
+          gender
         }
         mother {
           name {
@@ -126,13 +162,16 @@ export function VerifyDetails() {
   const { id } = useParams()
   const offlineData = useSelector(getOfflineData)
   const registerForm = useSelector(getRegisterForm)
-  const dispatch = useDispatch()
 
   return (
     <Query query={VERIFY_QUERY} variables={{ id }}>
       {({ data, loading, error, refetch }) => {
         if (loading) {
-          return <Spinner id="verify-details-loading" />
+          return (
+            <FullPageCenter>
+              <Spinner id="verify-details-loading" />
+            </FullPageCenter>
+          )
         } else if (error) {
           return (
             <ErrorToastNotification
@@ -144,35 +183,50 @@ export function VerifyDetails() {
           )
         } else if (data?.fetchRegistration) {
           const event = data.fetchRegistration.registration?.type ?? 'BIRTH'
-          const registerFormForEvent =
-            registerForm[camelCase(event) as 'birth' | 'death']
+          const registerFormForEvent = registerForm[getEvent(event)]
+          const regStatus = data.fetchRegistration.registration?.status[0]?.type
           const draftDeclarationData = gqlToDraftTransformer(
             registerFormForEvent,
             data.fetchRegistration,
             offlineData
           )
-          const declarationData = getDraftDeclarationData(
-            {
-              id,
-              data: draftDeclarationData,
-              event: camelCase(event)
-            } as IDeclaration,
-            offlineData,
-            intl,
-            data.fetchRegistration.registration?.trackingId ?? ''
-          )
+          const declarationData = {
+            ...getDraftDeclarationData(
+              {
+                id,
+                data: draftDeclarationData,
+                event: getEvent(event),
+                registrationStatus: regStatus
+              } as IDeclaration,
+              offlineData,
+              intl,
+              data.fetchRegistration.registration?.trackingId ?? ''
+            ),
+            status: regStatus
+          }
+
+          const templateInfo = {
+            registrationLocation: toString(
+              ', ',
+              draftDeclarationData.template?.registrationCentre,
+              draftDeclarationData.template?.registrationLGA,
+              draftDeclarationData.template?.registrationState
+            ),
+            registrationDate:
+              draftDeclarationData.template?.registrationDate?.toString(),
+            certificateDate:
+              draftDeclarationData.template?.certificateDate?.toString(),
+            registrar: draftDeclarationData.template?.registrarName?.toString(),
+            childGender: (
+              draftDeclarationData.template?.informantGender as IFormSectionData
+            )?.defaultMessage?.toString()
+          }
 
           return (
             <Content
               title={declarationData.name}
               icon={() => (
-                <DeclarationIcon
-                  color={
-                    STATUSTOCOLOR[
-                      data.fetchRegistration.registration?.status[0]?.type
-                    ] || 'green'
-                  }
-                />
+                <DeclarationIcon color={STATUSTOCOLOR[regStatus] || 'green'} />
               )}
               size={ContentSize.LARGE}
               topActionButtons={[]}
@@ -182,17 +236,21 @@ export function VerifyDetails() {
                 isDownloaded
                 actions={[]}
                 intl={intl}
-                declaration={{
-                  id: declarationData.id,
-                  status:
-                    data.fetchRegistration.registration?.status[0]?.type ?? '',
-                  type: declarationData.type,
-                  trackingId: declarationData.trackingId,
-                  brnDrn: declarationData.brnDrn,
-                  dateOfBirth: declarationData.dateOfBirth,
-                  placeOfBirth: declarationData.placeOfBirth,
-                  informant: declarationData.informant
-                }}
+                declaration={declarationData}
+                additionalInfo={templateInfo}
+                order={[
+                  'status',
+                  'type',
+                  'trackingId',
+                  getEvent(event) === Event.Death ? 'drn' : 'brn',
+                  'registrationLocation',
+                  'registrationDate',
+                  'childGender',
+                  'dateOfBirth',
+                  'placeOfBirth',
+                  'registrar',
+                  'certificateDate'
+                ]}
               />
             </Content>
           )
