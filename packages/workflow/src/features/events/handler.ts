6a135affb3c164081e0c5bda0adf68a259b47f28
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2022-11-21 11:05:16 +0600

    Added an identifier to system initiated tasks

--- packages/workflow/src/features/events/handler.ts
+++ packages/workflow/src/features/events/handler.ts
@@ -48,6 +48,7 @@ import {
   REINSTATED_EXTENSION_URL,
   VIEWED_EXTENSION_URL
 } from '@workflow/features/task/fhir/constants'
+import { setupSystemIdentifier } from '@workflow/features/registration/fhir/fhir-bundle-modifier'
 
 // TODO: Change these event names to be closer in definition to the comments
 // https://jembiprojects.jira.com/browse/OCRVS-2767
@@ -244,6 +245,8 @@ export async function fhirWorkflowEventHandler(
   ) {
     return h.response().code(401)
   }
+
+  setupSystemIdentifier(request)
 
   let response
 
