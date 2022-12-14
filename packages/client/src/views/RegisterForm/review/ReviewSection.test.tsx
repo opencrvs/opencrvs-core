f4ab9ef2cf421b5b49e48bc3ecf7cd4716de8afb
Favour Ifeoma <forajiaku@seamhealthgroup.com>
2022-12-14 12:25:27 +0100

    resolved failing test

--- packages/client/src/views/RegisterForm/review/ReviewSection.test.tsx
+++ packages/client/src/views/RegisterForm/review/ReviewSection.test.tsx
@@ -466,7 +466,8 @@ describe('when in device of large viewport', () => {
               informantPhoneMother: '011123456789'
             }
           }
-        }
+        },
+        documents: {}
       }
 
       const simpleDraft = createReviewDeclaration(
@@ -565,7 +566,8 @@ describe('when in device of large viewport', () => {
       const data = {
         child: {
           birthLocation: '0d8474da-0361-4d32-979e-af91f012340a'
-        }
+        },
+        documents: {}
       }
 
       const simpleDraft = createReviewDeclaration(
@@ -679,6 +681,13 @@ describe('when in device of small viewport', () => {
         uploadDocForMother: [
           {
             optionValues: ['MOTHER', 'Birth Registration'],
+            type: 'image/png',
+            data: 'data:image/png;base64,abcd'
+          }
+        ],
+        uploadDocForChildDOB: [
+          {
+            optionValues: ['CHILD', 'Birth Registration'],
             type: 'image/png',
             data: 'data:image/png;base64,abcd'
           }
