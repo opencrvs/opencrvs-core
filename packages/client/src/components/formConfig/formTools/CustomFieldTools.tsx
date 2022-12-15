9de9e29f7b8f6e1c61dde5fd2c2a6a0819b5068a
Ifoghale Cornelius <collinscornelius9@gmail.com>
2022-12-15 08:50:49 +0100

    ocrvs-4410: Custom Field in form configuration becomes conditional field (#4426)
    * ocrvs-4410: Custom Field in form configuration becomes conditional field
    
    * fix update issues

--- packages/client/src/components/formConfig/formTools/CustomFieldTools.tsx
+++ packages/client/src/components/formConfig/formTools/CustomFieldTools.tsx
@@ -393,9 +393,7 @@ class CustomFieldToolsComp extends React.Component<
 
     return {
       isFieldDuplicate: false,
-      handleBars:
-        getCertificateHandlebar(formField) ||
-        camelCase(fieldForms[defaultLanguage].label),
+      handleBars: camelCase(fieldForms[defaultLanguage].label),
       selectedLanguage: defaultLanguage,
       conditionalField: {
         fieldId: conditionalfield?.fieldId ?? EMPTY_STRING,
