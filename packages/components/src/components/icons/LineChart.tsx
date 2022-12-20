209b579e7912a5889969961fbded146b6f02eae0
Riku Rouvila <riku.rouvila@gmail.com>
2022-12-20 09:55:35 +0200

    make line chart icon a bit smaller

--- packages/components/src/components/icons/LineChart.tsx
+++ packages/components/src/components/icons/LineChart.tsx
@@ -14,8 +14,8 @@ import * as React from 'react'
 export const LineChart = (props: React.HTMLAttributes<SVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     fill="none"
     stroke="#5B5B5B"
