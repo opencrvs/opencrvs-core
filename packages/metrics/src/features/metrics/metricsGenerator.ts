e5d0402a4934f13d2572469b88f4f7aba9d093b3
kayumuzzaman <i.kayumuzzaman@gmail.com>
2023-01-10 16:25:40 +0600

    influxdb current month not showing correctly for performance fixed

--- packages/metrics/src/features/metrics/metricsGenerator.ts
+++ packages/metrics/src/features/metrics/metricsGenerator.ts
@@ -9,6 +9,7 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import { INFLUXDB_URL } from '@metrics/constants'
 import {
   FEMALE,
   MALE,
@@ -28,7 +29,9 @@ import {
 } from '@metrics/features/metrics/utils'
 import { IAuthHeader } from '@metrics/features/registration'
 import { query } from '@metrics/influxdb/client'
+import { csvToJSON } from '@metrics/utils/csvHelper'
 import { format } from 'date-fns'
+import fetch from 'node-fetch'
 interface IGroupedByGender {
   total: number
   gender: string
@@ -993,26 +996,67 @@ export async function fetchRegistrationsGroupByTime(
     event === EVENT_TYPE.BIRTH ? 'birth_registration' : 'death_registration'
   const column = event === EVENT_TYPE.BIRTH ? 'ageInDays' : 'deathDays'
 
-  const result: IMetricsTotalGroupByTime[] = await query(
-    `SELECT COUNT(${column}) AS total
-      FROM ${measurement}    
-      WHERE time > '${timeFrom}'
-      AND time <= '${timeTo}' 
-      ${
-        locationId
-          ? `AND ( locationLevel2 = '${locationId}'
-      OR locationLevel3 = '${locationId}'
-      OR locationLevel4 = '${locationId}'
-      OR locationLevel5 = '${locationId}'
-      OR officeLocation = '${locationId}')`
-          : ``
-      }   
-    GROUP BY time(30d), timeLabel, eventLocationType ORDER BY time DESC
+  const fluxQuery = `
+   from(bucket:"ocrvs/")
+   |> range(start: ${timeFrom}, stop: ${timeTo})
+   |> filter(fn: (r) => 
+      r._measurement == "${measurement}" and
+      (r.locationLevel2 == "${locationId}" or
+       r.locationLevel3 == "${locationId}" or 
+       r.locationLevel4 == "${locationId}" or
+       r.locationLevel5 == "${locationId}" or
+       r.officeLocation == "${locationId}"))
+    |> filter(fn: (r) => r._field == "${column}")
+    |> group(columns: ["timeLabel", "eventLocationType"])
+    |> aggregateWindow(every: 1mo, fn: count, timeSrc: "_start")
+    |> sort(columns: ["_time"], desc: true)
+    |> rename(columns: {_value: "total"})
+   `
 
-    `
-  )
+  const res = await fetch(`${INFLUXDB_URL}/api/v2/query`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/csv',
+      'Content-type': 'application/vnd.flux'
+    },
+    body: fluxQuery
+  })
+  const fluxJson = (await csvToJSON(await res.text())).slice(4)
+  const keys = ['eventLocationType', 'timeLabel', 'total', 'time']
+  const fluxRes: IMetricsTotalGroupByTime[] = fluxJson
+    .slice(0, -1)
+    .map((item) => {
+      const obj: Partial<IMetricsTotalGroupByTime> = {}
+      keys.forEach((key, i) => {
+        obj[key] = item[i + 5]
+        if (key === 'total') {
+          obj[key] = Number(item[i + 5])
+        }
+      })
+      return obj as IMetricsTotalGroupByTime
+    })
 
-  return result
+  // const result: IMetricsTotalGroupByTime[] = await query(
+  //   `SELECT COUNT(${column}) AS total
+  //     FROM ${measurement}
+  //     WHERE time > '${timeFrom}'
+  //     AND time <= '${timeTo}'
+  //     ${
+  //       locationId
+  //         ? `AND ( locationLevel2 = '${locationId}'
+  //     OR locationLevel3 = '${locationId}'
+  //     OR locationLevel4 = '${locationId}'
+  //     OR locationLevel5 = '${locationId}'
+  //     OR officeLocation = '${locationId}')`
+  //         : ``
+  //     }
+  //   GROUP BY time(30d), timeLabel, eventLocationType ORDER BY time DESC
+
+  //   `
+  // )
+  // console.log(result)
+
+  return fluxRes
 }
 
 function populateGenderBasisMetrics(
