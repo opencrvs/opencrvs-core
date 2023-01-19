8da6bbd6c924446cefa96adbf77c0fa052c5482e
kayumuzzaman <i.kayumuzzaman@gmail.com>
2023-01-19 18:43:05 +0600

    remove log

--- packages/client/src/views/SysAdmin/Performance/RegistrationsReport.tsx
+++ packages/client/src/views/SysAdmin/Performance/RegistrationsReport.tsx
@@ -51,8 +51,6 @@ export function RegistrationsReport({
   timeEnd,
   locationId
 }: RegistrationsReportProps) {
-  console.log(data)
-
   const dispatch = useDispatch()
   const intl = useIntl()
   return (
