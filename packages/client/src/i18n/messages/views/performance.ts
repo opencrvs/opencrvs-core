3ca9d4842a55c44ab24e0bd76a9b22a07caf2040
septlowe <septime@l-agence.digital>
2023-01-25 18:01:00 +0000

    ocrvs-4568 In Performance, wrong calculation is showing in Source of application> Field agent, Health system(integration) and registration agent #4568

--- packages/client/src/i18n/messages/views/performance.ts
+++ packages/client/src/i18n/messages/views/performance.ts
@@ -57,7 +57,7 @@ const messagesToDefine = {
   performanceApplicationSourcesSubHeader: {
     id: 'performance.reports.subHeader.applicationSources',
     defaultMessage:
-      'The number and percentage of declarations started by the each system roles that have been registered',
+      'The number and percentage of declarations started by the each system role that has been registered',
     description: 'Subheader for sources of registrations'
   },
   performanceTotalLabel: {
