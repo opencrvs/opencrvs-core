183bc1c7026d142ddeb92af7c66fd8b8ab79d25b
JPF <jonathan@pye-finch.co.uk>
2023-02-07 09:56:27 +0100

    fixes imports

--- packages/client/src/components/formConfig/SectionNavigation.tsx
+++ packages/client/src/components/formConfig/SectionNavigation.tsx
@@ -15,7 +15,7 @@ import styled from '@client/styledComponents'
 import {
   NavigationSubItem,
   LabelContainer
-} from '@opencrvs/components/lib/SideNavigation/NavigationSubItem'
+} from '@opencrvs/components/lib/SideNav/NavigationSubItem'
 import { BirthSection, DeathSection, WizardSection } from '@client/forms'
 import { Event } from '@client/utils/gateway'
 import { useIntl } from 'react-intl'
