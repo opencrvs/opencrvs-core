4918e4e65630ca990e478af37dcfc9fa5dc2d38e
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2023-02-09 16:57:04 +0600

    ocrvs-2920: Fix login background preview width issue in config page

--- packages/components/src/icons/CountryLogo.tsx
+++ packages/components/src/icons/CountryLogo.tsx
@@ -14,6 +14,7 @@ import styled from 'styled-components'
 
 const Logo = styled.img<{ size: ISize }>`
   max-height: ${({ size }) => (size === 'small' ? 104 : 128)}px;
+  max-width: 100%;
 `
 
 type ISize = 'small' | 'medium'
