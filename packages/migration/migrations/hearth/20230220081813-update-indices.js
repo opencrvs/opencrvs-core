bf83e333dcc6540b4f78daffb27466b70f656f9a
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2023-02-23 21:13:36 +0600

    Add logs in migration script

--- packages/migration/migrations/hearth/20230220081813-update-indices.js
+++ packages/migration/migrations/hearth/20230220081813-update-indices.js
@@ -13,22 +13,95 @@ export const up = async (db, client) => {
   const session = client.startSession()
   try {
     await session.withTransaction(async () => {
-      await db
-        .collection('Task')
-        .createIndexes([{ 'focus.reference': 1 }, { id: 1 }])
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Task')
+      await db.collection('Task').createIndex({ id: 1 })
+      if (
+        await db
+          .collection('Task')
+          .count({ 'focus.reference': { $exists: true } })
+      ) {
+        await db.collection('Task').createIndex({ 'focus.reference': 1 })
+      }
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Task')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Composition')
       await db.collection('Composition').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Composition')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Location')
       await db.collection('Location').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Location')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Encounter')
       await db.collection('Encounter').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Encounter')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Observation')
       await db.collection('Observation').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Observation')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Composition')
       await db.collection('Composition').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Composition')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Encounter')
       await db.collection('Encounter').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Encounter')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Observation')
       await db.collection('Observation').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Observation')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: DocumentReference')
       await db.collection('DocumentReference').createIndex({ id: 1 })
-      await db.collection('Practitioner').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: DocumentReference')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Practitioner')
+      await db.collection('Practitioner').createIndex({ id: 1 }) // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Practitioner')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: PractitionerRole')
       await db.collection('PractitionerRole').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: PractitionerRole')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: RelatedPerson')
       await db.collection('RelatedPerson').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: RelatedPerson')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: Patient')
       await db.collection('Patient').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: Patient')
+
+      // eslint-disable-next-line no-console
+      console.log('Updating indices for collection: PaymentReconciliation')
       await db.collection('PaymentReconciliation').createIndex({ id: 1 })
+      // eslint-disable-next-line no-console
+      console.log('Done updating indices for collection: PaymentReconciliation')
     })
   } finally {
     await session.endSession()
