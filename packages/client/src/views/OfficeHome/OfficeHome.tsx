b9df91aaea69eadf8e3687f146db149c4f9b9ac7
ove <isiful.ovi@gmail.com>
2023-03-30 13:25:02 +0600

    Manage user restriction

--- packages/client/src/views/OfficeHome/OfficeHome.tsx
+++ packages/client/src/views/OfficeHome/OfficeHome.tsx
@@ -39,7 +39,8 @@ import {
   FIELD_AGENT_ROLES,
   NATL_ADMIN_ROLES,
   SYS_ADMIN_ROLES,
-  PERFORMANCE_MANAGEMENT_ROLES
+  PERFORMANCE_MANAGEMENT_ROLES,
+  NATIONAL_REGISTRAR_ROLES
 } from '@client/utils/constants'
 import { Toast } from '@opencrvs/components/lib/Toast'
 import { Spinner } from '@opencrvs/components/lib/Spinner'
@@ -58,7 +59,10 @@ import {
   WORKQUEUE_TABS
 } from '@client/components/interface/Navigation'
 import { isDeclarationInReadyToReviewStatus } from '@client/utils/draftUtils'
-import { PERFORMANCE_HOME } from '@client/navigation/routes'
+import {
+  PERFORMANCE_DASHBOARD,
+  PERFORMANCE_HOME
+} from '@client/navigation/routes'
 import { navigationMessages } from '@client/i18n/messages/views/navigation'
 import { Frame } from '@opencrvs/components/lib/Frame'
 import { constantsMessages } from '@client/i18n/messages'
@@ -302,10 +306,11 @@ class OfficeHomeView extends React.Component<
     return (
       <>
         {this.role &&
-          (NATL_ADMIN_ROLES.includes(this.role) ||
-            PERFORMANCE_MANAGEMENT_ROLES.includes(this.role)) && (
-            <Redirect to={PERFORMANCE_HOME} />
-          )}
+          [
+            ...NATL_ADMIN_ROLES,
+            ...PERFORMANCE_MANAGEMENT_ROLES,
+            ...NATIONAL_REGISTRAR_ROLES
+          ].includes(this.role) && <Redirect to={PERFORMANCE_DASHBOARD} />}
         {this.role && SYS_ADMIN_ROLES.includes(this.role) && (
           <Redirect
             to={{
