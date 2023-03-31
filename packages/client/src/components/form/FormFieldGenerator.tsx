3942ca1bca7b44b5d3ad12e25ebaf67981acb87c
Sadman Anik <sadman.anik@dsinnovators.com>
2023-04-01 00:56:38 +0600

    Fixed already added attachment not shwoing on document section after click

--- packages/client/src/components/form/FormFieldGenerator.tsx
+++ packages/client/src/components/form/FormFieldGenerator.tsx
@@ -256,6 +256,16 @@ function GeneratedInputField({
     )
   }
   if (fieldDefinition.type === DOCUMENT_UPLOADER_WITH_OPTION) {
+    ;(value as IFileValue[]).forEach((file) => {
+      const isMinioUrl =
+        file.data.split('/').length > 1 &&
+        file.data.split('/')[1] === window.config.MINIO_BUCKET
+      if (isMinioUrl) {
+        file.data = `${window.config.MINIO_URL}${file.data}`
+      }
+      return value
+    })
+
     return (
       <DocumentUploaderWithOption
         {...inputProps}
