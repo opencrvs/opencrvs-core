3c3210916a038a2404016b51f9244ce110deaf0b
ronald ndi <wilrona@gmail.com>
2023-04-11 13:09:17 +0100

    add helper comparision for certificate template

--- packages/client/src/views/PrintCertificate/PDFUtils.ts
+++ packages/client/src/views/PrintCertificate/PDFUtils.ts
@@ -70,6 +70,28 @@ export function executeHandlebarsTemplate(
   templateString: string,
   data: Record<string, any> = {}
 ): string {
+  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+      case '===':
+        return v1 === v2 ? options.fn(this) : options.inverse(this)
+      case '!==':
+        return v1 !== v2 ? options.fn(this) : options.inverse(this)
+      case '<':
+        return v1 < v2 ? options.fn(this) : options.inverse(this)
+      case '<=':
+        return v1 <= v2 ? options.fn(this) : options.inverse(this)
+      case '>':
+        return v1 > v2 ? options.fn(this) : options.inverse(this)
+      case '>=':
+        return v1 >= v2 ? options.fn(this) : options.inverse(this)
+      case '&&':
+        return v1 && v2 ? options.fn(this) : options.inverse(this)
+      case '||':
+        return v1 || v2 ? options.fn(this) : options.inverse(this)
+      default:
+        return options.inverse(this)
+    }
+  })
   const template = Handlebars.compile(templateString)
   const formattedTemplateData = formatAllNonStringValues(data)
   const output = template(formattedTemplateData)
