65380c317eeb2a7e305d64bf8fa69386b1a3b3b9
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2023-05-09 12:03:57 +0600

    Revert "Make place of birth location search required false"
    This reverts commit c62c7612bbf4c5a200bc6925abc7ffc580dd3865.

--- packages/client/src/forms/configuration/default/birth.ts
+++ packages/client/src/forms/configuration/default/birth.ts
@@ -949,7 +949,7 @@ export const birthRegisterForms: ISerializedForm = {
               type: 'LOCATION_SEARCH_INPUT',
               label: formMessageDescriptors.healthInstitution,
               previewGroup: 'placeOfBirth',
-              required: false,
+              required: true,
               initialValue: '',
               searchableResource: ['facilities'],
               searchableType: ['HEALTH_FACILITY'],
