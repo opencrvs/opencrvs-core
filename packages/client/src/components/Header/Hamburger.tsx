6a757c8bad5587fccf8988b094f2caebae4cb554
Pyry Rouvila <pyry.rouvila@gmail.com>
2023-05-10 10:52:12 +0300

    fix hamburger menu not working (#5267)
    * fix hamburger menu not working
    
    * add size medium and color primary explicitly

--- packages/client/src/components/Header/Hamburger.tsx
+++ packages/client/src/components/Header/Hamburger.tsx
@@ -49,7 +49,7 @@ export function Hamburger() {
   return (
     <>
       <Button type="icon" size="medium" onClick={toggleMenu}>
-        <Icon name="List" />
+        <Icon name="List" size="medium" color="primary" />
       </Button>
       <ExpandingMenu
         showMenu={showMenu}
