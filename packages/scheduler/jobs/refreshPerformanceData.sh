2b46d628c111d02ca60b43ddaf67586df57544c5
Sadman Anik <sadman.anik@dsinnovators.com>
2023-05-15 12:54:24 +0600

    Modified some suggested changes

--- packages/scheduler/jobs/refreshPerformanceData.sh
+++ packages/scheduler/jobs/refreshPerformanceData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ( -z "${QA_ENV}" || "${QA_ENV}" != true ) && "${NODE_ENV}" == "production" ]]; then
+if [[ ( -z "${QA_ENV}" || "${QA_ENV}" != "true" ) && "${NODE_ENV}" == "production" ]]; then
   echo 'Calling metabase data refresh end-point in metrics...'
   curl "$METRICS_URL/refreshPerformanceData"
 else
