4145369381aa371b242782a6ec723d2113078e5e
naftis <pyry.rouvila@gmail.com>
2023-05-15 10:27:25 +0300

    show the value even if it is disabled

--- packages/client/src/views/RegisterForm/review/ReviewSection.tsx
+++ packages/client/src/views/RegisterForm/review/ReviewSection.tsx
@@ -960,7 +960,6 @@ class ReviewSectionComp extends React.Component<FullProps, State> {
     )
     return (
       !conditionalActions.includes('hide') &&
-      !conditionalActions.includes('disable') &&
       !conditionalActions.includes('hideInPreview')
     )
   }
