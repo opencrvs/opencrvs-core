4e0b366a87242b351f452a7f4e21c4ede613d52e
jamil314 <jamil31415926@gmail.com>
2023-05-16 17:50:20 +0600

    updated the method for differentiating between React components and DOM elements

--- packages/components/src/InputField/InputField.tsx
+++ packages/components/src/InputField/InputField.tsx
@@ -108,17 +108,12 @@ export class InputField extends React.Component<IInputFieldProps, {}> {
       color = colors.white
       hideBorder = true
     }
-    const isDomElement = (
-      nodeType: string | React.JSXElementConstructor<any>
-    ) => {
-      return typeof nodeType === 'string'
-    }
     const children = React.Children.map(
       this.props.children,
       (node: React.ReactElement) => {
-        return isDomElement(node.type)
-          ? node
-          : React.cloneElement(node, { hideBorder })
+        return React.isValidElement(node.type)
+          ? React.cloneElement(node, { hideBorder })
+          : node
       }
     )
 
