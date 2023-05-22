2b52397d50783fb53d5a2b1ff8244b947bdf0611
naftis <pyry.rouvila@gmail.com>
2023-05-22 13:02:09 +0300

    make non-demo users pending to force them to re-hash passwords

---
+++ packages/migration/migrations/user-mgnt/20230522081120-make-users-status-pending.js
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+
+// Set users status to pending, as the password hashing method has changed to more secure, so we need to invalidate the old hashes.
+export const up = async (db, client) => {
+  const session = client.startSession()
+
+  try {
+    await db.users.updateMany(
+      { scope: { $exists: true }, scope: { $nin: ['demo'] } },
+      { $set: { status: 'pending' } }
+    )
+  } finally {
+    await session.endSession()
+  }
+}
+
+export const down = async (db, client) => {
+  // Cannot set the status back to active here, as the hashes may have already changed.
+  // So even when migrating down, the users should re-apply their passwords.
+}
