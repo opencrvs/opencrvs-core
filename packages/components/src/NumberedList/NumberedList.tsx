d515639b21e387675ce2e7e76061806e082f0914
Jonathan Pye-Finch <46478402+jpye-finch@users.noreply.github.com>
2023-05-30 15:54:10 +0100

    refactor: numbered and bullet lists (#5025)
    Co-authored-by: naftis <pyry.rouvila@gmail.com>

--- packages/components/src/NumberedList/NumberedList.tsx
+++ packages/components/src/NumberedList/NumberedList.tsx
@@ -13,21 +13,21 @@ import React from 'react'
 import styled from 'styled-components'
 import { Text } from '../Text'
 
-export interface BulletListProps {
+export interface NumberedListProps {
   id?: string
   items: string[]
   font: 'reg12' | 'reg14' | 'reg16' | 'reg18' | 'h4' | 'h3' | 'h2' | 'h1'
 }
 
-const List = styled.ul`
-  list-style-type: disc;
+const List = styled.ol`
+  list-style-type: decimal;
 `
 const ListItem = styled.li`
   margin-bottom: 12px;
   padding-left: 8px;
 `
 
-export function BulletList({ id, font, items }: BulletListProps) {
+export function NumberedList({ id, font, items }: NumberedListProps) {
   return (
     <List id={id}>
       <Text variant={font} element="p" color="grey600">
