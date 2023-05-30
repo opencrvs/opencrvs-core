849d769ad62cb43404f4ce699deb0a37bb512527
naftis <pyry.rouvila@gmail.com>
2023-05-30 16:01:31 +0300

    add license info

--- packages/components/src/BulletList/BulletList.stories.tsx
+++ packages/components/src/BulletList/BulletList.stories.tsx
@@ -1,13 +1,26 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
 import React from 'react'
-import { Story, Meta } from '@storybook/react/types-6-0'
-import { BulletList, BulletListProps } from './BulletList'
+import { ComponentStory } from '@storybook/react'
+import { BulletList } from './BulletList'
 
 export default {
   title: 'Typography/Bullet List',
   component: BulletList
-} as Meta
+}
 
-const Template: Story<BulletListProps> = (args) => <BulletList {...args} />
+const Template: ComponentStory<typeof BulletList> = (args) => (
+  <BulletList {...args} />
+)
 
 export const Default = Template.bind({})
 Default.args = {
