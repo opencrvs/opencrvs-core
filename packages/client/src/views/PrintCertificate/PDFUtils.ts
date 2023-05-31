538916b0e540cc0a84bd2104fb8f9e0824de2723
ronald ndi <wilrona@gmail.com>
2023-05-31 09:47:42 +0100

    change intl handlerBar

--- packages/client/src/views/PrintCertificate/PDFUtils.ts
+++ packages/client/src/views/PrintCertificate/PDFUtils.ts
@@ -183,24 +183,24 @@ export function executeHandlebarsTemplate(
         return ''
       }
 
-      const id =
-        idParts.length === 3
-          ? idParts.slice(0, -1).join('.')
-          : idParts.join('.')
+      const id = idParts.join('.')
 
-      const value = intl.formatMessage({
+      // const value = intl.formatMessage({
+      //   id,
+      //   defaultMessage: 'Missing translation for ' + id
+      // })
+
+      // if (
+      //   idParts.includes('uppercase') &&
+      //   !value.split(' ').includes('Missing')
+      // ) {
+      //   return value.toUpperCase()
+      // }
+
+      return intl.formatMessage({
         id,
         defaultMessage: 'Missing translation for ' + id
       })
-
-      if (
-        idParts.includes('uppercase') &&
-        !value.split(' ').includes('Missing')
-      ) {
-        return value.toUpperCase()
-      }
-
-      return value
     } as any /* This is here because Handlebars typing is insufficient and we can make the function type stricter */
   )
 
