eb97bd6a67d12d40748e8ccef4cdee86fc960325
Sadman Anik <sadman.anik@dsinnovators.com>
2023-06-05 12:48:42 +0600

    Show state, district, city properly on correction view after location change

--- packages/client/src/views/CorrectionForm/utils.ts
+++ packages/client/src/views/CorrectionForm/utils.ts
@@ -304,8 +304,12 @@ export function renderSelectDynamicLabel(
     if (options.resource) {
       let selectedLocation: ILocation
       const locationId = value as string
+      // HOTFIX for handling international address
       if (options.resource === 'locations') {
-        selectedLocation = resources[OFFLINE_LOCATIONS_KEY][locationId]
+        selectedLocation = resources[OFFLINE_LOCATIONS_KEY][locationId] || {
+          name: locationId,
+          alias: locationId
+        }
       } else {
         selectedLocation = resources[OFFLINE_FACILITIES_KEY][locationId]
       }
@@ -386,11 +390,15 @@ export const renderValue = (
     [
       'statePrimary',
       'districtPrimary',
+      'cityUrbanOptionPrimary',
       'internationalStatePrimary',
       'internationalDistrictPrimary',
+      'internationalCityPrimary',
       'stateSecondary',
       'districtSecondary',
+      'cityUrbanOptionSecondary',
       'internationalStateSecondary',
+      'internationalCitySecondary',
       'internationalDistrictSecondary'
     ].includes(field.name)
   ) {
--- packages/components/src/Tooltip/Tooltip.tsx
+++ packages/components/src/Tooltip/Tooltip.tsx
@@ -1,3 +1,15 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+
 import React from 'react'
 import styled from 'styled-components'
 
@@ -18,8 +30,8 @@ const TooltipContent = styled.div<{ position: TooltipProps['position'] }>`
   opacity: 0;
   transition: opacity 0.3s;
   white-space: nowrap;
-  
-${({ position }) => {
+
+  ${({ position }) => {
     switch (position) {
       case 'top':
         return `
