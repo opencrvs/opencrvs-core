9c415c9ba4c8dd93a0d0b2033e514ac7b60b27b0
Robin <46846503+kayumuzzaman@users.noreply.github.com>
2023-06-23 11:21:53 +0600

    ocrvs-5459 add marriage configuration on/off for vital event (#5497)
    * add marriage configuration on/off for vitatl event
    
    * modify some area regarding marriage config on/off
    
    * add missing data for test marriage config on/off
    
    * Update 20230208081755-add-marriage-configuration.js

--- packages/client/src/views/SelectVitalEvent/SelectVitalEvent.tsx
+++ packages/client/src/views/SelectVitalEvent/SelectVitalEvent.tsx
@@ -169,21 +169,23 @@ class SelectVitalEventView extends React.Component<
                   this.setState({ goTo: 'death', noEventSelectedError: false })
                 }
               />
-              <RadioButton
-                size="large"
-                key="marriagevent"
-                name="marriageevent"
-                label={intl.formatMessage(constantsMessages.marriage)}
-                value="marriage"
-                id="select_marriage_event"
-                selected={this.state.goTo === 'marriage' ? 'marriage' : ''}
-                onChange={() =>
-                  this.setState({
-                    goTo: 'marriage',
-                    noEventSelectedError: false
-                  })
-                }
-              />
+              {window.config.MARRIAGE_REGISTRATION && (
+                <RadioButton
+                  size="large"
+                  key="marriagevent"
+                  name="marriageevent"
+                  label={intl.formatMessage(constantsMessages.marriage)}
+                  value="marriage"
+                  id="select_marriage_event"
+                  selected={this.state.goTo === 'marriage' ? 'marriage' : ''}
+                  onChange={() =>
+                    this.setState({
+                      goTo: 'marriage',
+                      noEventSelectedError: false
+                    })
+                  }
+                />
+              )}
             </Actions>
             <PrimaryButton id="continue" onClick={this.handleContinue}>
               {intl.formatMessage(buttonMessages.continueButton)}
