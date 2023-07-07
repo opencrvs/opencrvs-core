8a439f11a87261f076d2483b6d0334b69838c46a
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2023-07-07 19:35:56 +0600

    ocrvs-5540 Seed roles (#5580)
    * Create post endpoint for systemRole
    
    * Show validation errors from user-mgnt in dev
    
    * Seed user roles from country metadata

--- packages/gateway/src/features/dataSeeding/index.ts
+++ packages/gateway/src/features/dataSeeding/index.ts
@@ -9,8 +9,17 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
-
-import { AUTH_URL, SUPER_USER_PASSWORD } from '@gateway/constants'
+import {
+  AUTH_URL,
+  COUNTRY_CONFIG_URL,
+  SUPER_USER_PASSWORD,
+  USER_MANAGEMENT_URL
+} from '@gateway/constants'
+import {
+  GQLRoleInput,
+  GQLSystemRole,
+  GQLSystemRoleInput
+} from '@gateway/graphql/schema'
 import fetch from 'node-fetch'
 
 async function getToken(): Promise<string> {
@@ -27,12 +36,93 @@ async function getToken(): Promise<string> {
   })
   const body = await res.json()
   if (!res.ok) {
-    throw Error(`authenticating super user failed with error ${body}`)
+    throw Error(
+      `authenticating super user failed with error ${JSON.stringify(body)}`
+    )
   }
   return body.token
 }
 
+const SYSTEM_ROLES = [
+  'FIELD_AGENT',
+  'LOCAL_REGISTRAR',
+  'LOCAL_SYSTEM_ADMIN',
+  'NATIONAL_REGISTRAR',
+  'NATIONAL_SYSTEM_ADMIN',
+  'PERFORMANCE_MANAGEMENT',
+  'REGISTRATION_AGENT'
+] as const
+
+type SystemRole = Omit<GQLSystemRole, 'id'> & {
+  _id: string
+  roles: GQLRoleInput[]
+}
+
+async function createSystemRoles(token: string): Promise<SystemRole[]> {
+  const url = new URL('systemRole', USER_MANAGEMENT_URL).toString()
+  return Promise.all(
+    SYSTEM_ROLES.map((systemRole) =>
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          value: systemRole,
+          roles: []
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        }
+      }).then((res) => res.json())
+    )
+  )
+}
+
+type RoleResponse = {
+  [K in typeof SYSTEM_ROLES[number]]?: GQLRoleInput[]
+}
+
+async function getCountryRoles() {
+  const url = new URL('roles', COUNTRY_CONFIG_URL).toString()
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error(`Expected to get the roles from ${url}`)
+  }
+  return res.json() as Promise<RoleResponse>
+}
+
+async function updateRoles(token: string, systemRoles: GQLSystemRoleInput[]) {
+  const url = new URL('updateRole', USER_MANAGEMENT_URL).toString()
+  return Promise.all(
+    systemRoles.map((systemRole) =>
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(systemRole),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        }
+      }).then((res) => res.json() as Promise<{ msg: string }>)
+    )
+  )
+}
+
 export async function seedData() {
   const token = await getToken()
-  console.log(token)
+  const systemRoles = await createSystemRoles(token)
+  const countryRoles = await getCountryRoles()
+  const usedSystemRoles = Object.keys(
+    countryRoles
+  ) as typeof SYSTEM_ROLES[number][]
+  const res = await updateRoles(
+    token,
+    systemRoles
+      .filter(({ value }) => usedSystemRoles.includes(value))
+      .map(({ _id, value, active }) => ({
+        id: _id,
+        value,
+        active,
+        roles: countryRoles[value]!
+      }))
+  )
+  console.log(res)
 }
