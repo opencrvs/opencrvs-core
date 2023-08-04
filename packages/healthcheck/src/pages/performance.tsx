be359c1df274287d0852b8ab420e51df3b87a7f3
Asaph Irabaruta <asaphirabaruta1@gmail.com>
2023-08-04 16:14:27 +0200

    removed typescript warning by replacing frameborder prop of iframe

--- packages/healthcheck/src/pages/performance.tsx
+++ packages/healthcheck/src/pages/performance.tsx
@@ -15,13 +15,13 @@ export default function performance() {
             src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=2"
             width="100"
             height="100"
-            frameborder="0"
+            style={{ border: '0px' }}
           />
           <iframe
             src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=7"
             width="100"
             height="100"
-            frameborder="0"
+            style={{ border: '0px' }}
           />
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
@@ -29,32 +29,32 @@ export default function performance() {
             src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=9"
             width="100"
             height="100"
-            frameborder="0"
+            style={{ border: '0px' }}
           />
           <iframe
             src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=8"
             width="100"
             height="100"
-            frameborder="0"
+            style={{ border: '0px' }}
           />
         </div>
         <iframe
           src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=6"
           width="240"
           height="205"
-          frameborder="0"
+          style={{ border: '0px' }}
         />
         <iframe
           src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=4"
           width="240"
           height="205"
-          frameborder="0"
+          style={{ border: '0px' }}
         />
         <iframe
           src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&panelId=5"
           width="240"
           height="205"
-          frameborder="0"
+          style={{ border: '0px' }}
         />
       </div>
       <div style={{ display: 'flex', marginLeft: '20px', marginTop: '10px' }}>
@@ -62,24 +62,15 @@ export default function performance() {
           src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&tab=transform&refresh=5s&panelId=1"
           width="480"
           height="350"
-          frameBorder="0"
+          style={{ border: '0px' }}
         />
         <iframe
-          style={{ marginLeft: '10px' }}
+          style={{ marginLeft: '10px', border: '0px' }}
           src="http://localhost:3002/d-solo/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&tab=transform&refresh=5s&panelId=3"
           width="480"
           height="350"
-          frameborder="0"
         />
       </div>
     </div>
-    // <div>
-    //   <iframe
-    //     src="http://localhost:3002/d/f662c48a-63b1-4093-af9b-e8677f77152e/host-resource-usage?orgId=1&tab=transform&refresh=5s"
-    //     width="1000"
-    //     height="700"
-    //     frameborder="0"
-    //   ></iframe>
-    // </div>
   )
 }
