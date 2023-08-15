d3f85b7cac84a5a97bc5907ac2563e0602a32ee2
Eezi <tomi.a.salo@outlook.com>
2023-08-15 16:38:50 +0300

    Changes

--- packages/gateway/src/features/user/database.ts
+++ packages/gateway/src/features/user/database.ts
@@ -33,7 +33,7 @@ export async function start() {
   logger.info(`REDIS_HOST, ${JSON.stringify(REDIS_HOST)}`)
   redisClient = redis.createClient({
     host: REDIS_HOST,
-    retry_strategy: (options) => {
+    retry_strategy: () => {
       return 1000
     }
   })
