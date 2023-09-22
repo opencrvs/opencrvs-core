2785ea677351728403f8effe755a1ed8769eb962
Md. Ashikul Alam <32668488+Nil20@users.noreply.github.com>
2023-09-22 16:14:56 +0600

    back button hide for first section (#5963)
    Co-authored-by: Pyry Rouvila <pyry.rouvila@gmail.com>

--- packages/client/src/views/RegisterForm/RegisterForm.tsx
+++ packages/client/src/views/RegisterForm/RegisterForm.tsx
@@ -971,6 +971,8 @@ class RegisterFormView extends React.Component<FullProps, State> {
     const isErrorOccured = this.state.hasError
     const debouncedModifyDeclaration = debounce(this.modifyDeclaration, 300)
     const isDocumentUploadPage = this.props.match.params.pageId === 'documents'
+    const introSection =
+      findFirstVisibleSection(registerForm.sections).id === activeSection.id
     return (
       <>
         <TimeMounted
@@ -1042,16 +1044,18 @@ class RegisterFormView extends React.Component<FullProps, State> {
                 <>
                   <Frame.LayoutForm>
                     <Frame.SectionFormBackAction>
-                      <BackButtonContainer>
-                        <Button
-                          type="tertiary"
-                          size="small"
-                          onClick={this.props.goBack}
-                        >
-                          <Icon name="ArrowLeft" size="medium" />
-                          {intl.formatMessage(buttonMessages.back)}
-                        </Button>
-                      </BackButtonContainer>
+                      {!introSection && (
+                        <BackButtonContainer>
+                          <Button
+                            type="tertiary"
+                            size="small"
+                            onClick={this.props.goBack}
+                          >
+                            <Icon name="ArrowLeft" size="medium" />
+                            {intl.formatMessage(buttonMessages.back)}
+                          </Button>
+                        </BackButtonContainer>
+                      )}
                     </Frame.SectionFormBackAction>
                     <Frame.Section>
                       <Content
@@ -1292,7 +1296,7 @@ function getValidSectionGroup(
 } {
   const currentSection = sectionId
     ? sections.find((sec) => sec.id === sectionId)
-    : firstVisibleSection(sections)
+    : findFirstVisibleSection(sections)
   if (!currentSection) {
     throw new Error(`Section with id "${sectionId}" not found `)
   }
@@ -1355,14 +1359,14 @@ export function replaceInitialValues(
   }))
 }
 
-function firstVisibleSection(sections: IFormSection[]) {
+function findFirstVisibleSection(sections: IFormSection[]) {
   return sections.filter(({ viewType }) => viewType !== 'hidden')[0]
 }
 
 function mapStateToProps(state: IStoreState, props: IFormProps & RouteProps) {
   const { match, registerForm, declaration } = props
   const sectionId =
-    match.params.pageId || firstVisibleSection(registerForm.sections).id
+    match.params.pageId || findFirstVisibleSection(registerForm.sections).id
   const userDetails = getUserDetails(state)
   const groupId = match.params.groupId
   const { activeSection, activeSectionGroup } = getValidSectionGroup(
