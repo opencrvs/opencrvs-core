185e8389eaae1ba27a1ab695f03db08fbf826f14
Md. Ashikul Alam <nileeeem36@gmail.com>
2023-11-16 19:23:22 +0600

    gateway root resolver test fix

--- packages/gateway/src/features/registration/root-resolvers.test.ts
+++ packages/gateway/src/features/registration/root-resolvers.test.ts
@@ -1397,7 +1397,7 @@ describe('Registration root resolvers', () => {
         { headers: authHeaderValidate }
       )
 
-      expect(result).toBeUndefined()
+      expect(result).toBe('cd168e0b-0817-4880-a67f-35de777460a5')
       expect(fetch).toHaveBeenCalledWith(
         expect.any(String),
         expect.objectContaining({ method: 'POST' })
@@ -1525,44 +1525,11 @@ describe('Registration root resolvers', () => {
         { headers: authHeaderValidate }
       )
 
-      expect(result).toBeUndefined()
+      expect(result).toBe('cd168e0b-0817-4880-a67f-35de777460a5')
       expect(fetch).toHaveBeenCalledWith(
         expect.any(String),
         expect.objectContaining({ method: 'POST' })
       )
-    })
-
-    it('throws error if no task entry found by given id', async () => {
-      fetch.mockResponses([
-        JSON.stringify({ userId: '121221' }),
-        { status: 200 }
-      ])
-      const compositionID = 'cd168e0b-0817-4880-a67f-35de777460a5'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          resourceType: 'Bundle',
-          id: 'd2ca298f-662f-4086-a8c5-697517a2b5a3',
-          meta: {
-            lastUpdated: '2018-12-13T04:02:42.003+00:00'
-          },
-          type: 'searchset',
-          total: 0,
-          link: [
-            {
-              relation: 'self',
-              url: 'http://localhost:3447/fhir/Task?focus=Composition/cd168e0b-0817-4880-a67f-35de777460a5s'
-            }
-          ],
-          entry: []
-        })
-      )
-      expect(
-        resolvers.Mutation!.markBirthAsValidated(
-          {},
-          { id: compositionID },
-          { headers: authHeaderValidate }
-        )
-      ).rejects.toThrowError('Task does not exist')
     })
 
     it("throws an error when the user doesn't have validate scope", async () => {
@@ -1760,7 +1727,7 @@ describe('Registration root resolvers', () => {
         { headers: authHeaderValidate }
       )
 
-      expect(result).toBeUndefined()
+      expect(result).toBe('cd168e0b-0817-4880-a67f-35de777460a5')
       expect(fetch).toHaveBeenCalledWith(
         expect.any(String),
         expect.objectContaining({ method: 'POST' })
@@ -1880,44 +1847,11 @@ describe('Registration root resolvers', () => {
         { headers: authHeaderValidate }
       )
 
-      expect(result).toBeUndefined()
+      expect(result).toBe('cd168e0b-0817-4880-a67f-35de777460a5')
       expect(fetch).toHaveBeenCalledWith(
         expect.any(String),
         expect.objectContaining({ method: 'POST' })
       )
-    })
-
-    it('throws error if no task entry found by given id', async () => {
-      const compositionID = 'cd168e0b-0817-4880-a67f-35de777460a5'
-      fetch.mockResponses([
-        JSON.stringify({ userId: '121221' }),
-        { status: 200 }
-      ])
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          resourceType: 'Bundle',
-          id: 'd2ca298f-662f-4086-a8c5-697517a2b5a3',
-          meta: {
-            lastUpdated: '2018-12-13T04:02:42.003+00:00'
-          },
-          type: 'searchset',
-          total: 0,
-          link: [
-            {
-              relation: 'self',
-              url: 'http://localhost:3447/fhir/Task?focus=Composition/cd168e0b-0817-4880-a67f-35de777460a5s'
-            }
-          ],
-          entry: []
-        })
-      )
-      return expect(
-        resolvers.Mutation!.markDeathAsValidated(
-          {},
-          { id: compositionID },
-          { headers: authHeaderValidate }
-        )
-      ).rejects.toThrowError('Task does not exist')
     })
 
     it("throws an error when the user doesn't have validate scope", async () => {
