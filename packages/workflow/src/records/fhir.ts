57478073ca13bf42f6c60ea529a2a8731916b911
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2024-01-26 14:33:28 +0600

    Keep signature extensions from previous task

--- packages/workflow/src/records/fhir.ts
+++ packages/workflow/src/records/fhir.ts
@@ -1032,7 +1032,11 @@ export function createCertifiedTask(
         [
           'http://opencrvs.org/specs/extension/contact-person-phone-number',
           'http://opencrvs.org/specs/extension/informants-signature',
-          'http://opencrvs.org/specs/extension/contact-person-email'
+          'http://opencrvs.org/specs/extension/contact-person-email',
+          'http://opencrvs.org/specs/extension/bride-signature',
+          'http://opencrvs.org/specs/extension/groom-signature',
+          'http://opencrvs.org/specs/extension/witness-one-signature',
+          'http://opencrvs.org/specs/extension/witness-two-signature'
         ].includes(extension.url)
       )
     ],
@@ -1068,7 +1072,11 @@ export function createIssuedTask(
         [
           'http://opencrvs.org/specs/extension/contact-person-phone-number',
           'http://opencrvs.org/specs/extension/informants-signature',
-          'http://opencrvs.org/specs/extension/contact-person-email'
+          'http://opencrvs.org/specs/extension/contact-person-email',
+          'http://opencrvs.org/specs/extension/bride-signature',
+          'http://opencrvs.org/specs/extension/groom-signature',
+          'http://opencrvs.org/specs/extension/witness-one-signature',
+          'http://opencrvs.org/specs/extension/witness-two-signature'
         ].includes(extension.url)
       )
     ],
