d6052abc1efcfc1215e1bd2e8d313f214837925c
naftis <pyry.rouvila@gmail.com>
2024-03-18 14:24:59 +0200

    fix: use node-fetch instead of implicit fetch

--- packages/workflow/src/records/webhooks.ts
+++ packages/workflow/src/records/webhooks.ts
@@ -8,7 +8,7 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-
+import fetch from 'node-fetch'
 import { EVENT_TYPE, RegisteredRecord } from '@opencrvs/commons/types'
 import { WEBHOOKS_URL } from '@workflow/constants'
 
