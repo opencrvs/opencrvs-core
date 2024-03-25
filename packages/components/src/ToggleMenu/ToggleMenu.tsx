opencrvs/opencrvs-core

commit 4eac74b065157e894aa617036f4cd953f38be1f9

convert class based React components to function components (#6591)
* Converted ResetCredentialsSuccessView component

* Converted ToggleMenu component

* Converted Table and PasswordInput components

* Removed unused ListTable component

* Converted WorkqueueComp component

* Removed ListTable export

* Converted DocumentViewer component

* Removed unused ListTable import

* Added back the LoadingGrey component

* Changed the PasswordInput props type

* Changed the src to lib

* run yarn recreate-src-index-ts to fix tests

---------

Co-authored-by: naftis <pyry.rouvila@gmail.com>

packages/components/src/ToggleMenu/ToggleMenu.tsx
@@ -9,7 +9,7 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 import styled from 'styled-components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../Button'
 import { noop } from 'lodash'
 
@@ -85,93 +85,78 @@ interface IProps {
   hide?: boolean
 }
 
-interface IState {
-  showSubmenu: boolean
-}
+export const ToggleMenu = ({
+  id,
+  menuHeader,
+  toggleButton,
+  menuItems,
+  hide
+}: IProps) => {
+  const [showSubmenu, setShowSubmenu] = useState(false)
 
-export class ToggleMenu extends React.Component<IProps, IState> {
-  constructor(props: IProps & IState) {
-    super(props)
-    this.state = {
-      showSubmenu: false
-    }
-    this.toggleMenu = this.toggleMenu.bind(this)
-    this.closeMenu = this.closeMenu.bind(this)
-    this.closeMenuOnEscape = this.closeMenuOnEscape.bind(this)
+  const closeMenu = () => {
+    setShowSubmenu(false)
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('click', this.closeMenu)
-    document.removeEventListener('click', this.toggleMenu)
-    document.removeEventListener('keyup', this.closeMenuOnEscape)
-  }
-  toggleMenu() {
-    this.setState(() => ({
-      showSubmenu: !this.state.showSubmenu
-    }))
-
-    if (!this.state.showSubmenu) {
-      //https://github.com/facebook/react/issues/24657#issuecomment-1150119055
-      setTimeout(() => document.addEventListener('click', this.closeMenu), 0)
-      setTimeout(
-        () => document.addEventListener('keyup', this.closeMenuOnEscape),
-        0
-      )
-    }
-  }
-
-  closeMenu() {
-    this.setState(() => ({
-      showSubmenu: false
-    }))
-    document.removeEventListener('click', this.closeMenu)
-    document.removeEventListener('keyup', this.closeMenuOnEscape)
-  }
-
-  closeMenuOnEscape(e: KeyboardEvent) {
+  const closeMenuOnEscape = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
-      this.closeMenu()
+      closeMenu()
     }
   }
 
-  render() {
-    const { id, toggleButton, menuHeader, menuItems, hide } = this.props
-    if (hide) {
-      return null
+  useEffect(() => {
+    if (showSubmenu) {
+      // To ensure the event listeners are correctly cleaned up, avoid using setTimeout if possible.
+      document.addEventListener('click', closeMenu)
+      document.addEventListener('keyup', closeMenuOnEscape)
     }
-    return (
-      <>
-        <ToggleMenuContainer aria-expanded={this.state.showSubmenu}>
-          <Button
-            type="icon"
-            size="large"
-            id={`${id}ToggleButton`}
-            onClick={this.toggleMenu}
-          >
-            {toggleButton}
-          </Button>
-          {this.state.showSubmenu && (
-            <MenuContainer id={`${id}SubMenu`}>
-              {menuHeader && <MenuHeader>{menuHeader}</MenuHeader>}
-              {menuItems.map((mi: IToggleMenuItem, index) => (
-                <MenuItem
-                  id={`${id}Item${index}`}
-                  key={`${id}-${index}`}
-                  onClick={mi.handler}
-                  onKeyUp={(e) =>
-                    e.key === 'Enter' || e.key === ' ' ? mi.handler() : noop
-                  }
-                  tabIndex={0}
-                  role="button"
-                >
-                  {mi.icon && mi.icon}
-                  {mi.label}
-                </MenuItem>
-              ))}
-            </MenuContainer>
-          )}
-        </ToggleMenuContainer>
-      </>
-    )
+
+    return () => {
+      document.removeEventListener('click', closeMenu)
+      document.removeEventListener('keyup', closeMenuOnEscape)
+    }
+  }, [showSubmenu])
+
+  const toggleMenu = () => {
+    setShowSubmenu(!showSubmenu)
   }
+
+  if (hide) {
+    return null
+  }
+
+  return (
+    <>
+      <ToggleMenuContainer aria-expanded={showSubmenu}>
+        <Button
+          type="icon"
+          size="large"
+          id={`${id}ToggleButton`}
+          onClick={toggleMenu}
+        >
+          {toggleButton}
+        </Button>
+        {showSubmenu && (
+          <MenuContainer id={`${id}SubMenu`}>
+            {menuHeader && <MenuHeader>{menuHeader}</MenuHeader>}
+            {menuItems.map((mi: IToggleMenuItem, index) => (
+              <MenuItem
+                id={`${id}Item${index}`}
+                key={`${id}-${index}`}
+                onClick={mi.handler}
+                onKeyUp={(e) =>
+                  e.key === 'Enter' || e.key === ' ' ? mi.handler() : noop
+                }
+                tabIndex={0}
+                role="button"
+              >
+                {mi.icon && mi.icon}
+                {mi.label}
+              </MenuItem>
+            ))}
+          </MenuContainer>
+        )}
+      </ToggleMenuContainer>
+    </>
+  )
 }

packages/client/src/views/SysAdmin/Performance/reports/completenessRates/CompletenessDataTable.tsx
@@ -16,7 +16,6 @@ import {
   COMPLETENESS_RATE_REPORT_BASE
 } from '@client/views/SysAdmin/Performance/CompletenessRates'
 import { SortArrow } from '@opencrvs/components/lib/icons'
-import { ListTable } from '@opencrvs/components/lib/ListTable'
 import { orderBy } from 'lodash'
 import * as React from 'react'
 import { injectIntl, WrappedComponentProps } from 'react-intl'

packages/client/src/views/SysAdmin/SysAdminContentWrapper.tsx
@@ -13,7 +13,7 @@ import { MarginedHeader } from '@client/components/Header/Header'
 import { CircleButton } from '@opencrvs/components/lib/buttons'
 import { BackArrowDeepBlue } from '@opencrvs/components/lib/icons'
 import { BodyContent, Container } from '@opencrvs/components/lib/Content'
-import { LoadingGrey } from '@opencrvs/components/lib/ListTable'
+import { LoadingGrey } from '@opencrvs/components/lib/LoadingGrey'
 import * as React from 'react'
 import styled from 'styled-components'
 import { FixedNavigation } from '@client/components/interface/Navigation'

packages/client/src/views/UserAudit/UserAuditHistory.tsx
@@ -24,7 +24,7 @@ import type {
   GQLUserAuditLogResultSet
 } from '@client/utils/gateway-deprecated-do-not-use'
 import { ArrowDownBlue } from '@opencrvs/components/lib/icons'
-import { LoadingGrey } from '@opencrvs/components/lib/ListTable'
+import { LoadingGrey } from '@opencrvs/components/lib/LoadingGrey'
 import { Table } from '@opencrvs/components/lib/Table'
 import { GenericErrorToast } from '@client/components/GenericErrorToast'
 import { DateRangePicker } from '@client/components/DateRangePicker'

packages/components/src/DocumentViewer/DocumentViewer.tsx
@@ -8,12 +8,11 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Select, ISelectOption as SelectComponentOptions } from '../Select'
 import PanViewer from './components/PanViewer'
 import PanControls from './components/PanControls'
-import { isEqual } from 'lodash'
 
 const ViewerWrapper = styled.div`
   position: relative;
@@ -61,113 +60,80 @@ interface IProps {
   children?: React.ReactNode
 }
 
-interface IState {
-  selectedOption: string
-  selectedDocument: string
-  zoom: number
-  rotation: number
-}
+export const DocumentViewer = ({ id, options, children }: IProps) => {
+  const [selectedOption, setSelectedOption] = useState(
+    options.selectOptions[0]?.value || ''
+  )
+  const [selectedDocument, setSelectedDocument] = useState(
+    options.documentOptions[0]?.value || ''
+  )
+  const [zoom, setZoom] = useState(1)
+  const [rotation, setRotation] = useState(0)
 
-export class DocumentViewer extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
+  useEffect(() => {
+    setSelectedOption(options.selectOptions[0]?.value || '')
+    setSelectedDocument(options.documentOptions[0]?.value || '')
+  }, [options])
 
-    this.state = {
-      selectedOption:
-        typeof this.props.options.selectOptions[0] !== 'undefined'
-          ? this.props.options.selectOptions[0].value
-          : '',
-      selectedDocument:
-        typeof this.props.options.documentOptions[0] !== 'undefined'
-          ? this.props.options.documentOptions[0].value
-          : '',
-      zoom: 1,
-      rotation: 0
-    }
+  const zoomIn = () => {
+    setZoom((prev) => prev + 0.2)
   }
 
-  componentDidUpdate(prevProps: IProps) {
-    if (!isEqual(prevProps.options, this.props.options)) {
-      this.setState({
-        selectedOption:
-          typeof this.props.options.selectOptions[0] !== 'undefined'
-            ? this.props.options.selectOptions[0].value
-            : '',
-        selectedDocument:
-          typeof this.props.options.documentOptions[0] !== 'undefined'
-            ? this.props.options.documentOptions[0].value
-            : ''
-      })
-    }
-  }
-
-  zoomIn = () => {
-    this.setState((prevState) => ({ ...prevState, zoom: prevState.zoom + 0.2 }))
-  }
-
-  zoomOut = () => {
-    this.setState((prevState) => {
-      if (prevState.zoom >= 1) {
-        return { ...prevState, zoom: prevState.zoom - 0.2 }
-      } else {
-        return prevState
+  const zoomOut = () => {
+    setZoom((prev) => {
+      if (prev >= 1) {
+        return prev - 0.2
       }
+      return prev
     })
   }
 
-  rotateLeft = () => {
-    this.setState((prevState) => ({
-      rotation: (prevState.rotation - 90) % 360
-    }))
+  const rotateLeft = () => {
+    setRotation((prev) => (prev - 90) % 360)
   }
 
-  render() {
-    const { options, children, id } = this.props
-    const isSupportingDocumentsEmpty =
-      this.state.selectedDocument && this.state.selectedDocument.length > 0
+  const isSupportingDocumentsEmpty =
+    selectedDocument && selectedDocument.length > 0
 
-    return (
-      <ViewerWrapper id={id}>
-        <>
-          <ViewerContainer>
-            <ViewerHeader>
-              <Select
-                id="select_document"
-                options={options.selectOptions}
-                color="inherit"
-                value={this.state.selectedOption as string}
-                onChange={(val: string) => {
-                  const imgArray = options.documentOptions.filter((doc) => {
-                    return doc.label === val
-                  })
-                  if (imgArray[0]) {
-                    this.setState({
-                      selectedOption: val,
-                      selectedDocument: imgArray[0].value
-                    })
-                  }
-                }}
+  return (
+    <ViewerWrapper id={id}>
+      <>
+        <ViewerContainer>
+          <ViewerHeader>
+            <Select
+              id="select_document"
+              options={options.selectOptions}
+              color="inherit"
+              value={selectedOption}
+              onChange={(val: string) => {
+                const imgArray = options.documentOptions.filter((doc) => {
+                  return doc.label === val
+                })
+                if (imgArray[0]) {
+                  setSelectedDocument(imgArray[0].value)
+                  setSelectedOption(val)
+                }
+              }}
+            />
+            <PanControls
+              zoomIn={zoomIn}
+              zoomOut={zoomOut}
+              rotateLeft={rotateLeft}
+            />
+          </ViewerHeader>
+          {isSupportingDocumentsEmpty && (
+            <ViewerImage>
+              <PanViewer
+                id="document_image"
+                image={selectedDocument}
+                zoom={zoom}
+                rotation={rotation}
               />
-              <PanControls
-                zoomIn={this.zoomIn}
-                zoomOut={this.zoomOut}
-                rotateLeft={this.rotateLeft}
-              />
-            </ViewerHeader>
-            {isSupportingDocumentsEmpty && (
-              <ViewerImage>
-                <PanViewer
-                  id="document_image"
-                  image={this.state.selectedDocument}
-                  zoom={this.state.zoom}
-                  rotation={this.state.rotation}
-                />
-              </ViewerImage>
-            )}
-            {children}
-          </ViewerContainer>
-        </>
-      </ViewerWrapper>
-    )
-  }
+            </ViewerImage>
+          )}
+          {children}
+        </ViewerContainer>
+      </>
+    </ViewerWrapper>
+  )
 }

commit 782d19997baaa455ac0c9c4690027d966fbfb660

Fix bug introduced in #6591

packages/components/src/ToggleMenu/ToggleMenu.tsx
@@ -9,7 +9,7 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 import styled from 'styled-components'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button } from '../Button'
 import { noop } from 'lodash'
 
@@ -94,21 +94,23 @@ export const ToggleMenu = ({
 }: IProps) => {
   const [showSubmenu, setShowSubmenu] = useState(false)
 
-  const closeMenu = () => {
+  const closeMenuRef = useRef(() => {
     setShowSubmenu(false)
-  }
+  })
 
-  const closeMenuOnEscape = (e: KeyboardEvent) => {
+  const closeMenuOnEscapeRef = useRef((e: KeyboardEvent) => {
     if (e.key === 'Escape') {
-      closeMenu()
+      closeMenuRef.current()
     }
-  }
+  })
 
   useEffect(() => {
+    const closeMenu = closeMenuRef.current
+    const closeMenuOnEscape = closeMenuOnEscapeRef.current
     if (showSubmenu) {
-      // To ensure the event listeners are correctly cleaned up, avoid using setTimeout if possible.
-      document.addEventListener('click', closeMenu)
-      document.addEventListener('keyup', closeMenuOnEscape)
+      //https://github.com/facebook/react/issues/24657#issuecomment-1150119055
+      setTimeout(() => document.addEventListener('click', closeMenu), 0)
+      setTimeout(() => document.addEventListener('keyup', closeMenuOnEscape), 0)
     }
 
     return () => {