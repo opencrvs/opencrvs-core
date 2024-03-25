5c402eabc5956ce0c74064fa4f2f4143065670ab
Pyry Rouvila <pyry.rouvila@gmail.com>
2024-03-25 11:33:55 +0200

    chore: enable webhooks after state transition work (#6632)
    * chore: rename filenames to be kebab-case instead of camelCase
    
    * refactor: define marriage event configuration for notifications
    
    * feat: allow sending webhooks by creating a boilerplate
    
    they are not being sent yet
    
    * feat: type-check WEBHOOK_URLS
    
    * feat: invoke webhooks from workflow
    
    * feat: create the permission bundle without re-fetching the items
    
    * fix: add webhooks url to constant mocks in workflow
    
    * fix: use node-fetch instead of implicit fetch
    
    * fix(tests): workflow tests didnt trigger webhooks
    
    * fix(tests): mock workflow register test for webhooks
    
    * feat(tests): improve and clarify how the permission bundle is composed

--- packages/workflow/src/records/handler/issue.ts
+++ packages/workflow/src/records/handler/issue.ts
@@ -11,7 +11,7 @@
 import { IssuedRecord } from '@opencrvs/commons/types'
 import { createRoute } from '@workflow/states'
 import { validateRequest } from '@workflow/utils'
-import { getToken } from '@workflow/utils/authUtils'
+import { getToken } from '@workflow/utils/auth-utils'
 import { IssueRequestSchema } from '@workflow/records/validations'
 import { toIssued } from '@workflow/records/state-transitions'
 import { uploadCertificateAttachmentsToDocumentsStore } from '@workflow/documents'
