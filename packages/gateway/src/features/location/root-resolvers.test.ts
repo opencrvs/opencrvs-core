f4d49026f38f5190e83658bc60f15b9a4943e139
naftis <pyry.rouvila@gmail.com>
2024-04-26 16:39:22 +0300

    remove the tests of queries that arent used

--- packages/gateway/src/features/location/root-resolvers.test.ts
+++ packages/gateway/src/features/location/root-resolvers.test.ts
@@ -14,34 +14,6 @@ import * as fetchAny from 'jest-fetch-mock'
 const fetch = fetchAny as any
 
 describe('Location root resolvers', () => {
-  describe('locationsByParent()', () => {
-    it('returns an array of location results', async () => {
-      fetch.mockResponseOnce(JSON.stringify({ entry: [{}, {}] }))
-      // @ts-ignore
-      const compositions = await resolvers.Query!.locationsByParent(
-        {},
-        { parentId: '1' },
-        { headers: undefined }
-      )
-
-      expect(compositions).toBeDefined()
-      expect(compositions).toBeInstanceOf(Array)
-      expect(compositions).toHaveLength(2)
-    })
-  })
-  describe('locationById()', () => {
-    it('returns a location object', async () => {
-      fetch.mockResponseOnce(JSON.stringify({ resourceType: 'Location' }))
-      // @ts-ignore
-      const composition = await resolvers.Query!.locationById(
-        {},
-        { locationId: '1' },
-        { headers: undefined }
-      )
-
-      expect(composition).toBeDefined()
-    })
-  })
   describe('hasChildLocation()', () => {
     it('returns a location object if found', async () => {
       fetch.mockResponseOnce(
