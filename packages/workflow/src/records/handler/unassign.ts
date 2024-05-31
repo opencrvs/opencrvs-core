4feae339fd76a0699817adedecad408ac164c8f3
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2024-05-31 18:52:43 +0600

    chore(workflow): amend assignment check

--- packages/workflow/src/records/handler/unassign.ts
+++ packages/workflow/src/records/handler/unassign.ts
@@ -18,19 +18,37 @@ import { indexBundleToRoute } from '@workflow/records/search'
 import { sendBundleToHearth } from '@workflow/records/fhir'
 import { auditEvent } from '@workflow/records/audit'
 import {
+  Extension,
   findExtension,
   isTaskOrTaskHistory,
   Resource,
-  resourceIdentifierToUUID
+  resourceIdentifierToUUID,
+  Task
 } from '@opencrvs/commons/types'
 import { getTokenPayload } from '@opencrvs/commons/authentication'
 import { getUserOrSystemByCriteria } from '@workflow/records/user'
+import { getTaskBusinessStatus } from '@workflow/features/task/fhir/utils'
 
 function sortDescending(
   a: Resource & { lastModified: string },
   b: Resource & { lastModified: string }
 ) {
   return new Date(b.lastModified).valueOf() - new Date(a.lastModified).valueOf()
+}
+
+function findTaskIndexByExtension(
+  tasks: Task[],
+  extensionUrl: Extension['url']
+) {
+  return tasks.findIndex((entry) =>
+    entry.extension.some((ext) => ext.url === extensionUrl)
+  )
+}
+
+const getLastUnassignedTaskIndex = (index1: number, index2: number) => {
+  if (index1 === -1) return index2
+  if (index2 === -1) return index1
+  return Math.min(index1, index2)
 }
 
 export async function unassignRecordHandler(
@@ -66,13 +84,38 @@ export async function unassignRecordHandler(
     .filter(isTaskOrTaskHistory)
     .sort(sortDescending)
 
-  const task = allTasks.find((entry) =>
-    entry.extension.find(
-      (ext) => ext.url === 'http://opencrvs.org/specs/extension/regAssigned'
-    )
+  // declaration is unassigned when status changes
+  const latestChangedStatusIndex = allTasks.findIndex(
+    (entry, index, array) =>
+      index < array.length - 1 &&
+      getTaskBusinessStatus(entry) !== getTaskBusinessStatus(array[index + 1])
   )
 
-  if (!task) throw new Error('No task found with assignment')
+  const unassignIndex = findTaskIndexByExtension(
+    allTasks,
+    'http://opencrvs.org/specs/extension/regUnassigned'
+  )
+
+  const lastUnassignedTaskIndex = getLastUnassignedTaskIndex(
+    latestChangedStatusIndex,
+    unassignIndex
+  )
+
+  const lastAssignedTaskIndex = findTaskIndexByExtension(
+    allTasks,
+    'http://opencrvs.org/specs/extension/regAssigned'
+  )
+
+  if (lastAssignedTaskIndex === -1) throw new Error('Not assigned yet')
+
+  // check for unassigned task and status change
+  if (
+    lastUnassignedTaskIndex !== -1 &&
+    lastAssignedTaskIndex > lastUnassignedTaskIndex
+  )
+    throw new Error('Declaration is found unassigned already')
+
+  const task = allTasks[lastAssignedTaskIndex]
 
   const regLastUser = findExtension(
     'http://opencrvs.org/specs/extension/regLastUser',
