35a850fbb955a52b904f35fcac06c667ae07eed8
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2024-06-04 13:10:37 +0600

    fix: show correction rejection reason in history

--- packages/client/src/views/RecordAudit/ActionDetailsModal.tsx
+++ packages/client/src/views/RecordAudit/ActionDetailsModal.tsx
@@ -454,14 +454,13 @@ const ActionDetailsModalListTable = ({
           />
         )}
       {/* Correction rejected */}
-      {actionDetailsData.requester &&
-        actionDetailsData.action === RegAction.RejectedCorrection && (
-          <Table
-            noResultText=" "
-            columns={reasonColumn}
-            content={[{ text: actionDetailsData.reason }]}
-          />
-        )}
+      {actionDetailsData.action === RegAction.RejectedCorrection && (
+        <Table
+          noResultText=" "
+          columns={reasonColumn}
+          content={[{ text: actionDetailsData.reason }]}
+        />
+      )}
 
       {/* Correction Requester Id Verified */}
       {actionDetailsData.action === RegAction.RequestedCorrection &&
