4120ea908757b55ddec3cfd495bc6eaa242d336b
Pyry Rouvila <pyry@opencrvs.org>
2024-06-28 10:14:10 +0300

    refactor: when reindexing, remove all indices except the latest one (#7262)
    * fix: number of replicas should be 0 instead of 1
    
    * refactor: simplify pruning and remove all except the latest one
    
    * refactor: remove indices that don't have 'ocrvs' alias

--- packages/search/src/features/reindex/reindex.ts
+++ packages/search/src/features/reindex/reindex.ts
@@ -54,7 +54,7 @@ export const reindex = async () => {
     body: {
       settings: {
         number_of_shards: 1,
-        number_of_replicas: 1
+        number_of_replicas: 0
       }
     }
   })
