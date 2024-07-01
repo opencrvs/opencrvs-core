1ad5ae62371c9c7fea4146e239e7770df482f9c7
Pyry Rouvila <pyry@opencrvs.org>
2024-07-01 13:31:29 +0300

    fix: date of certification not being visible on death (#7290)
    * fix: improve type safety and fix the problems arised
    
    * fix: remove unnecessary null check

--- packages/client/src/views/VerifyCertificate/useVerificationRecordDetails.ts
+++ packages/client/src/views/VerifyCertificate/useVerificationRecordDetails.ts
@@ -10,7 +10,7 @@
  */
 
 import { gql, useQuery } from '@apollo/client'
-import { Query } from '@client/utils/gateway'
+import { FetchRecordDetailsForVerificationQuery } from '@client/utils/gateway'
 import { useParams } from 'react-router'
 
 const FETCH_RECORD_DETAILS_FOR_VERIFICATION = gql`
@@ -98,6 +98,7 @@ const FETCH_RECORD_DETAILS_FOR_VERIFICATION = gql`
         history {
           action
           regStatus
+          date
           user {
             primaryOffice {
               hierarchy {
@@ -123,7 +124,7 @@ export const useVerificationRecordDetails = () => {
     loading,
     error,
     data: queryData
-  } = useQuery<Pick<Query, 'fetchRecordDetailsForVerification'>>(
+  } = useQuery<FetchRecordDetailsForVerificationQuery>(
     FETCH_RECORD_DETAILS_FOR_VERIFICATION,
     {
       variables: { id: declarationId },
@@ -134,3 +135,7 @@ export const useVerificationRecordDetails = () => {
 
   return { loading, error, data }
 }
+
+export type RegistrationToBeVerified = NonNullable<
+  FetchRecordDetailsForVerificationQuery['fetchRecordDetailsForVerification']
+>
