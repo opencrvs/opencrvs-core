ea94ae97566c50dacd227a620b2a7d09a06aa4e1
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2024-07-12 18:26:05 +0600

    fix: bring back certificate & notification config

--- packages/client/src/components/interface/Navigation.test.tsx
+++ packages/client/src/components/interface/Navigation.test.tsx
@@ -109,6 +109,7 @@ describe('Navigation for national system admin related tests', () => {
     expect(testComponent.exists('#navigation_config_main')).toBeTruthy()
     testComponent.find('#navigation_config_main').hostNodes().simulate('click')
     testComponent.update()
+    expect(testComponent.exists('#navigation_certificate')).toBeTruthy()
   })
 
   it('No application related tabs', async () => {
