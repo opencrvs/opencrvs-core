f664c3abf0a918074ac45451713593db37e1e376
Tareq <tareq.aziz@dsinnovators.com>
2024-07-15 14:15:41 +0600

    debug.sh color refactor

--- debug-service-in-chrome.sh
+++ debug-service-in-chrome.sh
@@ -22,6 +22,16 @@ SERVICES=(
   "countryconfig:3040"
 )
 
+# ANSI color codes
+ROYAL_BLUE="\033[38;5;21m"
+RESET="\033[0m"
+PURPLE="\033[35m"
+YELLOW="\033[33m"
+CYAN="\033[36m"
+UNDERLINE="\033[4m"
+RED="\033[31m"
+GREEN="\033[32m"
+
 # Function to get port by service name
 get_port() {
   local service_name=$1
@@ -37,16 +47,16 @@ get_port() {
 
 # Function to display all supported services and their ports
 display_supported_services() {
-  echo -e "\033[33mSupported services and their ports:\033[0m"
+  echo -e "${YELLOW}Supported services and their ports:${RESET}"
   for service in "${SERVICES[@]}"; do
     IFS=":" read -r name port <<< "$service"
-    echo -e "\033[36m- $name: $port\033[0m"
+    echo -e "${ROYAL_BLUE}- $name: $port${RESET}"
   done
 }
 
 # Check if a service name is provided
 if [ -z "$1" ]; then
-  echo -e "\033[31mUsage: $0 <service-name>\033[0m"
+  echo -e "${RED}Usage: $0 <service-name>${RESET}"
   display_supported_services
   exit 1
 fi
@@ -56,7 +66,7 @@ PORT=$(get_port "$SERVICE_NAME")
 
 # Check if the port number was found for the given service name
 if [ -z "$PORT" ]; then
-  echo -e "\033[31mNo port found for service name '$SERVICE_NAME'.\033[0m"
+  echo -e "${RED}No port found for service name '$SERVICE_NAME'.${RESET}"
   display_supported_services
   exit 1
 fi
@@ -66,28 +76,30 @@ PID=$(lsof -i:$PORT -t)
 
 # Check if the PID was found
 if [ -z "$PID" ]; then
-  echo -e "\033[31mNo process found listening on port $PORT for service '$SERVICE_NAME'.\033[0m"
+  echo -e "${RED}No process found listening on port $PORT for service '$SERVICE_NAME'.${RESET}"
   display_supported_services
   exit 1
 else
   # Send the SIGUSR1 signal to the process
   kill -SIGUSR1 $PID
-  echo -e "\033[32mSent SIGUSR1 to process with PID $PID for service '$SERVICE_NAME' on port $PORT.\033[0m"
+  echo -e "${GREEN}Sent SIGUSR1 to process with PID $PID for service '$SERVICE_NAME' on port $PORT.${RESET}"
 fi
 
 # Open Chrome with the specified URL
 open -a "Google Chrome" "chrome://inspect/#devices"
 
+
+
 # Display information message
-echo -e "\033[34m====================================================\033[0m"
-echo -e "\033[33mThis script helps you debug '$SERVICE_NAME' service running on http://localhost:$PORT in Google Chrome DevTools.\033[0m"
-echo -e "\033[34m====================================================\033[0m"
-echo -e "\033[36m1. Once the DevTools are open, click 'inspect' below the 'Remote Target' link.\033[0m"
-echo -e "\033[36m2. A new window will pop up.\033[0m"
-echo -e "\033[36m3. Go to the 'Sources' tab.\033[0m"
-echo -e "\033[36m4. Press \033[35mCtrl/Command + P\033[36m and type the filename to select the file.\033[0m"
-echo -e "\033[36m5. Add breakpoints and debug your Node.js application.\033[0m"
-echo -e "\033[36m6. If you don't see your service listed as a remote target, click on 'Configure', check if 'localhost:9229' exists. If not, add it, then run the script again.\033[0m"
-echo -e "\033[36mFor more details on debugging Node.js applications, click here: \033[34m\033[4mhttps://nodejs.org/en/learn/getting-started/debugging\033[0m\033[36m.\033[0m"
-echo -e "\033[34m====================================================\033[0m"
-echo -e "\033[31mNote: If you previously ran this script for a different service (e.g., 'workflow'), try to run another service (e.g., 'auth') and it's still the previous one showing up in the remote target, ensure you stop the previous service and refresh Chrome DevTools.\033[0m"
+echo -e "${PURPLE}====================================================${RESET}"
+echo -e "${YELLOW}This script helps you debug '$SERVICE_NAME' service running on http://localhost:$PORT in Google Chrome DevTools.${RESET}"
+echo -e "${PURPLE}====================================================${RESET}"
+echo -e "${ROYAL_BLUE}1. Once the DevTools are open, click 'inspect' below the 'Remote Target' link.${RESET}"
+echo -e "${ROYAL_BLUE}2. A new window will pop up.${RESET}"
+echo -e "${ROYAL_BLUE}3. Go to the 'Sources' tab.${RESET}"
+echo -e "${ROYAL_BLUE}4. Press \033[35mCtrl/Command + P${ROYAL_BLUE} and type the filename to select the file.${RESET}"
+echo -e "${ROYAL_BLUE}5. Add breakpoints and debug your Node.js application.${RESET}"
+echo -e "${ROYAL_BLUE}6. If you don't see your service listed as a remote target, click on 'Configure', check if 'localhost:9229' exists. If not, add it, then run the script again.${RESET}"
+echo -e "${ROYAL_BLUE}For more details on debugging Node.js applications, click here: ${PURPLE}\033[4mhttps://nodejs.org/en/learn/getting-started/debugging${RESET}${ROYAL_BLUE}.${RESET}"
+echo -e "${PURPLE}====================================================${RESET}"
+echo -e "${RED}Note: If you previously ran this script for a different service (e.g., 'workflow'), try to run another service (e.g., 'auth') and it's still the previous one showing up in the remote target, ensure you stop the previous service and refresh Chrome DevTools.${RESET}"
