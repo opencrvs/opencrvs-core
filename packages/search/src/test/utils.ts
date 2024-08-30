a26e8c3a4f8710e0a8536ae497a42f9aa103d7a1
Md. Ashikul Alam <32668488+Nil20@users.noreply.github.com>
2024-08-30 18:27:08 +0600

    Fix migration issues for countries migrating from 1.2 (#7513)
    * fix(migration): check collection before creating/dropping in user-mgnt
    
    * fix: make populationEstimatePerDay collection's id more unique
    
    * fix: clean up with review changes
    
    ---------
    
    Co-authored-by: Riku Rouvila <riku.rouvila@gmail.com>

--- packages/search/src/test/utils.ts
+++ packages/search/src/test/utils.ts
@@ -2800,7 +2800,6 @@ export const mockSearchResponse = {
       hits: [
         {
           _index: 'ocrvs',
-          _type: 'compositions',
           _id: 'c99e8d62-335e-458d-9fcc-45ec5836c404',
           _score: 2.7509375,
           _source: {
@@ -2833,7 +2832,6 @@ export const mockSearchResponseWithoutCreatedBy = {
       hits: [
         {
           _index: 'ocrvs',
-          _type: 'compositions',
           _id: 'c99e8d62-335e-458d-9fcc-45ec5836c404',
           _score: 2.7509375,
           _source: {
@@ -3387,7 +3385,6 @@ export const mockSearchResult = {
       hits: [
         {
           _index: 'ocrvs',
-          _type: 'compositions',
           _id: 'BGM9CA2',
           _score: 1,
           _source: {
@@ -3404,7 +3401,6 @@ export const mockSearchResult = {
         },
         {
           _index: 'ocrvs',
-          _type: 'compositions',
           _id: 'DGM9CA2',
           _score: 1,
           _source: {
