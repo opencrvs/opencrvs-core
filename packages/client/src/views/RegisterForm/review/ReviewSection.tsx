5f364a7d21aaff9e6fc2d304fc68f6433b7bb3ba
Barry Dwyer <barry.dwyer@decodedev.co.za>
2024-09-18 16:27:28 +0200

    Improvement to only show items with values
    Missing address lines which were not required showed up as empty, making the UI look a little messy

--- packages/client/src/views/RegisterForm/review/ReviewSection.tsx
+++ packages/client/src/views/RegisterForm/review/ReviewSection.tsx
@@ -1102,7 +1102,7 @@ class ReviewSectionComp extends React.Component<FullProps, State> {
         .map((field) =>
           this.getValueOrError(section, draft.data, field, errorsOnFields)
         )
-        .filter((value) => value)
+        .filter((value) => value.props.children)
       let completeValue = values[0]
       values.shift()
       values.forEach(
@@ -1146,7 +1146,7 @@ class ReviewSectionComp extends React.Component<FullProps, State> {
               true
             )
           )
-          .filter((value) => value)
+          .filter((value) => value.props.children)
         let previousCompleteValue = <Deleted>{previousValues[0]}</Deleted>
         previousValues.shift()
         previousValues.forEach(
