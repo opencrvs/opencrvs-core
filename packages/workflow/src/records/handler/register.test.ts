b72601b6f9b1af66301f35b28e1f8ca1c5f0b670
naftis <pyry.rouvila@gmail.com>
2024-10-09 22:16:30 +0300

    chore: clean up testcontainers

--- packages/workflow/src/records/handler/register.test.ts
+++ packages/workflow/src/records/handler/register.test.ts
@@ -26,18 +26,10 @@ import { READY_FOR_REVIEW_BIRTH_RECORD } from '@test/mocks/records/readyForRevie
 describe('Register record endpoint', () => {
   const mswServer = useRequestMocks(...handlers)
 
-  let server: Awaited<ReturnType<typeof createServer>>
-
-  beforeAll(async () => {
-    server = await createServer()
+  it('flows the registration to waiting for validation', async () => {
+    const server = await createServer()
     await server.start()
-  })
 
-  afterAll(async () => {
-    await server.stop()
-  })
-
-  it('returns OK for a correctly authenticated user rejecting a birth declaration', async () => {
     const token = jwt.sign(
       { scope: ['register'] },
       readFileSync('./test/cert.key'),
@@ -58,13 +50,6 @@ describe('Register record endpoint', () => {
       )
     )
 
-    // Mock response from country-config
-    mswServer.use(
-      rest.post('http://localhost:3040/event-registration', (_, res, ctx) => {
-        return res(ctx.status(400))
-      })
-    )
-
     // Mock response from hearth
     mswServer.use(
       rest.post('http://localhost:3447/fhir', (_, res, ctx) => {
@@ -82,6 +67,13 @@ describe('Register record endpoint', () => {
           ]
         }
         return res(ctx.json(responseBundle))
+      })
+    )
+
+    // Mock response from country-config
+    mswServer.use(
+      rest.post('http://localhost:3040/event-registration', (_, res, ctx) => {
+        return res(ctx.status(200))
       })
     )
 
@@ -104,79 +96,7 @@ describe('Register record endpoint', () => {
     const businessStatus = getStatusFromTask(task)
 
     expect(response.statusCode).toBe(200)
-    expect(businessStatus).toBe('REJECTED')
-  })
-
-  it('returns OK for a correctly authenticated user registering a birth declaration', async () => {
-    const token = jwt.sign(
-      { scope: ['register'] },
-      readFileSync('./test/cert.key'),
-      {
-        algorithm: 'RS256',
-        issuer: 'opencrvs:auth-service',
-        audience: 'opencrvs:workflow-user'
-      }
-    )
-
-    // Gets record by id via getRecordById endpoint
-    mswServer.use(
-      rest.get(
-        'http://localhost:9090/records/7c3af302-08c9-41af-8701-92de9a71a3e4',
-        (_, res, ctx) => {
-          return res(ctx.json(READY_FOR_REVIEW_BIRTH_RECORD))
-        }
-      )
-    )
-
-    // Mock response from hearth
-    mswServer.use(
-      rest.post('http://localhost:3447/fhir', (_, res, ctx) => {
-        const responseBundle: TransactionResponse = {
-          resourceType: 'Bundle',
-          type: 'batch-response',
-          entry: [
-            {
-              response: {
-                status: '200',
-                location:
-                  '/fhir/Task/f00e742a-0900-488b-b7c1-9625d7b7e456/_history/dc39332f-a5d7-4422-ba7b-bc99a958e8cb' as URLReference
-              }
-            },
-            {
-              response: {
-                status: '200',
-                location:
-                  '/fhir/Patient/8cb74e54-1c02-41a7-86a3-415c4031c9ba/_history/dc39332f-a5d7-4422-ba7b-bc99a958e8cb' as URLReference
-              }
-            }
-          ]
-        }
-        return res(ctx.json(responseBundle))
-      }),
-      rest.post(
-        'http://localhost:2525/events/birth/mark-registered',
-        (_, res, ctx) => res(ctx.status(200))
-      )
-    )
-
-    const response = await server.server.inject({
-      method: 'POST',
-      url: '/confirm/registration',
-      payload: {
-        registrationNumber: '1234',
-        compositionId: '7c3af302-08c9-41af-8701-92de9a71a3e4'
-      },
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    const task = getTaskFromSavedBundle(
-      JSON.parse(response.payload) as ValidRecord
-    )
-    const businessStatus = getStatusFromTask(task)
-
-    expect(response.statusCode).toBe(200)
-    expect(businessStatus).toBe('REGISTERED')
+    expect(businessStatus).toBe('WAITING_VALIDATION')
+    await server.stop()
   })
 })
