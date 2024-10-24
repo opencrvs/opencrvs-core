152c24aad5185b1e943e3bdad6d3862e0e384da9
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2024-10-24 16:29:21 +0600

    test: add tests for isLeafLevelLocation query

--- packages/gateway/src/features/location/root-resolvers.test.ts
+++ packages/gateway/src/features/location/root-resolvers.test.ts
@@ -8,6 +8,10 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
+import {
+  savedAdministrativeLocation,
+  savedLocation
+} from '@opencrvs/commons/fixtures'
 import { resolvers } from '@gateway/features/location/root-resolvers'
 import * as fetchAny from 'jest-fetch-mock'
 
@@ -25,6 +29,38 @@ describe('Location root resolvers', () => {
       )
 
       expect(composition).toBeDefined()
+    })
+  })
+  describe('isLeafLevelLocation', () => {
+    it('returns false if a location has administrative locations as its children', async () => {
+      fetch.mockResponseOnce(
+        JSON.stringify([
+          savedAdministrativeLocation({ partOf: { reference: 'Location/1' } })
+        ])
+      )
+      // @ts-ignore
+      const isLeafLevelLocation = await resolvers.Query!.isLeafLevelLocation(
+        {},
+        { locationId: '1' },
+        { headers: undefined }
+      )
+      expect(isLeafLevelLocation).toBe(false)
+    })
+
+    it('returns true if a location has no administrative locations as its children', async () => {
+      fetch.mockResponseOnce(
+        JSON.stringify([
+          savedLocation({ partOf: { reference: 'Location/1' } }),
+          savedAdministrativeLocation({ partOf: { reference: 'Location/2' } })
+        ])
+      )
+      // @ts-ignore
+      const isLeafLevelLocation = await resolvers.Query!.isLeafLevelLocation(
+        {},
+        { locationId: '1' },
+        { headers: undefined }
+      )
+      expect(isLeafLevelLocation).toBe(true)
     })
   })
 })
