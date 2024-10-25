00d2c67ee698436ba0102d1bfe48dc8d36c53039
jamil314 <jamil31415926@gmail.com>
2024-10-25 19:10:20 +0600

    chore: set up knip

---
+++ knip-reporter.ts
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+import type { Reporter } from 'knip'
+
+const reporter: Reporter = function (options) {
+  const { counters } = options
+  console.log('Total unused exports found with knip:', counters.total)
+}
+
+export default reporter
