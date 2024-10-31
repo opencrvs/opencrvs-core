6107334ac6302e52be17f5786bf6541817569ef0
Jamil <jamil31415926@gmail.com>
2024-10-31 15:59:39 +0600

    Update packages/user-mgnt/src/config/plugins.ts
    Co-authored-by: Riku Rouvila <riku.rouvila@gmail.com>

--- packages/user-mgnt/src/config/plugins.ts
+++ packages/user-mgnt/src/config/plugins.ts
@@ -39,7 +39,7 @@ export default function getPlugins() {
           environment: process.env.DOMAIN,
           dsn: SENTRY_DSN,
           initialScope: {
-            tags: { service: 'ocrvs_user-mgnt' }
+            tags: { service: 'user-mgnt' }
           }
         },
         catchLogErrors: true
