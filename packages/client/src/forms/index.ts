d6072022aec9c413278c76c0d686f42ffa137643
tahmidrahman-dsi <tahmid.rahman@dsinnovators.com>
2024-11-21 23:17:06 +0600

    feat: disable continue button conditionally on a loading value

--- packages/client/src/forms/index.ts
+++ packages/client/src/forms/index.ts
@@ -1002,6 +1002,7 @@ export interface IFormSection {
   optional?: boolean
   notice?: MessageDescriptor
   mapping?: IFormSectionMapping
+  canContinue?: string
 }
 
 export type ISerializedFormSectionGroup = Omit<IFormSectionGroup, 'fields'> & {
