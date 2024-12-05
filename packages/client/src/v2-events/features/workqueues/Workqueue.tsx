2d8fbf7bfbea433d497f4debb589d97f5b6ab9b3
Markus <markus@agilepursuit.fi>
2024-12-05 14:19:40 +0200

    feat: implement basic sorting

--- packages/client/src/v2-events/features/workqueues/Workqueue.tsx
+++ packages/client/src/v2-events/features/workqueues/Workqueue.tsx
@@ -12,7 +12,8 @@
 import {
   Workqueue,
   COLUMNS,
-  SORT_ORDER
+  SORT_ORDER,
+  ColumnContentAlignment
 } from '@opencrvs/components/lib/Workqueue'
 import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
@@ -24,8 +25,7 @@ import {
   navigationMessages
 } from './messages'
 import styled, { useTheme } from 'styled-components'
-// @TODO: Implement sorting
-// import { changeSortedColumn } from '@client/views/OfficeHome/utils'
+import orderBy from 'lodash-es/orderBy'
 import { WQContentWrapper } from './ContentWrapper'
 import { useWindowSize } from '@opencrvs/components/lib/hooks'
 import { useEvents } from '@client/v2-events/features/events/useEvents'
@@ -36,6 +36,29 @@ const ToolTipContainer = styled.span`
   text-align: center;
 `
 
+const changeSortedColumn = (
+  columnName: string,
+  presentSortedCol: string,
+  presentSortOrder: SORT_ORDER
+) => {
+  const initialSortOrder = SORT_ORDER.ASCENDING
+  const isInitialSortOrder = presentSortOrder === initialSortOrder
+  const isSameColumn = columnName === presentSortedCol
+
+  const toggle = () => {
+    if (isSameColumn) {
+      return isInitialSortOrder ? SORT_ORDER.DESCENDING : SORT_ORDER.ASCENDING
+    }
+
+    return initialSortOrder
+  }
+
+  return {
+    newSortedCol: columnName,
+    newSortOrder: toggle()
+  }
+}
+
 export const WorkqueueIndex = () => {
   const intl = useIntl()
   const theme = useTheme()
@@ -44,32 +67,40 @@ export const WorkqueueIndex = () => {
 
   const events = getEvents.useQuery()
 
-  const statusEvents =
+  const statusEvents = (
     events.data?.filter((event) => event.status === searchParams.status) ?? []
+  ).map((event) => ({
+    assignedTo: event.assignedTo,
+    type: event.type,
+    createdAt: new Date(event.createdAt).toLocaleDateString(),
+    modifiedAt: new Date(event.modifiedAt).toLocaleString(),
+    id: event.id
+  }))
 
   const { width } = useWindowSize()
-  const [sortedCol, setSortedCol] = useState(COLUMNS.SENT_FOR_REVIEW)
+  const [sortedCol, setSortedCol] = useState('createdAt')
   const [sortOrder, setSortOrder] = useState(SORT_ORDER.DESCENDING)
 
   const onColumnClick = (columnName: string) => {
-    // @TODO: Implement sorting
-    return
-    // const { newSortedCol, newSortOrder } = changeSortedColumn(
-    //   columnName,
-    //   sortedCol,
-    //   sortOrder
-    // )
-    // setSortedCol(newSortedCol)
-    // setSortOrder(newSortOrder)
+    const { newSortedCol, newSortOrder } = changeSortedColumn(
+      columnName,
+      sortedCol,
+      sortOrder
+    )
+    setSortedCol(newSortedCol)
+    setSortOrder(newSortOrder)
   }
 
+  // @TODO: separate types for action button vs other columns
   const getColumns = (): Array<{
-    label: string
+    label?: string
     width: number
     // @TODO: Format payload and include contents of data somehow.
-    key: keyof (typeof statusEvents)[number]
+    key: keyof (typeof statusEvents)[number] | string
     sortFunction?: (columnName: string) => void
-    isSorted: boolean
+    isActionColumn?: boolean
+    isSorted?: boolean
+    alignment?: ColumnContentAlignment
   }> => {
     if (width > theme.grid.breakpoints.lg) {
       return [
@@ -78,35 +109,35 @@ export const WorkqueueIndex = () => {
           width: 30,
           key: 'assignedTo',
           sortFunction: onColumnClick,
-          isSorted: sortedCol === COLUMNS.NAME
+          isSorted: sortedCol === 'assignedTo'
         },
         {
           label: intl.formatMessage(constantsMessages.event),
           width: 16,
           key: 'type',
           sortFunction: onColumnClick,
-          isSorted: sortedCol === COLUMNS.EVENT
+          isSorted: sortedCol === 'type'
         },
         {
           label: intl.formatMessage(constantsMessages.eventDate),
           width: 18,
           key: 'createdAt',
           sortFunction: onColumnClick,
-          isSorted: sortedCol === COLUMNS.DATE_OF_EVENT
+          isSorted: sortedCol === 'createdAt'
         },
         {
           label: intl.formatMessage(constantsMessages.sentForReview),
           width: 18,
           key: 'modifiedAt',
           sortFunction: onColumnClick,
-          isSorted: sortedCol === COLUMNS.SENT_FOR_REVIEW
+          isSorted: sortedCol === 'modifiedAt'
+        },
+        {
+          width: 18,
+          key: COLUMNS.ACTIONS,
+          isActionColumn: true,
+          alignment: ColumnContentAlignment.RIGHT
         }
-        // {
-        //   width: 18,
-        //   key: COLUMNS.ACTIONS,
-        //   isActionColumn: true,
-        //   alignment: ColumnContentAlignment.RIGHT
-        // }
       ]
     } else {
       return [
@@ -115,14 +146,14 @@ export const WorkqueueIndex = () => {
           width: 70,
           key: 'assignedTo',
           sortFunction: onColumnClick,
-          isSorted: sortedCol === COLUMNS.NAME
+          isSorted: sortedCol === 'assignedTo'
+        },
+        {
+          width: 30,
+          alignment: ColumnContentAlignment.RIGHT,
+          key: COLUMNS.ACTIONS,
+          isActionColumn: true
         }
-        // {
-        //   width: 30,
-        //   alignment: ColumnContentAlignment.RIGHT,
-        //   key: COLUMNS.ACTIONS,
-        //   isActionColumn: true
-        // }
       ]
     }
   }
@@ -151,7 +182,7 @@ export const WorkqueueIndex = () => {
         </ToolTipContainer>
       </ReactTooltip>
       <Workqueue
-        content={statusEvents}
+        content={orderBy(statusEvents, sortedCol, sortOrder)}
         columns={getColumns()}
         loading={events.isLoading}
         sortOrder={sortOrder}
