818b51abfdd394c25ebb3717afe48cc176336430
Riku Rouvila <riku.rouvila@gmail.com>
2025-01-09 13:02:59 +0900

    add a validate action (#8302)

--- packages/client/src/v2-events/features/events/useEvents/procedures/action.ts
+++ packages/client/src/v2-events/features/events/useEvents/procedures/action.ts
@@ -9,11 +9,16 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import { useMutation } from '@tanstack/react-query'
-import { getMutationKey } from '@trpc/react-query'
+import { Mutation as TanstackMutation } from '@tanstack/query-core'
+import { hashKey, useMutation } from '@tanstack/react-query'
+import { getMutationKey, getQueryKey } from '@trpc/react-query'
+import {
+  DraftActionInput,
+  EventDocument,
+  getCurrentEventState
+} from '@opencrvs/commons/client'
 import { ActionFormData } from '@opencrvs/commons'
-import { EventDocument, getCurrentEventState } from '@opencrvs/commons/client'
-import { api, utils } from '@client/v2-events/trpc'
+import { api, queryClient, utils } from '@client/v2-events/trpc'
 
 async function updateLocalEvent(updatedEvent: EventDocument) {
   utils.event.get.setData(updatedEvent.id, updatedEvent)
@@ -46,17 +51,54 @@ type Mutation =
   | typeof api.event.actions.draft
   | typeof api.event.actions.notify
   | typeof api.event.actions.register
+  | typeof api.event.actions.validate
 
 type Procedure =
   | typeof utils.event.actions.declare
   | typeof utils.event.actions.draft
   | typeof utils.event.actions.notify
   | typeof utils.event.actions.register
+  | typeof utils.event.actions.validate
+
+/*
+ * This makes sure that if you are offline and do
+ * 1. Create record
+ * 2. Create draft
+ * 3. Declare the record
+ * 4. Connect to the internet
+ * The draft stage in the middle will be cancelled. This is to prevent race conditions
+ * between when the backend receives the draft when it receives the declare action.
+ */
+function cancelOngoingDraftRequests(eventId: string) {
+  const mutationCache = queryClient.getMutationCache()
+
+  const isDraftMutation = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mutation: TanstackMutation<unknown, Error, any, unknown>
+  ): mutation is TanstackMutation<unknown, Error, DraftActionInput, unknown> =>
+    mutation.options.mutationKey
+      ? hashKey(mutation.options.mutationKey) ===
+        hashKey(getQueryKey(api.event.actions.draft))
+      : false
+
+  mutationCache
+    .getAll()
+    .filter(
+      (mutation) =>
+        isDraftMutation(mutation) &&
+        mutation.state.variables?.eventId === eventId
+    )
+    .forEach((mutation) => {
+      mutationCache.remove(mutation)
+    })
+}
 
 function updateEventOptimistically<
   T extends { eventId: string; data: ActionFormData }
 >(actionType: 'DECLARE' | 'DRAFT') {
   return (variables: T) => {
+    cancelOngoingDraftRequests(variables.eventId)
+
     const localEvent = utils.event.get.getData(variables.eventId)
     if (!localEvent) {
       return
@@ -74,6 +116,7 @@ function updateEventOptimistically<
         }
       ]
     }
+
     utils.events.get.setData(undefined, (eventIndices) =>
       eventIndices
         ?.filter((ei) => ei.id !== optimisticEvent.id)
@@ -106,6 +149,13 @@ utils.event.actions.register.setMutationDefaults(({ canonicalMutationFn }) => ({
 }))
 
 utils.event.actions.notify.setMutationDefaults(({ canonicalMutationFn }) => ({
+  retry: true,
+  retryDelay: 10000,
+  mutationFn: waitUntilEventIsCreated(canonicalMutationFn),
+  onSuccess: updateLocalEvent
+}))
+
+utils.event.actions.validate.setMutationDefaults(({ canonicalMutationFn }) => ({
   retry: true,
   retryDelay: 10000,
   mutationFn: waitUntilEventIsCreated(canonicalMutationFn),
