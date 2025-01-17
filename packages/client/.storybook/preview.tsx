dbc3f20bd15c615fd14df43eb4407b2edfa751bf
Riku Rouvila <riku.rouvila@gmail.com>
2025-01-17 16:16:22 +0900

    add missing license headers

--- packages/client/.storybook/preview.tsx
+++ packages/client/.storybook/preview.tsx
@@ -1,3 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+
 import { getTheme } from '@opencrvs/components/lib/theme'
 import type { Preview } from '@storybook/react'
 import { graphql, http, HttpResponse } from 'msw'
