979e3bb7ed35d411947fb071c89a0c84a9348e26
tareq89 <tareqaziz.aust.cse@gmail.com>
2025-01-24 17:29:05 +0600

    default certificate form is defined in toolkit

--- packages/toolkit/build.sh
+++ packages/toolkit/build.sh
@@ -33,6 +33,10 @@ cp -r ../commons/build/dist/common/conditionals/*.d.ts ./dist/commons/conditiona
 npx esbuild src/api/index.ts --bundle --format=cjs --outdir=./dist/api --allow-overwrite --packages=external
 mkdir -p ./dist/commons/api
 cp -r ../events/build/types/router/router.d.ts ./dist/commons/api
+
+# Build deafult
+npx esbuild src/default/index.ts --bundle --format=cjs --outdir=./dist/default --allow-overwrite --packages=external
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's|@opencrvs/events/build/types|../commons/api|g' dist/api/index.d.ts
   find dist -type f -exec sed -i '' 's|@opencrvs/commons|../commons|g' {} +
