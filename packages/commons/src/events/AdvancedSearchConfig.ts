0cc10b29390eac1be70828c494c934d5b6fda618
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-02-05 17:29:13 +0600

    chore: amend advanced search config with type field

--- packages/commons/src/events/AdvancedSearchConfig.ts
+++ packages/commons/src/events/AdvancedSearchConfig.ts
@@ -12,10 +12,22 @@ import { z } from 'zod'
 import { TranslationConfig } from './TranslationConfig'
 
 export const AdvancedSearchConfig = z.object({
-  id: z.string().describe('Advanced search tab button id'),
+  id: z.string().describe('Advanced search section id'),
   title: TranslationConfig.describe('Advanced search tab title'),
   fields: z
-    .array(z.object({ fieldId: z.string() }))
+    .array(
+      z.object({
+        fieldId: z.string(),
+        config: z
+          .object({
+            type: z
+              .enum(['FUZZY', 'EXACT', 'RANGE'])
+              .describe('Determines the type of field')
+          })
+          .optional()
+          .describe('Configuration options for the field')
+      })
+    )
     .optional()
     .default([])
     .describe('Advanced search fields.')
