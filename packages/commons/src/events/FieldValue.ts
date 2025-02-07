dd50986dd701892b93aff3acffabb19b4dde7687
Markus <markus@agilepursuit.fi>
2025-02-07 08:56:37 +0200

    chore: upgrade toolkit

--- packages/commons/src/events/FieldValue.ts
+++ packages/commons/src/events/FieldValue.ts
@@ -16,6 +16,7 @@ export const DateValue = z
   .string()
   .date()
   .describe('Date in the format YYYY-MM-DD')
+
 export const FileFieldValue = z.object({
   filename: z.string(),
   originalFilename: z.string(),
