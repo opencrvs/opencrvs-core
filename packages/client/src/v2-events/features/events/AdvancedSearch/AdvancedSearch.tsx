35ff47c40706c26d2143bb952f1cb739b69a794c
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-02-11 19:36:27 +0600

    chore: refactor tab search into different component

--- packages/client/src/v2-events/features/events/AdvancedSearch/AdvancedSearch.tsx
+++ packages/client/src/v2-events/features/events/AdvancedSearch/AdvancedSearch.tsx
@@ -10,26 +10,16 @@
  */
 import React, { useState } from 'react'
 import { defineMessages, useIntl } from 'react-intl'
-import styled from 'styled-components'
 import {
-  Accordion,
   Content,
   ContentSize,
   FormTabs,
   IFormTabProps
 } from '@opencrvs/components'
-import { AdvancedSearchConfig, FieldValue } from '@opencrvs/commons/client'
-import { Button } from '@opencrvs/components/lib/Button'
-import { Icon } from '@opencrvs/components/lib/Icon'
-import { FormFieldGenerator } from '@client/v2-events/components/forms/FormFieldGenerator'
 import { useEventConfigurations } from '@client/v2-events/features/events/useEventConfiguration'
+import { TabSearch } from './TabSearch'
 
 const messagesToDefine = {
-  search: {
-    defaultMessage: 'Search',
-    description: 'The title of search input submit button',
-    id: 'v2.buttons.search'
-  },
   advancedSearch: {
     id: 'v2.config.advanced.search',
     defaultMessage: 'Advanced Search',
@@ -40,124 +30,10 @@ const messagesToDefine = {
     defaultMessage:
       'Select the options to build an advanced search. A minimum of two search parameters is required.',
     description: 'This is used for the advanced search'
-  },
-  hide: {
-    defaultMessage: 'Hide',
-    description: 'Label for hide button when accordion is closed',
-    id: 'v2.advancedSearch.accordion.hide'
-  },
-  show: {
-    defaultMessage: 'Show',
-    description: 'Label for show button when accordion is closed',
-    id: 'v2.advancedSearch.accordion.show'
   }
 }
 
 const messages = defineMessages(messagesToDefine)
-
-const SearchButton = styled(Button)`
-  margin-top: 32px;
-`
-
-function TabSearch({
-  sections,
-  activeTabId
-}: {
-  sections: AdvancedSearchConfig[]
-  activeTabId: string
-}) {
-  const intl = useIntl()
-  const events = useEventConfigurations()
-  const [formValues, setFormValues] = useState<Record<string, FieldValue>>({})
-
-  const currentEvent = events.filter((e) => e.id === activeTabId)
-
-  // Extract unique fields
-  const allUniqueFields = [
-    ...new Map(
-      currentEvent
-        .flatMap((event) =>
-          event.actions.flatMap((action) =>
-            action.forms.flatMap((form) =>
-              form.pages.flatMap((page) => page.fields)
-            )
-          )
-        )
-        .map((field) => [field.id, field])
-    ).values()
-  ]
-
-  const handleFieldChange = (fieldId: string, value: FieldValue) => {
-    setFormValues((prev) => ({
-      ...prev,
-      [fieldId]: value
-    }))
-  }
-
-  const SectionFields = sections.map((section) => {
-    const advancedSearchFieldId = section.fields.map(
-      (f: { fieldId: string }) => f.fieldId
-    )
-    const fields = allUniqueFields.filter((field) =>
-      advancedSearchFieldId.includes(field.id)
-    )
-
-    const modifiedFields = fields.map((f) => {
-      return {
-        ...f,
-        required: false // advanced search fields need not be required
-      }
-    })
-
-    return (
-      <Accordion
-        key={section.id}
-        expand={true}
-        label={intl.formatMessage(section.title)}
-        labelForHideAction={intl.formatMessage(messages.hide)}
-        labelForShowAction={intl.formatMessage(messages.show)}
-        name={section.id}
-      >
-        <FormFieldGenerator
-          fields={modifiedFields}
-          formData={formValues}
-          id={section.id}
-          setAllFieldsDirty={false}
-          onChange={(updatedValues) => {
-            Object.entries(updatedValues).forEach(([fieldId, value]) => {
-              handleFieldChange(fieldId, value)
-            })
-          }}
-        />
-      </Accordion>
-    )
-  })
-
-  const hasEnoughParams = Object.entries(formValues).length > 0
-  const Search = (
-    <SearchButton
-      key="search"
-      fullWidth
-      disabled={!hasEnoughParams}
-      id="search"
-      size="large"
-      type="primary"
-      onClick={() => {
-        alert(JSON.stringify(formValues)) // @todo replace with actual search
-      }}
-    >
-      <Icon name={'MagnifyingGlass'} />
-      {intl.formatMessage(messages.search)}
-    </SearchButton>
-  )
-
-  return (
-    <>
-      {SectionFields}
-      {Search}
-    </>
-  )
-}
 
 function AdvancedSearch() {
   const intl = useIntl()
---
+++ packages/client/src/v2-events/features/events/AdvancedSearch/TabSearch.tsx
@@ -0,0 +1,145 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+import * as React from 'react'
+import styled from 'styled-components'
+import { useIntl, defineMessages } from 'react-intl'
+import { Accordion } from '@opencrvs/components'
+import { AdvancedSearchConfig, FieldValue } from '@opencrvs/commons/client'
+import { Icon } from '@opencrvs/components/lib/Icon'
+import { Button } from '@opencrvs/components/lib/Button'
+import { FormFieldGenerator } from '@client/v2-events/components/forms/FormFieldGenerator'
+import { useEventConfigurations } from '@client/v2-events/features/events/useEventConfiguration'
+
+const SearchButton = styled(Button)`
+  margin-top: 32px;
+`
+
+const messagesToDefine = {
+  search: {
+    defaultMessage: 'Search',
+    description: 'The title of search input submit button',
+    id: 'v2.buttons.search'
+  },
+  hide: {
+    defaultMessage: 'Hide',
+    description: 'Label for hide button when accordion is closed',
+    id: 'v2.advancedSearch.accordion.hide'
+  },
+  show: {
+    defaultMessage: 'Show',
+    description: 'Label for show button when accordion is closed',
+    id: 'v2.advancedSearch.accordion.show'
+  }
+}
+
+const messages = defineMessages(messagesToDefine)
+
+export function TabSearch({
+  sections,
+  activeTabId
+}: {
+  sections: AdvancedSearchConfig[]
+  activeTabId: string
+}) {
+  const intl = useIntl()
+  const events = useEventConfigurations()
+  const [formValues, setFormValues] = React.useState<
+    Record<string, FieldValue>
+  >({})
+
+  const currentEvent = events.filter((e) => e.id === activeTabId)
+
+  // Extract unique fields
+  const allUniqueFields = [
+    ...new Map(
+      currentEvent
+        .flatMap((event) =>
+          event.actions.flatMap((action) =>
+            action.forms.flatMap((form) =>
+              form.pages.flatMap((page) => page.fields)
+            )
+          )
+        )
+        .map((field) => [field.id, field])
+    ).values()
+  ]
+
+  const handleFieldChange = (fieldId: string, value: FieldValue) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [fieldId]: value
+    }))
+  }
+
+  const SectionFields = sections.map((section) => {
+    const advancedSearchFieldId = section.fields.map(
+      (f: { fieldId: string }) => f.fieldId
+    )
+    const fields = allUniqueFields.filter((field) =>
+      advancedSearchFieldId.includes(field.id)
+    )
+
+    const modifiedFields = fields.map((f) => {
+      return {
+        ...f,
+        required: false // advanced search fields need not be required
+      }
+    })
+
+    return (
+      <Accordion
+        key={section.id}
+        expand={true}
+        label={intl.formatMessage(section.title)}
+        labelForHideAction={intl.formatMessage(messages.hide)}
+        labelForShowAction={intl.formatMessage(messages.show)}
+        name={section.id}
+      >
+        <FormFieldGenerator
+          fields={modifiedFields}
+          formData={formValues}
+          id={section.id}
+          setAllFieldsDirty={false}
+          onChange={(updatedValues) => {
+            Object.entries(updatedValues).forEach(([fieldId, value]) => {
+              handleFieldChange(fieldId, value)
+            })
+          }}
+        />
+      </Accordion>
+    )
+  })
+
+  const hasEnoughParams = Object.entries(formValues).length > 0
+  const Search = (
+    <SearchButton
+      key="search"
+      fullWidth
+      disabled={!hasEnoughParams}
+      id="search"
+      size="large"
+      type="primary"
+      onClick={() => {
+        alert(JSON.stringify(formValues)) // @todo replace with actual search
+      }}
+    >
+      <Icon name={'MagnifyingGlass'} />
+      {intl.formatMessage(messages.search)}
+    </SearchButton>
+  )
+
+  return (
+    <>
+      {SectionFields}
+      {Search}
+    </>
+  )
+}
