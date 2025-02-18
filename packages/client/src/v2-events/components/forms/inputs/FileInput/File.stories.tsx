9470fd25c2d5ac3cb2d97e0f42104f2a8d6b26a6
jamil314 <jamil31415926@gmail.com>
2025-02-18 14:59:23 +0600

    chore: update storybook

--- packages/client/src/v2-events/components/forms/inputs/FileInput/File.stories.tsx
+++ packages/client/src/v2-events/components/forms/inputs/FileInput/File.stories.tsx
@@ -15,6 +15,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { FormFieldGenerator } from '@client/v2-events/components/forms/FormFieldGenerator'
 import { TRPCProvider } from '@client/v2-events/trpc'
+import { Review } from '@client/v2-events/features/events/components/Review'
+import { birthEvent } from './fixtures'
 
 const meta: Meta<typeof FormFieldGenerator> = {
   title: 'Inputs/File',
@@ -30,8 +32,10 @@ const meta: Meta<typeof FormFieldGenerator> = {
 
 export default meta
 
-const StyledFormFieldGenerator = styled(FormFieldGenerator)`
-  width: 400px;
+const StyledFormFieldGenerator = styled(FormFieldGenerator)<{
+  fullWidth?: boolean
+}>`
+  width: '400px';
 `
 export const FileInputWithOption: StoryObj<typeof FormFieldGenerator> = {
   name: 'File input with option',
@@ -107,7 +111,11 @@ export const FileInputWithOption: StoryObj<typeof FormFieldGenerator> = {
   }
 }
 
-export const FileInputWithoutOption: StoryObj<typeof FormFieldGenerator> = {
+export const FileInputWithoutOption: StoryObj<
+  typeof StyledFormFieldGenerator
+> & {
+  fullWidth?: boolean
+} = {
   name: 'File input without option',
   parameters: {
     layout: 'centered'
@@ -124,7 +132,8 @@ export const FileInputWithoutOption: StoryObj<typeof FormFieldGenerator> = {
               id: 'storybook.file2.label',
               defaultMessage: 'Upload your photo',
               description: 'The title for the file input'
-            }
+            },
+            options: { style: { fullWidth: Boolean(args.fullWidth) } }
           }
         ]}
         formData={formData}
@@ -135,6 +144,73 @@ export const FileInputWithoutOption: StoryObj<typeof FormFieldGenerator> = {
           setFormData(data)
         }}
       />
+    )
+  },
+  argTypes: {
+    fullWidth: {
+      control: 'boolean',
+      description: 'Toggle to make the input full width'
+    }
+  },
+  args: { fullWidth: false }
+}
+
+export const FileReview: StoryObj<typeof Review> = {
+  name: 'Review output',
+  parameters: {
+    layout: 'center'
+  },
+  render: function Component() {
+    return (
+      <div>
+        <Review.Body
+          eventConfig={birthEvent}
+          form={{
+            'documents.proofOfBirth': {
+              filename: '2d2c06e7-cadb-47b7-9779-419e6125b681.jpg',
+              originalFilename: '8073450b-c9e5-486b-bef4-52db3dc39f2e.jpg',
+              type: 'image/jpeg'
+            },
+            'documents.proofOfMother': [
+              {
+                filename: '30555050-01b9-4b90-9172-da7eaa97bdf1.jpeg',
+                originalFilename: 'abcd.jpeg',
+                type: 'image/jpeg',
+                option: 'NATIONAL_ID'
+              },
+              {
+                filename: 'b7bf34d7-4a93-4f18-8fb7-e04a5801c0bd.jpg',
+                originalFilename: 'abcd.jpg',
+                type: 'image/jpeg',
+                option: 'PASSPORT'
+              },
+              {
+                filename: '019dc6d3-1e09-4fbc-854f-4b1b10d9d42e.jpg',
+                originalFilename: 'abcd.jpg',
+                type: 'image/jpeg',
+                option: 'BIRTH_REGISTRATION_NUMBER'
+              },
+              {
+                filename: '01cbaef6-bada-458b-8ce6-69f456df25e4.png',
+                originalFilename: 'abcd.png',
+                type: 'image/png',
+                option: 'NONE'
+              }
+            ],
+            'documents.proofOther': {
+              filename: '9b797687-c294-42d6-b504-2a20a72fb9bb.png',
+              originalFilename: 'abcd.png',
+              type: 'image/png'
+            }
+          }}
+          formConfig={birthEvent.actions[0].forms[0]}
+          title="File explorer"
+          // eslint-disable-next-line no-console
+          onEdit={(values) => console.log(values)}
+        >
+          <div />
+        </Review.Body>
+      </div>
     )
   }
 }
