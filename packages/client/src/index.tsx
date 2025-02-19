2d88b4226aac67c195eec3e8f3cc97dee023d1ea
jamil314 <jamil31415926@gmail.com>
2025-02-19 15:36:21 +0600

    chore: enable v7_skipActionErrorRevalidation future flag

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -68,7 +68,8 @@ const router = createBrowserRouter(routesConfig, {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
     v7_normalizeFormMethod: true,
-    v7_partialHydration: true
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true
   }
 })
 
--- packages/login/src/index.tsx
+++ packages/login/src/index.tsx
@@ -55,7 +55,8 @@ const router = createBrowserRouter(routesConfig, {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
     v7_normalizeFormMethod: true,
-    v7_partialHydration: true
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true
   }
 })
 async function renderAppWithConfig() {
