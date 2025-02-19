5ccdf4054f8d2df5b893c3e1ee78db14973d2d71
jamil314 <jamil31415926@gmail.com>
2025-02-19 15:32:26 +0600

    chore: enable v7_partialHydration future flag

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -67,7 +67,8 @@ const router = createBrowserRouter(routesConfig, {
   future: {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true
   }
 })
 
--- packages/login/src/index.tsx
+++ packages/login/src/index.tsx
@@ -54,7 +54,8 @@ const router = createBrowserRouter(routesConfig, {
   future: {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true
   }
 })
 async function renderAppWithConfig() {
