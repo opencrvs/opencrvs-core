d2ef0362db19d3d55f74a3d940ba699caa24028d
Siyasanga <siyasanga.mtshokotsha@decodedev.co.za>
2025-02-24 19:12:22 +0200

    Deprecate INFORMANT_MINIMUM_AGE & isInformantOfLegalAge
    We no longer need these as we now have the isDateBetween validator for Date and Age fields
    
    https://github.com/opencrvs/opencrvs-core/issues/7636

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -56,6 +56,9 @@ export const AVATAR_API =
 export const ACCUMULATED_FILE_SIZE = 20480000
 
 export const DESKTOP_TIME_OUT_MILLISECONDS = 900000
+/**
+ * @deprecated This validator is deprecated and will be removed in future releases. Use the `isDateBetween` validator for date and age fields instead
+ */
 export const INFORMANT_MINIMUM_AGE = 16
 
 /** Current application version used in the left navigation. It's saved to localStorage to determine if a user logged into a newer version of the app for the first time */
