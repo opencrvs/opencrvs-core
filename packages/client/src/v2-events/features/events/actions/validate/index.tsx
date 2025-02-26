c9ffbd2e6a5d4374d178c478ba47e43ef8486155
Riku Rouvila <riku.rouvila@gmail.com>
2025-02-26 14:26:31 +0200

    Events v2 demo environment (#8761)
    Co-authored-by: Markus Laurila <makelicious@users.noreply.github.com>

--- packages/client/src/v2-events/features/events/actions/validate/index.tsx
+++ packages/client/src/v2-events/features/events/actions/validate/index.tsx
@@ -9,15 +9,14 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import React, { useEffect, useState } from 'react'
-import { useTypedParams } from 'react-router-typesafe-routes/dom'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useTypedParams } from 'react-router-typesafe-routes/dom'
 import { v4 as uuid } from 'uuid'
 import { ROUTES } from '@client/v2-events/routes'
 import { useEvents } from '@client/v2-events/features/events/useEvents/useEvents'
 
 export function ValidateEvent() {
-  const [validated, setValidated] = useState(false)
   const { eventId } = useTypedParams(ROUTES.V2.EVENTS.VALIDATE)
   const navigate = useNavigate()
 
@@ -27,19 +26,18 @@ export function ValidateEvent() {
   const transactionId = uuid()
 
   useEffect(() => {
-    if (!validated) {
-      validateEvent.mutate(
-        {
-          eventId,
-          data: {},
-          transactionId,
-          duplicates: []
-        },
-        { onSettled: () => navigate(ROUTES.V2.path) }
-      )
-      setValidated(true)
-    }
-  }, [validateEvent, eventId, navigate, transactionId, validated])
+    validateEvent.mutate({
+      eventId,
+      data: {},
+      transactionId,
+      duplicates: []
+    })
+    navigate(ROUTES.V2.path)
+    // If you add deleteEvent to the dependencies, it will cause the delete
+    // to be called >1 times. This is because the deleteEvent contains updating data fields describing
+    // the state of the request
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return <div />
 }
