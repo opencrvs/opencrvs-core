263fe46b7c5bb8c4a9ad7190f09ced0df321239d
Riku Rouvila <riku.rouvila@gmail.com>
2025-03-03 17:27:55 +0200

    fix how form is hydrated on mount

--- packages/client/src/v2-events/features/events/actions/declare/Action.tsx
+++ packages/client/src/v2-events/features/events/actions/declare/Action.tsx
@@ -9,8 +9,14 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren, useEffect, useMemo } from 'react'
 import { useTypedParams } from 'react-router-typesafe-routes/dom'
+import {
+  useLocation,
+  useMatches,
+  useNavigate,
+  useViewTransitionState
+} from 'react-router-dom'
 import {
   ActionType,
   getCurrentEventStateWithDrafts,
@@ -24,24 +30,26 @@ import { ROUTES } from '@client/v2-events/routes'
 export function Action({ children }: PropsWithChildren) {
   const params = useTypedParams(ROUTES.V2.EVENTS.DECLARE.PAGES)
   const { getEvent, getDrafts } = useEvents()
-  const setFormValues = useEventFormData((state) => state.setFormValues)
+  const setFormValues = useEventFormData((state) => state.setFormValuesIfEmpty)
   const clearFormValues = useEventFormData((state) => state.clear)
   const setMetadata = useEventMetadata((state) => state.setMetadata)
   const clearMetadata = useEventMetadata((state) => state.clear)
 
   const [event] = getEvent.useSuspenseQuery(params.eventId)
   const drafts = getDrafts()
-  const draftsForThisEvent = drafts.filter((d) => d.eventId === event.id)
-  const eventDataWithDrafts = getCurrentEventStateWithDrafts(
-    event,
-    draftsForThisEvent
+  const draftsForThisEvent = useMemo(
+    () => drafts.filter((d) => d.eventId === event.id),
+    [drafts, event.id]
   )
 
-  const declareMetadata = getMetadataForAction(
-    event,
-    ActionType.DECLARE,
-    draftsForThisEvent
+  const eventDataWithDrafts = useMemo(
+    () => getCurrentEventStateWithDrafts(event, draftsForThisEvent),
+    [draftsForThisEvent, event]
   )
+
+  const declareMetadata = useMemo(() => {
+    return getMetadataForAction(event, ActionType.DECLARE, draftsForThisEvent)
+  }, [draftsForThisEvent, event])
 
   useEffect(() => {
     setFormValues(eventDataWithDrafts.id, eventDataWithDrafts.data)
