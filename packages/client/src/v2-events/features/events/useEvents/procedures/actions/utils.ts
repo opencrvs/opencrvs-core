ecf9250ffe6d162e2f4d01726ee2781b8cd44a6d
Keksike <cihan.m.bebek@gmail.com>
2025-03-06 12:07:15 +0200

    remove unnecessary function

--- packages/client/src/v2-events/features/events/useEvents/procedures/actions/utils.ts
+++ packages/client/src/v2-events/features/events/useEvents/procedures/actions/utils.ts
@@ -8,8 +8,6 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-
-import { Mutation as TanstackMutation } from '@tanstack/query-core'
 import {
   ActionInput,
   ActionType,
@@ -22,56 +20,10 @@ import {
 } from '@client/v2-events/features/events/useEvents/api'
 import { queryClient, trpcOptionsProxy } from '@client/v2-events/trpc'
 
-/*
- * This makes sure that if you are offline and do
- * 1. Create record
- * 2. Create draft
- * 3. Declare the record
- * 4. Connect to the internet
- * The draft stage in the middle will be cancelled. This is to prevent race conditions
- * between when the backend receives the draft and when it receives the declare action.
- */
-function cancelOngoingDraftRequests({ eventId, draft }: ActionInput) {
-  const mutationCache = queryClient.getMutationCache()
-
-  const isDraftMutation = (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    mutation: TanstackMutation<unknown, Error, any, unknown>
-  ): mutation is TanstackMutation<unknown, Error, ActionInput, unknown> => {
-    const mutationKey = mutation.options.mutationKey
-    if (!mutationKey || !mutationKey[0]) {
-      return false
-    }
-    return ['event', 'actions'].every((key) => mutationKey.flat().includes(key))
-  }
-
-  const draftMutationsForThisEvent = mutationCache
-    .getAll()
-    .filter(
-      (mutation) =>
-        isDraftMutation(mutation) &&
-        mutation.state.variables?.eventId === eventId &&
-        mutation.state.variables.draft
-    )
-
-  if (!draft) {
-    draftMutationsForThisEvent.forEach((mutation) => {
-      mutationCache.remove(mutation)
-    })
-  } else {
-    // Keep the last draft mutation as it's this current request
-    draftMutationsForThisEvent.slice(0, -1).forEach((mutation) => {
-      mutationCache.remove(mutation)
-    })
-  }
-}
-
 export function updateEventOptimistically<T extends ActionInput>(
   actionType: typeof ActionType.DECLARE
 ) {
   return (variables: T) => {
-    cancelOngoingDraftRequests(variables)
-
     const localEvent = queryClient.getQueryData(
       trpcOptionsProxy.event.get.queryKey(variables.eventId)
     )
