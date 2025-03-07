0faeddd93ac55c6e962d4e7f82f0adea96e2231d
Riku Rouvila <riku.rouvila@gmail.com>
2025-03-07 14:00:17 +0200

    fix linting issues

--- packages/client/src/v2-events/features/events/useEvents/api.ts
+++ packages/client/src/v2-events/features/events/useEvents/api.ts
@@ -9,7 +9,6 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import { v4 as uuid } from 'uuid'
 import { Draft, EventDocument, EventIndex } from '@opencrvs/commons/client'
 import { queryClient, trpcOptionsProxy } from '@client/v2-events/trpc'
 
