5393691b229a6fa4b13c591c5eead4f90e410f2b
adskyiproger <vadym.mudryi@gmail.com>
2025-03-13 16:27:34 +0200

    K8s (#8959)
    * work
    
    * work
    
    * work
    
    * revert schema, fix setup.sh for ubuntu
    
    * fixes for hearth and config
    
    * use dev script for running dashboards for now
    
    * fix auth and client
    
    * Restore/fix gateway
    
    * Fix documents
    
    * Fix metrics
    
    * Make Services LoadBalancer to reveal them on localhost of laptop rather than just in cluster - this purely for dev debuggary
    
    * Couple more fixes around migration/webhooks but also fix wrong search url across
    
    * Basic skaffolding for traefik, not hooked up to services but dashboard ok
    
    * Hook up auth to traefik
    
    * Add middlwares for Traefik, configure client, config and countryconfig
    
    * All services hooked up to Traefik
    
    * whitelist->allowlist
    
    * whitelist->allowlist
    
    * Reveal traefik on hostPort to get it on localhost with microk8s
    
    * Rebase, add events
    
    * add ssl
    
    * missed ssl
    
    * Upgraded to use ghcr.io
    
    * minor fixes
    
    * refactoring
    
    * add traefik ssl
    
    ---------
    
    Co-authored-by: Niko Kurtti <niko@kurtti.eu>

---
+++ packages/client/nginx/nginx-deploy-config.sh
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# OpenCRVS is also distributed under the terms of the Civil Registration
+# & Healthcare Disclaimer located at http://opencrvs.org/license.
+#
+# Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+set -e
+env
+sed -i -e s~{{COUNTRY_CONFIG_URL_INTERNAL}}~$COUNTRY_CONFIG_URL_INTERNAL~g -e s~{{GATEWAY_URL_INTERNAL}}~$GATEWAY_URL_INTERNAL~g /etc/nginx/conf.d/default.conf
+sed -i s~{{COUNTRY_CONFIG_URL_INTERNAL}}~$COUNTRY_CONFIG_URL_INTERNAL~g /usr/share/nginx/html/index.html
+sed -i s~{{CONTENT_SECURITY_POLICY_WILDCARD}}~$CONTENT_SECURITY_POLICY_WILDCARD~g /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
