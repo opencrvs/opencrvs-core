6de289b3ab663f78becccdbf4f7548eb22a3a9c0
Riku Rouvila <riku.rouvila@gmail.com>
2025-03-14 15:01:18 +0200

    implement a navigation wrapper component that prevents user from back-navigating to action flows

--- packages/client/src/v2-events/features/events/components/Action.tsx
+++ packages/client/src/v2-events/features/events/components/Action.tsx
@@ -9,7 +9,7 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import { PropsWithChildren, useEffect, useMemo } from 'react'
+import React, { PropsWithChildren, useEffect, useMemo } from 'react'
 import { useTypedParams } from 'react-router-typesafe-routes/dom'
 import {
   ActionType,
@@ -24,6 +24,7 @@ import { useDrafts } from '@client/v2-events/features/drafts/useDrafts'
 import { createTemporaryId } from '@client/v2-events/utils'
 import { useEvents } from '@client/v2-events/features/events/useEvents/useEvents'
 import { ROUTES } from '@client/v2-events/routes'
+import { NavigationStack } from '@client/v2-events/components/NavigationStack'
 
 type Props = PropsWithChildren<{ type: ActionType }>
 function ActionComponent({ children, type }: Props) {
@@ -133,7 +134,7 @@ function ActionComponent({ children, type }: Props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return children
+  return <NavigationStack>{children}</NavigationStack>
 }
 
 export const Action = withSuspense(ActionComponent)
