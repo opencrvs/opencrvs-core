0cc56154209be17c5a72064c161e9b46e4c1528d
Keksike <cihan.m.bebek@gmail.com>
2025-03-18 11:37:13 +0200

    add import

--- packages/client/src/v2-events/components/forms/inputs/FileInput/fixtures.ts
+++ packages/client/src/v2-events/components/forms/inputs/FileInput/fixtures.ts
@@ -8,7 +8,7 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-import { EventConfig } from '@opencrvs/commons/client'
+import { EventConfig, PageType } from '@opencrvs/commons/client'
 
 export const birthEvent = {
   id: 'birth',
