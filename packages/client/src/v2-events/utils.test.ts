4043f996774007b79d9c3bc9c58184bdea6cd30d
jamil314 <jamil31415926@gmail.com>
2025-03-19 11:47:45 +0600

    fix: test: use FAR as const instead of Farajaland

--- packages/client/src/v2-events/utils.test.ts
+++ packages/client/src/v2-events/utils.test.ts
@@ -63,7 +63,7 @@ const testCases = [
   {
     currentValue: undefined,
     defaultValue: {
-      country: 'Farajaland',
+      country: 'FAR' as const,
       district: '$user.district',
       province: '$user.province',
       urbanOrRural: 'URBAN'
@@ -75,7 +75,7 @@ const testCases = [
       }
     },
     expected: {
-      country: 'Farajaland',
+      country: 'FAR' as const,
       district: 'Ibombo',
       province: 'Central',
       urbanOrRural: 'URBAN'
