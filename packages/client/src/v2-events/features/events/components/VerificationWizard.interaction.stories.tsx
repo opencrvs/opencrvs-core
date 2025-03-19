3e5ee27b36282463f26a4a8ad6871393989fa285
Keksike <cihan.m.bebek@gmail.com>
2025-03-19 17:38:13 +0200

    add verificationwizard story

---
+++ packages/client/src/v2-events/features/events/components/VerificationWizard.interaction.stories.tsx
@@ -0,0 +1,96 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
+import React from 'react'
+import { expect, fn } from '@storybook/test'
+import { VerificationWizard } from './VerificationWizard'
+
+const meta: Meta<typeof VerificationWizard> = {
+  title: 'Components/VerificationWizard/Interaction'
+}
+
+export default meta
+
+type Story = StoryObj<typeof VerificationWizard>
+
+const onNextPageSpy = fn()
+const onVerifyActionSpy = fn()
+const onSubmitSpy = fn()
+
+export const VerificationWizardModal: Story = {
+  parameters: {
+    chromatic: { disableSnapshot: true }
+  },
+  render: function Component(args) {
+    return (
+      <VerificationWizard
+        currentPage={0}
+        pageConfig={{
+          verify: {
+            label: {
+              id: 'v2.buttons.verify',
+              defaultMessage: 'Verify',
+              description: 'Verify button label'
+            }
+          },
+          cancel: {
+            label: {
+              id: 'v2.buttons.cancel',
+              defaultMessage: 'Cancel',
+              description: 'Cancel button label'
+            },
+            confirmation: {
+              title: {
+                id: 'v2.buttons.cancel',
+                defaultMessage: 'Cancel',
+                description: 'Cancel button title'
+              },
+              body: {
+                id: 'v2.buttons.cancel',
+                defaultMessage: 'Are you sure you want to cancel?',
+                description: 'Cancel button body'
+              }
+            }
+          }
+        }}
+        pageTitle="Verification Wizard"
+        showReviewButton={false}
+        totalPages={1}
+        onNextPage={onNextPageSpy}
+        onPreviousPage={() => {}}
+        onSubmit={onSubmitSpy}
+        onVerifyAction={onVerifyActionSpy}
+      />
+    )
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement)
+    await step('Modal opens on cancel button press', async () => {
+      await userEvent.click(
+        await canvas.findByRole('button', { name: 'Cancel' })
+      )
+
+      await expect(await canvas.findByRole('dialog')).toBeInTheDocument()
+    })
+
+    await step('Modal closes on cancel button press', async () => {
+      const modal = await canvas.findByRole('dialog')
+      await userEvent.click(
+        await within(modal).findByRole('button', { name: 'Cancel' })
+      )
+
+      await expect(modal).not.toBeInTheDocument()
+    })
+
+    await step(
+      'onSubmit and onVerifyAction is called on verify button press',
+      async () => {
+        await userEvent.click(
+          await canvas.findByRole('button', { name: 'Verify' })
+        )
+
+        await expect(onSubmitSpy).toHaveBeenCalled()
+        await expect(onVerifyActionSpy).toHaveBeenCalled()
+      }
+    )
+  }
+}
