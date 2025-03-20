a81b4da9c61b78ac7cacad4779e33d903af2a6ca
jamil314 <jamil31415926@gmail.com>
2025-03-20 13:20:06 +0600

    refactor: move interaction visual tests in separate file

--- packages/client/src/v2-events/features/events/registered-fields/Address.stories.tsx
+++ packages/client/src/v2-events/features/events/registered-fields/Address.stories.tsx
@@ -10,10 +10,8 @@
  */
 
 import type { Meta, StoryObj } from '@storybook/react'
-import { expect, fn } from '@storybook/test'
-import { userEvent, within } from '@storybook/testing-library'
+import { fn } from '@storybook/test'
 import React from 'react'
-import * as selectEvent from 'react-select-event'
 import styled from 'styled-components'
 import {
   ActionType,
@@ -106,80 +104,6 @@ export const AddressFieldWithUserPrimaryOfficeAddress: StoryObj<
               district: '$user.district',
               urbanOrRural: 'URBAN'
             }
-          }
-        ]}
-        formData={formData}
-        id="my-form"
-        setAllFieldsDirty={false}
-        onChange={(data) => {
-          args.onChange(data)
-          setFormData(data)
-        }}
-      />
-    )
-  }
-}
-
-export const AddressFieldInteraction: StoryObj<typeof FormFieldGenerator> = {
-  name: 'Interaction between fields',
-  parameters: {
-    layout: 'centered'
-  },
-  play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement)
-
-    await canvas.findByText(/Address/)
-
-    await step(
-      'Admin structure dropdowns are shown gradually as the inputs are filled',
-      async () => {
-        // Verify that `District` select is not visible initially
-        await expect(canvas.queryByTestId('location__district')).toBeNull()
-
-        // Select a province
-        const province = await canvas.findByTestId('location__province')
-        await userEvent.click(province)
-        await selectEvent.select(province, 'Central')
-
-        // Verify that `District` becomes visible
-        const district = await canvas.findByTestId('location__district')
-        await expect(district).toBeInTheDocument()
-
-        // Select a district
-        await userEvent.click(district)
-        await selectEvent.select(district, 'Ibombo')
-      }
-    )
-
-    await step(
-      'Selecting "Rural" for address details type hides detailed street information',
-      async () => {
-        // Click on the "RURAL" radio option
-        const ruralRadio = await canvas.findByTestId('radio-option__RURAL')
-        await userEvent.click(ruralRadio)
-
-        // Verify that the "village" input appears
-        const villageInput = await canvas.findByTestId('text__village')
-        await expect(villageInput).toBeInTheDocument()
-      }
-    )
-  },
-  render: function Component(args) {
-    const [formData, setFormData] = React.useState({})
-    return (
-      <StyledFormFieldGenerator
-        fields={[
-          {
-            id: 'storybook.address',
-            type: FieldType.ADDRESS,
-            label: {
-              id: 'storybook.address.label',
-              defaultMessage: 'Address',
-              description: 'The title for the address input'
-            },
-            defaultValue: {
-              country: 'FAR'
-            } as AddressFieldValue
           }
         ]}
         formData={formData}
@@ -389,74 +313,6 @@ export const AddressInCopy: StoryObj<typeof Review> = {
         </p>
         <pre>{JSON.stringify(stringifier(allFields, FORM_DATA), null, 2)}</pre>
       </div>
-    )
-  }
-}
-
-export const GenericAddressFields: StoryObj<typeof FormFieldGenerator> = {
-  name: 'Country is other than Farajaland',
-  parameters: {
-    layout: 'centered'
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-
-    // Verify that Farajaland specific fields are not visible
-    await expect(canvas.queryByTestId('location__province')).toBeNull()
-    await expect(canvas.queryByTestId('location__district')).toBeNull()
-
-    await userEvent.type(await canvas.findByTestId('text__state'), 'Dhaka')
-    await userEvent.type(
-      await canvas.findByTestId('text__district2'),
-      'Dhaka North'
-    )
-    await userEvent.type(
-      await canvas.findByTestId('text__cityOrTown'),
-      'Mohakhali'
-    )
-    await userEvent.type(
-      await canvas.findByTestId('text__addressLine1'),
-      'DOHS'
-    )
-    await userEvent.type(
-      await canvas.findByTestId('text__addressLine2'),
-      'Road 4'
-    )
-    await userEvent.type(
-      await canvas.findByTestId('text__addressLine3'),
-      'House 142'
-    )
-    await userEvent.type(
-      await canvas.findByTestId('text__postcodeOrZip'),
-      '3300'
-    )
-  },
-  render: function Component(args) {
-    const [formData, setFormData] = React.useState({})
-    return (
-      <StyledFormFieldGenerator
-        fields={[
-          {
-            id: 'storybook.address',
-            type: FieldType.ADDRESS,
-            label: {
-              id: 'storybook.address.label',
-              defaultMessage: 'Address',
-              description: 'The title for the address input'
-            },
-            defaultValue: {
-              country: 'BGD'
-            } as AddressFieldValue
-          }
-        ]}
-        formData={formData}
-        id="my-form"
-        setAllFieldsDirty={false}
-        onChange={(data) => {
-          args.onChange(data)
-          setFormData(data)
-        }}
-      />
     )
   }
 }
