7cd1984e377e47c5c0e83cff601c806054c6c1e6
cibelius <cihan.m.bebek@gmail.com>
2025-04-09 09:44:30 +0300

    field reference validation passes if reference field is undefined

--- packages/commons/src/conditionals/conditionals.ts
+++ packages/commons/src/conditionals/conditionals.ts
@@ -180,29 +180,27 @@ function getDateFromNow(days: number) {
     .toISOString()
     .split('T')[0]
 }
-
-/* This function will output JSONSchema which looks for example like this:
-{
-  "type": "object",
-  "properties": {
-    "mother.dob": {
-      "type": "string",
-      "format": "date",
-      "formatMaximum": {
-        "$data": "1/child.dob"
-      }
-    },
-    "child.dob": {
-      "type": "string",
-      "format": "date"
-    }
-  },
-  "required": [
-    "mother.dob",
-    "child.dob"
-  ]
-}
-*/
+/**
+ * This function will output JSONSchema which looks for example like this:
+ * @example
+ * {
+ *   "type": "object",
+ *   "properties": {
+ *     "mother.dob": {
+ *       "type": "string",
+ *       "format": "date",
+ *       "formatMinimum": {
+ *         "$data": "1/child.dob"
+ *       }
+ *     },
+ *     "child.dob": {
+ *       "type": "string",
+ *       "format": "date"
+ *     }
+ *   },
+ *   "required": ["mother.dob"]
+ * }
+ */
 function getDateRangeToFieldReference(
   fieldId: string,
   comparedFieldId: string,
@@ -218,7 +216,7 @@ function getDateRangeToFieldReference(
       },
       [comparedFieldId]: { type: 'string', format: 'date' }
     },
-    required: [fieldId, comparedFieldId]
+    required: [fieldId]
   }
 }
 
