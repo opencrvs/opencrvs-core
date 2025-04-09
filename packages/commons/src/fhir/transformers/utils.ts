aa49b6b7876f4210dc4f379fda33e5c03bae4a3b
pankaj-pant <44195004+pankaj-pant@users.noreply.github.com>
2025-04-09 15:32:15 +0300

    fix updates to informant type from any to mother, father (#9192)

--- packages/commons/src/fhir/transformers/utils.ts
+++ packages/commons/src/fhir/transformers/utils.ts
@@ -61,7 +61,6 @@ import {
   isObservation,
   isURLReference,
   urlReferenceToResourceIdentifier,
-  BundleEntryWithFullUrl,
   findEntryFromBundle
 } from '..'
 
@@ -804,9 +803,9 @@ export function setInformantReference<T extends CompositionSectionCode>(
     throw new Error(`${sectionCode} not found in composition!`)
   }
   const personSectionEntry = section.entry[0]
-  const personEntry = fhirBundle.entry.find(
-    (entry): entry is BundleEntryWithFullUrl<Patient> =>
-      entry.fullUrl === personSectionEntry.reference
+  const personEntry = findEntryFromBundle(
+    fhirBundle,
+    personSectionEntry.reference
   )
   if (!personEntry) {
     return
