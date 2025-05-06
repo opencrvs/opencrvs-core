0dbfcaa333e1bc32a3de5718aaea35fcc528a498
naftis <pyry.rouvila@gmail.com>
2025-05-06 13:01:30 +0300

    chore: remove unused HIDDEN FieldType

--- packages/commons/src/events/FieldTypeMapping.ts
+++ packages/commons/src/events/FieldTypeMapping.ts
@@ -97,9 +97,6 @@ export function mapFieldTypeToZod(type: FieldType, required?: boolean) {
     case FieldType.FACILITY:
     case FieldType.OFFICE:
     case FieldType.SIGNATURE:
-    case FieldType.HIDDEN:
-      schema = required ? NonEmptyTextValue : TextValue
-      break
     case FieldType.NUMBER:
       schema = NumberFieldValue
       break
