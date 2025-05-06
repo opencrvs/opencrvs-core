2ef7da493ada8e88da81a591a5f233a061e155f9
Markus <markus@agilepursuit.fi>
2025-05-06 13:55:13 +0300

    chore: change the way string is found

--- packages/client/src/v2-events/features/workqueues/EventOverview/EventOverview.stories.tsx
+++ packages/client/src/v2-events/features/workqueues/EventOverview/EventOverview.stories.tsx
@@ -10,7 +10,7 @@
  */
 
 import type { Meta, StoryObj } from '@storybook/react'
-import { expect, within } from '@storybook/test'
+import { expect, waitFor, within } from '@storybook/test'
 import { createTRPCMsw, httpLink } from '@vafanassieff/msw-trpc'
 import React from 'react'
 import superjson from 'superjson'
@@ -51,12 +51,11 @@ const tRPCMsw = createTRPCMsw<AppRouter>({
 
 export const Overview: Story = {
   play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement)
-    const surname = await canvas.findByTestId('applicant.surname')
     await step('active draft values are shown to the user', async () => {
-      await expect(surname.textContent).toBe(
-        "Applicant's last nameThis value is from a draft"
-      )
+      const canvas = within(canvasElement)
+      await waitFor(async () => {
+        await canvas.findByText('This value is from a draft')
+      })
     })
   },
   parameters: {
