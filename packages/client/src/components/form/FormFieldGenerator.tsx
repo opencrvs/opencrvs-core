92236c94813fd636b16d93be18a82da2fd8a331f
euanmillar <euanmillar77@gmail.com>
2025-05-06 18:15:36 +0530

    Add typescript-eslint/no-explicit-any to fix linting issue

--- packages/client/src/components/form/FormFieldGenerator.tsx
+++ packages/client/src/components/form/FormFieldGenerator.tsx
@@ -1337,6 +1337,7 @@ class FormSectionComponent extends React.Component<Props> {
                 ignoreBottomMargin={field.ignoreBottomMargin}
               >
                 <Field name={field.name}>
+                  {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
                   {(formikFieldProps: FieldProps<any>) => {
                     return (
                       <MemoizedLocationList field={field}>
