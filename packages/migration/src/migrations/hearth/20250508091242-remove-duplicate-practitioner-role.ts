2a3d2648db08f6ffa3129947d0d0f7b04f971ae1
Barry Dwyer <barry.dwyer@decodedev.co.za>
2025-05-09 09:10:05 +0200

    fix: Add transaction and cursor to delete in batches
    This avoids hitting a potential BSON 16mb limit if run of a large data set

--- packages/migration/src/migrations/hearth/20250508091242-remove-duplicate-practitioner-role.ts
+++ packages/migration/src/migrations/hearth/20250508091242-remove-duplicate-practitioner-role.ts
@@ -9,65 +9,80 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-import { Db } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 
 // Migration to remove redundant historical entries in PractitionerRole_history
 // but also handles the case where a Practioner may change their role and change it back
 // e.g. From Registrar to Registration Agent and back to Registrar
 
-export const up = async (db: Db) => {
+export const up = async (db: Db, client: MongoClient) => {
   const collection = db.collection('PractitionerRole_history')
+  const session = client.startSession()
 
-  const docs = await collection
-    .find(
-      {
-        practitioner: { $exists: true },
-        code: { $exists: true },
-        'code.0.coding.0.code': { $exists: true },
-        'meta.lastUpdated': { $exists: true }
-      },
-      {
-        projection: {
-          practitioner: 1,
-          code: 1,
-          meta: 1
+  try {
+    await session.withTransaction(async () => {
+      const cursor = collection
+        .find(
+          {
+            practitioner: { $exists: true },
+            code: { $exists: true },
+            'code.0.coding.0.code': { $exists: true },
+            'meta.lastUpdated': { $exists: true }
+          },
+          {
+            projection: {
+              practitioner: 1,
+              code: 1,
+              meta: 1
+            }
+          }
+        )
+        .sort({ 'practitioner.reference': 1, 'meta.lastUpdated': 1 })
+
+      const toDelete: any[] = []
+
+      let prevId: string | undefined
+      let prevRole: string | undefined
+
+      while (await cursor.hasNext()) {
+        const doc = await cursor.next()
+        if (!doc) continue
+
+        const practitionerId = doc.practitioner?.reference
+        const role = doc.code?.[0]?.coding?.[0]?.code
+
+        if (!practitionerId || !role) continue
+
+        if (practitionerId !== prevId) {
+          prevId = practitionerId
+          prevRole = role
+          continue
+        }
+
+        if (role === prevRole) {
+          toDelete.push(doc._id)
+        } else {
+          prevRole = role
         }
       }
-    )
-    .sort({ 'practitioner.reference': 1, 'meta.lastUpdated': 1 })
-    .toArray()
+      const batches = createBatches(toDelete, 1000)
 
-  const toDelete: any[] = []
-
-  let prevId: string | undefined
-  let prevRole: string | undefined
-
-  for (let i = 0; i < docs.length; i++) {
-    const doc = docs[i]
-    const practitionerId = doc.practitioner?.reference
-    const role = doc.code?.[0]?.coding?.[0]?.code
-
-    if (!practitionerId || !role) continue
-
-    if (practitionerId !== prevId) {
-      prevId = practitionerId
-      prevRole = role
-      continue
-    }
-
-    if (role === prevRole) {
-      toDelete.push(doc._id)
-    } else {
-      prevRole = role
-    }
-  }
-
-  if (toDelete.length) {
-    const result = await collection.deleteMany({ _id: { $in: toDelete } })
-    console.log(`Deleted ${result.deletedCount} redundant historical entries.`)
-  } else {
-    console.log('No redundant entries found.')
+      for (const batch of batches) {
+        await collection.deleteMany({ _id: { $in: batch } })
+        console.log(`Deleted batch of ${batch.length} duplicates`)
+      }
+    })
+  } finally {
+    await session.endSession()
   }
 }
 
 export const down = async () => {}
+
+function createBatches<T>(array: T[], batchSize: number): T[][] {
+  const result = []
+  for (let i = 0; i < array.length; i += batchSize) {
+    result.push(array.slice(i, i + batchSize))
+  }
+  return result
+}
