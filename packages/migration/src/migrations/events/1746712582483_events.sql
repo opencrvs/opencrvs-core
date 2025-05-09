bf8739d8b728f1c0248a3a3a726d5cd38cdf597f
Pyry <pyry.rouvila@gmail.com>
2025-05-09 15:08:19 +0300

    chore: add transaction ids and created by role

--- packages/migration/src/migrations/events/1746712582483_events.sql
+++ packages/migration/src/migrations/events/1746712582483_events.sql
@@ -1,14 +1,9 @@
 -- Up Migration
 
-CREATE TYPE secure.action_status AS ENUM (
-  'Requested',
-  'Accepted',
-  'Rejected'
-);
 
 CREATE TABLE secure.events (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  transaction_id text NOT NULL,
+  transaction_id text NOT NULL UNIQUE,
   type text NOT NULL,
   data jsonb NOT NULL,
   tracking_id text NOT NULL,
@@ -16,15 +11,22 @@ CREATE TABLE secure.events (
   updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
+CREATE TYPE secure.action_status AS ENUM (
+  'Requested',
+  'Accepted',
+  'Rejected'
+);
+
 CREATE TABLE secure.event_actions (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  transaction_id text UNIQUE NOT NULL,
   event_id uuid NOT NULL REFERENCES secure.events(id),
   action_type text NOT NULL,
   declaration jsonb DEFAULT '{}'::jsonb NOT NULL,
   annotations jsonb DEFAULT '{}'::jsonb NOT NULL,
   status secure.action_status NOT NULL,
-  transaction_id text NOT NULL,
-  created_by uuid NOT NULL,
+  created_by text NOT NULL,
+  created_by_role text NOT NULL,
   created_at_location text NOT NULL,
   created_at timestamp with time zone DEFAULT now() NOT NULL
 );
@@ -32,5 +34,5 @@ CREATE TABLE secure.event_actions (
 -- Down Migration
 
 DROP TABLE secure.event_actions;
-DROP TABLE secure.events;
 DROP TYPE secure.action_status;
+DROP TABLE secure.events;
