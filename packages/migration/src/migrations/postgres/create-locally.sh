074d2c9182da22e5c83d06ca9ce47f313bfb8a07
Pyry <pyry.rouvila@gmail.com>
2025-05-09 13:26:35 +0300

    chore: remove initial unneeded scripts

--- packages/migration/src/migrations/postgres/create-locally.sh
+++
@@ -1,5 +0,0 @@
-docker run -d \
-  --name opencrvs \
-  -e POSTGRES_PASSWORD=postgres \
-  -p 5432:5432 \
-  postgres:17
--- packages/migration/src/migrations/postgres/schema-dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker exec -t opencrvs pg_dump \
-  -U events_user \
-  -d events \
-  --schema-only \
-  > events_schema.sql
