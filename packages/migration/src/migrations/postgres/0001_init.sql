d647aca081f360d66ac5cab6e21c96a0ed198dd2
naftis <pyry.rouvila@gmail.com>
2025-05-14 16:45:54 +0300

    feat: move privileges when we are connected to app schema with migrator

--- packages/migration/src/migrations/postgres/0001_init.sql
+++ packages/migration/src/migrations/postgres/0001_init.sql
@@ -12,10 +12,5 @@ REVOKE CREATE ON SCHEMA public FROM events_migrator;
 
 CREATE SCHEMA app AUTHORIZATION events_migrator;
 
-GRANT USAGE ON SCHEMA app TO events_app;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA app
-  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO events_app;
-
 ALTER ROLE events_migrator SET search_path = app;
 ALTER ROLE events_app SET search_path = app;
