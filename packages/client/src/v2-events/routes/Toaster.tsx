57b79010a87a35a06f94e10ac4778700db33c1af
cibelius <cihan.m.bebek@gmail.com>
2025-05-16 11:41:54 +0300

    minor refactor

--- packages/client/src/v2-events/routes/Toaster.tsx
+++ packages/client/src/v2-events/routes/Toaster.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import { Toaster as ReactHotToaster, ToastBar, toast } from 'react-hot-toast'
+import {
+  Toaster as ReactHotToaster,
+  Renderable,
+  ToastBar,
+  toast
+} from 'react-hot-toast'
 import { MessageDescriptor, useIntl } from 'react-intl'
+import styled from 'styled-components'
 import { Button } from '@opencrvs/components/lib/buttons'
 import { Icon } from '@opencrvs/components/lib/Icon'
 
@@ -16,38 +22,36 @@ const availableToasts: Record<ToastKey, MessageDescriptor> = {
   }
 }
 
+const Message = styled.span`
+  padding: 0 1rem;
+`
+
+// TODO CIHAN: fix font
+
 export function Toaster() {
   const { formatMessage } = useIntl()
+
+  // When calling the react-hot-toast toast.<success/error/etc.>() function, the string message is transformed into a React element.
+  // This function extracts the actual message from the React element, so we can fetch the matching translation with it.
+  function getMessage(message: Renderable) {
+    const msgElement = message?.valueOf() as React.ReactElement
+    const messageValue = msgElement.props.children
+    const translation =
+      messageValue in availableToasts &&
+      availableToasts[messageValue as ToastKey]
+
+    return translation ? formatMessage(translation) : message
+  }
 
   return (
     <ReactHotToaster position="bottom-center" toastOptions={{ duration: 6000 }}>
       {(t) => (
         <ToastBar toast={t}>
           {({ icon, message }) => {
-            const msgElement = message?.valueOf() as React.ReactElement
-            const messageValue = msgElement.props?.children
-            console.log(messageValue)
-            const toastTranslation =
-              messageValue in availableToasts &&
-              availableToasts[messageValue as ToastKey]
-
-            console.log(toastTranslation)
-
-            const displayedMessage = toastTranslation
-              ? formatMessage(toastTranslation)
-              : message
-
-            console.log(displayedMessage)
-
-            // console.log(JSON.stringify(message, null, 2))
-            // console.log(rest)
-            // const toastConfig =  availableToasts[message]
-            // const toastMessage = formatMessage(toastConfig.message)
-
             return (
               <>
                 {icon}
-                {displayedMessage}
+                <Message>{getMessage(message)}</Message>
                 {t.type !== 'loading' && (
                   <Button onClick={() => toast.dismiss(t.id)}>
                     <Icon color="primary" name="Close" size="large" />
