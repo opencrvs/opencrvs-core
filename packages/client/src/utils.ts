aed0f9f12aac8b4518f6440147403bdf5900f739
tahmidrahman-dsi <tahmid.rahman@dsinnovators.com>
2025-05-30 15:32:28 +0600

    refactor: improve stringify polyfill function

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -64,18 +64,82 @@ export interface IndexMap<T> {
   [id: string]: T | undefined
 }
 
-// browser polyfill for 'querystring module's stringify function
-export function stringify(
-  obj: Record<string, string | number | boolean | null | undefined>
-): string {
-  const params = new URLSearchParams()
+/**
+ * browser polyfill for 'querystring module's stringify function
+ * https://github.com/vitejs/vite/discussions/18671
+ * below is a typescript copy of the code querystring-es3
+ * https://github.com/mike-spainhower/querystring/blob/master/encode.js
+ */
 
-  for (const key in obj) {
-    const value = obj[key]
-    if (value !== undefined && value !== null) {
-      params.append(key, String(value))
+type Primitive = string | number | boolean | null | undefined
+const objectKeys =
+  Object.keys ||
+  function (obj) {
+    const res = []
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) res.push(key)
     }
+    return res
   }
 
-  return params.toString()
+const isArray =
+  Array.isArray ||
+  function (xs) {
+    return Object.prototype.toString.call(xs) === '[object Array]'
+  }
+
+function stringifyPrimitive(value: Primitive): string {
+  switch (typeof value) {
+    case 'string':
+      return value
+
+    case 'boolean':
+      return value ? 'true' : 'false'
+
+    case 'number':
+      return isFinite(value) ? value.toString() : ''
+
+    default:
+      return ''
+  }
+}
+
+export function stringify(
+  obj: Record<string, unknown> | null | undefined,
+  sep = '&',
+  eq = '=',
+  name: Primitive | null = null
+) {
+  if (obj === null) {
+    obj = undefined
+  }
+
+  if (typeof obj === 'object') {
+    return map(objectKeys(obj), function (k: string) {
+      const ks = encodeURIComponent(stringifyPrimitive(k as Primitive)) + eq
+      if (isArray(obj[k])) {
+        return map(obj[k], function (v: Primitive) {
+          return ks + encodeURIComponent(stringifyPrimitive(v))
+        }).join(sep)
+      } else {
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k] as Primitive))
+      }
+    }).join(sep)
+  }
+
+  if (!name) return ''
+  return (
+    encodeURIComponent(stringifyPrimitive(name)) +
+    eq +
+    encodeURIComponent(stringifyPrimitive(obj))
+  )
+}
+
+function map(xs: string[], f: (x: string, i: number) => string): string[] {
+  if (xs.map) return xs.map(f)
+  const res = []
+  for (let i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i))
+  }
+  return res
 }
