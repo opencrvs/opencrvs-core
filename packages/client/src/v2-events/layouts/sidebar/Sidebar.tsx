1271fa80330078f65952054b3c79b1152b728f7a
jamil314 <jamil31415926@gmail.com>
2025-06-02 20:15:38 +0600

    fix: logout navigaiton

--- packages/client/src/v2-events/layouts/sidebar/Sidebar.tsx
+++ packages/client/src/v2-events/layouts/sidebar/Sidebar.tsx
@@ -79,7 +79,7 @@ export const Sidebar = ({
     removeToken()
     await removeUserDetails()
     window.location.assign(
-      `${window.config.LOGIN_URL}?lang=${await storage.getItem('language')}&redirectTo=${window.location.origin}/${ROUTES.V2.buildPath({})}`
+      `${window.config.LOGIN_URL}?lang=${await storage.getItem('language')}&redirectTo=${window.location.origin}${ROUTES.V2.buildPath({})}`
     )
   }
 
