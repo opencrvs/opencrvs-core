8c698318815bf8ce52ace4ec198f6a74ad39679f
tareq89 <tareqaziz.aust.cse@gmail.com>
2025-06-04 07:02:01 +0600

    refactor AdvancedSearchTab.interaction

--- packages/client/src/v2-events/features/events/AdvancedSearch/AdvancedSearch.interaction.stories.tsx
+++ packages/client/src/v2-events/features/events/AdvancedSearch/AdvancedSearch.interaction.stories.tsx
@@ -8,15 +8,18 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
+
 import React from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 import { userEvent, within, expect } from '@storybook/test'
 import { createTRPCMsw, httpLink } from '@vafanassieff/msw-trpc'
 import superjson from 'superjson'
+
 import { TENNIS_CLUB_MEMBERSHIP } from '@opencrvs/commons/client'
 import { TRPCProvider, AppRouter } from '@client/v2-events/trpc'
 import { ROUTES, routesConfig } from '@client/v2-events/routes'
 import { createDeclarationTrpcMsw } from '@client/tests/v2-events/declaration.utils'
+
 import { AdvancedSearch } from './index'
 
 const meta: Meta<typeof AdvancedSearch> = {
@@ -31,17 +34,15 @@ const meta: Meta<typeof AdvancedSearch> = {
   ]
 }
 
+export default meta
+
+type Story = StoryObj<typeof AdvancedSearch>
+
 const tRPCMsw = createTRPCMsw<AppRouter>({
-  links: [
-    httpLink({
-      url: '/api/events'
-    })
-  ],
+  links: [httpLink({ url: '/api/events' })],
   transformer: { input: superjson, output: superjson }
 })
 
-export default meta
-type Story = StoryObj<typeof AdvancedSearch>
 const declarationTrpcMsw = createDeclarationTrpcMsw(tRPCMsw)
 
 export const AdvancedSearchStory: Story = {
@@ -60,6 +61,7 @@ export const AdvancedSearchStory: Story = {
   },
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
+
     await step('Opens up advanced tennis club membership tab', async () => {
       await canvas.findByText('Tennis club membership application')
     })
@@ -74,13 +76,11 @@ export const AdvancedSearchStory: Story = {
     )
 
     await step(
-      'Search button should be disabled when less than 2 fields are not filled or there are search field errors',
+      'Search button should be disabled with less than 2 fields or errors',
       async () => {
         const searchButton = (
-          await canvas.findAllByRole('button', {
-            name: 'Search'
-          })
-        ).find((x) => x.id === 'search')
+          await canvas.findAllByRole('button', { name: 'Search' })
+        ).find((btn) => btn.id === 'search')
 
         await expect(searchButton).toBeVisible()
         await expect(searchButton).toBeDisabled()
@@ -88,14 +88,11 @@ export const AdvancedSearchStory: Story = {
     )
 
     await step(
-      'Should be able to fill up Place of registration and Status of record and go to search page',
+      'Fill up Place of registration and Status to enable search',
       async () => {
         const searchButton = (
-          await canvas.findAllByRole('button', {
-            name: 'Search'
-          })
-        ).find((x) => x.id === 'search')
-        await expect(searchButton).toBeVisible()
+          await canvas.findAllByRole('button', { name: 'Search' })
+        ).find((btn) => btn.id === 'search')
 
         const registrationAccordion = await canvas.findByTestId(
           'accordion-v2.advancedSearch.form.registrationDetails'
@@ -106,28 +103,23 @@ export const AdvancedSearchStory: Story = {
           })
         )
 
-        const placeOfRegistration = await canvas.findByTestId(
+        const placeInput = await canvas.findByTestId(
           'event____legalStatus____REGISTERED____createdAtLocation'
         )
-
-        await userEvent.type(placeOfRegistration, 'Ibombo', {
-          delay: 100
-        })
+        await userEvent.type(placeInput, 'Ibombo', { delay: 100 })
         const placeOption = await canvas.findAllByText('Ibombo District Office')
         await userEvent.click(placeOption[0])
         await expect(searchButton).toBeDisabled()
 
-        const eventStatusContainer = await canvas.findByTestId(
+        const statusContainer = await canvas.findByTestId(
           'select__event____status'
         )
-        const eventStatusInput =
-          await within(eventStatusContainer).findByRole('textbox')
-
-        await userEvent.click(eventStatusInput)
+        const statusInput = await within(statusContainer).findByRole('textbox')
+        await userEvent.click(statusInput)
         const anyStatusOption = await canvas.findAllByText('Any status')
         await userEvent.click(anyStatusOption[0])
-        await expect(searchButton).toBeEnabled()
 
+        await expect(searchButton).toBeEnabled()
         if (searchButton) {
           await userEvent.click(searchButton)
         }
@@ -152,162 +144,123 @@ export const AdvancedSearchTabsBehaviour: Story = {
   },
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement)
+
     const checkRegistrationDetails = async () => {
       await canvas.findByText('Tennis club membership application')
 
-      const registrationAccordion = await canvas.findByTestId(
+      const accordion = await canvas.findByTestId(
         'accordion-v2.advancedSearch.form.registrationDetails'
       )
 
-      await within(registrationAccordion).findByRole('button', {
-        name: 'Hide'
-      })
+      await within(accordion).findByRole('button', { name: 'Hide' })
+      await expect(
+        await canvas.findByTestId(
+          'event____legalStatus____REGISTERED____createdAtLocation'
+        )
+      ).toHaveValue('Ibombo District Office')
 
-      const placeOfRegistration = await canvas.findByTestId(
-        'event____legalStatus____REGISTERED____createdAtLocation'
-      )
-
-      await expect(placeOfRegistration).toHaveValue('Ibombo District Office')
-
-      await within(registrationAccordion).findByText('June 2024 to June 2025')
-
-      await within(registrationAccordion).findByText('Any status')
+      await within(accordion).findByText('June 2024 to June 2025')
+      await within(accordion).findByText('Any status')
     }
+
     await step(
-      'Tennis club membership tab > Registration details form is pre populated from the url even if accordion is hide and show again',
+      'Prepopulate Registration details from URL and toggle accordion',
       async () => {
         await checkRegistrationDetails()
 
-        const registrationAccordion = await canvas.findByTestId(
+        const accordion = await canvas.findByTestId(
           'accordion-v2.advancedSearch.form.registrationDetails'
         )
+        const toggle = await within(accordion).findByRole('button', {
+          name: 'Hide'
+        })
+        await userEvent.click(toggle)
         await userEvent.click(
-          await within(registrationAccordion).findByRole('button', {
-            name: 'Hide'
-          })
-        )
-        await userEvent.click(
-          await within(registrationAccordion).findByRole('button', {
-            name: 'Show'
-          })
+          await within(accordion).findByRole('button', { name: 'Show' })
         )
         await checkRegistrationDetails()
       }
     )
 
     const checkApplicantDetails = async () => {
-      const applicantAccordion = await canvas.findByTestId(
+      const accordion = await canvas.findByTestId(
         'accordion-v2.event.tennis-club-membership.search.applicants'
       )
-
-      await within(applicantAccordion).findByRole('button', {
-        name: 'Hide'
-      })
-
-      const firstName = await canvas.findByTestId(
-        'text__applicant____firstname'
-      )
-
-      await expect(firstName).toHaveValue('Nina')
-      const surname = await canvas.findByTestId('text__applicant____surname')
-
-      await expect(surname).toHaveValue('Roy')
+      await within(accordion).findByRole('button', { name: 'Hide' })
+      await expect(
+        await canvas.findByTestId('text__applicant____firstname')
+      ).toHaveValue('Nina')
+      await expect(
+        await canvas.findByTestId('text__applicant____surname')
+      ).toHaveValue('Roy')
     }
-    await step(
-      "Tennis club membership tab > Applicant's details form is pre populated from the url",
-      async () => {
-        await checkApplicantDetails()
-      }
-    )
+
+    await step("Prepopulate Applicant's details from URL", async () => {
+      await checkApplicantDetails()
+    })
 
     const checkRecommenderDetails = async () => {
-      const applicantAccordion = await canvas.findByTestId(
+      const accordion = await canvas.findByTestId(
         'accordion-v2.event.tennis-club-membership.search.recommender'
       )
-
-      await within(applicantAccordion).findByRole('button', {
-        name: 'Hide'
-      })
-
-      const firstName = await canvas.findByTestId(
-        'text__recommender____firstname'
-      )
-
-      await expect(firstName).toHaveValue('Annina')
-      const surname = await canvas.findByTestId('text__recommender____surname')
-
-      await expect(surname).toHaveValue('')
+      await within(accordion).findByRole('button', { name: 'Hide' })
+      await expect(
+        await canvas.findByTestId('text__recommender____firstname')
+      ).toHaveValue('Annina')
+      await expect(
+        await canvas.findByTestId('text__recommender____surname')
+      ).toHaveValue('')
     }
-    await step(
-      "Tennis club membership tab > Reccomender's details form is pre populated from the url",
-      async () => {
-        await checkRecommenderDetails()
-      }
-    )
 
-    const fillupRegisterLocation = async () => {
-      const registrationAccordion = await canvas.findByTestId(
+    await step("Prepopulate Recommender's details from URL", async () => {
+      await checkRecommenderDetails()
+    })
+
+    const fillRegisterLocation = async () => {
+      const accordion = await canvas.findByTestId(
         'accordion-v2.advancedSearch.form.registrationDetails'
       )
-
-      const accordionButton = await within(registrationAccordion).findByRole(
-        'button',
-        {
-          name: 'Show'
-        }
+      await userEvent.click(
+        await within(accordion).findByRole('button', { name: 'Show' })
       )
-
-      await userEvent.click(accordionButton)
-
-      const placeOfRegistration = await canvas.findByTestId(
+      const placeInput = await canvas.findByTestId(
         'event____legalStatus____REGISTERED____createdAtLocation'
       )
-
-      await userEvent.type(placeOfRegistration, 'Ibombo', {
-        delay: 100
-      })
-      const placeOption = await canvas.findAllByText('Ibombo District Office')
-      await userEvent.click(placeOption[0])
+      await userEvent.type(placeInput, 'Ibombo', { delay: 100 })
+      const option = await canvas.findAllByText('Ibombo District Office')
+      await userEvent.click(option[0])
     }
 
     const checkRegisterLocation = async () => {
-      const registrationAccordion = await canvas.findByTestId(
+      const accordion = await canvas.findByTestId(
         'accordion-v2.advancedSearch.form.registrationDetails'
       )
-
-      await within(registrationAccordion).findByRole('button', {
-        name: 'Hide'
-      })
-
-      const placeOfRegistration = await canvas.findByTestId(
-        'event____legalStatus____REGISTERED____createdAtLocation'
-      )
-
-      await expect(placeOfRegistration).toHaveValue('Ibombo District Office')
+      await within(accordion).findByRole('button', { name: 'Hide' })
+      await expect(
+        await canvas.findByTestId(
+          'event____legalStatus____REGISTERED____createdAtLocation'
+        )
+      ).toHaveValue('Ibombo District Office')
     }
 
-    await step(
-      'Select football tab, populate some fields and then reselect tennis club tab to check if the forms are still populated',
-      async () => {
-        const footballTab = await canvas.findByRole('button', {
-          name: 'Football club membership application'
-        })
-        await userEvent.click(footballTab)
+    await step('Switch tabs and check if form values persist', async () => {
+      const footballTab = await canvas.findByRole('button', {
+        name: 'Football club membership application'
+      })
+      const tennisTab = await canvas.findByRole('button', {
+        name: 'Tennis club membership application'
+      })
 
-        await fillupRegisterLocation()
+      await userEvent.click(footballTab)
+      await fillRegisterLocation()
+      await userEvent.click(tennisTab)
 
-        const tennisClubTab = await canvas.findByRole('button', {
-          name: 'Tennis club membership application'
-        })
-        await userEvent.click(tennisClubTab)
+      await checkRegistrationDetails()
+      await checkApplicantDetails()
+      await checkRecommenderDetails()
 
-        await checkRegistrationDetails()
-        await checkApplicantDetails()
-        await checkRecommenderDetails()
-
-        await userEvent.click(footballTab)
-        await checkRegisterLocation()
-      }
-    )
+      await userEvent.click(footballTab)
+      await checkRegisterLocation()
+    })
   }
 }
