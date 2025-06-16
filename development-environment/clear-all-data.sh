df0a7f7b51b609064d764ee67f75657e0a5c44ba
naftis <pyry.rouvila@gmail.com>
2025-06-16 22:54:50 +0300

    revert: unnecessary changes for clear-all-data to lessen diff

--- development-environment/clear-all-data.sh
+++ development-environment/clear-all-data.sh
@@ -14,10 +14,6 @@ print_usage_and_exit () {
 
 DIR=$(pwd)
 
-#################
-# Clear MongoDB #
-#################
-
 # It's fine if these fail as it might be that the databases do not exist at this point
 docker run --rm --network=opencrvs_default mongo:4.4 mongo --host mongo1 --eval "\
 db.getSiblingDB('hearth-dev').dropDatabase();\
@@ -36,12 +32,9 @@ curl -s "http://localhost:9200/_cat/indices?h=index" | while read -r index; do
     curl -XDELETE "http://elasticsearch:9200/$index" -v
 done
 
-###############
-# Clear Minio #
-###############
-
 docker run --rm --network=opencrvs_default appropriate/curl curl -X POST 'http://influxdb:8086/query?db=ocrvs' --data-urlencode "q=DROP SERIES FROM /.*/" -v
 PATH_TO_MINIO_DIR="$DIR/data/minio/ocrvs"
+# Clear Minio Data
 if [ -d $PATH_TO_MINIO_DIR ] ; then
  # Locally, as this script is called from the country config repo, the path to core is unknown
  # So we delete the data from the running shared volume location
