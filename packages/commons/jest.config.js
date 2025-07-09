82b748eee7276b23f3b92a80b2252348709ce0c9
Fahim Jubayer <fahimjubayer.dsi@gmail.com>
2025-07-09 20:42:04 +0600

    Update jest.config.js

--- packages/commons/jest.config.js
+++ packages/commons/jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['./test/setupJest.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json'
