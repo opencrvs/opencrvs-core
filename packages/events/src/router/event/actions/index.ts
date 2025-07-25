0738740ff02031466c5d843bbf4a3003c83e7db2
tahmidrahman-dsi <tahmid.rahman@dsinnovators.com>
2025-07-25 12:39:39 +0600

    fix: remove duplicate properties

--- packages/events/src/router/event/actions/index.ts
+++ packages/events/src/router/event/actions/index.ts
@@ -99,19 +99,16 @@ const ACTION_PROCEDURE_CONFIG = {
 
 type ActionProcedure = {
   request: MutationProcedure<{
-    meta: OpenApiMeta
     input: ActionInput
     output: EventDocument
     meta: OpenApiMeta
   }>
   accept: MutationProcedure<{
-    meta: OpenApiMeta
     input: ActionInput & { actionId: string }
     output: EventDocument
     meta: OpenApiMeta
   }>
   reject: MutationProcedure<{
-    meta: OpenApiMeta
     input: { eventId: string; actionId: string; transactionId: string }
     output: EventDocument
     meta: OpenApiMeta
