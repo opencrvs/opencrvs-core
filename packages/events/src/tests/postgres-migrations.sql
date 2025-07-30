14d9c2efa046b2ac3d4306b43fdb530c8bde9f32
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2025-07-30 15:20:19 +0600

    Make Correction V2 (#10063)
    * make correction action implemented
    
    * make correction added in record audit list
    
    * correction details title fixed
    
    * ActionType.CORRECT removed from events
    
    * isImmediateCorrection added in import procedure
    
    * isImmediateCorrection moved to annotation
    
    * rename MAKE_CORRECTION to CORRECTED for clearer history and UI labels
    
    * simplify immediate correction merge logic using map-filter pattern
    
    * EventHistoryDialog.stories.tsx test fixed

--- packages/events/src/tests/postgres-migrations.sql
+++ packages/events/src/tests/postgres-migrations.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 17.4 (Debian 17.4-1.pgdg120+2)
--- Dumped by pg_dump version 17.4 (Debian 17.4-1.pgdg120+2)
+-- Dumped from database version 17.5 (Debian 17.5-1.pgdg120+1)
+-- Dumped by pg_dump version 17.5 (Debian 17.5-1.pgdg120+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
