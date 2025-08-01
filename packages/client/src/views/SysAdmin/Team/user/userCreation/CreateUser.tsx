49cc8fcb6726b416174380a5129d205c0adc02e7
ABHISHEK-DEY-64150 <abhishekdey538@gmail.com>
2025-08-01 18:58:08 +0600

    separated create and update user components

---
+++ packages/client/src/views/SysAdmin/Team/user/userCreation/CreateUser.tsx
@@ -0,0 +1,59 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+
+import { clearUserFormData } from '@client/user/userReducer'
+import { withApollo, WithApolloClient } from '@apollo/client/react/hoc'
+import React, { useEffect } from 'react'
+import { injectIntl, WrappedComponentProps as IntlShapeProps } from 'react-intl'
+import { connect } from 'react-redux'
+import { CREATE_USER_ON_LOCATION } from '@client/navigation/routes'
+import {
+  RouteComponentProps,
+  withRouter
+} from '@client/components/WithRouterProps'
+import { UserFormPage } from './UserFormPage'
+
+interface IDispatchProps {
+  clearUserFormData: typeof clearUserFormData
+}
+
+type Props = RouteComponentProps & IDispatchProps & IntlShapeProps
+
+const CreateNewUserComponent = (props: WithApolloClient<Props>) => {
+  const { router, clearUserFormData } = props
+
+  useEffect(() => {
+    const initialize = async () => {
+      if (
+        router.location.pathname.includes(
+          CREATE_USER_ON_LOCATION.split('/:')[0]
+        )
+      ) {
+        clearUserFormData()
+      }
+    }
+
+    initialize()
+
+    return () => {
+      clearUserFormData()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <UserFormPage />
+}
+
+export const CreateUser = withRouter(
+  connect(null, {
+    clearUserFormData
+  })(injectIntl(withApollo<Props>(CreateNewUserComponent)))
+)
---
+++ packages/client/src/views/SysAdmin/Team/user/userCreation/UpdateUser.tsx
@@ -0,0 +1,71 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+import {
+  IFormSection,
+  IFormSectionData,
+  IFormSectionGroup
+} from '@client/forms'
+import {
+  clearUserFormData,
+  fetchAndStoreUserData
+} from '@client/user/userReducer'
+import { ApolloClient } from '@apollo/client'
+import { withApollo, WithApolloClient } from '@apollo/client/react/hoc'
+import React, { useEffect } from 'react'
+import { injectIntl, WrappedComponentProps as IntlShapeProps } from 'react-intl'
+import { connect } from 'react-redux'
+import {
+  RouteComponentProps,
+  withRouter
+} from '@client/components/WithRouterProps'
+import { UserFormPage } from './UserFormPage'
+
+interface IDispatchProps {
+  clearUserFormData: typeof clearUserFormData
+  fetchAndStoreUserData: typeof fetchAndStoreUserData
+}
+
+type Props = RouteComponentProps & IDispatchProps & IntlShapeProps
+
+const UpdateUserComponent = (props: WithApolloClient<Props>) => {
+  const { clearUserFormData, fetchAndStoreUserData, client, router } = props
+
+  const { userId } = router.params
+
+  if (!userId) {
+    throw new Error('userId not found')
+  }
+
+  useEffect(() => {
+    const initialize = async () => {
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      fetchAndStoreUserData(client as ApolloClient<any>, {
+        userId
+      })
+    }
+
+    initialize()
+
+    return () => {
+      clearUserFormData()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <UserFormPage />
+}
+
+export const UpdateUser = withRouter(
+  connect(null, {
+    clearUserFormData,
+    fetchAndStoreUserData
+  })(injectIntl(withApollo<Props>(UpdateUserComponent)))
+)
