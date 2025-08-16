f807e0d85245d08374e590752e65d4df5f9a8153
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-08-16 16:31:23 +0600

    chore: amend storybook tests and scope

--- packages/client/.storybook/default-request-handlers.ts
+++ packages/client/.storybook/default-request-handlers.ts
@@ -1138,7 +1138,7 @@ export const handlers = {
       return HttpResponse.json({
         data: {
           getUser: {
-            id: '679397db138339c63cdc24e1',
+            id: testDataGenerator().user.id.localRegistrar,
             userMgntUserID: '679397db138339c63cdc24e1',
             creationDate: '1737725915295',
             username: 'k.mweene',
