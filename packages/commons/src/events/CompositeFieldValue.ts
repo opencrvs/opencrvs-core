426b8e511fcf9337ed72481a1bcde6cb3c58cec9
naftis <pyry.rouvila@gmail.com>
2025-08-19 14:27:31 +0300

    nulls get dropped in favor of empty key-value - nullish fixes tRPC & Zod

--- packages/commons/src/events/CompositeFieldValue.ts
+++ packages/commons/src/events/CompositeFieldValue.ts
@@ -144,7 +144,7 @@ export type FileFieldWithOptionValue = z.infer<typeof FileFieldWithOptionValue>
 
 export const HttpFieldValue = z.object({
   loading: z.boolean(),
-  error: z.object({ statusCode: z.number(), message: z.string() }).nullable(),
+  error: z.object({ statusCode: z.number(), message: z.string() }).nullish(),
   data: z.any()
 })
 export type HttpFieldValue = z.infer<typeof HttpFieldValue>
