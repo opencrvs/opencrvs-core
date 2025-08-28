db92e5033e13f9b71aa2506dde3d47b89a4ff55c
Pankaj Pant <44195004+pankaj-pant@users.noreply.github.com>
2025-08-28 14:35:55 +0300

    update snapshot and storybook fixes

--- packages/commons/src/fixtures/forms.ts
+++ packages/commons/src/fixtures/forms.ts
@@ -859,6 +859,17 @@ export const TENNIS_CLUB_DECLARATION_FORM = defineDeclarationForm({
             description: 'This is the label for the field',
             id: 'v2.event.tennis-club-membership.action.declare.form.section.who.field.address.label'
           },
+          validation: [
+            {
+              message: {
+                defaultMessage: 'Invalid input',
+                description: 'Error message when generic field is invalid',
+                id: 'v2.error.invalid'
+              },
+              validator:
+                field('applicant.address').isValidAdministrativeLeafLevel()
+            }
+          ],
           configuration: {
             streetAddressForm: [
               {
