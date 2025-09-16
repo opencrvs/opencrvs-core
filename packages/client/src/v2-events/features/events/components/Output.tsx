86f0c8eaa42a33a227bbb2ae08a3655e49c3e729
jamil314 <jamil31415926@gmail.com>
2025-09-16 18:13:42 +0600

    fix: show deleted file in review page

--- packages/client/src/v2-events/features/events/components/Output.tsx
+++ packages/client/src/v2-events/features/events/components/Output.tsx
@@ -300,19 +300,15 @@ export function Output({
 
   // Note, checking for previousValue !== value is not enough, as we have composite fields.
   if (hasPreviousValue && !_.isEqual(previousValue, value)) {
-    const valueOutput = <ValueOutput config={field} value={value} />
+    let valueOutput = <ValueOutput config={field} value={value} />
 
     if (isEmptyValue(field, value)) {
       if (displayEmptyAsDash) {
-        return '-'
+        valueOutput = <>{'-'}</>
       }
 
-      return null
+      valueOutput = <>{null}</>
     }
-
-    const previousValueOutput = (
-      <ValueOutput config={previousValueField ?? field} value={previousValue} />
-    )
 
     return (
       <>
