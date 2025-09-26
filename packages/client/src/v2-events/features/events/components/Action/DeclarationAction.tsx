bb6eefcae35313f9c56793be5f30f20e8f5e26bd
cibelius <cihan.m.bebek@gmail.com>
2025-09-26 15:18:58 +0300

    remove unnecessary change to DeclarationAction

--- packages/client/src/v2-events/features/events/components/Action/DeclarationAction.tsx
+++ packages/client/src/v2-events/features/events/components/Action/DeclarationAction.tsx
@@ -18,7 +18,9 @@ import {
   findActiveDraftForEvent,
   dangerouslyGetCurrentEventStateWithDrafts,
   getActionAnnotation,
+  DeclarationUpdateActionType,
   ActionType,
+  Action,
   deepMerge,
   getUUID,
   deepDropNulls,
@@ -72,9 +74,6 @@ function useActionGuard(
     )
   }
 }
-
-type Props = PropsWithChildren<{ actionType: AvailableActionTypes }>
-
 /**
  * Creates a wrapper component for the declaration action.
  * Manages the state of the declaration action and its local draft.
@@ -84,7 +83,12 @@ type Props = PropsWithChildren<{ actionType: AvailableActionTypes }>
  *
  * This differs from AnnotationAction, which modify the annotation, and can be triggered multiple times.
  */
-function DeclarationActionComponent({ children, actionType }: Props) {
+function DeclarationActionComponent({
+  children,
+  actionType
+}: PropsWithChildren<{
+  actionType: AvailableActionTypes
+}>) {
   const { eventId } = useTypedParams(ROUTES.V2.EVENTS.DECLARE.PAGES)
 
   const events = useEvents()
@@ -258,13 +262,7 @@ function DeclarationActionComponent({ children, actionType }: Props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return children
+  return <NavigationStack>{children}</NavigationStack>
 }
 
-export const DeclarationAction = withSuspense((props: Props) => {
-  return (
-    <NavigationStack>
-      <DeclarationActionComponent {...props} />
-    </NavigationStack>
-  )
-})
+export const DeclarationAction = withSuspense(DeclarationActionComponent)
