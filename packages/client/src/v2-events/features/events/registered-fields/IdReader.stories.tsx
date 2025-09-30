4e0df23335ed77e1a894b5c8774c6480275f41df
tahmidrahman-dsi <tahmid.rahman@dsinnovators.com>
2025-09-30 21:16:41 +0600

    chore: add a story to demo usage id reader with qr reader

---
+++ packages/client/src/v2-events/features/events/registered-fields/IdReader.stories.tsx
@@ -0,0 +1,113 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import { fn } from '@storybook/test'
+import { generate } from 'lean-qr'
+import { makeAsyncComponent } from 'lean-qr/extras/react'
+import styled from 'styled-components'
+import {
+  ConditionalType,
+  field,
+  FieldType,
+  never
+} from '@opencrvs/commons/client'
+import { Stack, Text } from '@opencrvs/components'
+import { TRPCProvider } from '@client/v2-events/trpc'
+import { FormFieldGenerator } from '@client/v2-events/components/forms/FormFieldGenerator'
+
+const QR = makeAsyncComponent(React, generate)
+
+const StyledQR = styled(QR)`
+  width: 300px;
+`
+
+interface Args {
+  onScan: (data: unknown) => void
+}
+
+const meta: Meta<Args> = {
+  title: 'Inputs/IdReader',
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <TRPCProvider>
+        <Story />
+      </TRPCProvider>
+    )
+  ]
+}
+
+export default meta
+
+export const Default: StoryObj<Args> = {
+  name: 'Default Demo',
+  render: () => {
+    return (
+      <Stack direction="column">
+        <Text element="h2" variant="h2">
+          {'Scan below QR code to fill the form'}
+        </Text>
+        <Stack>
+          <StyledQR
+            content={JSON.stringify({
+              name: { firstname: 'John', surname: 'Doe' }
+            })}
+          />
+          <FormFieldGenerator
+            fields={[
+              {
+                id: 'id-reader',
+                type: FieldType.ID_READER,
+                label: {
+                  id: 'events.id-reader.label',
+                  defaultMessage: 'ID Reader',
+                  description: 'Label for the ID reader'
+                },
+                methods: [
+                  {
+                    id: 'id-reader.qr',
+                    label: {
+                      id: 'events.qr-reader.label',
+                      defaultMessage: 'QR Reader',
+                      description: 'Label for the QR reader'
+                    },
+                    type: FieldType.QR_READER
+                  }
+                ]
+              },
+              {
+                id: 'name',
+                type: FieldType.NAME,
+                parent: field('id-reader'),
+                label: {
+                  id: 'events.name.label',
+                  defaultMessage: 'Fields',
+                  description: 'Label for the name'
+                },
+                conditionals: [
+                  {
+                    type: ConditionalType.ENABLE,
+                    conditional: never()
+                  }
+                ],
+                value: field('id-reader').get('name')
+              }
+            ]}
+            id="id-form"
+            onChange={fn()}
+          />
+        </Stack>
+      </Stack>
+    )
+  }
+}
