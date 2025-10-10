96c22dea5b443133fe0c634ece01e2cc1b749179
tahmidrahman-dsi <tahmid.rahman@dsinnovators.com>
2025-10-10 13:44:43 +0600

    fix: add loader case in indexing

--- packages/events/src/service/indexing/indexing.ts
+++ packages/events/src/service/indexing/indexing.ts
@@ -97,6 +97,7 @@ function mapFieldTypeToElasticsearch(
     case FieldType.TIME:
     case FieldType.LINK_BUTTON:
     case FieldType.QUERY_PARAM_READER:
+    case FieldType.LOADER:
       return { type: 'text' }
     case FieldType.EMAIL:
       return {
