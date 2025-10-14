af81359c23b432831a7c14ca6eeecc13d4c58310
Markus <markus@agilepursuit.fi>
2025-10-14 08:48:33 +0300

    chore: add test case for UserAuditHistory

---
+++ packages/client/src/views/UserAudit/UserAuditHistory.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import { UserAuditHistory } from './UserAuditHistory'
+import superjson from 'superjson'
+import { TestUserRole } from '@opencrvs/commons/client'
+
+import { createTRPCMsw, httpLink } from '@vafanassieff/msw-trpc'
+import { AppRouter } from '@events/router'
+import { TRPCProvider } from '../../v2-events/trpc'
+
+const meta: Meta<typeof UserAuditHistory> = {
+  title: 'UserAudit/UserAuditHistory',
+  component: UserAuditHistory,
+  parameters: {
+    userRole: TestUserRole.enum.LOCAL_REGISTRAR
+  },
+  decorators: [
+    (Story) => (
+      <TRPCProvider>
+        <Story />
+      </TRPCProvider>
+    )
+  ]
+}
+
+export default meta
+type Story = StoryObj<typeof UserAuditHistory>
+
+export const Default: Story = {}
