c050ffd3a53ee792477c03a742888183d0d437c1
jamil314 <jamil31415926@gmail.com>
2025-10-14 17:53:34 +0600

    Revert "fix: remove mutaions with 409 errors from cache"
    This reverts commit ec1910d20499001f3d3c12b0b081aba380417a60.

--- packages/client/src/v2-events/features/events/useEvents/outbox.ts
+++ packages/client/src/v2-events/features/events/useEvents/outbox.ts
@@ -44,15 +44,10 @@ export function useOutbox() {
 
   const pendingMutations = useMutationState({
     filters: {
-      predicate: (mutation) => {
-        if (hasConflict(mutation.state.error)) {
-          queryClient.getMutationCache().remove(mutation)
-        }
-        return (
-          mutation.state.status !== 'success' &&
-          !assignmentMutation(mutation.options.mutationKey as MutationKey)
-        )
-      }
+      predicate: (mutation) =>
+        mutation.state.status !== 'success' &&
+        !hasConflict(mutation.state.error) &&
+        !assignmentMutation(mutation.options.mutationKey as MutationKey)
     },
     select: (mutation) => mutation
   })
