f476f4b7f6be053a42d28ba2291d07c902eda60a
naftis <pyry.rouvila@gmail.com>
2025-10-17 15:11:29 +0300

    revert eslint comment

--- packages/client/src/v2-events/messages/utils.tsx
+++ packages/client/src/v2-events/messages/utils.tsx
@@ -39,6 +39,7 @@ function convertDotToTripleUnderscore(obj: EventState, parentKey = '') {
       key.replace(/\./g, INTERNAL_SEPARATOR)
     if (Array.isArray(value)) {
       value.forEach((val, id) => {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         if (typeof val === 'object' && val !== null) {
           Object.assign(
             result,
