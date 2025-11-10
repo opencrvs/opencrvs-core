eb12b1873de2bfaa596a28da34431a594e72e030
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2025-11-10 14:46:00 +0600

    style: format file

--- packages/migration/src/migrations/events/1762255452026_create-users-table.sql
+++ packages/migration/src/migrations/events/1762255452026_create-users-table.sql
@@ -1,12 +1,11 @@
 -- Up Migration
-
-CREATE TYPE status_type AS ENUM (
+CREATE TYPE status_type AS ENUM(
   'pending',
   'active',
   'deactivated'
 );
 
-CREATE TABLE users (
+CREATE TABLE users(
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   legacy_id text UNIQUE NULLS DISTINCT,
   firstname text,
@@ -22,21 +21,21 @@ CREATE TABLE users (
   CONSTRAINT email_or_mobile_not_null CHECK (email IS NOT NULL OR mobile IS NOT NULL)
 );
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON users TO ${EVENTS_DB_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO $ {EVENTS_DB_USER};
 
 COMMENT ON COLUMN users.legacy_id IS 'References the user id from the legacy database.';
 
-CREATE TABLE user_credentials (
+CREATE TABLE user_credentials(
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   username text UNIQUE NOT NULL,
   password_hash text NOT NULL,
   salt text NOT NULL,
-  security_questions jsonb DEFAULT '{}':: jsonb NOT NULL
+  security_questions jsonb DEFAULT '{}' ::jsonb NOT NULL
 );
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON user_credentials TO ${EVENTS_DB_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON user_credentials TO $ {EVENTS_DB_USER};
 
 -- Down Migration
-
 DROP TABLE IF EXISTS users;
+
