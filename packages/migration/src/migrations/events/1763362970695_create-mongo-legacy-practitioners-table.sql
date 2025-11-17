47e2ecad8fe2c8f9d6ff49ac8ff9b6cbaa75797f
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2025-11-17 19:59:39 +0600

    fix: column name

--- packages/migration/src/migrations/events/1763362970695_create-mongo-legacy-practitioners-table.sql
+++ packages/migration/src/migrations/events/1763362970695_create-mongo-legacy-practitioners-table.sql
@@ -2,7 +2,7 @@
 CREATE FOREIGN TABLE legacy_practitioners(
   _id name,
   id text,
-  extensions json
+  extension json
 ) SERVER mongo OPTIONS(
   database 'hearth-dev',
   collection 'Practitioner'
