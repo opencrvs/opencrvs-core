5a724e7eda971c7ea98fc4ba7a584d9b10541c50
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2025-11-21 15:41:33 +0600

    fix: clear storage during storybook initialization

--- packages/client/src/storage.ts
+++ packages/client/src/storage.ts
@@ -8,7 +8,7 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-import { createStore, get, set, del } from 'idb-keyval'
+import { createStore, get, set, del, entries, clear } from 'idb-keyval'
 import { validateApplicationVersion } from '@client/utils'
 
 const DATABASE_NAME = 'OpenCRVS'
@@ -50,8 +50,13 @@ async function removeItem(key: string) {
   return await del(key, store)
 }
 
+async function clearStorage() {
+  return clear(store)
+}
+
 export const storage = {
   configStorage,
+  clearStorage,
   getItem,
   setItem,
   removeItem
