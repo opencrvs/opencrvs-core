956ed8437a11a4bea129d124cfa914f5317712d1
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2025-11-26 20:23:21 +0600

    chore(wip): tests

--- packages/client/src/v2-events/features/events/registered-fields/AlphaPrintButton.interaction.stories.tsx
+++ packages/client/src/v2-events/features/events/registered-fields/AlphaPrintButton.interaction.stories.tsx
@@ -25,7 +25,6 @@ import {
   TestUserRole,
   user,
   not,
-  ValidatorContext,
   EventDocument
 } from '@opencrvs/commons/client'
 import { ROUTES } from '@client/v2-events/routes'
@@ -36,20 +35,6 @@ import { noop } from '@client/v2-events'
 import { tennisClubMembershipEventDocument } from '../fixtures'
 import { getTestValidatorContext } from '../../../../../.storybook/decorators'
 
-const meta: Meta<typeof FormFieldGenerator> = {
-  title: 'Inputs/AlphaPrintButton',
-  args: { onChange: fn() },
-  decorators: [
-    (Story) => (
-      <TRPCProvider>
-        <Story />
-      </TRPCProvider>
-    )
-  ]
-}
-
-export default meta
-
 const tRPCMsw = createTRPCMsw<AppRouter>({
   links: [
     httpLink({
@@ -59,144 +44,84 @@ const tRPCMsw = createTRPCMsw<AppRouter>({
   transformer: { input: superjson, output: superjson }
 })
 
+const meta: Meta<typeof FormFieldGenerator> = {
+  title: 'Inputs/AlphaPrintButton/Interaction',
+  args: { onChange: fn() },
+  decorators: [
+    (Story) => (
+      <TRPCProvider>
+        <Story />
+      </TRPCProvider>
+    )
+  ],
+  parameters: {
+    reactRouter: {
+      router: {
+        path: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
+          eventId: tennisClubMembershipEventDocument.id
+        })
+      },
+      initialPath: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
+        eventId: tennisClubMembershipEventDocument.id
+      })
+    },
+    msw: {
+      handlers: {
+        event: [
+          tRPCMsw.event.config.get.query(() => {
+            return [tennisClubMembershipEvent]
+          })
+        ]
+      }
+    }
+  }
+}
+
+export default meta
+
 const StyledFormFieldGenerator = styled(FormFieldGenerator)`
   width: 400px;
 `
-export const Default: StoryObj<typeof FormFieldGenerator> = {
-  parameters: {
-    reactRouter: {
-      router: {
-        path: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-          eventId: tennisClubMembershipEventDocument.id
-        }),
-        element: (
-          <StyledFormFieldGenerator
-            fields={[
-              {
-                id: 'storybook.name',
-                type: FieldType.ALPHA_PRINT_BUTTON,
-                label: {
-                  id: 'storybook.name.label',
-                  defaultMessage: 'Name',
-                  description: 'The title for the name input'
-                },
-                configuration: {
-                  template: 'simple-certificate'
-                }
-              }
-            ]}
-            id="my-form"
-            validatorContext={getTestValidatorContext()}
-            onChange={() => noop()}
-          />
-        )
-      },
-      initialPath: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-        eventId: tennisClubMembershipEventDocument.id
-      })
-    },
-    msw: {
-      handlers: {
-        event: [
-          tRPCMsw.event.config.get.query(() => {
-            return [tennisClubMembershipEvent]
-          })
-        ]
-      }
-    }
-  }
-}
 
-function getParameters(element: React.ReactNode = null) {
-  return {
-    parameters: {
-      reactRouter: {
-        router: {
-          path: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-            eventId: tennisClubMembershipEventDocument.id
-          })
-        },
-        initialPath: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-          eventId: tennisClubMembershipEventDocument.id
-        }),
-        element
-      },
-      msw: {
-        handlers: {
-          event: [
-            tRPCMsw.event.config.get.query(() => {
-              return [tennisClubMembershipEvent]
-            })
+export const WithEnableCondition: StoryObj<{}> = {
+  render: () => (
+    <StyledFormFieldGenerator
+      fields={[
+        {
+          id: 'storybook.name',
+          type: FieldType.ALPHA_PRINT_BUTTON,
+          label: {
+            id: 'storybook.name.label',
+            defaultMessage: 'Name',
+            description: 'The title for the name input'
+          },
+          configuration: {
+            template: 'simple-certificate'
+          },
+          conditionals: [
+            {
+              type: ConditionalType.ENABLE,
+              conditional: and(
+                user.hasRole(TestUserRole.Enum.LOCAL_REGISTRAR),
+                not(event.hasAction(ActionType.DECLARE))
+              )
+            }
           ]
         }
-      }
-    }
-  }
-}
-
-export const WithEnableCondition: StoryObj<{
-  role: TestUserRole
-  fullEvent: EventDocument
-}> = {
-  parameters: {
-    reactRouter: {
-      router: {
-        path: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-          eventId: tennisClubMembershipEventDocument.id
-        }),
-        element: (
-          <StyledFormFieldGenerator
-            fields={[
-              {
-                id: 'storybook.name',
-                type: FieldType.ALPHA_PRINT_BUTTON,
-                label: {
-                  id: 'storybook.name.label',
-                  defaultMessage: 'Name',
-                  description: 'The title for the name input'
-                },
-                configuration: {
-                  template: 'simple-certificate'
-                },
-                conditionals: [
-                  {
-                    type: ConditionalType.ENABLE,
-                    conditional: and(
-                      user.hasRole(TestUserRole.Enum.LOCAL_REGISTRAR),
-                      not(event.hasAction(ActionType.DECLARE))
-                    )
-                  }
-                ]
-              }
-            ]}
-            id="my-form"
-            validatorContext={getTestValidatorContext(
-              TestUserRole.Enum.LOCAL_REGISTRAR,
-              {
-                ...tennisClubMembershipEventDocument,
-                actions: tennisClubMembershipEventDocument.actions.filter(
-                  (action) => action.type === ActionType.CREATE
-                )
-              }
-            )}
-            onChange={() => noop()}
-          />
-        )
-      },
-      initialPath: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-        eventId: tennisClubMembershipEventDocument.id
-      })
-    },
-    msw: {
-      handlers: {
-        event: [
-          tRPCMsw.event.config.get.query(() => {
-            return [tennisClubMembershipEvent]
-          })
-        ]
-      }
-    }
-  },
+      ]}
+      id="my-form"
+      validatorContext={getTestValidatorContext(
+        TestUserRole.Enum.LOCAL_REGISTRAR,
+        {
+          ...tennisClubMembershipEventDocument,
+          actions: tennisClubMembershipEventDocument.actions.filter(
+            (action) => action.type === ActionType.CREATE
+          )
+        }
+      )}
+      onChange={() => noop()}
+    />
+  ),
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
     const button = await canvas.findByTestId('storybook____name')
@@ -205,64 +130,40 @@ export const WithEnableCondition: StoryObj<{
   }
 }
 
-export const WithDisableCondition: StoryObj<{
-  role: TestUserRole
-  fullEvent: EventDocument
-}> = {
-  parameters: {
-    reactRouter: {
-      router: {
-        path: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-          eventId: tennisClubMembershipEventDocument.id
-        }),
-        element: (
-          <StyledFormFieldGenerator
-            fields={[
-              {
-                id: 'storybook.name',
-                type: FieldType.ALPHA_PRINT_BUTTON,
-                label: {
-                  id: 'storybook.name.label',
-                  defaultMessage: 'Name',
-                  description: 'The title for the name input'
-                },
-                configuration: {
-                  template: 'simple-certificate'
-                },
-                conditionals: [
-                  {
-                    type: ConditionalType.ENABLE,
-                    conditional: and(
-                      user.hasRole(TestUserRole.Enum.LOCAL_REGISTRAR),
-                      not(event.hasAction(ActionType.DECLARE))
-                    )
-                  }
-                ]
-              }
-            ]}
-            id="my-form"
-            validatorContext={getTestValidatorContext(
-              TestUserRole.Enum.LOCAL_REGISTRAR,
-              tennisClubMembershipEventDocument
-            )}
-            onChange={() => noop()}
-          />
-        )
-      },
-      initialPath: ROUTES.V2.EVENTS.DECLARE.REVIEW.buildPath({
-        eventId: tennisClubMembershipEventDocument.id
-      })
-    },
-    msw: {
-      handlers: {
-        event: [
-          tRPCMsw.event.config.get.query(() => {
-            return [tennisClubMembershipEvent]
-          })
-        ]
-      }
-    }
-  },
+export const WithDisableCondition: StoryObj<{}> = {
+  render: () => (
+    <StyledFormFieldGenerator
+      fields={[
+        {
+          id: 'storybook.name',
+          type: FieldType.ALPHA_PRINT_BUTTON,
+          label: {
+            id: 'storybook.name.label',
+            defaultMessage: 'Name',
+            description: 'The title for the name input'
+          },
+          configuration: {
+            template: 'simple-certificate'
+          },
+          conditionals: [
+            {
+              type: ConditionalType.ENABLE,
+              conditional: and(
+                user.hasRole(TestUserRole.Enum.LOCAL_REGISTRAR),
+                not(event.hasAction(ActionType.DECLARE))
+              )
+            }
+          ]
+        }
+      ]}
+      id="my-form"
+      validatorContext={getTestValidatorContext(
+        TestUserRole.Enum.LOCAL_REGISTRAR,
+        tennisClubMembershipEventDocument
+      )}
+      onChange={() => noop()}
+    />
+  ),
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
     const button = await canvas.findByTestId('storybook____name')
