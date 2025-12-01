7d3569d361e1c95a9214bb85c8e7cde1964ef68b
naftis <pyry.rouvila@gmail.com>
2025-12-01 12:54:39 +0200

    fix the bash shebang not being applied on build.sh

--- packages/toolkit/build.sh
+++ packages/toolkit/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -6,8 +7,6 @@
 # & Healthcare Disclaimer located at http://opencrvs.org/license.
 #
 # Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
-
-#!/bin/bash
 
 set -e
 
