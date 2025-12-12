5a8c801856a4f889845045c9df8548ebaa063ff2
cibelius <cihan.m.bebek@gmail.com>
2025-12-12 17:43:26 +0700

    storybook test fixes

--- packages/client/src/v2-events/features/workqueues/EventOverview/components/ActionMenuStories/LocalRegistrarInteraction/Rejected.stories.tsx
+++ packages/client/src/v2-events/features/workqueues/EventOverview/components/ActionMenuStories/LocalRegistrarInteraction/Rejected.stories.tsx
@@ -18,8 +18,7 @@ import {
   createStoriesFromScenarios,
   AssertType,
   Scenario,
-  UserRoles,
-  REJECTED_DECLARE_AS_REVIEW
+  UserRoles
 } from '../ActionMenu.common'
 
 export default {
@@ -41,7 +40,7 @@ const rejectedScenariosForLocalRegistrar: Scenario[] = [
     expected: {
       ...getHiddenActions(),
       ['Assign']: AssertType.ENABLED,
-      ['Review']: AssertType.DISABLED,
+      ['Edit']: AssertType.DISABLED,
       ['Archive']: AssertType.DISABLED
     }
   },
@@ -57,7 +56,7 @@ const rejectedScenariosForLocalRegistrar: Scenario[] = [
     expected: {
       ...getHiddenActions(),
       ['Unassign']: AssertType.ENABLED,
-      ['Review']: AssertType.ENABLED,
+      ['Edit']: AssertType.ENABLED,
       ['Archive']: AssertType.ENABLED
     }
   },
@@ -73,7 +72,7 @@ const rejectedScenariosForLocalRegistrar: Scenario[] = [
     expected: {
       ...getHiddenActions(),
       ['Unassign']: AssertType.ENABLED,
-      ['Review']: AssertType.DISABLED,
+      ['Edit']: AssertType.DISABLED,
       ['Archive']: AssertType.DISABLED
     }
   }
