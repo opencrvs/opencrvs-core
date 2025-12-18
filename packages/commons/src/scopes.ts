b500c8a4d4bc7d0f5c0f0f414cfad06dffaaa45b
jamil314 <jamil31415926@gmail.com>
2025-12-18 13:19:45 +0600

    fix: update v2-scope export

--- packages/commons/src/scopes.ts
+++ packages/commons/src/scopes.ts
@@ -11,7 +11,7 @@
 
 import * as z from 'zod/v4'
 import { SearchScopeAccessLevels } from './events'
-export * from './scopes-v2'
+export { encodeScope, AnyScope } from './scopes-v2'
 
 export const SCOPES = {
   // TODO v1.8 legacy scopes
