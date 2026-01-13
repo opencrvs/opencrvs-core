7069c32f5cec0e3d128ee695ef6b2354a02e8fbc
naftis <pyry.rouvila@gmail.com>
2026-01-13 18:30:15 +0200

    export trpc error from toolkit
    helps try catching the error in external repositories

--- packages/toolkit/src/api/index.ts
+++ packages/toolkit/src/api/index.ts
@@ -8,7 +8,12 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-import { CreateTRPCClient, createTRPCClient, httpBatchLink } from '@trpc/client'
+import {
+  CreateTRPCClient,
+  createTRPCClient,
+  httpBatchLink,
+  TRPCClientError
+} from '@trpc/client'
 import superjson from 'superjson'
 import { AppRouter } from '@opencrvs/events/build/types/router'
 
@@ -37,3 +42,5 @@ export function createClient(
     ]
   })
 }
+
+export { TRPCClientError }
