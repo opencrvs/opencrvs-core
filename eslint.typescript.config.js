908a318a48b00ab18d969132e42fab3ba57fab0b
Markus <markus@agilepursuit.fi>
2026-01-22 15:26:51 +0200

    chore: remove no-misused promises rule

--- eslint.typescript.config.js
+++ eslint.typescript.config.js
@@ -15,7 +15,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/await-thenable': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
     '@typescript-eslint/promise-function-async': 'error',
     '@typescript-eslint/no-unnecessary-condition': 'warn',
     '@typescript-eslint/require-await': 'error',
