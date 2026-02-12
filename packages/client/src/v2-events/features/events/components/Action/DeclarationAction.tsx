e5641d1db8d574cdc32e15dbfb8f8821c206c424
cibelius <cihan.m.bebek@gmail.com>
2026-02-12 10:48:11 +0200

    allow declare flow even if user can only notify

--- packages/client/src/v2-events/features/events/components/Action/DeclarationAction.tsx
+++ packages/client/src/v2-events/features/events/components/Action/DeclarationAction.tsx
@@ -13,7 +13,6 @@ import React, { PropsWithChildren, useEffect, useMemo } from 'react'
 import { useTypedParams } from 'react-router-typesafe-routes/dom'
 import { useNavigate } from 'react-router-dom'
 import {
-  Draft,
   createEmptyDraft,
   findActiveDraftForEvent,
   getActionAnnotation,
@@ -90,8 +89,16 @@ function useActionGuard(
     )
   }
 
-  // If the user may not perform the action, redirect to the unauthorized page
-  if (!isActionAllowed(actionType)) {
+  // In the declare flow, user is allowed if they have permission for either DECLARE or NOTIFY;
+  // otherwise strict permission by action type.
+  const hasDeclareOrNotifyPermission =
+    actionType === ActionType.DECLARE &&
+    (isActionAllowed(ActionType.DECLARE) || isActionAllowed(ActionType.NOTIFY))
+
+  const hasActionPermission =
+    actionType !== ActionType.DECLARE && isActionAllowed(actionType)
+
+  if (!hasDeclareOrNotifyPermission && !hasActionPermission) {
     throw new Error(
       `User does not have permission to perform action ${actionType} on event ${event.id}`
     )
@@ -125,7 +132,7 @@ function DeclarationActionComponent({
 
   useActionGuard(actionType, event, configuration)
 
-  const remoteDraft: Draft | undefined = getRemoteDraftByEventId(event.id)
+  const remoteDraft = getRemoteDraftByEventId(event.id)
 
   const activeRemoteDraft = remoteDraft
     ? findActiveDraftForEvent(event, remoteDraft)
@@ -186,7 +193,7 @@ function DeclarationActionComponent({
     }
   }
 
-  const mergedDraft: Draft = activeRemoteDraft
+  const mergedDraft = activeRemoteDraft
     ? mergeDrafts(activeRemoteDraft, localDraftWithAdjustedTimestamp)
     : localDraftWithAdjustedTimestamp
 
