b7dc5ced637618cf2276559d274955047bfcd3f3
Markus <markus@agilepursuit.fi>
2026-02-27 12:26:33 +0200

    ad ts-expect-error to compile

--- packages/events/src/router/event/actions/index.ts
+++ packages/events/src/router/event/actions/index.ts
@@ -362,6 +362,7 @@ export function getDefaultActionProcedures(
       .meta(meta)
       .use(canAccessEventMiddleware)
       .input(actionConfig.inputSchema.strict())
+      // @ts-expect-error - deprecated by the end of 2.0
       .use(middleware.eventTypeAuthorization)
       .use(middleware.requireAssignment)
       .use(middleware.validateAction)
