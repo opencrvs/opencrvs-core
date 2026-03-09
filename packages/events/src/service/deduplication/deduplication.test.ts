e35fc40af83eccdefc22240bd8862de14836ae47
Riku Rouvila <riku.rouvila@gmail.com>
2026-03-09 14:55:39 +0200

    fix license

--- packages/events/src/service/deduplication/deduplication.test.ts
+++ packages/events/src/service/deduplication/deduplication.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 /*
 * This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,7 +9,6 @@
 *
 * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
 */
-/* eslint-disable max-lines */
 import {
   DeduplicationConfig,
   getUUID,
