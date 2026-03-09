8c840ae6638e44f37d695be1ccf375d6563db81a
jamil314 <jamil31415926@gmail.com>
2026-03-09 15:26:25 +0600

    refactor: add `defineApplicationConfig()` to toolkit

--- packages/client/typings/window.d.ts
+++ packages/client/typings/window.d.ts
@@ -9,46 +9,12 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-interface Window {
-  config: {
-    APPLICATION_NAME: string
-    COUNTRY: string
-    COUNTRY_LOGO: {
-      fileName: string
-      file: string
-    }
-    CURRENCY: {
-      isoCode: string
-      languagesAndCountry: string[]
-    }
-    FEATURES: {
-      V2_EVENTS: boolean
-    }
-    REGISTER_BACKGROUND: {
-      backgroundColor?: string
-      backgroundImage?: string
-      imageFit?: string
-    }
-    LANGUAGES: string[]
-    MINIO_URL: string
-    MINIO_BASE_URL: string // URL without path/bucket information, used for file uploads, v2
-    MINIO_BUCKET: string
-    SHOW_FARAJALAND_IN_COUNTRY_LISTS: boolean
-    USER_NOTIFICATION_DELIVERY_METHOD: 'sms' | 'email'
-    INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'sms' | 'email'
-    PHONE_NUMBER_PATTERN: RegExp
-    SENTRY: string
-    DASHBOARDS: Array<{
-      id: string
-      title: {
-        id: string
-        defaultMessage: string
-        description: string
-      }
-      url: string
-    }>
-    SYSTEM_IANA_TIMEZONE: string
+import { ClientConfig, ApplicationConfig } from '@opencrvs/commons'
+
+declare global {
+  interface Window {
+    config: ApplicationConfig & ClientConfig
+    __localeId__: string
+    __WB_MANIFEST: Array<{ url: string; revision: string }>
   }
-  __localeId__: string
-  __WB_MANIFEST: Array<{ url: string; revision: string }>
 }
