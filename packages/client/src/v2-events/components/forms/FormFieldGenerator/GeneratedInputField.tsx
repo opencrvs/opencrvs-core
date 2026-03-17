d598f0ac2870656e339f399b5eb63c18346dc37e
cibelius <cihan.m.bebek@gmail.com>
2026-03-17 13:02:10 +0200

    fix lint issues

--- packages/client/src/v2-events/components/forms/FormFieldGenerator/GeneratedInputField.tsx
+++ packages/client/src/v2-events/components/forms/FormFieldGenerator/GeneratedInputField.tsx
@@ -687,6 +687,7 @@ export const GeneratedInputField = React.memo(
             maxFileSize={field.config.configuration.maxFileSize}
             maxImageSize={field.config.configuration.maxImageSize}
             options={field.config.options}
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
             value={field.value ?? []}
             onChange={handleFileWithOptionChange}
           />
