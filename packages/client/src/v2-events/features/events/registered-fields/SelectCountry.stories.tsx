08ec865f8329c05166093fee8ea95e15357fcd26
Tameem Bin Haider <haidertameem@gmail.com>
2026-03-27 16:33:36 +0600

    test: country option override

---
+++ packages/client/src/v2-events/features/events/registered-fields/SelectCountry.stories.tsx
@@ -0,0 +1,142 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import styled from 'styled-components'
+import { within, expect } from '@storybook/test'
+import { userEvent } from '@storybook/testing-library'
+import {
+  ConditionalType,
+  FieldType,
+  not,
+  alwaysTrue
+} from '@opencrvs/commons/client'
+import {
+  FormFieldGenerator,
+  FormFieldGeneratorPropsWithoutRef
+} from '@client/v2-events/components/forms/FormFieldGenerator'
+import { TRPCProvider } from '@client/v2-events/trpc'
+import { withValidatorContext } from '../../../../../.storybook/decorators'
+
+const meta: Meta<FormFieldGeneratorPropsWithoutRef> = {
+  title: 'Inputs/SelectCountry',
+  parameters: {
+    chromatic: { disableSnapshot: true }
+  },
+  decorators: [
+    (Story, context) => (
+      <TRPCProvider>
+        <Story {...context} />
+      </TRPCProvider>
+    ),
+    withValidatorContext
+  ]
+}
+
+export default meta
+
+const StyledFormFieldGenerator = styled(FormFieldGenerator)`
+  width: 400px;
+`
+
+type Story = StoryObj<FormFieldGeneratorPropsWithoutRef>
+
+export const WithHiddenOption: Story = {
+  name: 'With hidden country option',
+  parameters: {
+    layout: 'centered'
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(await canvas.findByRole('textbox'))
+    await expect(canvas.queryByText('Bangladesh')).toBeInTheDocument()
+    await expect(canvas.queryByText('Finland')).not.toBeInTheDocument()
+    await expect(canvas.queryByText('United States of America')).toBeInTheDocument()
+  },
+  render: function Component(args) {
+    return (
+      <StyledFormFieldGenerator
+        {...args}
+        fields={[
+          {
+            id: 'storybook.country',
+            type: FieldType.COUNTRY,
+            label: {
+              id: 'storybook.country.label',
+              defaultMessage: 'Country',
+              description: 'The label for the country select input'
+            },
+            optionOverrides: [
+              {
+                value: 'FIN',
+                conditionals: [
+                  {
+                    type: ConditionalType.SHOW,
+                    conditional: not(alwaysTrue())
+                  }
+                ]
+              }
+            ]
+          }
+        ]}
+        id="my-form"
+      />
+    )
+  }
+}
+
+export const WithDisabledOption: Story = {
+  name: 'With disabled country option',
+  parameters: {
+    layout: 'centered'
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(await canvas.findByRole('textbox'))
+    const finlandOption = canvas.queryByText('Finland')
+    await expect(finlandOption).toBeInTheDocument()
+    await expect(finlandOption).toHaveClass('react-select__option--is-disabled')
+    await expect(canvas.queryByText('Bangladesh')).not.toHaveClass(
+      'react-select__option--is-disabled'
+    )
+  },
+  render: function Component(args) {
+    return (
+      <StyledFormFieldGenerator
+        {...args}
+        fields={[
+          {
+            id: 'storybook.country',
+            type: FieldType.COUNTRY,
+            label: {
+              id: 'storybook.country.label',
+              defaultMessage: 'Country',
+              description: 'The label for the country select input'
+            },
+            optionOverrides: [
+              {
+                value: 'FIN',
+                conditionals: [
+                  {
+                    type: ConditionalType.ENABLE,
+                    conditional: not(alwaysTrue())
+                  }
+                ]
+              }
+            ]
+          }
+        ]}
+        id="my-form"
+      />
+    )
+  }
+}
