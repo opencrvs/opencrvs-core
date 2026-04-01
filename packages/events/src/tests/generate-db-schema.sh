c78b9eba4837287dcf8bbd580c1ce04076fae693
Nil20 <ashikul.alam@dsinnovators.com>
2026-04-01 14:05:57 +0600

    chore(events): extract generate-db-schema into a bash script

---
+++ packages/events/src/tests/generate-db-schema.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# OpenCRVS is also distributed under the terms of the Civil Registration
+# & Healthcare Disclaimer located at http://opencrvs.org/license.
+#
+# Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+
+# Dumps the events DB schema from the local PostgreSQL instance and strips
+# artifacts that are incompatible with the Testcontainers test environment.
+#
+# Output: src/tests/postgres-migrations.sql
+
+if [[ $OSTYPE == darwin* ]]; then
+  HOST=host.docker.internal
+else
+  HOST=172.17.0.1
+fi
+
+docker run --rm postgres:17.6 pg_dump \
+  "postgres://events_migrator:migrator_password@${HOST}:5432/events" \
+  -s \
+  --exclude-schema=analytics \
+  --exclude-schema=reference_data \
+  --exclude-table=app.pgmigrations_legacy_data \
+  | bash "$(dirname "$0")/strip-pg-dump.sh" \
+  > "$(dirname "$0")/postgres-migrations.sql"
--- packages/events/src/tests/strip-pg-dump.sh
+++ packages/events/src/tests/strip-pg-dump.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# OpenCRVS is also distributed under the terms of the Civil Registration
+# & Healthcare Disclaimer located at http://opencrvs.org/license.
+#
+# Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
+
 # Cleans pg_dump output so it can be executed by the Node.js pg client in tests.
 #
 # Removes two categories of content that break test setup:
