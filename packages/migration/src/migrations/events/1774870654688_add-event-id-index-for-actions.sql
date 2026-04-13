f8187dfff09420a8c0b50108f1f2ab2d79ab2727
Riku Rouvila <riku.rouvila@gmail.com>
2026-04-13 12:50:26 +0800

    add index for original_action_id in event_actions

--- packages/migration/src/migrations/events/1774870654688_add-event-id-index-for-actions.sql
+++ packages/migration/src/migrations/events/1774870654688_add-event-id-index-for-actions.sql
@@ -1,5 +1,7 @@
 -- Up Migration
 CREATE INDEX idx_event_actions_event_id ON app.event_actions (event_id);
+CREATE INDEX idx_event_actions_original_action_id ON app.event_actions (original_action_id);
 
 -- Down Migration
 DROP INDEX idx_event_actions_event_id;
+DROP INDEX idx_event_actions_original_action_id;
