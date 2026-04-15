a03070d7d544102c37c071d906c3631dcdb03314
Markus <markus@agilepursuit.fi>
2026-04-15 13:54:52 +0300

    chore: export enum

--- packages/client/src/notification/actions.ts
+++ packages/client/src/notification/actions.ts
@@ -9,7 +9,7 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
-enum AUDIT_ACTION {
+export enum AUDIT_ACTION {
   DEACTIVATE = 'DEACTIVATE',
   REACTIVATE = 'REACTIVATE'
 }
