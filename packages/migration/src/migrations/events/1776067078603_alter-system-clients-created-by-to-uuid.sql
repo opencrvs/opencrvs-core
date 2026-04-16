8ea61b4edde134801b0a87a73b6bc2e9a0a8b1ad
jamil314 <jamil31415926@gmail.com>
2026-04-16 13:14:31 +0600

    fix: add foreign key

--- packages/migration/src/migrations/events/1776067078603_alter-system-clients-created-by-to-uuid.sql
+++ packages/migration/src/migrations/events/1776067078603_alter-system-clients-created-by-to-uuid.sql
@@ -1,5 +1,7 @@
 -- Up Migration
 ALTER TABLE system_clients ALTER COLUMN created_by TYPE UUID USING created_by::UUID;
+ALTER TABLE system_clients ADD CONSTRAINT fk_system_clients_created_by FOREIGN KEY (created_by) REFERENCES users(id);
 
 -- Down Migration
+ALTER TABLE system_clients DROP CONSTRAINT fk_system_clients_created_by;
 ALTER TABLE system_clients ALTER COLUMN created_by TYPE TEXT USING created_by::TEXT;
