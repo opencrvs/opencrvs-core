91da53a2cb6a98e205a3686396225d393bdd44b9
jamil314 <jamil31415926@gmail.com>
2026-04-28 13:05:16 +0600

    fix: update migration timestamp

--- packages/events/src/storage/postgres/events/schema/app/AppSchema.ts
+++ packages/events/src/storage/postgres/events/schema/app/AppSchema.ts
@@ -4,13 +4,13 @@
 import type { default as AnnouncementsTable } from './Announcements'
 import type { default as EventActionDraftsTable } from './EventActionDrafts'
 import type { default as SystemInitialisationTable } from './SystemInitialisation'
+import type { default as LocationsTable } from './Locations'
+import type { default as UserCredentialsTable } from './UserCredentials'
 import type { default as SystemClientsTable } from './SystemClients'
+import type { default as AdministrativeAreasTable } from './AdministrativeAreas'
 import type { default as EventActionsTable } from './EventActions'
 import type { default as AuditLogTable } from './AuditLog'
 import type { default as UsersTable } from './Users'
-import type { default as LocationsTable } from './Locations'
-import type { default as UserCredentialsTable } from './UserCredentials'
-import type { default as AdministrativeAreasTable } from './AdministrativeAreas'
 import type { default as EventsTable } from './Events'
 
 export default interface AppSchema {
@@ -20,19 +20,19 @@ export default interface AppSchema {
 
   systemInitialisation: SystemInitialisationTable
 
+  locations: LocationsTable
+
+  userCredentials: UserCredentialsTable
+
   systemClients: SystemClientsTable
+
+  administrativeAreas: AdministrativeAreasTable
 
   eventActions: EventActionsTable
 
   auditLog: AuditLogTable
 
   users: UsersTable
-
-  locations: LocationsTable
-
-  userCredentials: UserCredentialsTable
-
-  administrativeAreas: AdministrativeAreasTable
 
   events: EventsTable
 }
