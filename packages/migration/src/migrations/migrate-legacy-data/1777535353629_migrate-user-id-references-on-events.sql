97a20cf969b884d8c59576fe82d9cccfb2d4e3eb
cibelius <cihan.m.bebek@gmail.com>
2026-04-30 14:12:50 +0300

    fix

--- packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
+++ packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
@@ -9,19 +9,8 @@ SET created_by = system_clients.id::text
 FROM system_clients
 WHERE event_actions.created_by = system_clients.legacy_id;
 
-ALTER TABLE event_actions
-ALTER COLUMN created_by DROP NOT NULL;
-
-ALTER TABLE event_actions
-ALTER COLUMN created_by TYPE uuid
-USING NULLIF(created_by, '')::uuid;
-
 
 -- Down Migration
-ALTER TABLE event_actions
-ALTER COLUMN created_by TYPE text
-USING created_by::text;
-
 UPDATE event_actions
 SET created_by = users.legacy_id
 FROM users
@@ -31,10 +20,3 @@ UPDATE event_actions
 SET created_by = system_clients.legacy_id
 FROM system_clients
 WHERE event_actions.created_by = system_clients.id::text;
-
-UPDATE event_actions
-SET created_by = ''
-WHERE created_by IS NULL;
-
-ALTER TABLE event_actions
-ALTER COLUMN created_by SET NOT NULL;
