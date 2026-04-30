3941c77d47fd8d288d51369f41ced9f6a73ca4ed
cibelius <cihan.m.bebek@gmail.com>
2026-04-30 15:12:25 +0300

    allow empty

--- packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
+++ packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
@@ -37,24 +37,6 @@ WHERE event_actions.created_by = ''
   AND accepted_declare_actions.status = 'Accepted'
   AND system_clients.legacy_id = accepted_declare_actions.created_by;
 
-UPDATE event_actions
-SET assigned_to = users.id::text
-FROM event_actions accepted_declare_actions, users
-WHERE event_actions.assigned_to = ''
-  AND accepted_declare_actions.event_id = event_actions.event_id
-  AND accepted_declare_actions.action_type = 'DECLARE'
-  AND accepted_declare_actions.status = 'Accepted'
-  AND users.legacy_id = accepted_declare_actions.assigned_to;
-
-UPDATE event_actions
-SET assigned_to = system_clients.id::text
-FROM event_actions accepted_declare_actions, system_clients
-WHERE event_actions.assigned_to = ''
-  AND accepted_declare_actions.event_id = event_actions.event_id
-  AND accepted_declare_actions.action_type = 'DECLARE'
-  AND accepted_declare_actions.status = 'Accepted'
-  AND system_clients.legacy_id = accepted_declare_actions.assigned_to;
-
 ALTER TABLE event_actions
 ALTER COLUMN created_by TYPE uuid
 USING created_by::uuid;
