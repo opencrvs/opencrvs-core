c4ed9c8809de3e6b2f0cdf15502ea314d82f790b
cibelius <cihan.m.bebek@gmail.com>
2026-04-30 13:10:15 +0300

    remove uuid retype

--- packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
+++ packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
@@ -4,16 +4,7 @@ SET created_by = users.id::text
 FROM users
 WHERE event_actions.created_by = users.legacy_id;
 
-ALTER TABLE event_actions
-ALTER COLUMN created_by TYPE uuid
-USING created_by::uuid;
-
-
 -- Down Migration
-ALTER TABLE event_actions
-ALTER COLUMN created_by TYPE text
-USING created_by::text;
-
 UPDATE event_actions
 SET created_by = users.legacy_id
 FROM users
