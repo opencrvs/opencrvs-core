e1f588f1609e9c30f3875af34af2fd275443a202
cibelius <cihan.m.bebek@gmail.com>
2026-04-30 14:58:10 +0300

    fix

--- packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
+++ packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
@@ -9,8 +9,33 @@ SET created_by = system_clients.id::text
 FROM system_clients
 WHERE event_actions.created_by = system_clients.legacy_id;
 
+UPDATE event_actions
+SET created_by = users.id::text
+FROM event_actions accepted_declare_actions, users
+WHERE event_actions.created_by = ''
+  AND accepted_declare_actions.event_id = event_actions.event_id
+  AND accepted_declare_actions.action_type = 'DECLARE'
+  AND accepted_declare_actions.status = 'Accepted'
+  AND users.legacy_id = accepted_declare_actions.created_by;
+
+UPDATE event_actions
+SET created_by = system_clients.id::text
+FROM event_actions accepted_declare_actions, system_clients
+WHERE event_actions.created_by = ''
+  AND accepted_declare_actions.event_id = event_actions.event_id
+  AND accepted_declare_actions.action_type = 'DECLARE'
+  AND accepted_declare_actions.status = 'Accepted'
+  AND system_clients.legacy_id = accepted_declare_actions.created_by;
+
+ALTER TABLE event_actions
+ALTER COLUMN created_by TYPE uuid
+USING created_by::uuid;
 
 -- Down Migration
+ALTER TABLE event_actions
+ALTER COLUMN created_by TYPE text
+USING created_by::text;
+
 UPDATE event_actions
 SET created_by = users.legacy_id
 FROM users
