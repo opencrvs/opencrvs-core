ede78c9515ba8090747ffd8075ef8ea79b030458
cibelius <cihan.m.bebek@gmail.com>
2026-05-04 09:40:46 +0300

    fix migration

--- packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
+++ packages/migration/src/migrations/migrate-legacy-data/1777535353629_migrate-user-id-references-on-events.sql
@@ -37,6 +37,24 @@ WHERE event_actions.created_by = ''
   AND accepted_declare_actions.status = 'Accepted'
   AND system_clients.legacy_id = accepted_declare_actions.created_by;
 
+UPDATE event_actions
+SET created_by = users.id::text
+FROM event_actions accepted_declare_actions, users
+WHERE event_actions.created_by = ''
+  AND accepted_declare_actions.event_id = event_actions.event_id
+  AND accepted_declare_actions.action_type = 'NOTIFY'
+  AND accepted_declare_actions.status = 'Accepted'
+  AND users.legacy_id = accepted_declare_actions.created_by;
+
+UPDATE event_actions
+SET created_by = system_clients.id::text
+FROM event_actions accepted_declare_actions, system_clients
+WHERE event_actions.created_by = ''
+  AND accepted_declare_actions.event_id = event_actions.event_id
+  AND accepted_declare_actions.action_type = 'NOTIFY'
+  AND accepted_declare_actions.status = 'Accepted'
+  AND system_clients.legacy_id = accepted_declare_actions.created_by;
+
 ALTER TABLE event_actions
 ALTER COLUMN created_by TYPE uuid
 USING created_by::uuid;
