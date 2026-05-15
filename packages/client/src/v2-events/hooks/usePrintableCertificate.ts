2570419c22a31676388c0616fc0f92cdee4c8858
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2026-05-15 19:55:44 +0600

    fix(usePrintableCertificate): restore optimistic PRINT_CERTIFICATE action in preparePdfCertificate

--- packages/client/src/v2-events/hooks/usePrintableCertificate.ts
+++ packages/client/src/v2-events/hooks/usePrintableCertificate.ts
@@ -148,7 +148,7 @@ export const usePrintableCertificate = ({
         copiesPrintedForTemplate
       },
       $declaration: declarationWithResolvedImages,
-      $actions: getAcceptedActions(updatedEvent),
+      $actions: getAcceptedActions(event),
       locations,
       review: false,
       users,
