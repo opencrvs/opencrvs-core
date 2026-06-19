9430c517e4a3262e649e7c8b4d521e4923efcaba
Tameem Bin Haider <tameem.haider@dsinnovators.com>
2026-06-19 18:14:38 +0600

    Revert "feat: better UX when rate limit is hit in team page (#12987)"
    This reverts commit d6a9d28aef66875b4f7e264091c52d47f7eb364c.

--- packages/client/src/i18n/messages/errors.ts
+++ packages/client/src/i18n/messages/errors.ts
@@ -26,7 +26,6 @@ interface IErrorMessages
   registrationQueryError: MessageDescriptor
   unauthorized: MessageDescriptor
   userQueryError: MessageDescriptor
-  userQueryRateLimitError: MessageDescriptor
   loadingDeclarations: MessageDescriptor
   noDeclaration: MessageDescriptor
   waitingForConnection: MessageDescriptor
@@ -109,12 +108,6 @@ const messagesToDefine: IErrorMessages = {
     defaultMessage: 'An error occurred while loading system users',
     description: 'The text when error ocurred loading system users',
     id: 'system.user.queryError'
-  },
-  userQueryRateLimitError: {
-    defaultMessage: 'Too many requests. Please wait before retrying.',
-    description:
-      'Error text shown when the user list query is rate limited (HTTP 429)',
-    id: 'system.user.queryRateLimitError'
   },
   loadingDeclarations: {
     defaultMessage: 'Loading declarations...',
