edc7303277e568d7661cfa88d090c782644a74c6
cibelius <cihan.m.bebek@gmail.com>
2026-07-02 09:51:05 +0300

    fix export

--- packages/client/src/i18n/messages/constants.ts
+++ packages/client/src/i18n/messages/constants.ts
@@ -10,7 +10,7 @@
  */
 import { defineMessages, MessageDescriptor } from 'react-intl'
 
-const constantsMessages = defineMessages({
+export const constantsMessages = defineMessages({
   action: {
     defaultMessage: 'Action',
     description: 'Action Label',
