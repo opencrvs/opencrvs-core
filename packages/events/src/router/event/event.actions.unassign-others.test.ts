b7cd37d06690e83f4ec909055821924a7063d67b
cibelius <cihan.m.bebek@gmail.com>
2026-07-22 14:32:12 +0300

    minor events test improvements

--- packages/events/src/router/event/event.actions.unassign-others.test.ts
+++ packages/events/src/router/event/event.actions.unassign-others.test.ts
@@ -31,123 +31,119 @@ import {
 import { createIndex } from '@events/service/indexing/indexing'
 import { getEventIndexName } from '@events/storage/elasticsearch'
 
-test(
-  'Check scopes against event.actions.unassign',
-  { timeout: 120000 },
-  async () => {
-    await createIndex(
-      getEventIndexName('tennis-club-membership_premium'),
-      getDeclarationFields(tennisClubMembershipEvent)
-    )
-    // 1. Setup test fixture with a known set of users, administrative areas, and events.
-    const { users, isUnderAdministrativeArea, eventIds } =
-      await setupScopeTestFixture(
-        1243453343,
-        fc.constantFrom(
-          [ActionTypes.enum.DECLARE],
-          [ActionTypes.enum.DECLARE, ActionTypes.enum.REGISTER]
-        )
+test('Check scopes against event.actions.unassign', async () => {
+  await createIndex(
+    getEventIndexName('tennis-club-membership_premium'),
+    getDeclarationFields(tennisClubMembershipEvent)
+  )
+  // 1. Setup test fixture with a known set of users, administrative areas, and events.
+  const { users, isUnderAdministrativeArea, eventIds } =
+    await setupScopeTestFixture(
+      1243453343,
+      fc.constantFrom(
+        [ActionTypes.enum.DECLARE],
+        [ActionTypes.enum.DECLARE, ActionTypes.enum.REGISTER]
       )
-
-    const clientReadingAllEvents = createTestClient(users[0], [
-      encodeScope({
-        type: 'record.read'
-      })
-    ])
-
-    const jurisdictionOptions = fc.option(
-      fc.constantFrom(...JurisdictionFilter.options),
-      {
-        nil: undefined
-      }
     )
 
-    const userOptions = fc.option(fc.constant(UserFilter.enum.user), {
+  const clientReadingAllEvents = createTestClient(users[0], [
+    encodeScope({
+      type: 'record.read'
+    })
+  ])
+
+  const jurisdictionOptions = fc.option(
+    fc.constantFrom(...JurisdictionFilter.options),
+    {
       nil: undefined
-    })
+    }
+  )
 
-    // 2. Create option combinations for scopes and users
-    const combinations = fc.record({
-      user: fc.constantFrom(...users),
-      event: fc.option(
-        fc.constantFrom<string[]>(
-          [TENNIS_CLUB_MEMBERSHIP],
-          [TENNIS_CLUB_MEMBERSHIP, 'tennis-club-membership_premium'],
-          ['tennis-club-membership_premium']
-        ),
-        { nil: undefined }
+  const userOptions = fc.option(fc.constant(UserFilter.enum.user), {
+    nil: undefined
+  })
+
+  // 2. Create option combinations for scopes and users
+  const combinations = fc.record({
+    user: fc.constantFrom(...users),
+    event: fc.option(
+      fc.constantFrom<string[]>(
+        [TENNIS_CLUB_MEMBERSHIP],
+        [TENNIS_CLUB_MEMBERSHIP, 'tennis-club-membership_premium'],
+        ['tennis-club-membership_premium']
       ),
-      placeOfEvent: jurisdictionOptions,
-      declaredBy: userOptions,
-      declaredIn: jurisdictionOptions,
-      registeredBy: userOptions,
-      registeredIn: jurisdictionOptions
-    })
+      { nil: undefined }
+    ),
+    placeOfEvent: jurisdictionOptions,
+    declaredBy: userOptions,
+    declaredIn: jurisdictionOptions,
+    registeredBy: userOptions,
+    registeredIn: jurisdictionOptions
+  })
 
-    // 3. Test combination against random event and assert results
-    await fc.assert(
-      fc.asyncProperty(combinations, async ({ user, ...options }) => {
-        const scope = encodeScope({
-          type: 'record.unassign-others',
-          options
+  // 3. Test combination against random event and assert results
+  await fc.assert(
+    fc.asyncProperty(combinations, async ({ user, ...options }) => {
+      const scope = encodeScope({
+        type: 'record.unassign-others',
+        options
+      })
+
+      const randomIndex = Math.floor(Math.random() * eventIds.length)
+      const [eventId] = eventIds.splice(randomIndex, 1)
+
+      const testClient = createTestClient(user, [scope])
+
+      // 1. Fetch the event before performing the action. Doing it after unassign will alter the result.
+      const eventBeforeUnassign = await clientReadingAllEvents.event.get({
+        eventId
+      })
+
+      let result: { success: boolean; event: EventDocument }
+      try {
+        // 2. Perform the action with the given test client.
+        await testClient.event.actions.assignment.unassign({
+          eventId,
+          transactionId: getUUID(),
+          declaration: {},
+          assignedTo: null
         })
 
-        const randomIndex = Math.floor(Math.random() * eventIds.length)
-        const [eventId] = eventIds.splice(randomIndex, 1)
-
-        const testClient = createTestClient(user, [scope])
-
-        // 1. Fetch the event before performing the action. Doing it after unassign will alter the result.
-        const eventBeforeUnassign = await clientReadingAllEvents.event.get({
-          eventId
-        })
-
-        let result: { success: boolean; event: EventDocument }
-        try {
-          // 2. Perform the action with the given test client.
-          await testClient.event.actions.assignment.unassign({
-            eventId,
-            transactionId: getUUID(),
-            declaration: {},
-            assignedTo: null
+        // 3. Return the event before the unassignment went through.
+        result = { success: true, event: eventBeforeUnassign }
+      } catch (error) {
+        if (error instanceof TRPCError && error.code === 'FORBIDDEN') {
+          const eventFetchedAsAdmin = await clientReadingAllEvents.event.get({
+            eventId
           })
-
-          // 3. Return the event before the unassignment went through.
-          result = { success: true, event: eventBeforeUnassign }
-        } catch (error) {
-          if (error instanceof TRPCError && error.code === 'FORBIDDEN') {
-            const eventFetchedAsAdmin = await clientReadingAllEvents.event.get({
-              eventId
-            })
-            result = { success: false, event: eventFetchedAsAdmin }
-          } else {
-            throw error
-          }
-        }
-
-        const eventConfig =
-          result.event.type === TENNIS_CLUB_MEMBERSHIP
-            ? tennisClubMembershipEvent
-            : {
-                ...tennisClubMembershipEvent,
-                id: 'tennis-club-membership_premium'
-              }
-
-        const eventIndex = getCurrentEventState(result.event, eventConfig)
-        const wasAssignedToUser = eventIndex.assignedTo === user.id
-
-        // User should always be able to unassign if they were the assigned user, regardless of other scope options.
-        if (wasAssignedToUser) {
-          expect(result.success).toBe(true)
+          result = { success: false, event: eventFetchedAsAdmin }
         } else {
-          assertScopeResult(result, {
-            user,
-            isUnderAdministrativeArea,
-            ...options
-          })
+          throw error
         }
-      }),
-      { numRuns: 20 }
-    )
-  }
-)
+      }
+
+      const eventConfig =
+        result.event.type === TENNIS_CLUB_MEMBERSHIP
+          ? tennisClubMembershipEvent
+          : {
+              ...tennisClubMembershipEvent,
+              id: 'tennis-club-membership_premium'
+            }
+
+      const eventIndex = getCurrentEventState(result.event, eventConfig)
+      const wasAssignedToUser = eventIndex.assignedTo === user.id
+
+      // User should always be able to unassign if they were the assigned user, regardless of other scope options.
+      if (wasAssignedToUser) {
+        expect(result.success).toBe(true)
+      } else {
+        assertScopeResult(result, {
+          user,
+          isUnderAdministrativeArea,
+          ...options
+        })
+      }
+    }),
+    { numRuns: 20 }
+  )
+})
