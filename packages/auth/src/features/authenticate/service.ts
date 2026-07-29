37f72630b41c0efd98173a7a6a3ccde1682b745c
Riku Rouvila <riku.rouvila@gmail.com>
2026-07-29 14:27:26 +0300

    remove unused exports

--- packages/auth/src/features/authenticate/service.ts
+++ packages/auth/src/features/authenticate/service.ts
@@ -137,11 +137,7 @@ export interface ISystemAuthentication {
   scope: EncodedScope[]
 }
 
-export class UserInfoNotFoundError extends Error {}
-
-export function isUserInfoNotFoundError(err: Error) {
-  return err instanceof UserInfoNotFoundError
-}
+class UserInfoNotFoundError extends Error {}
 
 export async function authenticate(
   username: string,
@@ -279,7 +275,9 @@ export async function subjectCanAccessRecord(
   try {
     const event = await eventsClient.event.get.query(
       { eventId, waitFor: false },
-      { context: { headers: { Authorization: setBearerForToken(subjectToken) } } }
+      {
+        context: { headers: { Authorization: setBearerForToken(subjectToken) } }
+      }
     )
     return event.actions.some((action) => action.id === actionId)
   } catch (err) {
@@ -388,10 +386,6 @@ const tokenPayload = t.type({
   aud: t.array(t.string),
   userType: t.string
 })
-
-export type ITokenPayload = t.TypeOf<typeof tokenPayload> & {
-  scope: EncodedScope[]
-}
 
 function safeVerifyJwt(token: string) {
   return tryCatch(
