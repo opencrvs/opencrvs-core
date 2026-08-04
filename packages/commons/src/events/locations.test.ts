e2fc704f8f56f1893064ac71c6d0770bbe9a6d3a
Barry Dwyer <barry.dwyer@decodedev.co.za>
2026-08-04 16:48:43 +0200

    Fix max line linting test
    Tests shouldn't be subject to the same standard as code as far as number of lines goes

--- packages/commons/src/events/locations.test.ts
+++ packages/commons/src/events/locations.test.ts
@@ -9,6 +9,8 @@
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
+/* eslint-disable max-lines */
+
 import { createPrng, generateUuid, TestUserRole } from './test.utils'
 import { SystemContext, UserContext } from '../users/User'
 import {
