41683af62cb33e446dd29b0ceb0d22c831f63d88
Markus <markus@agilepursuit.fi>
2026-08-05 13:58:15 +0300

    fix: use CL and address for death declaration

--- packages/testland/e2e/testcases/death/declaration/death-declaration-11.spec.ts
+++ packages/testland/e2e/testcases/death/declaration/death-declaration-11.spec.ts
@@ -58,8 +58,13 @@ test.describe.serial('11. Death declaration case - 11', () => {
       causeOfDeathEstablished: true,
       sourceCauseDeath: 'Verbal autopsy',
       description: 'Hanging from ceiling',
-      placeOfDeath: 'Health Institution',
-      deathLocation: 'Klow Village Hospital'
+      placeOfDeath: 'Other',
+      deathLocationOther: {
+        country: 'Farajaland',
+        province: 'Central',
+        district: 'Ibombo',
+        village: 'Klow'
+      }
     },
     informant: {
       relation: 'Daughter',
@@ -121,9 +126,9 @@ test.describe.serial('11. Death declaration case - 11', () => {
     await page.close()
   })
 
-  test.describe('11.1 Declaration started by HO', async () => {
+  test.describe('11.1 Declaration started by CL', async () => {
     test.beforeAll(async () => {
-      await login(page, CREDENTIALS.HOSPITAL_OFFICIAL)
+      await login(page, CREDENTIALS.COMMUNITY_LEADER)
 
       await page.click('#header-new-event')
       await page.getByLabel('Death').click()
@@ -204,12 +209,6 @@ test.describe.serial('11. Death declaration case - 11', () => {
       await page
         .getByText(declaration.eventDetails.placeOfDeath, { exact: true })
         .click()
-
-      await page.locator('#eventDetails____deathLocation').fill('Klow Village')
-      await expect(
-        page.getByText(declaration.eventDetails.deathLocation)
-      ).toBeVisible()
-      await page.getByText(declaration.eventDetails.deathLocation).click()
 
       await continueForm(page)
     })
@@ -504,6 +503,12 @@ test.describe.serial('11. Death declaration case - 11', () => {
         page,
         'eventDetails.placeOfDeath',
         declaration.eventDetails.placeOfDeath
+      )
+
+      await expectRowValueWithChangeButton(
+        page,
+        'eventDetails.deathLocationOther',
+        `${declaration.eventDetails.deathLocationOther.country}${declaration.eventDetails.deathLocationOther.province}${declaration.eventDetails.deathLocationOther.district}${declaration.eventDetails.deathLocationOther.village}`
       )
 
       /*
