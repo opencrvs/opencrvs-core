415bf201e9b5c6af2c2ec990cfb80e0c10f49748
Nil20 <ashikul.alam@dsinnovators.com>
2026-08-12 22:37:20 +0600

    fix license

--- packages/testland/e2e/compute-shard-plan.ts
+++ packages/testland/e2e/compute-shard-plan.ts
@@ -5,6 +5,8 @@
  *
  * OpenCRVS is also distributed under the terms of the Civil Registration
  * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
 
 /**
