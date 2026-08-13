d1b0209c8b72c9b46532bd2bb93c979f169690e6
jamil314 <jamil31415926@gmail.com>
2026-08-13 13:52:14 +0600

    feat: assert user can access the potential duplicates with `record.review-duplicates`

--- packages/events/src/router/event/index.ts
+++ packages/events/src/router/event/index.ts
@@ -67,7 +67,10 @@ import { markAsDuplicate } from '@events/service/events/actions/mark-as-duplicat
 import { markNotDuplicate } from '@events/service/events/actions/mark-not-duplicate'
 import { cleanupUnreferencedFiles } from '@events/service/files'
 import { writeAuditLog } from '@events/storage/postgres/events/auditLog'
-import { getDuplicateEvents } from '../../service/deduplication/deduplication'
+import {
+  assertCanReviewDuplicatesOf,
+  getDuplicateEvents
+} from '../../service/deduplication/deduplication'
 import { declareActionProcedures } from './actions/declare'
 import { getDefaultActionProcedures } from './actions'
 import { customActionProcedures } from './actions/custom'
@@ -248,6 +251,8 @@ export const eventRouter = router({
     .query(async ({ input, ctx }) => {
       const event = await getEventById(input.eventId)
 
+      await assertCanReviewDuplicatesOf(event, ctx)
+
       return getDuplicateEvents(event, ctx)
     }),
   delete: userOnlyProcedure
@@ -397,6 +402,9 @@ export const eventRouter = router({
         .mutation(async (options) => {
           const { user, token } = options.ctx
           const event = await getEventById(options.input.eventId)
+
+          await assertCanReviewDuplicatesOf(event, options.ctx)
+
           const configuration = await getEventConfigurationById({
             token,
             eventType: event.type
@@ -430,6 +438,9 @@ export const eventRouter = router({
         .mutation(async (options) => {
           const { user, token } = options.ctx
           const event = await getEventById(options.input.eventId)
+
+          await assertCanReviewDuplicatesOf(event, options.ctx)
+
           const configuration = await getEventConfigurationById({
             token,
             eventType: event.type
