e25315454b630f9ec2fb8eb83df5ccee589b2d4c
Markus <markus@agilepursuit.fi>
2026-09-10 13:43:07 +0300

    fix: update custom test to match current functionality

--- packages/events/src/router/event/event.actions.custom.test.ts
+++ packages/events/src/router/event/event.actions.custom.test.ts
@@ -293,7 +293,7 @@ describe('event.actions.custom', () => {
   })
 
   describe('Asynchronous confirmation flow', () => {
-    function mockNotifyApi(status: number) {
+    function mockCustomActionApi(status: number) {
       return mswServer.use(
         http.post<never, { actionId: string }>(
           `${env.COUNTRY_CONFIG_URL}/trigger/events/tennis-club-membership/actions/CUSTOM`,
@@ -309,7 +309,7 @@ describe('event.actions.custom', () => {
         `type=record.custom-action&event=${TENNIS_CLUB_MEMBERSHIP}&customActionTypes=${CUSTOM_ACTION_TYPE}`
       ])
 
-      mockNotifyApi(202)
+      mockCustomActionApi(202)
 
       await expect(
         client.event.actions.custom.request(payload)
@@ -329,7 +329,7 @@ describe('event.actions.custom', () => {
 
       const eventId = payload.eventId
 
-      mockNotifyApi(202)
+      mockCustomActionApi(202)
 
       const requestResponse = await client.event.actions.custom.request(payload)
 
@@ -339,15 +339,6 @@ describe('event.actions.custom', () => {
         )?.id,
         'Could not find id for custom action'
       )
-
-      const createAction = requestResponse.actions.filter(
-        (action) => action.type === ActionType.CREATE
-      )
-
-      const assignmentInput = generator.event.actions.assign(payload.eventId, {
-        assignedTo: createAction[0].createdBy
-      })
-      await client.event.actions.assignment.assign(assignmentInput)
 
       const countryConfigClient = createCountryConfigClient(
         user,
