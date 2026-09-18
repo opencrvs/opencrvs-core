ab95cfef3402b4ef5405a0da50af3ab27ef78773
cibelius <cihan.m.bebek@gmail.com>
2026-09-18 16:01:35 +0300

    add translations, hide certain actions from audit log

--- packages/client/src/views/UserAudit/UserAuditHistory.tsx
+++ packages/client/src/views/UserAudit/UserAuditHistory.tsx
@@ -58,10 +58,15 @@ import { useEventConfigurations } from '@client/v2-events/features/events/useEve
 
 const DEFAULT_LIST_SIZE = 10
 
+// Location & administrative-area changes should be hidden from the audit log, as per decision by Jon:
+// https://github.com/opencrvs/opencrvs-core/issues/13292#issuecomment-5713761542
 const OPERATIONS_TO_HIDE = [
-  'event.search',
-  'event.actions.assign.request',
-  'event.actions.unassign.request'
+  'locations.create',
+  'locations.update',
+  'locations.withdrawVersion',
+  'administrativeAreas.create',
+  'administrativeAreas.update',
+  'administrativeAreas.withdrawVersion'
 ]
 
 const TableDiv = styled.div`
