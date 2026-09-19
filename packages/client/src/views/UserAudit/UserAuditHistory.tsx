5ad5ce0184b48dd4b8a8f7b2ce90c0489a651bbf
cibelius <cihan.m.bebek@gmail.com>
2026-09-19 15:44:11 +0300

    remove comment

--- packages/client/src/views/UserAudit/UserAuditHistory.tsx
+++ packages/client/src/views/UserAudit/UserAuditHistory.tsx
@@ -62,8 +62,6 @@ const OPERATIONS_TO_HIDE = [
   'event.search',
   'event.actions.assign.request',
   'event.actions.unassign.request',
-  // Location & administrative-area changes should be hidden from the audit log, as per decision by Jon:
-  // https://github.com/opencrvs/opencrvs-core/issues/13292#issuecomment-5713761542
   'locations.create',
   'locations.update',
   'locations.withdrawVersion',
