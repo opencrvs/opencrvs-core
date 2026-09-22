960c0c5cedd54755ee79cd6e51e32c56457e119d
jamil314 <jamil31415926@gmail.com>
2026-09-22 14:56:49 +0600

    Revert "chore: log administrative hierarchy cache size"
    This reverts commit af48ba01b163f63ba6076fd047a5ddfbdf9fe9a5.

--- packages/events/src/storage/postgres/administrative-hierarchy/locations.ts
+++ packages/events/src/storage/postgres/administrative-hierarchy/locations.ts
@@ -33,12 +33,10 @@ let administrativeHierarchyByIdCache: Record<
   string,
   Promise<UUID[]>
 > = Object.create(null)
-let administrativeHierarchyCacheSize = 0
 let leafLevelAdministrativeAreaIdsCache: Promise<{ id: UUID }[]> | null = null
 
 export function clearAdministrativeHierarchyCache() {
   administrativeHierarchyByIdCache = Object.create(null)
-  administrativeHierarchyCacheSize = 0
   leafLevelAdministrativeAreaIdsCache = null
 }
 
@@ -525,7 +523,6 @@ export type AdministrativeHierarchyStats = {
   hits: number
   misses: number
   dbMs: number
-  cacheSize: number
 }
 
 export function readAdministrativeHierarchyStats(
@@ -534,8 +531,7 @@ export function readAdministrativeHierarchyStats(
   return {
     hits: hierarchyStats.hits - (since?.hits ?? 0),
     misses: hierarchyStats.misses - (since?.misses ?? 0),
-    dbMs: hierarchyStats.dbMs - (since?.dbMs ?? 0),
-    cacheSize: administrativeHierarchyCacheSize
+    dbMs: hierarchyStats.dbMs - (since?.dbMs ?? 0)
   }
 }
 
@@ -562,7 +558,6 @@ export async function getAdministrativeHierarchyById(
   })
 
   administrativeHierarchyByIdCache[id] = promise
-  administrativeHierarchyCacheSize++
   return promise
 }
 
