7b11ea1e9164f2c604a9a18acf218b0119fc8804
Markus <markus@agilepursuit.fi>
2026-09-24 13:03:31 +0300

    chore: show trace on retries

--- packages/testland/playwright.config.ts
+++ packages/testland/playwright.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
     /* Capture screenshot on failure */
     screenshot: 'only-on-failure',
     /* Collect trace when the test failed. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-all-retries',
     // Ignore HTTPS errors (like untrusted or self-signed certificates) during Playwright tests on CI
     // This is useful for Let's Encrypt staging certificates that aren't publicly trusted.
     ignoreHTTPSErrors
